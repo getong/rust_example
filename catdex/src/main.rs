5cfd7581d7f7892add8d30634714b8ee87b8e754
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-02-10 00:00:10 +0800

    add file sevice

--- catdex/src/main.rs
+++ catdex/src/main.rs
@@ -1,4 +1,4 @@
-use actix_files::NamedFile;
+use actix_files::{Files, NamedFile};
 use actix_web::{web, App, HttpServer, Result};
 async fn index() -> Result<NamedFile> {
     Ok(NamedFile::open("./static/index.html")?)
@@ -6,8 +6,12 @@ async fn index() -> Result<NamedFile> {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Listening on port 18080");
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind("127.0.0.1:18080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/static", "static").show_files_listing())
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:18080")?
+    .run()
+    .await
 }
