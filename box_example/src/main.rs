5d3632e37bbbcbe34126230bb326a17d15bb3d71
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-02-23 23:08:43 +0800

    add box example

---
+++ box_example/src/main.rs
@@ -0,0 +1,29 @@
+fn box_ref<T>(b: T) -> Box<T> {
+    let a = b;
+    Box::new(a)
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Foo;
+
+#[derive(Debug)]
+struct Node {
+    data: u32,
+    next: Option<Box<Node>>,
+}
+
+fn main() {
+    //println!("Hello, world!");
+    let boxed_one = Box::new(Foo);
+    println!("unboxed_one at {:?} is {:?}", boxed_one, boxed_one);
+    let unboxed_one = *boxed_one;
+    println!("unboxed_one at {:?} is {:?}", unboxed_one, unboxed_one);
+    let boxed_two = box_ref(unboxed_one);
+    println!("boxed_two at {:?} is {:?} ", boxed_two, *boxed_two);
+
+    let node = Node {
+        data: 33,
+        next: None,
+    };
+    println!("node : {:?}", node);
+}
