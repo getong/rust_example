72f010211ca8f763a3afbeb4e2a7e840a5f10367
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-02-24 12:25:02 +0800

    add pointer address

--- box_example/src/main.rs
+++ box_example/src/main.rs
@@ -15,11 +15,11 @@ struct Node {
 fn main() {
     //println!("Hello, world!");
     let boxed_one = Box::new(Foo);
-    println!("unboxed_one at {:?} is {:?}", boxed_one, boxed_one);
+    println!("unboxed_one at {:p} is {:?}", boxed_one, boxed_one);
     let unboxed_one = *boxed_one;
     println!("unboxed_one at {:?} is {:?}", unboxed_one, unboxed_one);
     let boxed_two = box_ref(unboxed_one);
-    println!("boxed_two at {:?} is {:?} ", boxed_two, *boxed_two);
+    println!("boxed_two at {:p} is {:?} ", boxed_two, *boxed_two);
 
     let node = Node {
         data: 33,
