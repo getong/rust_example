06162e550181393483f56b5e19672858bac5e37f
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-01 00:06:26 +0800

    add thread arc

---
+++ thread_arc/src/main.rs
@@ -0,0 +1,17 @@
+use std::sync::Arc;
+use std::thread;
+
+fn main() {
+    let nums = Arc::new(vec![0, 1, 2, 3, 4]);
+    let mut childs = vec![];
+    for n in 0..5 {
+        let ns = Arc::clone(&nums);
+        let c = thread::spawn(move || {
+            println!("{}", ns[n]);
+        });
+        childs.push(c);
+    }
+    for c in childs {
+        c.join().unwrap();
+    }
+}
