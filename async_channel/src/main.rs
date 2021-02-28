2f3c102a09a9fd675f625e81203426cdd5aa5383
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-01 00:18:36 +0800

    add async channel

---
+++ async_channel/src/main.rs
@@ -0,0 +1,17 @@
+// async_channels.rs
+use std::sync::mpsc::channel;
+use std::thread;
+
+fn main() {
+    let (tx, rx) = channel();
+    let join_handle = thread::spawn(move || {
+        while let Ok(n) = rx.recv() {
+            println!("Received {}", n);
+        }
+    });
+
+    for i in 0..10 {
+        tx.send(i).unwrap();
+    }
+    join_handle.join().unwrap();
+}
