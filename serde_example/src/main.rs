78b81ee2321ebe594abcf9880de4a5989625c46a
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-02-28 16:23:21 +0800

    add serde example

---
+++ serde_example/src/main.rs
@@ -0,0 +1,26 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Foo {
+    a: String,
+    b: u64,
+}
+
+impl Foo {
+    fn new(a: &str, b: u64) -> Self {
+        Self {
+            a: a.to_string(),
+            b,
+        }
+    }
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let foo_json = serde_json::to_string(&Foo::new("It's that simple", 101)).unwrap();
+
+    println!("{:?}", foo_json);
+
+    let foo_value: Foo = serde_json::from_str(&foo_json).unwrap();
+    println!("{:?}", foo_value);
+}
