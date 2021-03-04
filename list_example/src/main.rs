5f23870ef54b30b0b4edfed5a3aa669a2182033e
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-05 00:02:42 +0800

    add code

--- list_example/src/main.rs
+++ list_example/src/main.rs
@@ -11,11 +11,26 @@ struct Node {
 
 impl Node {
     // A nice and short way of creating a new node
-    fn new(value: String) -> Rc<RefCell<Node>> {
+    pub fn new(value: String) -> Rc<RefCell<Node>> {
         Rc::new(RefCell::new(Node {
             value: value,
             next: None,
         }))
+    }
+}
+struct TransactionLog {
+    head: Option<Rc<RefCell<Node>>>,
+    tail: Option<Rc<RefCell<Node>>>,
+    pub length: u64,
+}
+
+impl TransactionLog {
+    pub fn new_empty() -> TransactionLog {
+        TransactionLog {
+            head: None,
+            tail: None,
+            length: 0,
+        }
     }
 
     pub fn pop(&mut self) -> Option<String> {
@@ -33,20 +48,15 @@ impl Node {
                 .value
         })
     }
-}
-struct TransactionLog {
-    head: Option<Rc<RefCell<Node>>>,
-    tail: Option<Rc<RefCell<Node>>>,
-    pub length: u64,
-}
 
-impl TransactionLog {
-    pub fn new_empty() -> TransactionLog {
-        TransactionLog {
-            head: None,
-            tail: None,
-            length: 0,
-        }
+    pub fn append(&mut self, value: String) {
+        let new = Node::new(value);
+        match self.tail.take() {
+            Some(old) => old.borrow_mut().next = Some(new.clone()),
+            None => self.head = Some(new.clone()),
+        };
+        self.length += 1;
+        self.tail = Some(new);
     }
 }
 
