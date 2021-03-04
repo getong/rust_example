9ba21430cdf82562a6f2cce5fde23aa754c9cea3
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-04 23:58:52 +0800

    add list example

---
+++ list_example/src/main.rs
@@ -0,0 +1,55 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type SingleLink = Option<Rc<RefCell<Node>>>;
+
+#[derive(Clone)]
+struct Node {
+    value: String,
+    next: SingleLink,
+}
+
+impl Node {
+    // A nice and short way of creating a new node
+    fn new(value: String) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node {
+            value: value,
+            next: None,
+        }))
+    }
+
+    pub fn pop(&mut self) -> Option<String> {
+        self.head.take().map(|head| {
+            if let Some(next) = head.borrow_mut().next.take() {
+                self.head = Some(next);
+            } else {
+                self.tail.take();
+            }
+            self.length -= 1;
+            Rc::try_unwrap(head)
+                .ok()
+                .expect("Something is terribly wrong")
+                .into_inner()
+                .value
+        })
+    }
+}
+struct TransactionLog {
+    head: Option<Rc<RefCell<Node>>>,
+    tail: Option<Rc<RefCell<Node>>>,
+    pub length: u64,
+}
+
+impl TransactionLog {
+    pub fn new_empty() -> TransactionLog {
+        TransactionLog {
+            head: None,
+            tail: None,
+            length: 0,
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
