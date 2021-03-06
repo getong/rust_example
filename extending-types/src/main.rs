703d5701fc4ed162f2da4bc114e4c5b84cf03b4f
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-06 15:46:00 +0800

    add extending types example

---
+++ extending-types/src/main.rs
@@ -0,0 +1,16 @@
+// chapter2/extending-types.rs
+// Trait for our behavior
+trait Sawtooth {
+    fn sawtooth(&self) -> Self;
+}
+
+// Extending the builtin f64 type
+impl Sawtooth for f64 {
+    fn sawtooth(&self) -> f64 {
+        self - self.floor()
+    }
+}
+
+fn main() {
+    println!("{}", 2.34f64.sawtooth());
+}
