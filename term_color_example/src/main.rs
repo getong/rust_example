f0e3f87d06e1571b28c5559d8b73eaa460016ef8
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-06 14:58:01 +0800

    add term color example

---
+++ term_color_example/src/main.rs
@@ -0,0 +1,12 @@
+extern crate term;
+fn main() {
+    println!("Hello, world!");
+    let mut t = term::stdout().unwrap();
+    t.fg(term::color::WHITE).unwrap();
+    write!(t, "hello, ").unwrap();
+
+    t.fg(term::color::BLUE).unwrap();
+    writeln!(t, "world!").unwrap();
+
+    t.reset().unwrap();
+}
