2c48b90caba1ed71e7dc7d7e303230075869918b
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-06 20:45:14 +0800

    add tcp echo server

---
+++ tcp-echo-server/src/main.rs
@@ -0,0 +1,32 @@
+use std::io::{Error, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+// Handles a single client
+fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+    println!("Incoming connection from: {}", stream.peer_addr()?);
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf)?;
+        if bytes_read == 0 {
+            return Ok(());
+        }
+        stream.write(&buf[..bytes_read])?;
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:8888").expect("Could not bind");
+    for stream in listener.incoming() {
+        match stream {
+            Err(e) => {
+                eprintln!("failed: {}", e)
+            }
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                });
+            }
+        }
+    }
+}
