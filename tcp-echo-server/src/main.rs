ce744a219495cb512bdb76b69b0a126615fd9c82
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-07 00:09:41 +0800

    update code

--- tcp-echo-server/src/main.rs
+++ tcp-echo-server/src/main.rs
@@ -1,16 +1,26 @@
+extern crate rand;
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use std::io::{Error, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use std::time::Duration;
 
 // Handles a single client
 fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
     println!("Incoming connection from: {}", stream.peer_addr()?);
     let mut buf = [0; 512];
+    let mut rng = thread_rng();
     loop {
         let bytes_read = stream.read(&mut buf)?;
         if bytes_read == 0 {
             return Ok(());
         }
+
+        let sleep = Duration::from_secs(*[0, 1, 2, 3, 4, 5].choose(&mut rng).unwrap());
+        println!("Sleeping for {:?} before replying", sleep);
+        std::thread::sleep(sleep);
         stream.write(&buf[..bytes_read])?;
     }
 }
