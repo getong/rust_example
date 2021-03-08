1a78bcaec914714139e52fe1195c6b61e0b5c977
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-08 12:16:04 +0800

    add std env example

---
+++ std-env-example/src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+fn main() {
+    // println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let path: &str = &args[0];
+    if path.contains("/debug/") {
+        println!("The development app is running");
+    } else if path.contains("/release/") {
+        println!("The production server is running");
+    } else {
+        panic!("The setting is neither debug or release");
+    }
+}
