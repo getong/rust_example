57e13edc3d0dd9828d4ff1978dbc847568d40e96
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-10 23:03:02 +0800

    add PathBuf

--- fs_example/src/main.rs
+++ fs_example/src/main.rs
@@ -1,6 +1,9 @@
 use std::fs;
+use std::fs::DirBuilder;
 use std::fs::File;
 use std::fs::OpenOptions;
+use std::path::Path;
+use std::path::PathBuf;
 
 fn main() {
     println!("Hello, world!");
@@ -61,4 +64,43 @@ fn main() {
     permissions.set_readonly(true);
     let _ = fs::set_permissions("stats.txt", permissions).expect("Unable to set permission");
     fs::write("stats.txt", "Hello- Can you see me?").expect("Unable to write to file");
+
+    // directory operation
+    let dir_entries = fs::read_dir(".").expect("Unable to read directory contents");
+    // Read directory contents
+    for entry in dir_entries {
+        //Get details of each directory entry
+        let entry = entry.unwrap();
+        let entry_path = entry.path();
+        let entry_metadata = entry.metadata().unwrap();
+        let entry_file_type = entry.file_type().unwrap();
+        let entry_file_name = entry.file_name();
+        println!(
+            "Path is {:?}.\n Metadata is {:?}\n File_type is {:?}.\n Entry name is{:?}.\n",
+            entry_path, entry_metadata, entry_file_type, entry_file_name
+        );
+    }
+
+    // Get path components
+    let new_path = Path::new("/usr/d1/d2/d3/bar.txt");
+    println!("Path parent is: {:?}", new_path.parent());
+    for component in new_path.components() {
+        println!("Path component is: {:?}", component);
+    }
+
+    let dir_structure = "/tmp/dir1/dir2/dir3";
+    DirBuilder::new()
+        .recursive(true)
+        .create(dir_structure)
+        .unwrap();
+
+    // PathBuf
+    let mut f_path = PathBuf::new();
+    f_path.push(r"/tmp");
+    f_path.push("packt");
+    f_path.push("rust");
+    f_path.push("book");
+    f_path.set_extension("rs");
+    // output: Path constructed is "/tmp/packt/rust/book.rs"
+    println!("Path constructed is {:?}", f_path);
 }
