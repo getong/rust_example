f765fccb1e9d13978d7a7f1a6aba451396431f6b
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-12 12:35:46 +0800

    add example

--- string_example/src/main.rs
+++ string_example/src/main.rs
@@ -29,4 +29,19 @@ fn main() {
         "Length of the previously empty string is now {}",
         empty_string.len()
     );
+
+    let mut lang = String::from("rust");
+    let rust1 = add_version(&mut lang);
+    println!("{:?}", rust1);
+    let rust2 = add_lang(&mut lang);
+    println!("{:?}", rust2);
+}
+
+fn add_version(s: &mut String) -> String {
+    s.push_str(" 2019!!");
+    s.to_string()
+}
+fn add_lang(s: &mut String) -> String {
+    s.push_str(" lang.");
+    s.to_string()
 }
