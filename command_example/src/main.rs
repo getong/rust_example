7b539f53bb48801fbd7bac012c41b12935f05e43
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-12 21:40:42 +0800

    add example

--- command_example/src/main.rs
+++ command_example/src/main.rs
@@ -1,4 +1,7 @@
-use std::process::Command;
+//use std::process::Command;
+
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
 
 fn main() {
     Command::new("ls")
@@ -6,4 +9,27 @@ fn main() {
         .arg("-h")
         .spawn()
         .expect("ls command failed to start");
+
+    let status = Command::new("cat")
+        .arg("non-existent-file.txt")
+        .status()
+        .expect("failed to execute cat");
+
+    if status.success() {
+        println!("Successful operation");
+    } else {
+        println!("Unsuccessful operation");
+    }
+
+    // Spawn the `ps` command
+    let process = match Command::new("ps").stdout(Stdio::piped()).spawn() {
+        Err(err) => panic!("couldn't spawn ps: {}", err),
+        Ok(process) => process,
+    };
+
+    let mut ps_output = String::new();
+    match process.stdout.unwrap().read_to_string(&mut ps_output) {
+        Err(err) => panic!("couldn't read ps stdout: {}", err),
+        Ok(_) => print!("ps output from child process is : \n{}", ps_output),
+    }
 }
