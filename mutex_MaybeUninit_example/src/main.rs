51ec962850093c3a9a8835588ce7c1c1ce11faa6
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-16 23:00:29 +0800

    add mutex MaybeUninit example

---
+++ mutex_MaybeUninit_example/src/main.rs
@@ -0,0 +1,31 @@
+use std::mem::MaybeUninit;
+use std::sync::Mutex;
+use std::thread;
+
+static mut M: MaybeUninit<Mutex<u32>> = MaybeUninit::uninit();
+
+// deadlock example
+//fn main() {
+//    let a = Mutex::new(0);
+//    let b = a.lock().unwrap();
+//    let _c = a.lock().unwrap();
+//    println!("{}", *b);
+//}
+
+fn main() {
+    thread::spawn(move || unsafe {
+        let m = M.as_mut_ptr();
+        m.write(Mutex::new(3));
+    })
+    .join()
+    .expect("thread::spawn failed");
+
+    unsafe {
+        let m = M.as_mut_ptr();
+        m.write(Mutex::new(2));
+    }
+
+    unsafe {
+        assert_eq!(*M.as_ptr().read().lock().unwrap(), 2);
+    }
+}
