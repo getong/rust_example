eedfaa938d0ea03d99dd2571f2192e7c3a0950c2
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-17 00:11:53 +0800

    add vecdeque example

---
+++ vecdeque_example/src/main.rs
@@ -0,0 +1,43 @@
+use std::collections::VecDeque;
+
+fn check_remaining(input: &VecDeque<(&str, bool)>) {
+    // Each item is a (&str, bool)
+    for item in input {
+        if item.1 == false {
+            println!("You must: {}", item.0);
+        }
+    }
+}
+
+fn done(input: &mut VecDeque<(&str, bool)>) {
+    let mut task_done = input.pop_back().unwrap(); // pop off the back
+    task_done.1 = true; // now it's done - mark as true
+    input.push_front(task_done); // put it at the front now
+}
+
+fn main() {
+    let mut my_vec = VecDeque::from(vec![0; 600000]);
+    for _i in 0..600000 {
+        my_vec.pop_front(); // pop_front is like .pop but for the front
+    }
+
+    let mut my_vecdeque = VecDeque::new();
+    let things_to_do = vec![
+        "send email to customer",
+        "add new product to list",
+        "phone Loki back",
+    ];
+
+    for thing in things_to_do {
+        my_vecdeque.push_front((thing, false));
+    }
+
+    done(&mut my_vecdeque);
+    done(&mut my_vecdeque);
+
+    check_remaining(&my_vecdeque);
+
+    for task in my_vecdeque {
+        print!("{:?} ", task);
+    }
+}
