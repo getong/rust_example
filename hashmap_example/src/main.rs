619e07017ec024085faf83b5477380f6b78cd54b
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-16 23:52:18 +0800

    add get example

--- hashmap_example/src/main.rs
+++ hashmap_example/src/main.rs
@@ -22,4 +22,14 @@ fn main() {
             year, tallinn.name, population
         );
     }
+
+    let mut book_hashmap = HashMap::new();
+    book_hashmap.insert(1, "L'Allemagne Moderne");
+
+    if book_hashmap.get(&1).is_none() {
+        // is_none() returns a bool: true if it's None, false if it's Some
+        book_hashmap.insert(1, "Le Petit Prince");
+    }
+
+    println!("{:?}", book_hashmap.get(&1));
 }
