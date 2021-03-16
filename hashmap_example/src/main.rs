08cd18bd82a1b16677277a7a60590c40eb381512
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-16 23:48:50 +0800

    add hashmap example

---
+++ hashmap_example/src/main.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap; // This is so we can just write HashMap instead of std::collections::HashMap every time
+
+struct City {
+    name: String,
+    population: HashMap<u32, u32>, // This will have the year and the population for the year
+}
+
+fn main() {
+    let mut tallinn = City {
+        name: "Tallinn".to_string(),
+        population: HashMap::new(), // So far the HashMap is empty
+    };
+
+    tallinn.population.insert(1372, 3_250); // insert three dates
+    tallinn.population.insert(1851, 24_000);
+    tallinn.population.insert(2020, 437_619);
+
+    for (year, population) in tallinn.population {
+        // The HashMap is HashMap<u32, u32> so it returns a two items each time
+        println!(
+            "In the year {} the city of {} had a population of {}.",
+            year, tallinn.name, population
+        );
+    }
+}
