d7b9d38d16d263ab18959199ac057cda3469b180
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-16 23:54:43 +0800

    add more code

--- hashmap_example/src/main.rs
+++ hashmap_example/src/main.rs
@@ -25,11 +25,57 @@ fn main() {
 
     let mut book_hashmap = HashMap::new();
     book_hashmap.insert(1, "L'Allemagne Moderne");
-
     if book_hashmap.get(&1).is_none() {
         // is_none() returns a bool: true if it's None, false if it's Some
         book_hashmap.insert(1, "Le Petit Prince");
     }
-
     println!("{:?}", book_hashmap.get(&1));
+
+    let book_collection = vec![
+        "L'Allemagne Moderne",
+        "Le Petit Prince",
+        "Eye of the World",
+        "Eye of the World",
+    ]; // Eye of the World appears twice
+    let mut book_hashmap = HashMap::new();
+    for book in book_collection {
+        book_hashmap.entry(book).or_insert(true);
+    }
+    for (book, true_or_false) in book_hashmap {
+        println!("Do we have {}? {}", book, true_or_false);
+    }
+
+    let book_collection = vec![
+        "L'Allemagne Moderne",
+        "Le Petit Prince",
+        "Eye of the World",
+        "Eye of the World",
+    ];
+    let mut book_hashmap = HashMap::new();
+    for book in book_collection {
+        let return_value = book_hashmap.entry(book).or_insert(0); // return_value is a mutable reference. If nothing is there, it will be 0
+        *return_value += 1; // Now return_value is at least 1. And if there was another book, it will go up by 1
+    }
+    for (book, number) in book_hashmap {
+        println!("{}, {}", book, number);
+    }
+
+    let data = vec![
+        // This is the raw data
+        ("male", 9),
+        ("female", 5),
+        ("male", 0),
+        ("female", 6),
+        ("female", 5),
+        ("male", 10),
+    ];
+
+    let mut survey_hash = HashMap::new();
+    for item in data {
+        // This gives a tuple of (&str, i32)
+        survey_hash.entry(item.0).or_insert(Vec::new()).push(item.1); // This pushes the number into the Vec inside
+    }
+    for (male_or_female, numbers) in survey_hash {
+        println!("{:?}: {:?}", male_or_female, numbers);
+    }
 }
