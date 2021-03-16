6d4ad826619dd1e4884a845e89c18e9b5a37b3c8
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-16 22:53:34 +0800

    add pretty print example

---
+++ pretty_print_example/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let my_number = {
+        let second_number = 8;
+        second_number + 9
+    };
+
+    println!("My number is: {:#?}", my_number); // my_number is ()
+
+    let letter = "a";
+    println!("{:ㅎ^11}", letter);
+    println!("{:ㅎ<11}", letter);
+    println!("{:ㅎ>11}", letter);
+}
