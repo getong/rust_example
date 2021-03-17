f47dc81963361a4a0a4594cf9ffdd7a019779141
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-17 21:19:01 +0800

    update code

--- iterator_example/src/main.rs
+++ iterator_example/src/main.rs
@@ -10,6 +10,13 @@ fn main() {
     println!("{:?}", vector2);
     println!("{:?}", vector1_b);
 
+    let arr1 = [1, 2, 3]; // we will use .iter() and .into_iter() on this one
+    let arr1_a = arr1.iter().map(|x| x + 1).collect::<Vec<i32>>();
+    let mut arr2 = [10, 20, 30]; // we will use .iter_mut() on this one
+    arr2.iter_mut().for_each(|x| *x += 100);
+    println!("{:?}", arr1_a);
+    println!("{:?}", arr2);
+
     let my_vec = vec!['a', 'b', '거', '柳']; // Just a regular Vec
     let mut my_vec_iter = my_vec.iter(); // This is an Iterator type now, but we haven't called it yet
     assert_eq!(my_vec_iter.next(), Some(&'a')); // Call the first item with .next()
