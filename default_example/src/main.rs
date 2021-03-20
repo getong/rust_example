2dfbd4d23eee706c3cd3aa76b89d7546a2d9b410
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-03-20 23:54:54 +0800

    add default value

---
+++ default_example/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let default_i8: i8 = Default::default();
+    let default_str: String = Default::default();
+    let default_bool: bool = Default::default();
+
+    println!("'{}', '{}', '{}'", default_i8, default_str, default_bool);
+}
