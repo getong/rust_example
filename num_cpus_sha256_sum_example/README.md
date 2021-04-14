78f1034934916f246b26a2d7c5040570c21c7193
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-04-14 14:31:42 +0800

    add num_cpus sha256 sum example

---
+++ num_cpus_sha256_sum_example/README.md
@@ -0,0 +1,8 @@
+# num_cpus_sha256_sum_example
+
+## command
+
+``` shell
+touch a.iso
+cargo run
+```
---
+++ num_cpus_sha256_sum_example/src/main.rs
@@ -0,0 +1,58 @@
+use ring::digest::{Context, Digest, SHA256};
+use std::fs::File;
+use std::io::{BufReader, Error, Read};
+use std::path::Path;
+use std::sync::mpsc::channel;
+use threadpool::ThreadPool;
+use walkdir::WalkDir;
+
+// Verify the iso extension
+fn is_iso(entry: &Path) -> bool {
+    match entry.extension() {
+        Some(e) if e.to_string_lossy().to_lowercase() == "iso" => true,
+        _ => false,
+    }
+}
+
+fn compute_digest<P: AsRef<Path>>(filepath: P) -> Result<(Digest, P), Error> {
+    let mut buf_reader = BufReader::new(File::open(&filepath)?);
+    let mut context = Context::new(&SHA256);
+    let mut buffer = [0; 1024];
+
+    loop {
+        let count = buf_reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        context.update(&buffer[..count]);
+    }
+
+    Ok((context.finish(), filepath))
+}
+
+fn main() -> Result<(), Error> {
+    let pool = ThreadPool::new(num_cpus::get());
+
+    let (tx, rx) = channel();
+
+    for entry in WalkDir::new(".")
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| !e.path().is_dir() && is_iso(e.path()))
+    {
+        let path = entry.path().to_owned();
+        let tx = tx.clone();
+        pool.execute(move || {
+            let digest = compute_digest(path);
+            tx.send(digest).expect("Could not send data!");
+        });
+    }
+
+    drop(tx);
+    for t in rx.iter() {
+        let (sha, path) = t?;
+        println!("{:?} {:?}", sha, path);
+    }
+    Ok(())
+}
