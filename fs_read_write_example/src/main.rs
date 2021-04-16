b215de44b0f75a058837e30743436a955fe4230e
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-04-16 17:20:58 +0800

    add fs read and write example

---
+++ fs_read_write_example/src/main.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, Write};
+
+fn main() -> Result<(), Error> {
+    let path = "lines.txt";
+
+    let mut output = File::create(path)?;
+    write!(output, "Rust\n💖\nFun")?;
+
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
+}
