67d62d6194e23a460fb4bd64ae0570d9da982464
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-04-23 11:33:34 +0800

    add arc dbg example

---
+++ arc_dbg_example/src/main.rs
@@ -0,0 +1,17 @@
+use std::sync::Arc;
+
+fn main() {
+    let data: String = "hello".into();
+
+    let s1 = Arc::new(data);
+    let s2 = s1.clone();
+    let s3 = s1.clone();
+
+    dbg!(&s1 as *const _);
+    dbg!(&s2 as *const _);
+    dbg!(&s3 as *const _);
+
+    dbg!(s1.as_bytes() as *const _);
+    dbg!(s2.as_bytes() as *const _);
+    dbg!(s3.as_bytes() as *const _);
+}
