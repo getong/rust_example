253386a2f0e1fef8258ef8700b63317c9dec382c
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-04-27 16:19:53 +0800

    add combinator ok_or function example

---
+++ combinator_ok_or_example/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // println!("Hello, world!");
+    const ERR_DEFAULT: &str = "error message";
+
+    let s = Some("abcde");
+    let n: Option<&str> = None;
+
+    let o: Result<&str, &str> = Ok("abcde");
+    let e: Result<&str, &str> = Err(ERR_DEFAULT);
+
+    assert_eq!(s.ok_or(ERR_DEFAULT), o); // Some(T) -> Ok(T)
+    assert_eq!(n.ok_or(ERR_DEFAULT), e); // None -> Err(default)
+}
