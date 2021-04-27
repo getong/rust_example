7e83ea606bd49adac11742495f8b4478f713848f
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-04-27 15:36:12 +0800

    update code

--- pointer_example/src/main.rs
+++ pointer_example/src/main.rs
@@ -1,5 +1,27 @@
+use std::mem::transmute;
+
 #[derive(Debug)]
 struct Items(u32);
+
+fn fat_pointers_info() {
+    let mut arr = [1, 2, 3, 4];
+    let slice = &mut arr[1..4];
+    slice[0] = 100;
+    println!("{:?}", arr); // [1, 100, 3, 4]
+
+    let slice = &arr[1..4];
+    println!("{:p} {:?}", slice, unsafe {
+        transmute::<_, (usize, usize)>(slice)
+    });
+    // Output: 0x8a6c0ff4ac (594518471852, 3)
+    //      0x8a6c0ff4ac 594518471852 这两个值是相等的。
+    //      (594518471852, 3) 分别表示 具体数据的堆地址 和 长度 两个字段。
+    //      注意这里是用 slice，而不是 &slice。(&slice表示这个变量本身的栈地址)
+
+    println!("sizeof &[i32]:{}", std::mem::size_of::<&[i32]>());
+    // Output: sizeof &[i32]:16
+    // 因为包含了两个字段：地址 + 长度，所以其占用内存为 2 个 usize 类型大小
+}
 
 fn main() {
     let items = Items(2);
@@ -17,4 +39,6 @@ fn main() {
     }
     println!("{:?}", items);
     println!("{:?}", a); // without the above scope
+
+    fat_pointers_info();
 }
