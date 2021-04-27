c38b2c5d597ff6947547dd3cffcac6c03eed6927
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-04-27 16:23:48 +0800

    combinator ok_or_else function example

---
+++ combinator_ok_or_else_example/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // println!("Hello, world!");
+    let s = Some("abcde");
+    let n: Option<&str> = None;
+    let fn_err_message = || "error message";
+
+    let o: Result<&str, &str> = Ok("abcde");
+    let e: Result<&str, &str> = Err("error message");
+
+    assert_eq!(s.ok_or_else(fn_err_message), o); // Some(T) -> Ok(T)
+    assert_eq!(n.ok_or_else(fn_err_message), e); // None -> Err(default)
+}
