3ff5998922f5e098d238e4aca24683f9561e33f2
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-04-27 12:27:02 +0800

    add combinator or and function example

---
+++ combinator_or_and_example/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    // println!("Hello, world!");
+
+    let s1 = Some("some1");
+    let s2 = Some("some2");
+    let n: Option<&str> = None;
+
+    let o1: Result<&str, &str> = Ok("ok1");
+    let o2: Result<&str, &str> = Ok("ok2");
+    let e1: Result<&str, &str> = Err("error1");
+    let e2: Result<&str, &str> = Err("error2");
+
+    assert_eq!(s1.or(s2), s1); // Some1 or Some2 = Some1
+    assert_eq!(s1.or(n), s1); // Some or None = Some
+    assert_eq!(n.or(s1), s1); // None or Some = Some
+    assert_eq!(n.or(n), n); // None1 or None2 = None2
+
+    assert_eq!(o1.or(o2), o1); // Ok1 or Ok2 = Ok1
+    assert_eq!(o1.or(e1), o1); // Ok or Err = Ok
+    assert_eq!(e1.or(o1), o1); // Err or Ok = Ok
+    assert_eq!(e1.or(e2), e2); // Err1 or Err2 = Err2
+
+    assert_eq!(s1.and(s2), s2); // Some1 and Some2 = Some2
+    assert_eq!(s1.and(n), n); // Some and None = None
+    assert_eq!(n.and(s1), n); // None and Some = None
+    assert_eq!(n.and(n), n); // None1 and None2 = None1
+
+    assert_eq!(o1.and(o2), o2); // Ok1 and Ok2 = Ok2
+    assert_eq!(o1.and(e1), e1); // Ok and Err = Err
+    assert_eq!(e1.and(o1), e1); // Err and Ok = Err
+    assert_eq!(e1.and(e2), e1); // Err1 and Err2 = Err1
+}
