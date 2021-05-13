1cfa4cd0909489f297e58b8ea91f235061f12486
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-05-13 12:33:08 +0800

    add more code

--- reference_example/src/main.rs
+++ reference_example/src/main.rs
@@ -1,3 +1,7 @@
+fn print_type_of<T>(_: T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 fn main() {
     let my_number = 15; // This is an i32
     let single_reference = &my_number; //  This is a &i32
@@ -8,4 +12,14 @@ fn main() {
         "single_reference: {:p}, double_reference: {:p}, five_references:{:p}",
         single_reference, double_reference, five_references
     );
+
+    println!(
+        "single_reference: {}, double_reference: {}, five_references:{}",
+        single_reference, double_reference, five_references
+    );
+
+    print_type_of(my_number);
+    print_type_of(single_reference);
+    print_type_of(double_reference);
+    print_type_of(five_references);
 }
