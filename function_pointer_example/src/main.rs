ed92fe1a98a7fc5054c180e8b27669eaa05c01af
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-05-13 17:54:07 +0800

    add code

--- function_pointer_example/src/main.rs
+++ function_pointer_example/src/main.rs
@@ -2,6 +2,10 @@ fn hello() {
     println!("hello function pointer!");
 }
 
+fn print_type_of<T>(_: T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 fn main() {
     // println!("Hello, world!");
     let fn_ptr: fn() = hello;
@@ -12,4 +16,9 @@ fn main() {
 
     fn_ptr();
     other_fn();
+
+    // output fn()
+    print_type_of(fn_ptr);
+    // output function_pointer_example::hello
+    print_type_of(other_fn);
 }
