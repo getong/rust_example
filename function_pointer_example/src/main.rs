c0ee3cf29bee2c6e6453663340ea0f2ac135a75a
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-05-14 00:14:50 +0800

    add more code

--- function_pointer_example/src/main.rs
+++ function_pointer_example/src/main.rs
@@ -6,6 +6,18 @@ fn print_type_of<T>(_: T) {
     println!("{}", std::any::type_name::<T>())
 }
 
+fn one(n: i32) -> i32 {
+    n + 1
+}
+
+fn two(n: i32) -> i32 {
+    n + 2
+}
+
+fn three(n: i32) -> i32 {
+    n + 3
+}
+
 fn main() {
     // println!("Hello, world!");
     let fn_ptr: fn() = hello;
@@ -21,4 +33,14 @@ fn main() {
     print_type_of(fn_ptr);
     // output function_pointer_example::hello
     print_type_of(other_fn);
+
+    let f1: fn(i32) -> i32 = one;
+    let f2: fn(i32) -> i32 = two;
+    let f3: fn(i32) -> i32 = three;
+
+    let funcs = [f1, f2, f3];
+
+    for f in &funcs {
+        println!("{:?}", f(1));
+    }
 }
