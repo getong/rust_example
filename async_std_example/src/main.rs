507f9a958d3616113ebed7fdb13d4b28969b6509
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-05-21 23:16:10 +0800

    update code

--- async_std_example/src/main.rs
+++ async_std_example/src/main.rs
@@ -1,5 +1,6 @@
 use async_std::io::prelude::*;
 use async_std::net;
+use async_std::task;
 
 async fn cheapo_request(host: &str, port: u16, path: &str) -> std::io::Result<String> {
     let mut socket = net::TcpStream::connect((host, port)).await?;
@@ -13,7 +14,6 @@ async fn cheapo_request(host: &str, port: u16, path: &str) -> std::io::Result<St
 }
 
 fn main() -> std::io::Result<()> {
-    use async_std::task;
     let response = task::block_on(cheapo_request("baidu.com", 80, "/"))?;
     println!("{}", response);
     Ok(())
