ebb2876df26a9159bd9bf7655aa9de5db87e44d0
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-05-24 23:32:07 +0800

    add deref example3

---
+++ deref_example3/src/main.rs
@@ -0,0 +1,14 @@
+fn check(s: &str) {
+    println!("check finished {}", s);
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let a = Some(String::from("jack"));
+
+    let b: Option<&str> = a.as_deref();
+
+    check(b.unwrap());
+
+    println!("{:?}", a);
+}
