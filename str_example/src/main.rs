c95aacad737a9d72d826f39970e57f871d5ad1af
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-05-26 16:21:58 +0800

    add str example

---
+++ str_example/src/main.rs
@@ -0,0 +1,21 @@
+fn strtok<'a>(s: &'a mut &'a str, delimiter: char) -> &'a str {
+    if let Some(i) = s.find(delimiter) {
+        let prefix = &s[..i];
+        let suffix = &s[(i + delimiter.len_utf8())..];
+        *s = suffix;
+        prefix
+    } else {
+        let prefix = *s;
+        *s = "";
+        prefix
+    }
+}
+
+fn main() {
+    //println!("Hello, world!");
+
+    let x1 = "hello world".to_owned();
+    let mut x = x1.as_str();
+    let hello = strtok(&mut x, ' ');
+    assert_eq!(hello, "hello");
+}
