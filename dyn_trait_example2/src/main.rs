7a31f31d60c005a5756675904e7261f23438db42
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-05-28 10:53:08 +0800

    add dyn trait example 2

---
+++ dyn_trait_example2/src/main.rs
@@ -0,0 +1,34 @@
+use std::fmt::Debug;
+
+trait Animal: Debug {
+    fn walk(&self) {
+        println!("walk");
+    }
+}
+
+impl dyn Animal {
+    fn talk() {
+        println!("talk");
+    }
+}
+
+#[derive(Debug)]
+struct Person;
+
+impl Animal for Person {}
+
+fn demo() -> Box<dyn Animal> {
+    let p = Person;
+    Box::new(p)
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let p = Person;
+
+    p.walk();
+
+    let p1 = demo();
+    p1.walk();
+    p1.talk();
+}
