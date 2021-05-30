9a4266a3eda29e7f4f51f4b92037a26cad84c0b9
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-05-30 22:48:52 +0800

    add tokio_stream example

---
+++ tokio_stream_example/src/main.rs
@@ -0,0 +1,10 @@
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() {
+    let mut stream = tokio_stream::iter(&[1, 2, 3]);
+
+    while let Some(v) = stream.next().await {
+        println!("GOT = {:?}", v);
+    }
+}
