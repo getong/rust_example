e72827693d59ff381e33881c516a54f8d9832647
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-03 17:00:55 +0800

    add more example

--- iterator_example/src/main.rs
+++ iterator_example/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     let vector1 = vec![1, 2, 3]; // we will use .iter() and .into_iter() on this one
     let vector1_a = vector1.iter().map(|x| x + 1).collect::<Vec<i32>>();
+    println!("after use iter() function, vector1:{:?}\n", vector1);
     let vector1_b = vector1.into_iter().map(|x| x * 10).collect::<Vec<i32>>();
+
+    // println!("after use into_iter() function, vector1:{:?}\n", vector1);
 
     let mut vector2 = vec![10, 20, 30]; // we will use .iter_mut() on this one
     vector2.iter_mut().for_each(|x| *x += 100);
+    println!("after use into_mut() function, vector2:{:?}\n", vector2);
 
     println!("{:?}", vector1_a);
     println!("{:?}", vector2);
