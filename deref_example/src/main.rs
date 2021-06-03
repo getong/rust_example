92ce071a12ab8f00327b226a7ca9c37b0ccbce44
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-03 14:58:49 +0800

    add more example

--- deref_example/src/main.rs
+++ deref_example/src/main.rs
@@ -1,9 +1,11 @@
+use std::ops::Deref;
+
 #[derive(Debug)]
 struct DemoStruct {
     name: &'static str,
 }
 
-impl std::ops::Deref for DemoStruct {
+impl Deref for DemoStruct {
     type Target = str;
 
     fn deref(&self) -> &Self::Target {
@@ -16,8 +18,27 @@ fn check(s: &str) {
     println!("check finish {}", s)
 }
 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
 fn main() {
     // println!("Hello, world!");
     let a = DemoStruct { name: "jack" };
     check(&a);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, *y);
 }
