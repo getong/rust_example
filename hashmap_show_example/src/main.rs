5eb2b73a26e62894328dc80009c69337da4a3267
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-04 23:45:16 +0800

    add hashmap show example

---
+++ hashmap_show_example/src/main.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+
+type Table = HashMap<String, Vec<String>>;
+
+fn show(table: &Table) {
+    for (artist, works) in table {
+        println!("works by {}: ", artist);
+        for work in works {
+            println!("     {}", work);
+        }
+    }
+}
+
+fn sort_works(table: &mut Table) {
+    for (_artist, works) in table {
+        works.sort();
+    }
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let mut table = Table::new();
+
+    table.insert(
+        "Gesualdo".to_string(),
+        vec![
+            "many madrigals".to_string(),
+            "Tenebrae Responsoria".to_string(),
+        ],
+    );
+
+    table.insert(
+        "Caravaggio".to_string(),
+        vec![
+            "The Musicians".to_string(),
+            "The Calling of St. Matthew".to_string(),
+        ],
+    );
+
+    table.insert(
+        "Cellini".to_string(),
+        vec![
+            "Perseus with the head of Medusa".to_string(),
+            "a salt cellar".to_string(),
+        ],
+    );
+
+    println!("before sorting\n");
+    show(&table);
+
+    sort_works(&mut table);
+    println!("after sorting\n");
+
+    show(&table);
+
+    assert_eq!(table["Gesualdo"][1], "many madrigals");
+}
