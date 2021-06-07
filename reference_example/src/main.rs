ea0a8267d618c3455225bf7e57462e09b2b12cb8
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-07 17:59:43 +0800

    add more code

--- reference_example/src/main.rs
+++ reference_example/src/main.rs
@@ -2,6 +2,11 @@ fn print_type_of<T>(_: T) {
     println!("{}", std::any::type_name::<T>())
 }
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
     let my_number = 15; // This is an i32
     let single_reference = &my_number; //  This is a &i32
@@ -22,4 +27,11 @@ fn main() {
     print_type_of(single_reference);
     print_type_of(double_reference);
     print_type_of(five_references);
+
+    let point = Point { x: 1000, y: 729 };
+    let r: &Point = &point;
+    let rr: &&Point = &r;
+    let rrr: &&&Point = &rr;
+    assert_eq!(rrr.y, 729);
+    assert_eq!(rrr.x, 1000);
 }
