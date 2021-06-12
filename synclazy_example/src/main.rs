cfe914974497b37592bfef5af1e025ba3f153b7d
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-12 18:05:24 +0800

    add synclazy example

---
+++ synclazy_example/src/main.rs
@@ -0,0 +1,16 @@
+#![feature(once_cell)] // 1.53.0-nightly (2021-04-01 d474075a8f28ae9a410e)
+use std::{lazy::SyncLazy, sync::Mutex};
+
+static ARRAY: SyncLazy<Mutex<Vec<u8>>> = SyncLazy::new(|| Mutex::new(vec![]));
+
+fn do_a_call() {
+    ARRAY.lock().unwrap().push(1);
+}
+
+fn main() {
+    do_a_call();
+    do_a_call();
+    do_a_call();
+
+    println!("called {}", ARRAY.lock().unwrap().len());
+}
