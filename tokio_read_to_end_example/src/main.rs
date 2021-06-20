5099b0cd72ed399cf8cc1a2d4c5f7f03c8dcd1f1
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-20 21:32:07 +0800

    add tokio read_to_end example

---
+++ tokio_read_to_end_example/src/main.rs
@@ -0,0 +1,13 @@
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut f = File::open("foo.txt").await?;
+    let mut buffer = Vec::new();
+
+    // read the whole file
+    f.read_to_end(&mut buffer).await?;
+    println!("buffer : {:?}", buffer);
+    Ok(())
+}
