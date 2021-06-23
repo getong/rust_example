3c27576a3a1e7aede57e196b41abebbb12481253
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-23 12:34:07 +0800

    add tokio select example

---
+++ tokio_select_example/src/main.rs
@@ -0,0 +1,24 @@
+use tokio::sync::oneshot;
+
+#[tokio::main]
+async fn main() {
+    let (tx1, rx1) = oneshot::channel();
+    let (tx2, rx2) = oneshot::channel();
+
+    tokio::spawn(async {
+        let _ = tx1.send("one");
+    });
+
+    tokio::spawn(async {
+        let _ = tx2.send("two");
+    });
+
+    tokio::select! {
+        val = rx1 => {
+            println!("rx1 completed first with {:?}", val);
+        }
+        val = rx2 => {
+            println!("rx2 completed first with {:?}", val);
+        }
+    }
+}
