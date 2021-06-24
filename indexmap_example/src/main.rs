8a7d6a37f4b64b49acc5df07af6a4f306992c66e
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-24 12:12:40 +0800

    add indexmap example

---
+++ indexmap_example/src/main.rs
@@ -0,0 +1,15 @@
+use indexmap::IndexMap;
+
+fn main() {
+    //println!("Hello, world!");
+    // count the frequency of each letter in a sentence.
+    let mut letters = IndexMap::new();
+    for ch in "a short treatise on fungi".chars() {
+        *letters.entry(ch).or_insert(0) += 1;
+    }
+
+    assert_eq!(letters[&'s'], 2);
+    assert_eq!(letters[&'t'], 3);
+    assert_eq!(letters[&'u'], 1);
+    assert_eq!(letters.get(&'y'), None);
+}
