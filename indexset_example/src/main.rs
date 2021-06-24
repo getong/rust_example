ef22983a95599c31da453db5e2a67598643839d4
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-24 12:14:20 +0800

    add indexset example

---
+++ indexset_example/src/main.rs
@@ -0,0 +1,12 @@
+use indexmap::IndexSet;
+
+fn main() {
+    // println!("Hello, world!");
+    // Collects which letters appear in a sentence.
+    let letters: IndexSet<_> = "a short treatise on fungi".chars().collect();
+
+    assert!(letters.contains(&'s'));
+    assert!(letters.contains(&'t'));
+    assert!(letters.contains(&'u'));
+    assert!(!letters.contains(&'y'));
+}
