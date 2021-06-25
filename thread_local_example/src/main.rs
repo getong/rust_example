30908cd7bd8cb065aea7c602849aae7f32913977
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-25 12:02:05 +0800

    add thread_local! example

---
+++ thread_local_example/src/main.rs
@@ -0,0 +1,28 @@
+use std::cell::RefCell;
+use std::thread;
+
+thread_local!(static FOO: RefCell<u32> = RefCell::new(1));
+
+fn main() {
+    // println!("Hello, world!");
+    FOO.with(|f| {
+        assert_eq!(*f.borrow(), 1);
+        *f.borrow_mut() = 2;
+    });
+
+    // each thread starts out with the initial value of 1
+    let t = thread::spawn(move || {
+        FOO.with(|f| {
+            assert_eq!(*f.borrow(), 1);
+            *f.borrow_mut() = 3;
+        });
+    });
+
+    // wait for the thread to complete and bail out on panic
+    t.join().unwrap();
+
+    // we retain our original value of 2 despite the child thread
+    FOO.with(|f| {
+        assert_eq!(*f.borrow(), 2);
+    });
+}
