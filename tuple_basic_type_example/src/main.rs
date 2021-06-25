5fd5be88c76603d58e241d5c9c2aac7f4ef8c606
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-25 10:56:40 +0800

    add tuple basic type example

---
+++ tuple_basic_type_example/src/main.rs
@@ -0,0 +1,23 @@
+/// Id of responses handler.
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
+pub struct HandlerId(usize, bool);
+
+impl HandlerId {
+    fn new(id: usize, respondable: bool) -> Self {
+        HandlerId(id, respondable)
+    }
+    pub fn raw_id(self) -> usize {
+        self.0
+    }
+    /// Indicates if a handler id corresponds to callback in the Agent runtime.
+    pub fn is_respondable(self) -> bool {
+        self.1
+    }
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let a: HandlerId = HandlerId::new(1, true);
+    let b = a;
+    println!("a: {:?}, {:p}\nb: {:?}, {:p}", a, &a, b, &b);
+}
