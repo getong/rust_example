9383660c11b21ac608520f401b20f981c7049d91
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-06-29 17:56:22 +0800

    add nanoid example

---
+++ nanoid_example/src/main.rs
@@ -0,0 +1,17 @@
+use nanoid::nanoid;
+
+fn main() {
+    // println!("Hello, world!");
+    let id1 = nanoid!();
+    println!("id1 is {}", id1);
+
+    let id2 = nanoid!(10);
+    println!("id2 is {}", id2);
+
+    let alphabet: [char; 16] = [
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f',
+    ];
+
+    let id3 = nanoid!(10, &alphabet);
+    println!("id3 is {}", id3);
+}
