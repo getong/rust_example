e16a09696681c903018f7e352f719efa24859ea5
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-07-03 12:17:49 +0800

    add sled example

---
+++ sled_example/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // println!("Hello, world!");
+    let tree = sled::open("/tmp/welcome-to-sled").unwrap();
+
+    // insert and get, similar to std's BTreeMap
+    let _old_value = tree.insert("key", "value").unwrap();
+
+    assert_eq!(tree.get(&"key").unwrap(), Some(sled::IVec::from("value")));
+
+    // range queries
+    for _kv_result in tree.range("key_1".."key_9") {}
+
+    // deletion
+    let _old_value = tree.remove(&"key").unwrap();
+
+    // atomic compare and swap
+    let _ = tree
+        .compare_and_swap("key", Some("current_value"), Some("new_value"))
+        .unwrap();
+
+    // block until all operations are stable on disk
+    // (flush_async also available to get a Future)
+    let _ = tree.flush().unwrap();
+}
