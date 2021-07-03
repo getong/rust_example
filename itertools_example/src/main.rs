9913c08a5ef74d97627e4323cdc2a61ebd9dfdd4
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-07-03 23:59:08 +0800

    add itertools example

---
+++ itertools_example/src/main.rs
@@ -0,0 +1,21 @@
+extern crate itertools;
+use itertools::Itertools;
+
+fn main() {
+    // println!("Hello, world!");
+    let arr = [
+        10u32, 14, 5, 76, 84, 35, 23, 94, 100, 143, 23, 200, 12, 94, 72,
+    ];
+    for tuple in arr.iter().batching(|it| match it.next() {
+        None => None,
+        Some(x) => match it.next() {
+            None => None,
+            Some(z) => match it.next() {
+                None => None,
+                Some(y) => Some((x, y, z)),
+            },
+        },
+    }) {
+        println!("tuple: {:?}", tuple);
+    }
+}
