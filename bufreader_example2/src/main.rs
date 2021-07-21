81350ee582ff58d5927423723953c6adc2929704
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-07-21 21:17:49 +0800

    add bufreader example2

---
+++ bufreader_example2/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> std::result::Result<(), std::io::Error> {
+    let filename = "test.bed";
+    let fi = File::open(filename)?;
+    let mut bedreader = io::BufReader::new(fi); //note here, it is `mut`
+    let mut line = String::new(); // not here, it is `mut`
+    while bedreader.read_line(&mut line).unwrap() > 0 {
+        println!("{}", line.trim_end());
+        line.clear();
+    }
+    Ok(())
+}
