73a6b62ffb18b8f7a95df185630eb8e7bcc43705
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-07-24 23:49:56 +0800

    add poison error example

---
+++ poison_error_example/src/main.rs
@@ -0,0 +1,24 @@
+use std::sync::{Arc, RwLock};
+use std::thread;
+
+fn main() {
+    let lock = Arc::new(RwLock::new(11));
+    let c_lock = Arc::clone(&lock);
+
+    let _ = thread::spawn(move || {
+        let _lock = c_lock.write().unwrap();
+        panic!(); // the lock gets poisoned
+    })
+    .join();
+
+    let read = match lock.read() {
+        Ok(l) => *l,
+        Err(poisoned) => {
+            let r = poisoned.into_inner();
+            *r + 1
+        }
+    };
+
+    // It will be 12 because it was recovered from the poisoned lock
+    assert_eq!(read, 12);
+}
