50d1750ebd0b3605c54bba4f031ff21896a06c1b
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-07-26 10:20:59 +0800

    add more code

--- raw_pointer_example/src/main.rs
+++ raw_pointer_example/src/main.rs
@@ -21,4 +21,15 @@ fn raw_pointers_info() {
 fn main() {
     // println!("Hello, world!");
     raw_pointers_info();
+
+    let mut x = 10;
+    let ptr_x = &mut x as *mut i32;
+
+    let y = Box::new(20);
+    let ptr_y = &*y as *const i32;
+
+    unsafe {
+        *ptr_x += *ptr_y;
+    }
+    assert_eq!(x, 30);
 }
