2b810b7357cb44d1861e03be00f7aa22ec714181
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-07-27 22:04:10 +0800

    add more code

--- union_example/src/main.rs
+++ union_example/src/main.rs
@@ -4,23 +4,33 @@ union Metric {
     precise: f32,
 }
 
+union FloatOrInt {
+    f: f32,
+    i: i32,
+}
+
 fn main() {
     // println!("Hello, world!");
     let mut a = Metric { rounded: 323 };
     unsafe {
-        println!("{}", a.rounded);
+        println!("rounded: {}", a.rounded);
     }
 
     unsafe {
-        println!("{}", a.precise);
+        println!("precise: {}", a.precise);
     }
 
     a.precise = 33.3;
     unsafe {
-        println!("{}", a.precise);
+        println!("precise: {}", a.precise);
     }
 
     unsafe {
-        println!("{}", a.rounded);
+        println!("rounded: {}", a.rounded);
     }
+
+    let mut one = FloatOrInt { i: 1 };
+    assert_eq!(unsafe { one.i }, 0x00_00_00_01);
+    one.f = 1.0;
+    assert_eq!(unsafe { one.i }, 0x3F_80_00_00);
 }
