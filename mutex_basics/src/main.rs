be43357ddce116aced7b1857e50cbed9f32d0cfa
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-08-02 23:57:13 +0800

    add try_lock() method

--- mutex_basics/src/main.rs
+++ mutex_basics/src/main.rs
@@ -1,6 +1,7 @@
 // mutex_basics.rs
 use std::sync::Mutex;
 use std::thread;
+
 fn main() {
     let m = Mutex::new(0);
     let c = thread::spawn(move || {
@@ -11,4 +12,18 @@ fn main() {
     });
     let updated = c.join().unwrap();
     println!("{:?}", updated);
+
+    try_lock();
+}
+
+fn try_lock() {
+    let my_mutex = Mutex::new(5);
+    let _mutex_changer = my_mutex.lock().unwrap();
+    let other_mutex_changer = my_mutex.try_lock();
+
+    if let Ok(value) = other_mutex_changer {
+        println!("The MutexGuard has: {}", value)
+    } else {
+        println!("Didn't get the lock")
+    }
 }
