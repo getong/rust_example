6ea20ae2d1e5b5bee9e29ee1431c8132c68c24f8
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-08-14 20:02:17 +0800

    add new code

--- arc_mutex/src/main.rs
+++ arc_mutex/src/main.rs
@@ -1,6 +1,37 @@
 // arc_mutex.rs
 use std::sync::{Arc, Mutex};
-use std::thread;
+use std::thread::{self, JoinHandle};
+
+struct MySharedData {
+    pub counter: u32,
+}
+
+impl MySharedData {
+    pub fn new() -> MySharedData {
+        MySharedData { counter: 0 }
+    }
+}
+
+fn spawn_threads() {
+    let shared_data = Arc::new(Mutex::new(MySharedData::new()));
+    // Spawn a number of threads and collect their join handles
+    let handles: Vec<JoinHandle<_>> = (0..10)
+        .map(|_| {
+            let shared_data = shared_data.clone();
+            thread::spawn(move || {
+                let mut shared_data = shared_data.lock().unwrap();
+                shared_data.counter += 1;
+            })
+        })
+        .collect();
+    // Wait for each thread to complete
+    for h in handles {
+        let _ = h.join();
+    }
+    // Print the data
+    let shared_data = shared_data.lock().unwrap();
+    println!("Total = {}", shared_data.counter);
+}
 
 fn main() {
     let vec = Arc::new(Mutex::new(vec![]));
@@ -32,4 +63,8 @@ fn main() {
         });
     }
     println!("data: {}", data.lock().unwrap());
+
+    println!("----------------");
+
+    spawn_threads();
 }
