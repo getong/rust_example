73f1347c1001be3d7d2ff3b180bbbc80dd9260b2
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-08-21 21:42:19 +0800

    add tokio joinhandle abort example

---
+++ tokio_joinhandle_abort_example/README.md
@@ -0,0 +1,5 @@
+# tokio::task::JoinHandle abort example
+
+tokio::task::JoinError::is_cancelled
+pub fn is_cancelled(&self) -> bool
+Returns true if the error was caused by the task being cancelled
---
+++ tokio_joinhandle_abort_example/src/main.rs
@@ -0,0 +1,24 @@
+use tokio::time;
+
+#[tokio::main]
+async fn main() {
+    let mut handles = Vec::new();
+
+    handles.push(tokio::spawn(async {
+        time::sleep(time::Duration::from_secs(10)).await;
+        true
+    }));
+
+    handles.push(tokio::spawn(async {
+        time::sleep(time::Duration::from_secs(10)).await;
+        false
+    }));
+
+    for handle in &handles {
+        handle.abort();
+    }
+
+    for handle in handles {
+        assert!(handle.await.unwrap_err().is_cancelled());
+    }
+}
