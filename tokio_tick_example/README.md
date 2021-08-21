1c1dcf0958688bf917e855b423f0856229c34441
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-08-21 18:27:32 +0800

    add tokio tick example

---
+++ tokio_tick_example/README.md
@@ -0,0 +1,2 @@
+# tokio tick example
+copy from https://stackoverflow.com/questions/66863385/how-can-i-use-tokio-to-trigger-a-function-every-period-or-interval-in-seconds
---
+++ tokio_tick_example/src/main.rs
@@ -0,0 +1,20 @@
+use std::time::Duration;
+use tokio::{task, time}; // 1.3.0
+
+#[tokio::main]
+async fn main() {
+    let forever = task::spawn(async {
+        let mut interval = time::interval(Duration::from_millis(10));
+
+        loop {
+            interval.tick().await;
+            do_something().await;
+        }
+    });
+
+    let _ = forever.await;
+}
+
+async fn do_something() {
+    eprintln!("do_something");
+}
