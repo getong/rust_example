708026113b90cae3b42da5fada97c88d0a35e2ae
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-08-22 20:45:07 +0800

    add SystemTime example

---
+++ system_time_example/src/main.rs
@@ -0,0 +1,9 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn main() {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    println!("{:?}", since_the_epoch);
+}
