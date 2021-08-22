b2f644b0b0d884f3a16c19295c083c6b353403e7
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-08-22 16:22:58 +0800

    add tokio rand time tick example

---
+++ tokio_rand_time_tick_example/src/main.rs
@@ -0,0 +1,45 @@
+use chrono::Utc;
+use rand::distributions::{Distribution, Uniform};
+use std::time::Duration;
+use tokio::time;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let (send, recv) = tokio::sync::mpsc::channel(100);
+
+    let shutdown_trigger = tokio::spawn(async move {
+        let mut i = 1;
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        // We get the expected behavior by commenting out this line.
+        i += 1;
+        send.send(i).await.unwrap();
+    });
+
+    let done = run_with_shutdown(recv);
+    done.await;
+    shutdown_trigger.await.unwrap();
+
+    Ok(())
+}
+
+async fn run_with_shutdown(mut shutdown: tokio::sync::mpsc::Receiver<i32>) {
+    let mut interval = time::interval(time::Duration::from_secs(1));
+    let between = Uniform::from(1..4);
+    let mut rng = rand::thread_rng();
+
+    loop {
+        tokio::select! {
+            _ = interval.tick() => println!("tick, now: {:?}", Utc::now()),
+            number = shutdown.recv() =>  {
+                if let Some(i) = number {
+                    println!("shutting down, number is {:?}, now: {:?}", i, Utc::now());
+                    let rand_interval_time = between.sample(&mut rng);
+                    println!("rand_interval_time:{}", rand_interval_time);
+                    interval = time::interval(time::Duration::from_secs(rand_interval_time));
+                    // return
+                }
+            },
+        }
+    }
+}
