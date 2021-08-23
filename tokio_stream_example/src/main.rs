dc0cf2a8bd8ae5caa952c135bd5f23824008fb3e
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-08-23 23:58:03 +0800

    update tokio_stream code example

--- tokio_stream_example/src/main.rs
+++ tokio_stream_example/src/main.rs
@@ -1,3 +1,5 @@
+use tokio::sync::watch;
+use tokio_stream::wrappers::WatchStream;
 use tokio_stream::StreamExt;
 
 #[tokio::main]
@@ -7,4 +9,18 @@ async fn main() {
     while let Some(v) = stream.next().await {
         println!("GOT = {:?}", v);
     }
+
+    let (tx, rx) = watch::channel("hello");
+    let mut rx = WatchStream::new(rx);
+
+    assert_eq!(rx.next().await, Some("hello"));
+
+    tx.send("goodbye").unwrap();
+    assert_eq!(rx.next().await, Some("goodbye"));
+
+    let (tx, rx) = watch::channel("hello");
+    let mut rx = WatchStream::new(rx);
+
+    tx.send("goodbye").unwrap();
+    assert_eq!(rx.next().await, Some("goodbye"));
 }
