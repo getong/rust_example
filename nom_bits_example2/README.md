e26c7796e4a23e76bb01ea3b5723fe77fac3beb2
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-08-26 21:59:42 +0800

    add nom bits example 2

---
+++ nom_bits_example2/README.md
@@ -0,0 +1,2 @@
+# nom bits example 2
+copy from [How can I take N bits of byte in nom?](https://stackoverflow.com/questions/62763705/how-can-i-take-n-bits-of-byte-in-nom)
---
+++ nom_bits_example2/src/main.rs
@@ -0,0 +1,29 @@
+use nom::{
+    bits::{
+        bits,
+        complete::{tag, take},
+    },
+    IResult,
+};
+
+pub fn take_2_bits(i: (&[u8], usize)) -> IResult<(&[u8], usize), u8> {
+    take(2usize)(i)
+}
+
+pub fn check_tag(i: (&[u8], usize)) -> IResult<(&[u8], usize), u8> {
+    tag(0x01, 1usize)(i)
+}
+
+pub fn do_everything_bits(i: (&[u8], usize)) -> IResult<(&[u8], usize), (u8, u8)> {
+    let (i, a) = take_2_bits(i)?;
+    let (i, b) = check_tag(i)?;
+    Ok((i, (a, b)))
+}
+
+pub fn do_everything_bytes(i: &[u8]) -> IResult<&[u8], (u8, u8)> {
+    bits(do_everything_bits)(i)
+}
+
+fn main() {
+    println!("Hello, world!");
+}
