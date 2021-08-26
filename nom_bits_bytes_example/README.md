6157df7de630bf30ecf6f3888c8985cdb1ecc2e4
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-08-26 22:04:12 +0800

    add nom::bits::bytes example

---
+++ nom_bits_bytes_example/README.md
@@ -0,0 +1,3 @@
+# nom::bits::bytes exmaple
+
+copy from [Function nom::bits::bytes](https://docs.rs/nom/7.0.0/nom/bits/fn.bytes.html)
---
+++ nom_bits_bytes_example/src/main.rs
@@ -0,0 +1,20 @@
+use nom::bits::{bits, bytes, streaming::take};
+use nom::combinator::rest;
+use nom::error::Error;
+use nom::sequence::tuple;
+use nom::IResult;
+
+fn parse(input: &[u8]) -> IResult<&[u8], (u8, u8, &[u8])> {
+    bits::<_, _, Error<(&[u8], usize)>, _, _>(tuple((
+        take(4usize),
+        take(8usize),
+        bytes::<_, _, Error<&[u8]>, _, _>(rest),
+    )))(input)
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let input = &[0x12, 0x34, 0xff, 0xff];
+    assert_eq!(parse(input), Ok((&[][..], (0x01, 0x23, &[0xff, 0xff][..]))));
+}
