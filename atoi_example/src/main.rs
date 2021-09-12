56b1bfc17cb9e5aa1826025f336d85f602c4d1e0
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-12 19:33:13 +0800

    add example

---
+++ atoi_example/src/main.rs
@@ -0,0 +1,10 @@
+use atoi::atoi;
+use atoi::FromRadix10;
+
+fn main() {
+    // println!("Hello, world!");
+    assert_eq!(Some(42), atoi::<u32>(b"42"));
+
+    // Parsing to digits from a slice
+    assert_eq!((42, 2), u32::from_radix_10(b"42"));
+}
