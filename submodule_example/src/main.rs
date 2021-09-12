92a5e349a1df3a248c3fb5135aa602ae075ff2b0
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-12 22:22:10 +0800

    add example

--- submodule_example/src/main.rs
+++ submodule_example/src/main.rs
@@ -3,6 +3,17 @@ mod models;
 mod netmod;
 mod routes;
 
+mod food {
+    #[derive(Debug)]
+    pub struct Cake;
+    #[derive(Debug)]
+    struct Smoothie;
+    #[derive(Debug)]
+    struct Pizza;
+}
+
+use food::Cake;
+
 fn main() {
     routes::health_route::print_health_route();
     routes::user_route::print_user_route();
@@ -11,4 +22,7 @@ fn main() {
     netmod::sysmod::print_sysmod();
 
     println!("Hello, world!");
+
+    let eatable = Cake;
+    println!("eatable: {:?}", eatable);
 }
