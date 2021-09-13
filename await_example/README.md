17f0b04f20f2a3df56e59c34ff643a5d0367aed2
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-13 10:53:43 +0800

    add await example

---
+++ await_example/README.md
@@ -0,0 +1,3 @@
+# await example
+
+The owner of the future is responsible for advancing the computation by polling the future. This is done by calling Future::poll.
---
+++ await_example/src/main.rs
@@ -0,0 +1,32 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<&'static str> {
+        if Instant::now() >= self.when {
+            println!("Hello world");
+            Poll::Ready("done")
+        } else {
+            // Ignore this line for now.
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let when = Instant::now() + Duration::from_millis(10);
+    let future = Delay { when };
+
+    let out = future.await;
+    assert_eq!(out, "done");
+}
