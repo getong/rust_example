4bbeb31c107f7906d87722ab6a65fb010c3fd74a
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-13 19:49:45 +0800

    add tokio runtime spawn example

---
+++ tokio_runtime_spawn_example/src/main.rs
@@ -0,0 +1,37 @@
+use tokio::runtime::Builder;
+use tokio::time::{sleep, Duration};
+
+fn main() {
+    let runtime = Builder::new_multi_thread()
+        .worker_threads(1)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let mut handles = Vec::with_capacity(10);
+    for i in 0..10 {
+        handles.push(runtime.spawn(my_bg_task(i)));
+    }
+
+    // Do something time-consuming while the background tasks execute.
+    std::thread::sleep(Duration::from_millis(750));
+    println!("Finished time-consuming task.");
+
+    // Wait for all of them to complete.
+    for handle in handles {
+        // The `spawn` method returns a `JoinHandle`. A `JoinHandle` is
+        // a future, so we can wait for it using `block_on`.
+        runtime.block_on(handle).unwrap();
+    }
+}
+
+async fn my_bg_task(i: u64) {
+    // By subtracting, the tasks with larger values of i sleep for a
+    // shorter duration.
+    let millis = 1000 - 50 * i;
+    println!("Task {} sleeping for {} ms.", i, millis);
+
+    sleep(Duration::from_millis(millis)).await;
+
+    println!("Task {} stopping.", i);
+}
