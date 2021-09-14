b984a83d7cb0b344537497ca377c39453af41fc9
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-14 17:49:34 +0800

    add tokio broadcast example

---
+++ tokio_broadcast_example/src/main.rs
@@ -0,0 +1,20 @@
+use tokio::sync::broadcast;
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx1) = broadcast::channel(16);
+    let mut rx2 = tx.subscribe();
+
+    tokio::spawn(async move {
+        assert_eq!(rx1.recv().await.unwrap(), 10);
+        assert_eq!(rx1.recv().await.unwrap(), 20);
+    });
+
+    tokio::spawn(async move {
+        assert_eq!(rx2.recv().await.unwrap(), 10);
+        assert_eq!(rx2.recv().await.unwrap(), 20);
+    });
+
+    tx.send(10).unwrap();
+    tx.send(20).unwrap();
+}
