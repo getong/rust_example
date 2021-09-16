96b676a79ccdb137ed30ece69f9d8cc78ec41ddf
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-16 17:05:43 +0800

    add code

--- rusty_ulid_example/src/main.rs
+++ rusty_ulid_example/src/main.rs
@@ -4,7 +4,7 @@ use rusty_ulid::generate_ulid_string;
 use rusty_ulid::Ulid;
 use std::str::FromStr;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // println!("Hello, world!");
 
     // Generate a ULID string
@@ -27,4 +27,13 @@ fn main() {
     println!("ulid_string:{}", ulid_string);
 
     assert_eq!(Ok(ulid), result);
+
+    // Alternative way to parse a ULID string
+    // This example assumes a function returning a Result.
+    let ulid: Ulid = "01CAT3X5Y5G9A62FH1FA6T9GVR".parse()?;
+
+    let datetime = ulid.datetime();
+    assert_eq!(datetime.to_string(), "2018-04-11 10:27:03.749 UTC");
+    // Ok::<(), rusty_ulid::DecodingError>(())
+    Ok(())
 }
