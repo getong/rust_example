fa72a83288867b642ae9ab4c8b3ed8e9d715cd42
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-23 11:28:49 +0800

    add code

--- math_log_pow_example/src/main.rs
+++ math_log_pow_example/src/main.rs
@@ -22,3 +22,12 @@ fn max_i32_value() -> i32 {
 fn max_3_pow_value() -> i32 {
     3_i32.pow(i32::MAX.log(3))
 }
+
+//给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false 。
+// 整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3x
+// 链接：https://leetcode-cn.com/problems/power-of-three
+//impl Solution {
+//    pub fn is_power_of_three(n: i32) -> bool {
+//        return n > 0 && 1162261467 % n == 0;
+//    }
+//}
