dcbe329afb873c74f59b15ed15a10f82a9289587
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-27 14:34:12 +0800

    add pointer add example

---
+++ pointer_add_example/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // println!("Hello, world!");
+    let s: &str = "123";
+    let ptr: *const u8 = s.as_ptr();
+
+    unsafe {
+        println!("{}", *ptr.add(1) as char);
+        println!("{}", *ptr.add(2) as char);
+    }
+}
