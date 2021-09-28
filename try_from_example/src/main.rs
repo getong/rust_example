c42348faa08463fa92b994946024f82121225bfe
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-28 11:09:12 +0800

    add try_from example

---
+++ try_from_example/src/main.rs
@@ -0,0 +1,22 @@
+use std::convert::TryFrom;
+
+#[derive(Debug)]
+struct GreaterThanZero(i32);
+
+impl TryFrom<i32> for GreaterThanZero {
+    type Error = &'static str;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value <= 0 {
+            Err("GreaterThanZero only accepts value superior than zero!")
+        } else {
+            Ok(GreaterThanZero(value))
+        }
+    }
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let num = i32::try_from(2_i32).unwrap();
+    println!("num : {:?}", num);
+}
