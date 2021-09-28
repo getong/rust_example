32a885f2b43d1c16b42cc7e91a79dc0d757a532e
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-28 12:03:37 +0800

    add code

--- env_macro_example/src/main.rs
+++ env_macro_example/src/main.rs
@@ -1,5 +1,17 @@
+const PATH: &str = env!("PATH");
+
 fn main() {
     // println!("Hello, world!");
     let path: &'static str = env!("PATH");
-    println!("the $PATH variable at the time of compiling was: {}", path);
+    println!(
+        "the static $PATH variable at the time of compiling was: {}",
+        path
+    );
+
+    println!();
+
+    println!(
+        "the const $PATH variable at the time of compiling was: {}",
+        &*PATH
+    );
 }
