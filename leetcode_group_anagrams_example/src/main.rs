cd91c846e4b4d9e96e7a3332af9176a3517bf9b7
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-09-29 17:53:32 +0800

    add leetcode group anagrams example

--- leetcode_group_anagrams_example/src/main.rs
+++ leetcode_group_anagrams_example/src/main.rs
@@ -5,42 +5,30 @@ struct Solution;
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
         let mut vecs: Vec<Vec<String>> = Vec::new();
-        let mut used: Vec<bool> = vec![false; strs.len()];
+        let mut map: HashMap<String, Vec<String>> = HashMap::new();
 
         for i in 0..strs.len() {
-            let mut temp: Vec<String> = Vec::new();
-            if !used[i] {
-                temp.push(strs[i].clone());
-                for j in i + 1..strs.len() {
-                    let mut is_anagram: bool = true;
-                    if strs[i].len() != strs[j].len() {
-                        continue;
-                    }
-
-                    let mut map = HashMap::new();
-                    for c in strs[i].chars() {
-                        let count = map.entry(c).or_insert(0);
-                        *count += 1;
-                    }
-
-                    for c in strs[j].chars() {
-                        let count = map.entry(c).or_insert(0);
-                        *count -= 1;
-
-                        if *count < 0 {
-                            is_anagram = false;
-                            break;
-                        }
-                    }
-                    if is_anagram {
-                        used[j] = true;
-                        temp.push(strs[j].clone());
-                    }
-                }
+            let mut chars = vec![];
+            for c in strs[i].chars() {
+                chars.push(c);
             }
-            if !temp.is_empty() {
-                vecs.push(temp);
+            chars.sort();
+
+            let key: String = chars.into_iter().collect();
+
+            let value = map.get(&key);
+            if value != None {
+                let mut v = value.unwrap().to_vec();
+                v.push(strs[i].clone());
+                map.insert(key, v);
+            } else {
+                let v = vec![strs[i].clone()];
+                map.insert(key, v);
             }
+        }
+
+        for val in map.values() {
+            vecs.push(val.to_vec());
         }
         return vecs;
     }
@@ -49,19 +37,22 @@ impl Solution {
 fn main() {
     // println!("Hello, world!");
 
-    assert_eq!(
-        Solution::group_anagrams(vec![
-            "eat".to_string(),
-            "tea".to_string(),
-            "tan".to_string(),
-            "ate".to_string(),
-            "nat".to_string(),
-            "bat".to_string()
-        ]),
-        vec![
-            vec!["eat".to_string(), "tea".to_string(), "ate".to_string()],
-            vec!["tan".to_string(), "nat".to_string()],
-            vec!["bat".to_string()]
-        ]
-    );
+    let mut vec1 = Solution::group_anagrams(vec![
+        "eat".to_string(),
+        "tea".to_string(),
+        "tan".to_string(),
+        "ate".to_string(),
+        "nat".to_string(),
+        "bat".to_string(),
+    ]);
+    vec1.sort();
+
+    let mut vec2 = vec![
+        vec!["eat".to_string(), "tea".to_string(), "ate".to_string()],
+        vec!["tan".to_string(), "nat".to_string()],
+        vec!["bat".to_string()],
+    ];
+    vec2.sort();
+
+    assert_eq!(vec1, vec2);
 }
