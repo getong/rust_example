bea45b8bd38044ddd15dd86e6b769e9dbdbe404a
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-06 23:42:39 +0800

    add unsafe code

---
+++ unsafe_example/src/main.rs
@@ -0,0 +1,24 @@
+fn very_trustworthy(shared: &i32) {
+    unsafe {
+        let mutable = shared as *const i32 as *mut i32;
+        *mutable = 20;
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let num: u32 = 42;
+
+    let p: *const u32 = &num;
+
+    unsafe {
+        assert_eq!(*p, num);
+    }
+
+    let i: i32 = 10;
+
+    println!("before changed i is {}", i);
+    very_trustworthy(&i);
+    println!("after changed i is {}", i);
+}
