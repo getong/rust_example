2194114aada39545a5924bf0b3c3046f1aaf7ca1
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-11 11:49:13 +0800

    add label_break_value example

---
+++ label_break_value_example/src/main.rs
@@ -0,0 +1,16 @@
+#![feature(label_break_value)]
+
+fn main() {
+    // println!("Hello, world!");
+    let container = [0_i32, -1, 1];
+    let result = 'block: {
+        for &v in container.iter() {
+            if v > 0 {
+                break 'block v;
+            }
+        }
+        0
+    };
+
+    assert_eq!(result, 1);
+}
