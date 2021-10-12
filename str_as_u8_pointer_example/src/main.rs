a08816b29bc6784e95f25320577c45132e504244
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-12 19:41:39 +0800

    add str as u8 pointer example

---
+++ str_as_u8_pointer_example/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let s: &str = "123";
+    let ptr: *const u8 = s.as_ptr();
+
+    unsafe {
+        assert_eq!(*ptr.offset(1) as char, '2');
+        assert_eq!(*ptr.offset(2) as char, '3');
+    }
+}
