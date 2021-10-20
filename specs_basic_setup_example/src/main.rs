5eadc22d23b890fcc8a7beadd5ed0c3a64bf0fa1
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-20 12:03:33 +0800

    add specs basic setup example

---
+++ specs_basic_setup_example/src/main.rs
@@ -0,0 +1,35 @@
+use specs::prelude::*;
+
+#[derive(Default)]
+struct Gravity;
+
+struct Velocity;
+
+impl Component for Velocity {
+    type Storage = VecStorage<Self>;
+}
+
+struct SimulationSystem;
+
+impl<'a> System<'a> for SimulationSystem {
+    type SystemData = (Read<'a, Gravity>, WriteStorage<'a, Velocity>);
+
+    fn run(&mut self, _: Self::SystemData) {}
+}
+
+fn main() {
+    let mut world = World::new();
+    world.insert(Gravity);
+    world.register::<Velocity>();
+
+    for _ in 0..5 {
+        world.create_entity().with(Velocity).build();
+    }
+
+    let mut dispatcher = DispatcherBuilder::new()
+        .with(SimulationSystem, "simulation", &[])
+        .build();
+
+    dispatcher.dispatch(&mut world);
+    world.maintain();
+}
