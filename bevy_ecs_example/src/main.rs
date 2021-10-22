fd7fa48d09e3f9f74a97e5d04dc29f32b6c71d0d
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-22 21:02:52 +0800

    add bevy ecs example

---
+++ bevy_ecs_example/src/main.rs
@@ -0,0 +1,57 @@
+use bevy::app::App;
+
+use bevy::ecs::query::With;
+use bevy::ecs::system::Commands;
+use bevy::ecs::system::Query;
+use bevy::prelude::IntoSystem;
+//use bevy::transform::components::Transform;
+
+//struct Position {
+//    x: f32,
+//    y: f32,
+//}
+
+struct Person;
+
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+//fn print_position_system(query: Query<&Transform>) {
+//    for transform in query.iter() {
+//        println!("position: {:?}", transform.translation);
+//    }
+//}
+
+// struct Entity(u64);
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn main() {
+    App::build()
+        .add_startup_system(add_people.system())
+        .add_system(hello_world.system())
+        .add_system(greet_people.system())
+        .run();
+}
