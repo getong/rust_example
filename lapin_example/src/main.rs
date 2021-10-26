89b7582782393b838f530350bd826cd81f323b06
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-26 17:51:04 +0800

    add lapin example

---
+++ lapin_example/src/main.rs
@@ -0,0 +1,73 @@
+use futures_util::stream::StreamExt;
+use lapin::{
+    options::*, publisher_confirm::Confirmation, types::FieldTable, BasicProperties, Connection,
+    ConnectionProperties, Result,
+};
+use log::info;
+
+fn main() -> Result<()> {
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+
+    env_logger::init();
+
+    let addr = std::env::var("AMQP_ADDR")
+        .unwrap_or_else(|_| "amqp://user:Anaith7x@127.0.0.1:5672/abc".into());
+
+    async_global_executor::block_on(async {
+        let conn = Connection::connect(
+            &addr,
+            ConnectionProperties::default().with_default_executor(8),
+        )
+        .await?;
+
+        info!("CONNECTED");
+
+        let channel_a = conn.create_channel().await?;
+        let channel_b = conn.create_channel().await?;
+
+        let queue = channel_a
+            .queue_declare(
+                "hello",
+                QueueDeclareOptions::default(),
+                FieldTable::default(),
+            )
+            .await?;
+
+        info!("Declared queue {:?}", queue);
+
+        let mut consumer = channel_b
+            .basic_consume(
+                "hello",
+                "my_consumer",
+                BasicConsumeOptions::default(),
+                FieldTable::default(),
+            )
+            .await?;
+        async_global_executor::spawn(async move {
+            info!("will consume");
+            while let Some(delivery) = consumer.next().await {
+                let (_, delivery) = delivery.expect("error in consumer");
+                delivery.ack(BasicAckOptions::default()).await.expect("ack");
+            }
+        })
+        .detach();
+
+        let payload = b"Hello world!";
+
+        loop {
+            let confirm = channel_a
+                .basic_publish(
+                    "",
+                    "hello",
+                    BasicPublishOptions::default(),
+                    payload.to_vec(),
+                    BasicProperties::default(),
+                )
+                .await?
+                .await?;
+            assert_eq!(confirm, Confirmation::NotRequested);
+        }
+    })
+}
