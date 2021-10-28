5f93ff3d3a4b428fc3c960714b24743e2fb60261
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-28 17:48:54 +0800

    change code

--- deadpool_redis_once_cell_example/README.md
+++ deadpool_redis_once_cell_example/README.md
@@ -1,2 +1,24 @@
 # deadpool-redis and once_cell example
 The code is originally copied from [Crate deadpool_redis](https://docs.rs/deadpool-redis/0.10.0/deadpool_redis/)
+
+another code example is copied from [One global variable for MySQL connection](https://users.rust-lang.org/t/one-global-variable-for-mysql-connection/49063)
+
+``` rust
+use once_cell::sync::OnceCell;
+
+static MONGODB: OnceCell<Database> = OnceCell::new();
+
+pub async fn initialize() {
+    if MONGODB.get().is_some() {
+        return;
+    }
+
+    if let Ok(token) = env::var("CONNECTION") {
+        if let Ok(client_options) = ClientOptions::parse(token.as_str()).await {
+            if let Ok(client) = Client::with_options(client_options) {
+                let _ = MONGODB.set(client.database("my_db"));
+            }
+        }
+    }
+}
+```
--- deadpool_redis_once_cell_example/src/main.rs
+++ deadpool_redis_once_cell_example/src/main.rs
@@ -1,28 +1,51 @@
 use deadpool_redis::{redis::cmd, Config, Pool, Runtime};
-use once_cell::sync;
+use once_cell::sync::OnceCell;
 
-static GLOBAL_REDIS_POOL: sync::Lazy<Pool> = sync::Lazy::new(|| {
+static GLOBAL_REDIS_POOL: OnceCell<Pool> = OnceCell::new();
+
+pub async fn initial_database() {
+    if GLOBAL_REDIS_POOL.get().is_some() {
+        return;
+    }
+
     let cfg = Config::from_url("redis://bert:abc123@127.0.0.1:6379/");
-    cfg.create_pool(Some(Runtime::Tokio1)).unwrap()
-});
+    match cfg.create_pool(Some(Runtime::Tokio1)) {
+        Ok(pool) => {
+            let _ = GLOBAL_REDIS_POOL.set(pool);
+        }
+        Err(_) => panic!("connect to redis fail"),
+    }
+}
+
+//static GLOBAL_REDIS_POOL: sync::Lazy<Pool> = sync::Lazy::new(|| {
+//    let cfg = Config::from_url("redis://bert:abc123@127.0.0.1:6379/");
+//    cfg.create_pool(Some(Runtime::Tokio1)).unwrap()
+//});
 
 #[tokio::main]
 async fn main() {
-    match GLOBAL_REDIS_POOL.get().await {
-        Ok(mut conn) => {
-            cmd("SET")
-                .arg(&["deadpool/test_key", "42"])
-                .query_async::<_, ()>(&mut conn)
-                .await
-                .unwrap();
+    initial_database().await;
 
-            let value: String = cmd("GET")
-                .arg(&["deadpool/test_key"])
-                .query_async(&mut conn)
-                .await
-                .unwrap();
-            assert_eq!(value, "42".to_string());
-        }
+    match GLOBAL_REDIS_POOL.get() {
+        Some(connection_info) => match connection_info.get().await {
+            Ok(mut conn) => {
+                cmd("SET")
+                    .arg(&["deadpool/test_key", "42"])
+                    .query_async::<_, ()>(&mut conn)
+                    .await
+                    .unwrap();
+
+                let value: String = cmd("GET")
+                    .arg(&["deadpool/test_key"])
+                    .query_async(&mut conn)
+                    .await
+                    .unwrap();
+
+                assert_eq!(value, "42".to_string());
+            }
+            _ => println!("database connection error"),
+        },
+
         _ => println!("database connection error"),
     }
 }
