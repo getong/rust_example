036a45a0b387d79ab995cc3cc04aac40c306a1b4
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-28 16:57:52 +0800

    add code

--- deadpool_redis_once_cell_example/src/main.rs
+++ deadpool_redis_once_cell_example/src/main.rs
@@ -8,18 +8,21 @@ static GLOBAL_REDIS_POOL: sync::Lazy<Pool> = sync::Lazy::new(|| {
 
 #[tokio::main]
 async fn main() {
-    let mut conn = GLOBAL_REDIS_POOL.get().await.unwrap();
+    match GLOBAL_REDIS_POOL.get().await {
+        Ok(mut conn) => {
+            cmd("SET")
+                .arg(&["deadpool/test_key", "42"])
+                .query_async::<_, ()>(&mut conn)
+                .await
+                .unwrap();
 
-    cmd("SET")
-        .arg(&["deadpool/test_key", "42"])
-        .query_async::<_, ()>(&mut conn)
-        .await
-        .unwrap();
-
-    let value: String = cmd("GET")
-        .arg(&["deadpool/test_key"])
-        .query_async(&mut conn)
-        .await
-        .unwrap();
-    assert_eq!(value, "42".to_string());
+            let value: String = cmd("GET")
+                .arg(&["deadpool/test_key"])
+                .query_async(&mut conn)
+                .await
+                .unwrap();
+            assert_eq!(value, "42".to_string());
+        }
+        _ => println!("database connection error"),
+    }
 }
