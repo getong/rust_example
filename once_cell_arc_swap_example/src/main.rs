c860fe20f3f5c7586f5b96d4c4d87b4f35c7327f
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-29 11:04:16 +0800

    update code

--- once_cell_arc_swap_example/src/main.rs
+++ once_cell_arc_swap_example/src/main.rs
@@ -2,11 +2,13 @@ use arc_swap::ArcSwap;
 use once_cell::sync;
 use std::sync::Arc;
 
-static GLOBAL_CONFIG: sync::Lazy<ArcSwap<String>> =
-    sync::Lazy::new(|| ArcSwap::from(Arc::new("hello".to_string())));
+static GLOBAL_CONFIG: sync::Lazy<ArcSwap<Option<String>>> =
+    sync::Lazy::new(|| ArcSwap::from(Arc::new(Some("hello".into()))));
 
 fn main() {
-    assert_eq!(**GLOBAL_CONFIG.load(), "hello".to_owned());
-    GLOBAL_CONFIG.swap(Arc::from("world".to_string()));
-    assert_eq!(**GLOBAL_CONFIG.load(), "world".to_string());
+    assert_eq!(**GLOBAL_CONFIG.load(), Some("hello".to_owned()));
+    GLOBAL_CONFIG.swap(Arc::from(Some("world".to_string())));
+    assert_eq!(**GLOBAL_CONFIG.load(), Some("world".to_string()));
+    GLOBAL_CONFIG.swap(Arc::from(None));
+    assert_eq!(**GLOBAL_CONFIG.load(), None);
 }
