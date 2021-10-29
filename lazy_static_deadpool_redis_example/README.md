38bd4ff5e1c91f40168776979bb67b6d2b816b62
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-10-29 17:07:31 +0800

    add lazy_static deadpool-redis example

---
+++ lazy_static_deadpool_redis_example/README.md
@@ -0,0 +1,4 @@
+# lazy_static and deadpool-redis example
+The code is originally copied from [Crate deadpool_redis](https://docs.rs/deadpool-redis/0.10.0/deadpool_redis/)
+
+another code example is copied from [One global variable for MySQL connection](https://users.rust-lang.org/t/one-global-variable-for-mysql-connection/49063)
---
+++ lazy_static_deadpool_redis_example/src/main.rs
@@ -0,0 +1,37 @@
+use arc_swap::ArcSwap;
+
+use deadpool_redis::{redis::cmd, Config, Pool, Runtime};
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref GLOBAL_REDIS_SERVER: ArcSwap<Pool> = {
+        let cfg = Config::from_url("redis://bert:abc123@127.0.0.1:6379/");
+        match cfg.create_pool(Some(Runtime::Tokio1)) {
+            Ok(pool) => ArcSwap::from_pointee(pool),
+            Err(_) => panic!("connect to redis fail"),
+        }
+    };
+}
+
+#[tokio::main]
+async fn main() {
+    let pool = &**GLOBAL_REDIS_SERVER.load();
+    {
+        let mut conn = pool.get().await.unwrap();
+        cmd("SET")
+            .arg(&["deadpool/test_key", "72"])
+            .query_async::<_, ()>(&mut conn)
+            .await
+            .unwrap();
+    }
+    {
+        // let mut conn = (**GLOBAL_REDIS_SERVER.load()).get().await.unwrap();
+        let mut conn = pool.get().await.unwrap();
+        let value: String = cmd("GET")
+            .arg(&["deadpool/test_key"])
+            .query_async(&mut conn)
+            .await
+            .unwrap();
+        assert_eq!(value, "72".to_string());
+    }
+}
