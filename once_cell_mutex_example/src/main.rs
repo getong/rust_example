5b5acba4882c87239279975015bce81503dc8b25
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-02 10:51:31 +0800

    add once_cell mutex example

---
+++ once_cell_mutex_example/src/main.rs
@@ -0,0 +1,23 @@
+use once_cell::sync::OnceCell;
+use std::sync::Mutex;
+
+static LOG_FILE: OnceCell<Mutex<String>> = OnceCell::new();
+
+fn ensure_log_file() -> &'static Mutex<String> {
+    LOG_FILE.get_or_init(|| Mutex::new(String::new()))
+}
+
+pub fn get_log_file() -> String {
+    ensure_log_file().lock().unwrap().clone()
+}
+
+pub fn set_log_file(file: String) {
+    *ensure_log_file().lock().unwrap() = file;
+}
+
+fn main() {
+    // println!("Hello, world!");
+    println!("get_log_file, the string: {:?}", get_log_file());
+    set_log_file("hello".to_string());
+    println!("get_log_file, the string: {:?}", get_log_file());
+}
