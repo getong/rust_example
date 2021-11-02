bb96b54bdb818edf873491c7408c86cb1e83dd80
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-02 10:05:32 +0800

    add static atomic example

---
+++ static_atomic_example/src/main.rs
@@ -0,0 +1,29 @@
+use std::sync::atomic::{AtomicU8, Ordering};
+
+static LOG_LEVEL: AtomicU8 = AtomicU8::new(0);
+
+pub fn get_log_level() -> u8 {
+    LOG_LEVEL.load(Ordering::Relaxed)
+}
+
+pub fn set_log_level(level: u8) {
+    LOG_LEVEL.store(level, Ordering::Relaxed);
+}
+
+fn change_variable_one() {
+    println!("one before set level is {}", get_log_level());
+    std::thread::spawn(|| set_log_level(1));
+    println!("one after set level is {}", get_log_level());
+}
+
+fn change_variable_two() {
+    println!("two before set level is {}", get_log_level());
+    let handler = std::thread::spawn(|| set_log_level(2));
+    handler.join().unwrap();
+    println!("two after set level is {}", get_log_level());
+}
+
+fn main() {
+    change_variable_one();
+    change_variable_two();
+}
