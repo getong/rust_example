bfad52f0025172a3c78e8444c3fb888a2212c6f4
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-02 10:53:58 +0800

    add once_cell lazy mutex example

---
+++ once_cell_lazy_mutex_example/src/main.rs
@@ -0,0 +1,19 @@
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+static LOG_FILE: Lazy<Mutex<String>> = Lazy::new(|| Mutex::new(String::new()));
+
+pub fn get_log_file() -> String {
+    LOG_FILE.lock().unwrap().clone()
+}
+
+pub fn set_log_file(file: String) {
+    *LOG_FILE.lock().unwrap() = file;
+}
+
+fn main() {
+    // println!("Hello, world!");
+    println!("get log file , the string is {}", get_log_file());
+    set_log_file("hello".to_string());
+    println!("get log file , the string is {}", get_log_file());
+}
