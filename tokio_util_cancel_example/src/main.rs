c2c1f6ea95ca337d4994a2390d06871cbfda2a91
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-03 10:10:42 +0800

    add tokio_util cancel example

---
+++ tokio_util_cancel_example/src/main.rs
@@ -0,0 +1,28 @@
+use tokio::select;
+use tokio_util::sync::CancellationToken;
+
+#[tokio::main]
+async fn main() {
+    let token = CancellationToken::new();
+    let child_token = token.child_token();
+
+    let join_handle = tokio::spawn(async move {
+        // Wait for either cancellation or a very long time
+        select! {
+            _ = child_token.cancelled() => {
+                // The token was cancelled
+                5
+            }
+            _ = tokio::time::sleep(std::time::Duration::from_secs(9999)) => {
+                99
+            }
+        }
+    });
+
+    tokio::spawn(async move {
+        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+        token.cancel();
+    });
+
+    assert_eq!(5, join_handle.await.unwrap());
+}
