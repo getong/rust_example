f4926cd949da31c6d815a721ce3e6300ac777491
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-03 20:36:09 +0800

    add tokio select sleep example

---
+++ tokio_select_sleep_example/README.md
@@ -0,0 +1,3 @@
+# tokio select sleep example
+
+copy from [Struct actix::clock::Sleep](https://actix.rs/actix/actix/clock/struct.Sleep.html)
---
+++ tokio_select_sleep_example/src/main.rs
@@ -0,0 +1,16 @@
+use tokio::time::{self, Duration, Instant};
+
+#[tokio::main]
+async fn main() {
+    let sleep = time::sleep(Duration::from_millis(10));
+    tokio::pin!(sleep);
+
+    loop {
+        tokio::select! {
+            () = &mut sleep => {
+                println!("timer elapsed");
+                sleep.as_mut().reset(Instant::now() + Duration::from_millis(50));
+            },
+        }
+    }
+}
