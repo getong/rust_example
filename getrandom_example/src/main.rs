95dea8ee2a8bee9e2ec981e5781d93667d8200b8
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-04 10:05:37 +0800

    add getrandom example

---
+++ getrandom_example/src/main.rs
@@ -0,0 +1,13 @@
+fn get_random_buf() -> Result<[u8; 32], getrandom::Error> {
+    let mut buf = [0u8; 32];
+    getrandom::getrandom(&mut buf)?;
+    Ok(buf)
+}
+
+fn main() {
+    // println!("Hello, world!");
+    match get_random_buf() {
+        Ok(buf) => println!("buf is {:?}", buf),
+        Err(_) => println!("error"),
+    }
+}
