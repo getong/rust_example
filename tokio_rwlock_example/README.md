42cf6f841297752c81946a4fdcc9ad83a7098528
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-04 11:54:13 +0800

    add tokio rwlock example

---
+++ tokio_rwlock_example/README.md
@@ -0,0 +1,2 @@
+# tokio RwLock example
+copy from [Struct tokio::sync::RwLock](https://docs.rs/tokio/1.13.0/tokio/sync/struct.RwLock.html)
---
+++ tokio_rwlock_example/src/main.rs
@@ -0,0 +1,21 @@
+use tokio::sync::RwLock;
+
+#[tokio::main]
+async fn main() {
+    let lock = RwLock::new(5);
+
+    // many reader locks can be held at once
+    {
+        let r1 = lock.read().await;
+        let r2 = lock.read().await;
+        assert_eq!(*r1, 5);
+        assert_eq!(*r2, 5);
+    } // read locks are dropped at this point
+
+    // only one write lock may be held, however
+    {
+        let mut w = lock.write().await;
+        *w += 1;
+        assert_eq!(*w, 6);
+    } // write lock is dropped here
+}
