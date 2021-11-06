75ccecddd9a9c9ae1a8d5c565792fdb54f74bced
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-06 20:02:06 +0800

    add tokio pin example

---
+++ tokio_pin_example/README.md
@@ -0,0 +1,2 @@
+# tokio pin example
+copy from [Macro tokio::pin](https://docs.rs/tokio/1.13.0/tokio/macro.pin.html)
---
+++ tokio_pin_example/src/main.rs
@@ -0,0 +1,26 @@
+use tokio::{pin, select};
+use tokio_stream::{self as stream, StreamExt};
+
+async fn my_async_fn() {
+    // async logic here
+}
+
+#[tokio::main]
+async fn main() {
+    let mut stream = stream::iter(vec![1, 2, 3, 4]);
+
+    let future = my_async_fn();
+    pin!(future);
+
+    loop {
+        select! {
+            _ = &mut future => {
+                // Stop looping `future` will be polled after completion
+                break;
+            }
+            Some(val) = stream.next() => {
+                println!("got value = {}", val);
+            }
+        }
+    }
+}
