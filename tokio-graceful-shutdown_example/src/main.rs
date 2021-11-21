aa01875c0ee8653111727a9e8ad3c3d1700726dd
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-22 00:07:00 +0800

    update code

--- tokio-graceful-shutdown_example/src/main.rs
+++ tokio-graceful-shutdown_example/src/main.rs
@@ -1,18 +1,36 @@
-use std::time::Duration;
-use tokio_graceful_shutdown::SubsystemHandle;
-use tokio_graceful_shutdown::Toplevel;
+use anyhow::Result;
+use env_logger::{Builder, Env};
+use tokio::time::{sleep, Duration};
+use tokio_graceful_shutdown::{SubsystemHandle, Toplevel};
 
-async fn subsys1(subsys: SubsystemHandle) -> Result<()> {
-    log::info!("Subsystem1 started.");
-    subsys.on_shutdown_requested().await;
-    log::info!("Subsystem1 stopped.");
+async fn countdown() {
+    for i in (1..=5).rev() {
+        log::info!("Shutting down in: {}", i);
+        sleep(Duration::from_millis(1000)).await;
+    }
+}
+
+async fn countdown_subsystem(subsys: SubsystemHandle) -> Result<()> {
+    tokio::select! {
+        _ = subsys.on_shutdown_requested() => {
+            log::info!("Countdown cancelled.");
+        },
+        _ = countdown() => {
+            subsys.request_shutdown();
+        }
+    };
+
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Init logging
+    Builder::from_env(Env::default().default_filter_or("debug")).init();
+
+    // Create toplevel
     Toplevel::new()
-        .start("Subsys1", subsys1)
+        .start("Countdown", countdown_subsystem)
         .catch_signals()
         .wait_for_shutdown(Duration::from_millis(1000))
         .await
