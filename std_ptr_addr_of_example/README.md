a990b331c40b23c21450078205197ae5a44ee1ae
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-11-30 20:04:10 +0800

    add std::ptr::addr_of example

---
+++ std_ptr_addr_of_example/README.md
@@ -0,0 +1,2 @@
+# std::ptr::addr_of! example
+copy from [example](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=e8abac4699f491cb851eaddd52dddf99)
---
+++ std_ptr_addr_of_example/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // println!("Hello, world!");
+    let mut s1 = String::from("hello");
+    println!("String pointing to heap location {:?}", s1.as_ptr());
+    println!("String stored on stack {:?}", std::ptr::addr_of!(s1));
+    //    {
+    //      s1; // release it
+    //}
+    let s2 = String::from("hello, world");
+    // a long string literal will allocate a different area on heap.
+    // let s2 = String::from("123456789012345678901234567890");
+    println!("String pointing to heap location {:?}", s2.as_ptr());
+    println!("String stored on stack {:?}", std::ptr::addr_of!(s2));
+
+    s1 = s2;
+    // a shadow variable will allocate a different area on stack.
+    // let s1 = s2;
+    println!("String pointing to heap location {:?}", s1.as_ptr());
+    println!("String stored on stack {:?}", std::ptr::addr_of!(s1));
+}
