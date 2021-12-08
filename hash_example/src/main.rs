ba8a3bbaceef22f08b49b89ebf619e7fcb299f09
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-12-08 21:49:44 +0800

    add hash example

---
+++ hash_example/src/main.rs
@@ -0,0 +1,27 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+struct Person {
+    name: String,
+    phone: u64,
+}
+
+impl Hash for Person {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+        self.phone.hash(state);
+    }
+}
+
+fn main() {
+    let person = Person {
+        name: "abc".to_string(),
+        phone: 123,
+    };
+
+    let mut hasher = DefaultHasher::new();
+
+    person.hash(&mut hasher);
+
+    println!("Hash is {:x}!", hasher.finish());
+}
