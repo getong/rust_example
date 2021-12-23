814a643117521001c0b6d7c69ca8e7b3bab5674a
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-12-23 09:57:15 +0800

    add code

--- barrier_example/src/main.rs
+++ barrier_example/src/main.rs
@@ -2,18 +2,23 @@ use std::sync::{Arc, Barrier};
 use std::thread;
 
 fn main() {
-    // println!("Hello, world!");
     let mut handles = Vec::with_capacity(5);
     let barrier = Arc::new(Barrier::new(5));
 
     for _ in 0..5 {
+        // let c = Arc::clone(&barrier);
         let c = barrier.clone();
+
+        // The same messages will be printed together.
+        // You will NOT see any interleaving.
         handles.push(thread::spawn(move || {
             println!("before wait");
             c.wait();
             println!("after wait");
         }));
     }
+
+    // Wait for other threads to finish.
     for handle in handles {
         handle.join().unwrap();
     }
