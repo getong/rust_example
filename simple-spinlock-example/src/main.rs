e0296a55619c21ae5161d479c420dcda9220690c
19年梦醒 <3949379+getong@users.noreply.github.com>
2021-12-23 10:33:23 +0800

    add simple spinlock example

---
+++ simple-spinlock-example/src/main.rs
@@ -0,0 +1,21 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::{hint, thread};
+
+fn main() {
+    let spinlock = Arc::new(AtomicUsize::new(1));
+
+    let spinlock_clone = Arc::clone(&spinlock);
+    let thread = thread::spawn(move || {
+        spinlock_clone.store(0, Ordering::SeqCst);
+    });
+
+    // Wait for the other thread to release the lock
+    while spinlock.load(Ordering::SeqCst) != 0 {
+        hint::spin_loop();
+    }
+
+    if let Err(panic) = thread.join() {
+        println!("Thread had an error: {:?}", panic);
+    }
+}
