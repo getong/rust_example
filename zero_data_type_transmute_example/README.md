67297a0dfe5006dfa044fd4e31d62b57f87aef60
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-01-21 11:55:43 +0800

    add zero data type transmute example

---
+++ zero_data_type_transmute_example/README.md
@@ -0,0 +1,2 @@
+# zero data type transmute example
+copy from [类型的大小](https://zhuanlan.zhihu.com/p/21820917)
---
+++ zero_data_type_transmute_example/src/main.rs
@@ -0,0 +1,14 @@
+use std::mem::transmute;
+
+fn main() {
+    let x: Box<()> = Box::new(());
+    let y: Option<Box<()>> = None;
+    let z: Option<Box<()>> = Some(Box::new(()));
+
+    unsafe {
+        let value1: usize = transmute(x);
+        let value2: usize = transmute(y);
+        let value3: usize = transmute(z);
+        println!("{} {} {}", value1, value2, value3);
+    }
+}
