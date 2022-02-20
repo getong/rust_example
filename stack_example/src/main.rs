36c2fd52fcaaaf39bc2a0b6f2c04a13ee82d358f
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-02-20 23:59:07 +0800

    add code

--- stack_example/src/main.rs
+++ stack_example/src/main.rs
@@ -30,4 +30,8 @@ impl<T> Stack<T> {
 
 fn main() {
     // println!("Hello, world!");
+    let mut stack: Stack<isize> = Stack::new();
+    stack.push(1);
+    let item = stack.pop();
+    assert_eq!(item.unwrap(), 1);
 }
