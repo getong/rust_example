ee726f64a1a604094dc0b0ca611a44b7330519b0
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-02-20 23:57:36 +0800

    add stack example

---
+++ stack_example/src/main.rs
@@ -0,0 +1,33 @@
+pub struct Stack<T> {
+    stack: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Stack { stack: Vec::new() }
+    }
+
+    pub fn length(&self) -> usize {
+        self.stack.len()
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.stack.pop()
+    }
+
+    pub fn push(&mut self, item: T) {
+        self.stack.push(item)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.stack.is_empty()
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.stack.last()
+    }
+}
+
+fn main() {
+    // println!("Hello, world!");
+}
