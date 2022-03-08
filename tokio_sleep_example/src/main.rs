f2c21493175faf23b817b0d8ee961d33c4a283cd
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-08 21:01:07 +0800

    add tokio sleep example

---
+++ tokio_sleep_example/src/main.rs
@@ -0,0 +1,11 @@
+use chrono::Local;
+use tokio::runtime::Runtime;
+
+fn main() {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        println!("before sleep: {}", Local::now().format("%F %T.%3f"));
+        tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+        println!("after sleep: {}", Local::now().format("%F %T.%3f"));
+    });
+}
