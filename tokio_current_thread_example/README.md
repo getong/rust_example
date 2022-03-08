37264668361e0402690b53fd5e55af30fccf51da
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-08 20:21:19 +0800

    add tokio current thread example

---
+++ tokio_current_thread_example/README.md
@@ -0,0 +1,6 @@
+# tokio current_thread example
+
+``` shell
+ps -eLf | grep 'current' | nl
+```
+copy from [理解tokio核心(1): runtime](https://rust-book.junmajinlong.com/ch100/01_understand_tokio_runtime.html)
---
+++ tokio_current_thread_example/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // println!("Hello, world!");
+    // 创建单一线程的runtime
+    let _rt = tokio::runtime::Builder::new_current_thread()
+        .build()
+        .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(10));
+}
