c99ed208832356970cda42a4cef7d30bc9afd0f4
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-09 14:44:14 +0800

    add tokio reset sleep timer example

---
+++ tokio_reset_sleep_timer_example/README.md
@@ -0,0 +1,3 @@
+# tokio reset sleep timer
+
+copy from [使用tokio Timer](https://rust-book.junmajinlong.com/ch100/03_use_tokio_time.html)
---
+++ tokio_reset_sleep_timer_example/src/main.rs
@@ -0,0 +1,22 @@
+use chrono::Local;
+use tokio::{self, runtime::Runtime, time};
+
+#[allow(dead_code)]
+fn now() -> String {
+    Local::now().format("%F %T").to_string()
+}
+
+fn main() {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        println!("start: {}", now());
+        let slp = time::sleep(time::Duration::from_secs(1));
+        tokio::pin!(slp);
+
+        slp.as_mut()
+            .reset(time::Instant::now() + time::Duration::from_secs(2));
+
+        slp.await;
+        println!("end: {}", now());
+    });
+}
