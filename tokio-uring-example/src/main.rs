80eb6806bd874773138fe7b0a31f15d8f8908ee2
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-09 22:03:26 +0800

    add tokio-uring example

---
+++ tokio-uring-example/src/main.rs
@@ -0,0 +1,20 @@
+use tokio_uring::fs::File;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tokio_uring::start(async {
+        // Open a file
+        let file = File::open("hello.txt").await?;
+
+        let buf = vec![0; 4096];
+        // Read some data, the buffer is passed by ownership and
+        // submitted to the kernel. When the operation completes,
+        // we get the buffer back.
+        let (res, buf) = file.read_at(buf, 0).await;
+        let n = res?;
+
+        // Display the contents
+        println!("{:?}", &buf[..n]);
+
+        Ok(())
+    })
+}
