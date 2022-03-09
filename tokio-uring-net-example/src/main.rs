ac4995de2b1e415bcd970ca8e1695fd718d9ab77
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-09 22:14:01 +0800

    add tokio-uring net example

---
+++ tokio-uring-net-example/src/main.rs
@@ -0,0 +1,40 @@
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+use tokio_uring::fs::File;
+
+fn main() {
+    tokio_uring::start(async {
+        // Start a TCP listener
+        let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+        // Accept new sockets
+        loop {
+            let (mut socket, _) = listener.accept().await.unwrap();
+
+            // Spawn a task to send the file back to the socket
+            tokio_uring::spawn(async move {
+                // Open the file without blocking
+                let file = File::open("hello.txt").await.unwrap();
+                let mut buf = vec![0; 16 * 1_024];
+
+                // Track the current position in the file;
+                let mut pos = 0;
+
+                loop {
+                    // Read a chunk
+                    let (res, b) = file.read_at(buf, pos).await;
+                    let n = res.unwrap();
+
+                    if n == 0 {
+                        break;
+                    }
+
+                    socket.write_all(&b[..n]).await.unwrap();
+                    pos += n as u64;
+
+                    buf = b;
+                }
+            });
+        }
+    });
+}
