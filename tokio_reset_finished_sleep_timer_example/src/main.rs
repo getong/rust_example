07121b3c550b698e7e33648f3e2da56d086a3db2
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-09 14:48:23 +0800

    add tokio reset finished sleep timer example

---
+++ tokio_reset_finished_sleep_timer_example/src/main.rs
@@ -0,0 +1,26 @@
+use chrono::Local;
+use tokio::{self, runtime::Runtime, time};
+
+#[allow(dead_code)]
+fn now() -> String {
+    Local::now().format("%F %T").to_string()
+}
+
+fn main() {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        println!("start: {}", now());
+        let slp = time::sleep(time::Duration::from_secs(1));
+        tokio::pin!(slp);
+
+        //注意调用slp.as_mut().await，而不是slp.await，后者会move消费掉slp
+        slp.as_mut().await;
+        println!("end 1: {}", now());
+
+        slp.as_mut()
+            .reset(time::Instant::now() + time::Duration::from_secs(2));
+
+        slp.await;
+        println!("end 2: {}", now());
+    });
+}
