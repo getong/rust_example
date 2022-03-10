bda47181cc781a1b6c8e947466c3c9cdadf5f31e
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-10 11:57:59 +0800

    add seaorm basic connection example

---
+++ seaorm_basic_connection_example/README.md
@@ -0,0 +1,12 @@
+# seaorm basic connection example
+
+``` shell
+mkdir -p "$PWD/data"
+mkdir -p "$PWD/config-files"
+
+podman run --restart=always -v "$PWD/data":/var/lib/mysql -v "$PWD/config-files":/etc/mysql/conf.d  -p 3306:3306 -e MYSQL_DATABASE=test_db -e MYSQL_USER=user -e MYSQL_ROOT_PASSWORD=root --name mariadb_instance -d mariadb:10.7.1-focal --character-set-server=utf8mb4 --collation-server=utf8mb4_general_ci
+
+cargo run
+```
+
+copy from [Connection Pool](https://www.sea-ql.org/SeaORM/docs/install-and-config/connection)
---
+++ seaorm_basic_connection_example/src/main.rs
@@ -0,0 +1,20 @@
+use sea_orm::ConnectOptions;
+use sea_orm::Database;
+use std::time::Duration;
+
+fn main() {
+    // println!("Hello, world!");
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+        let mut opt = ConnectOptions::new("mysql://root:root@localhost/test_db".to_owned());
+        opt.max_connections(100)
+            .min_connections(5)
+            .connect_timeout(Duration::from_secs(8))
+            .idle_timeout(Duration::from_secs(8))
+            .max_lifetime(Duration::from_secs(8))
+            .sqlx_logging(true);
+
+        let db = Database::connect(opt).await.unwrap();
+        println!("db is {:?}", db);
+    });
+}
