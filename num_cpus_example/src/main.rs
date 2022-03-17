883c47305d54a1c058f728694bd2aaf60216d7a1
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-17 17:47:05 +0800

    use clippy advice to rewrite code

--- num_cpus_example/src/main.rs
+++ num_cpus_example/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 fn main() {
     // println!("Hello, world!");
     let logical_cpus = num_cpus::get();
@@ -7,21 +9,21 @@ fn main() {
         logical_cpus, physical_cpus
     );
 
-    if logical_cpus > physical_cpus {
-        println!(
+    match logical_cpus.cmp(&physical_cpus) {
+        Ordering::Greater => println!(
             "We have simultaneous multithreading with about {:.2} \
-              logical cores to 1 physical core.",
+                 logical cores to 1 physical core.",
             (logical_cpus as f64) / (physical_cpus as f64)
-        );
-    } else if logical_cpus == physical_cpus {
-        println!(
+        ),
+
+        Ordering::Equal => println!(
             "Either we don't have simultaneous multithreading, or our \
               system doesn't support getting the number of physical CPUs."
-        );
-    } else {
-        println!(
+        ),
+
+        Ordering::Less => println!(
             "We have less logical CPUs than physical CPUs, maybe we only have access to \
               some of the CPUs on our system."
-        );
+        ),
     }
 }
