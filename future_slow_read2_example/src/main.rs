e9e2c9699339a660418f2de1f44a5c3767714bbb
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-23 19:51:54 +0800

    future slow read 2 example

---
+++ future_slow_read2_example/src/main.rs
@@ -0,0 +1,47 @@
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use tokio::{
+    fs::File,
+    io::{AsyncRead, AsyncReadExt, ReadBuf},
+    time::Instant,
+};
+
+struct SlowRead<R> {
+    reader: Pin<Box<R>>,
+}
+
+impl<R> SlowRead<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader: Box::pin(reader),
+        }
+    }
+}
+
+impl<R> AsyncRead for SlowRead<R>
+where
+    R: AsyncRead,
+{
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        self.reader.as_mut().poll_read(cx, buf)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), tokio::io::Error> {
+    let mut buf = vec![0u8; 128 * 1024];
+
+    let mut f = SlowRead::new(File::open("/dev/urandom").await?);
+    let before = Instant::now();
+    f.read_exact(&mut buf).await?;
+    println!("Read {} bytes in {:?}", buf.len(), before.elapsed());
+
+    Ok(())
+}
