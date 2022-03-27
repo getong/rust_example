70dfe38da303b3efd133313ab874c3a55197c417
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-27 17:00:41 +0800

    add windows driver example

---
+++ windows_driver_example/src/main.rs
@@ -0,0 +1,7 @@
+use windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK};
+
+fn main() {
+    unsafe {
+        MessageBoxA(None, "Hello Windows 11 from Rust", "HelloMsg", MB_OK);
+    }
+}
