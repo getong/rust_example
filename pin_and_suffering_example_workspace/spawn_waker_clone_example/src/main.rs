c1dee898881047de97d8a90a7cd28b7bbb0d4ac7
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-28 23:22:26 +0800

    add spawn waker clone example

---
+++ pin_and_suffering_example_workspace/spawn_waker_clone_example/src/main.rs
@@ -0,0 +1,47 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+    time::Duration,
+};
+
+#[tokio::main]
+async fn main() {
+    let fut = MyFuture::new();
+    println!("Awaiting fut...");
+    fut.await;
+    println!("Awaiting fut... done!");
+}
+
+struct MyFuture {
+    slept: bool,
+}
+
+impl MyFuture {
+    fn new() -> Self {
+        Self { slept: false }
+    }
+}
+
+impl Future for MyFuture {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        println!("MyFuture::poll()");
+
+        match self.slept {
+            false => {
+                // make sure we're polled again in one second
+                let waker = cx.waker().clone();
+                std::thread::spawn(move || {
+                    std::thread::sleep(Duration::from_secs(1));
+                    waker.wake();
+                });
+                self.slept = true;
+
+                Poll::Pending
+            }
+            true => Poll::Ready(()),
+        }
+    }
+}
