5057a2bba4ad72e3d07ccdf75bfcdf4d12774384
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-29 18:03:33 +0800

    add pin and option take inner example

---
+++ pin_and_suffering_example_workspace/pin_and_option_take_inner_example/src/main.rs
@@ -0,0 +1,97 @@
+use std::future::Future;
+use std::task::Context;
+use tokio::fs::File;
+use tokio::io::AsyncRead;
+use tokio::io::AsyncReadExt;
+use tokio::io::ReadBuf;
+use tokio::macros::support::Pin;
+use tokio::macros::support::Poll;
+use tokio::time::Duration;
+use tokio::time::Instant;
+use tokio::time::Sleep;
+
+struct SlowRead<R> {
+    //       👇 now optional!
+    reader: Option<R>,
+    sleep: Sleep,
+}
+
+impl<R> SlowRead<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            //       👇
+            reader: Some(reader),
+            sleep: tokio::time::sleep(Default::default()),
+        }
+    }
+}
+
+impl<R> SlowRead<R>
+where
+    R: Unpin,
+{
+    // 👇 now takes pinned mutable reference to Self, and returns an option
+    fn take_inner(self: Pin<&mut Self>) -> Option<R> {
+        // self.reader.take()
+        let mut_self = unsafe { self.get_unchecked_mut() };
+        mut_self.reader.take()
+    }
+}
+
+impl<R> AsyncRead for SlowRead<R>
+where
+    R: AsyncRead + Unpin,
+{
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        // pin-project both fields
+        let (mut sleep, reader) = unsafe {
+            let this = self.get_unchecked_mut();
+            (Pin::new_unchecked(&mut this.sleep), &mut this.reader)
+        };
+
+        match sleep.as_mut().poll(cx) {
+            Poll::Ready(_) => {
+                sleep.reset(Instant::now() + Duration::from_millis(25));
+                match reader {
+                    Some(reader) => {
+                        // pin-project option:
+                        let reader = unsafe { Pin::new_unchecked(reader) };
+                        reader.poll_read(cx, buf)
+                    }
+                    None => {
+                        // simulate EOF
+                        Poll::Ready(Ok(()))
+                    }
+                }
+            }
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), tokio::io::Error> {
+    let mut buf = vec![0u8; 128 * 1024];
+    let f = File::open("/dev/urandom").await?;
+
+    let mut f = {
+        let f = SlowRead::new(f);
+        pin_utils::pin_mut!(f);
+
+        let before = Instant::now();
+        f.read_exact(&mut buf).await?;
+        println!("Read {} bytes in {:?}", buf.len(), before.elapsed());
+
+        f.take_inner().unwrap()
+    };
+
+    let before = Instant::now();
+    f.read_exact(&mut buf).await?;
+    println!("Read {} bytes in {:?}", buf.len(), before.elapsed());
+
+    Ok(())
+}
