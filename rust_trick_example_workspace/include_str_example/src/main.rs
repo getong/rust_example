e82ef1330426a1581d5bd5a5f26f90f915e8b58d
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-29 09:18:20 +0800

    add code

--- rust_trick_example_workspace/include_str_example/src/main.rs
+++ rust_trick_example_workspace/include_str_example/src/main.rs
@@ -1,7 +1,10 @@
 // Both of these files are read at *compile time*
-const FILE_CONTENT: &str = include_str!("./Cargo.toml");
-const BINARY_FILE_CONTENT: &[u8] = include_bytes!("./Cargo.toml");
+const FILE_CONTENT: &str = include_str!("../Cargo.toml");
+const BINARY_FILE_CONTENT: &[u8] = include_bytes!("../Cargo.toml");
 
 fn main() {
-    println!("{}", FILE_CONTENT); // Output: file content as string
+    println!("file:{}\n\n", FILE_CONTENT); // Output: file content as string
+    println!("binary:{:?}\n\n", BINARY_FILE_CONTENT); // Output: file content as string
+
+    println!("the main is :\n\n{}", include_str!("./main.rs"));
 }
