f2dd54192e86455d98f476e5b0107bcc9d24879c
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-29 11:21:02 +0800

    add tokio read_exact example

---
+++ tokio_read_exact_example/src/main.rs
@@ -0,0 +1,12 @@
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut f = File::open("/dev/urandom").await?;
+    let mut buffer = [0; 10];
+
+    // read exactly 10 bytes
+    f.read_exact(&mut buffer).await?;
+    Ok(())
+}
