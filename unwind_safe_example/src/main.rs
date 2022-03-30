0fe51e606efdfdf4f07c8d0058f2a757aa8d5d2b
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-30 19:54:36 +0800

    add unwind_safe example

---
+++ unwind_safe_example/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let mut body_called = false;
+    let mut finally_called = false;
+
+    // Let's imagine some code being run in a context where
+    // panics do not affect us (`panic::catch_unwind`), or some
+    // executor running stuff on another thread…
+    let _ = ::crossbeam::thread::scope(|s| {
+        drop(s.spawn(|_| {
+            let ft = {
+                ::unwind_safe::with_state(())
+                    .try_eval(|_| {
+                        body_called = true;
+                        if ::rand::random() {
+                            panic!();
+                        } else {
+                            42
+                        }
+                    })
+                    .finally(|_| {
+                        // <- The point of this crate!
+                        finally_called = true;
+                    })
+            };
+            // This is only reached when `try_eval` does not panic, obviously.
+            assert_eq!(ft, 42);
+        }))
+    });
+
+    // Whatever code path was taken, the finally block is always executed
+    // (that's the point of this crate!).
+    // From a place that survives the panic (if any), we thus observe:
+    assert!(body_called);
+    assert!(finally_called);
+}
