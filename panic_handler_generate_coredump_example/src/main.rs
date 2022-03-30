8448c9e98e49e3ca4178db1bf76e1edddad4c2f3
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-30 19:46:09 +0800

    add panic_handler_generate_coredump example

---
+++ panic_handler_generate_coredump_example/src/main.rs
@@ -0,0 +1,19 @@
+use libc::kill;
+use libc::SIGQUIT;
+
+pub fn panic_handler_generate_coredump() {
+    let default_panic = std::panic::take_hook();
+
+    std::panic::set_hook(Box::new(move |panic_info| {
+        default_panic(panic_info);
+
+        let pid = std::process::id();
+
+        unsafe { kill(pid.try_into().unwrap(), SIGQUIT) };
+    }));
+}
+
+fn main() {
+    panic_handler_generate_coredump();
+    panic!("don't Panic!")
+}
