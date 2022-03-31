dd29271f627d62d1e2ced9a9e7cea019719a8754
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-31 22:50:56 +0800

    add into_inner_unchecked example

---
+++ pin_api_example_workspace/into_inner_unchecked_example/src/main.rs
@@ -0,0 +1,20 @@
+use std::marker::PhantomPinned;
+use std::pin::Pin;
+
+#[derive(PartialEq, Debug)]
+struct Foo {
+    x: i32,
+    _pin: PhantomPinned,
+}
+
+fn main() {
+    let twos = Foo {
+        x: 2,
+        _pin: PhantomPinned,
+    };
+
+    let ptr = unsafe { Pin::new_unchecked(&twos) };
+    unsafe {
+        assert_eq!(Pin::into_inner_unchecked(ptr), &twos);
+    }
+}
