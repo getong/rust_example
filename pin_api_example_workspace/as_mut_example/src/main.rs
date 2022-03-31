2eec5d31fa4f37d8253d1a75b0493839b6990fa2
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-04-01 00:19:11 +0800

    add as_mut example

---
+++ pin_api_example_workspace/as_mut_example/src/main.rs
@@ -0,0 +1,29 @@
+use std::marker::PhantomPinned;
+use std::pin::Pin;
+
+#[derive(PartialEq, Debug)]
+struct Foo {
+    x: i32,
+    _pin: PhantomPinned,
+}
+
+fn main() {
+    let mut i = 9;
+    let mut pin = Pin::new(&mut i);
+    *pin.as_mut() = 10;
+    assert_eq!(i, 10);
+
+    // !Unpin
+    let mut twos = Foo {
+        x: 2,
+        _pin: PhantomPinned,
+    };
+    let mut pin = unsafe { Pin::new_unchecked(&mut twos) };
+    unsafe {
+        *pin.as_mut().get_unchecked_mut() = Foo {
+            x: 3,
+            _pin: PhantomPinned,
+        }
+    };
+    assert_eq!(twos.x, 3);
+}
