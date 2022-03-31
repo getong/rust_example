d3fa7f7c243a6b994bdf39fd8c27f6ef4a795f9e
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-03-31 22:40:31 +0800

    add new_unchecked example

---
+++ pin_api_example_workspace/new_unchecked_example/src/main.rs
@@ -0,0 +1,22 @@
+use std::marker::PhantomPinned;
+use std::pin::Pin;
+
+struct Foo {
+    x: i32,
+    _pin: PhantomPinned,
+}
+
+fn main() {
+    let mut twos = Foo {
+        x: 2,
+        _pin: PhantomPinned,
+    };
+    let mut ptr = unsafe { Pin::new_unchecked(&mut twos) };
+    let mut_ref: Pin<&mut Foo> = ptr.as_mut();
+    unsafe {
+        mut_ref.get_unchecked_mut().x = 22;
+    }
+    println!("twos: {}", twos.x);
+    twos.x = 222;
+    println!("twos: {}", twos.x);
+}
