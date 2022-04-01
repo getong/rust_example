58d668291193515c0528fd3f9160fe29b785b6aa
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-04-01 22:13:02 +0800

    add multiple ref mut example

---
+++ multiple_ref_mut_example/src/main.rs
@@ -0,0 +1,16 @@
+fn f(a: Option<&mut i32>) {
+    if let Some(&mut ref mut x) = a {
+        *x = 6;
+    }
+    // ...
+    if let Some(&mut ref mut x) = a {
+        *x = 7;
+    }
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let mut i = 5;
+    f(Some(&mut i));
+    println!("i: {}", i);
+}
