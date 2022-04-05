61179a71d21f8f989754dfec70e3ce20b66dfbef
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-04-05 11:32:20 +0800

    mut self.field example

---
+++ struct_ownership_example_workspace/mut_self_field_example/src/main.rs
@@ -0,0 +1,30 @@
+struct Example {
+    a: usize,
+    b: usize,
+}
+
+impl Example {
+    fn a(&mut self) -> &mut usize {
+        &mut self.a
+    }
+    fn b(&mut self) -> &mut usize {
+        &mut self.b
+    }
+}
+
+fn main() {
+    let mut s = Example { a: 1, b: 2 };
+
+    // References created by returning &mut self.a and &mut self.b
+    // from methods
+    let _x = s.a();
+    let _y = s.b();
+
+    // References create by accessing the field directly
+    let x = &mut s.a;
+    let y = &mut s.b;
+
+    // Ensure x/y aren't dropped due to non-lexical scope
+    println!("x: {}", x);
+    println!("y: {}", y);
+}
