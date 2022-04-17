0bf541f598980576eeca87102edfa5dc153a9022
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-04-17 23:58:21 +0800

    add code match example

---
+++ code_match_example/src/main.rs
@@ -0,0 +1,19 @@
+pub enum Protection {
+    Secure { version: u64 },
+    Insecure,
+}
+
+fn process(prot: Protection) {
+    match prot {
+        Protection::Secure { version } => {
+            println!("Hacker-safe thanks to protocol v{version}");
+        }
+        Protection::Insecure => {
+            println!("Come on in");
+        }
+    }
+}
+
+fn main() {
+    process(Protection::Secure { version: 2 })
+}
