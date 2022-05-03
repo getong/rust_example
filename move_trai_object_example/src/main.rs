137a6a1165bd03f77519cf1e34477081f9fdf435
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-05-04 00:00:20 +0800

    add move trait object example

---
+++ move_trai_object_example/src/main.rs
@@ -0,0 +1,26 @@
+trait Named {
+    fn name(self: Box<Self>) -> String;
+}
+
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Named for Person {
+    fn name(self: Box<Self>) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+}
+
+pub struct Mech<'a> {
+    driver: Box<dyn Named + 'a>,
+}
+
+impl<'a> Mech<'a> {
+    pub fn driver_name(self) -> String {
+        self.driver.name()
+    }
+}
+
+fn main() {}
