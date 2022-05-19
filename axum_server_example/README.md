2d3fb71753451b0581b1510f27382ddc79c52ab3
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-05-19 19:00:18 +0800

    add axum_server example

---
+++ axum_server_example/README.md
@@ -0,0 +1,9 @@
+# axum_server example
+copy from [Module axum_server::tls_rustls](https://programatik29.github.io/axum-server/axum_server/tls_rustls/index.html)
+
+``` shell
+cargo add axum-server --features "rustls arc-swap rustls-pemfile tls-rustls tokio-rustls pin-project-lite"
+cargo add tokio --features full
+cargo add axum
+cargo build
+```
---
+++ axum_server_example/src/main.rs
@@ -0,0 +1,22 @@
+use axum::{routing::get, Router};
+use axum_server::tls_rustls::RustlsConfig;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    let config = RustlsConfig::from_pem_file(
+        "self-signed-certs/cert.pem",
+        "self-signed-certs/key.pem",
+    )
+    .await
+    .unwrap();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
