a191c46c91b748b18e7adfd2502b5ecb4fbfc39f
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-05-23 02:09:44 +0800

    add nom-tag-example

---
+++ nom-workspace-example/nom-tag-example/src/main.rs
@@ -0,0 +1,10 @@
+use nom::{bytes::complete::tag, IResult};
+
+fn parse(input: &str) -> IResult<&str, &str> {
+    tag("#")(input)
+}
+
+fn main() {
+    let (remain, pattern) = parse("#ffffff").unwrap();
+    println!("the #fffff remain: {}, pattern: {}", remain, pattern);
+}
