470a37dbdd66381c42ae556fde6aa1c42969ec86
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-05-25 23:51:57 +0800

    add struct workspace example

---
+++ struct_workspace_example/struct_move_field_example/README.md
@@ -0,0 +1,3 @@
+# move a field of a struc
+
+copy from [Cannot move a field of a struct](https://users.rust-lang.org/t/cannot-move-a-field-of-a-struct/23949)
---
+++ struct_workspace_example/struct_move_field_example/src/main.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct Foo {}
+
+impl Foo {
+    pub fn new() -> Foo {
+        Foo {}
+    }
+}
+
+#[derive(Debug)]
+pub struct Bar {
+    foo: Foo,
+}
+
+impl Bar {
+    pub fn swap(&mut self) -> Foo {
+        // let ans = self.foo;
+        // self.foo = Foo::new();
+        // ans
+        std::mem::replace(&mut self.foo, Foo::new())
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+    let foo = Foo::new();
+
+    println!("foo:{:#?}", foo);
+    let mut bar = Bar { foo: foo };
+    bar.swap();
+    println!("bar:{:#?}", bar);
+}
