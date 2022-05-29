bc16ee78ddbfc52c7302984e2545a4e765ff091b
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-05-29 23:43:19 +0800

    update code

--- fred_redis_workspace_example/fred_pool_example/src/main.rs
+++ fred_redis_workspace_example/fred_pool_example/src/main.rs
@@ -1,8 +1,11 @@
 use fred::pool::RedisPool;
+use futures::stream::StreamExt;
 use fred::prelude::*;
+// use std::iter::Iterator;
+// use futures_util::stream::stream::StreamExt;
 // use chrono::Utc;
 
-use fred::types::RedisValue;
+// use fred::types::RedisValue;
 
 #[tokio::main]
 async fn main() -> Result<(), RedisError> {
@@ -22,12 +25,29 @@ async fn main() -> Result<(), RedisError> {
     // let hget_result :String = pool.next().hget(String::from("message::complete"), "content").await;
     // println!("hget_result:{:?}", hget_result);
 
+    let client = pool.next();
 
     // use the pool like any other RedisClient
-    let _ = pool.next().get("foo").await?;
-    let _ = pool.next().set("foo", "bar", None, None, false).await?;
-    let foo_set_result = pool.next().get("foo").await?;
+    let _ = client.get("foo").await?;
+    let _: () = client.set("foo", "bar", None, None, false).await?;
+    let foo_set_result: RedisValue = client.get("foo").await?;
     println!("foo_set_result:{:?}", foo_set_result);
+
+    let mut scan_stream = client.scan("foo*", Some(10), None);
+    while let Some(Ok(mut page)) = scan_stream.next().await {
+        if let Some(keys) = page.take_results() {
+            // println!("scan_result:{:?}", scan_result);
+            // let client = page.create_client();
+
+            for key in keys.into_iter() {
+                let value: RedisValue = client.get(&key).await?;
+                println!("Scanned {} -> {:?}", key.as_str_lossy(), value);
+                println!("value:{:?}", value);
+            }
+        }
+    }
+
+
 
     let _ = pool.quit_pool().await;
     Ok(())
