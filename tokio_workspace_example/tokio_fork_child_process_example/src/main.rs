f944226ec69f23522528cadd7644514735266fa7
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-01 13:01:32 +0800

    add tokio workspace example

---
+++ tokio_workspace_example/tokio_fork_child_process_example/src/main.rs
@@ -0,0 +1,34 @@
+// use tokio::prelude::*;
+use tokio::net::UnixStream;
+use nix::unistd::{fork, ForkResult};
+use nix::sys::wait;
+use std::io::Error;
+use std::io::ErrorKind;
+use tokio::io::AsyncWriteExt;
+use tokio::io::AsyncReadExt;
+use wait::wait;
+
+// Limit to 1 thread
+#[tokio::main(worker_threads = 1)]
+async fn main() -> Result<(), Error> {
+    let mut socks = UnixStream::pair()?;
+
+    match unsafe { fork()} {
+        Ok(ForkResult::Parent { .. }) => {
+            eprintln!("Writing!");
+            socks.0.write_u32(31337).await?;
+            eprintln!("Written!");
+            wait().unwrap();
+            Ok(())
+        }
+
+        Ok(ForkResult::Child) => {
+            eprintln!("Reading from master");
+            let msg = socks.1.read_u32().await?;
+            eprintln!("Read from master {}", msg);
+            Ok(())
+        }
+
+        Err(_) => Err(Error::new(ErrorKind::Other, "oh no!")),
+    }
+}
