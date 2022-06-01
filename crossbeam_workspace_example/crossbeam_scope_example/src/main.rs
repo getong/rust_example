5e860295e611f8429de090fecde75d0afe8ce717
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-02 01:53:00 +0800

    add crossbeam workspace example

---
+++ crossbeam_workspace_example/crossbeam_scope_example/src/main.rs
@@ -0,0 +1,15 @@
+use crossbeam_utils::thread;
+
+fn main() {
+    // println!("Hello, world!");
+    let people = vec!["Alice".to_string(), "Bob".to_string(), "Carol".to_string()];
+
+    thread::scope(|s| {
+        for person in &people {
+            s.spawn(move |_| {
+                println!("Hello, {}!", person);
+            });
+        }
+    })
+    .unwrap();
+}
