06fdb2a9380c6758a31f50759ab5ca0fdd3d9608
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-03 22:40:13 +0800

    add code

--- dashmap_example/src/main.rs
+++ dashmap_example/src/main.rs
@@ -1,5 +1,7 @@
 use dashmap::DashMap;
 
+use parking_lot::RwLock;
+use std::io;
 use std::sync::Arc;
 use std::thread::sleep;
 use std::time::Duration;
@@ -11,14 +13,25 @@ fn main() {
 
     println!("reviews:{:?}", reviews);
 
-    let dict: Arc<DashMap<String, u8>> = Arc::new(DashMap::<String, u8>::new());
+    let dict: Arc<DashMap<String, RwLock<u8>>> = Arc::new(DashMap::<String, RwLock<u8>>::new());
 
     let dict_clone = dict.clone();
-    std::thread::spawn(move || dict_clone.insert("a".to_owned(), 1u8));
+    std::thread::spawn(move || dict_clone.insert("a".to_owned(), RwLock::new(1u8)));
 
     let dict_clone = dict.clone();
-    std::thread::spawn(move || dict_clone.insert("b".to_owned(), 2u8));
+    std::thread::spawn(move || dict_clone.insert("b".to_owned(), RwLock::new(2u8)));
 
-    sleep(Duration::from_millis(10));
+    let dict_clone = dict.clone();
+    std::thread::spawn(move || {
+        let _ = dict_clone.entry("c".to_owned()).or_try_insert_with(|| {
+            if 3 > 2 {
+                Ok(RwLock::new(3u8))
+            } else {
+                Err(io::Error::new(io::ErrorKind::NotFound, "Chunk not found"))
+            }
+        });
+    });
+
+    sleep(Duration::from_millis(20));
     println!("dict: {:?}", dict);
 }
