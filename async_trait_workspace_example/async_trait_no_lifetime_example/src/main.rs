b580286c6e125c2a1fe297910f5e5deab039b2db
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-06 01:33:33 +0800

    add async_trait workspace example

---
+++ async_trait_workspace_example/async_trait_no_lifetime_example/src/main.rs
@@ -0,0 +1,40 @@
+use std::fmt::Debug;
+use async_trait::async_trait;
+
+#[derive(Debug)]
+struct Update;
+
+#[async_trait]
+trait Handler: Debug {
+    async fn update(&self, update: &Update) -> ();
+}
+
+#[derive(Default, Debug)]
+struct Dispatcher {
+    handlers: Vec<Box<dyn Handler>>,
+}
+
+impl Dispatcher {
+    pub fn push_handler(&mut self, handler: Box<dyn Handler>) {
+        self.handlers.push(handler);
+    }
+}
+
+// example handler
+#[derive(Default, Debug)]
+struct Foo {}
+#[async_trait]
+impl Handler for Foo {
+    async fn update(&self, update: &Update) -> () {
+        println!("Update: {:?}", update);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let mut dispatcher = Dispatcher::default();
+    let handler = Box::new(Foo {});
+
+    dispatcher.push_handler(handler);
+    println!("dispatcher: {:?}", dispatcher);
+}
