095b872de4a72a0c1e7f3f64621487654a59d973
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-05 12:48:10 +0800

    add tokio is_finished() example

---
+++ tokio_workspace_example/tokio_is_finished_example/src/main.rs
@@ -0,0 +1,18 @@
+use tokio::time;
+
+#[tokio::main]
+async fn main() {
+    let handle1 = tokio::spawn(async {
+        // do some stuff here
+    });
+
+    let handle2 = tokio::spawn(async {
+        // do some other stuff here
+        time::sleep(time::Duration::from_secs(10)).await;
+    });
+    // Wait for the task to finish
+    handle2.abort();
+    time::sleep(time::Duration::from_secs(1)).await;
+    assert!(handle1.is_finished());
+    assert!(handle2.is_finished());
+}
