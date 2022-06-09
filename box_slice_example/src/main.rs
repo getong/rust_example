8ab99dc9017c5f2e18268dd0985205c1215107d5
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-10 00:29:28 +0800

    add code example

--- box_slice_example/src/main.rs
+++ box_slice_example/src/main.rs
@@ -1,3 +1,4 @@
+use std::mem::size_of_val;
 use std::ops::Deref;
 
 fn main() {
@@ -26,4 +27,19 @@ fn main() {
     // b2 和 b3 相等，但 b3.deref() 和 v2 无法比较
     assert!(b2 == b3);
     // assert!(b3.deref() == v2);
+
+    another_box_slice_exmaple();
+}
+
+// copy from [The curse of strong typing](https://fasterthanli.me/articles/the-curse-of-strong-typing)
+fn another_box_slice_exmaple() {
+    let bbox: Box<[u8]> = Box::new([1, 2, 3, 4, 5]);
+    let slice = &bbox[1..4];
+    dbg!(size_of_val(&bbox));
+    dbg!(size_of_val(&slice));
+    print_byte_slice(slice);
+}
+
+fn print_byte_slice(slice: &[u8]) {
+    println!("{slice:?}");
 }
