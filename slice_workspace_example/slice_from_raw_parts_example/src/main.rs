a15949be375b173dea5c5b670dd9f410d3c2cd50
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-18 16:11:05 +0800

    add slice workspace example

---
+++ slice_workspace_example/slice_from_raw_parts_example/src/main.rs
@@ -0,0 +1,29 @@
+// copy from https://course.rs/advance/lifetime/static.html
+use std::{slice::from_raw_parts, str::from_utf8_unchecked};
+
+fn get_memory_location() -> (usize, usize) {
+    // “Hello World” 是字符串字面量，因此它的生命周期是 `'static`.
+    // 但持有它的变量 `string` 的生命周期就不一样了，它完全取决于变量作用域，对于该例子来说，也就是当前的函数范围
+    let string = "Hello World!";
+    let pointer = string.as_ptr() as usize;
+    let length = string.len();
+    (pointer, length)
+    // `string` 在这里被 drop 释放
+    // 虽然变量被释放，无法再被访问，但是数据依然还会继续存活
+}
+
+fn get_str_at_location(pointer: usize, length: usize) -> &'static str {
+    // 使用裸指针需要 `unsafe{}` 语句块
+    unsafe { from_utf8_unchecked(from_raw_parts(pointer as *const u8, length)) }
+}
+
+fn main() {
+    let (pointer, length) = get_memory_location();
+    let message = get_str_at_location(pointer, length);
+    println!(
+        "The {} bytes at 0x{:X} stored: {}",
+        length, pointer, message
+    );
+    // 如果大家想知道为何处理裸指针需要 `unsafe`，可以试着反注释以下代码
+    // let message = get_str_at_location(1000, 10);
+}
