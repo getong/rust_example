5118daa4d404889c75df2d557a98de06a9d29124
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-20 18:47:41 +0800

    add any trait workspace example

---
+++ any_trait_workspace_example/any_trait_reflection_example/src/main.rs
@@ -0,0 +1,37 @@
+use std::any::Any;
+use std::fmt::Debug;
+
+fn load_config<T: Any + Debug>(value: &T) -> Vec<String> {
+    let mut cfgs: Vec<String> = vec![];
+    let value = value as &dyn Any;
+    match value.downcast_ref::<String>() {
+        Some(cfp) => cfgs.push(cfp.clone()),
+        None => (),
+    };
+
+    match value.downcast_ref::<Vec<String>>() {
+        Some(v) => cfgs.extend_from_slice(&v),
+        None => (),
+    }
+
+    if cfgs.len() == 0 {
+        panic!("No Config File");
+    }
+    cfgs
+}
+
+fn main() {
+    let cfp = "/etc/wayslog.conf".to_string();
+    assert_eq!(load_config(&cfp), vec!["/etc/wayslog.conf".to_string()]);
+    let cfps = vec![
+        "/etc/wayslog.conf".to_string(),
+        "/etc/wayslog_sec.conf".to_string(),
+    ];
+    assert_eq!(
+        load_config(&cfps),
+        vec![
+            "/etc/wayslog.conf".to_string(),
+            "/etc/wayslog_sec.conf".to_string()
+        ]
+    );
+}
