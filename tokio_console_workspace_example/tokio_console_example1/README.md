b52df391b6711352cdef85994d628c535cd6367c
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-29 00:29:57 +0800

    add tokio console workspace example

---
+++ tokio_console_workspace_example/tokio_console_example1/README.md
@@ -0,0 +1,11 @@
+# tokio console example
+
+``` shell
+cargo install tokio-console
+tokio-console
+
+
+cargo clean
+cargo run
+```
+copy from [tokio-console: topみたいなRustのコマンドラインツール](https://zenn.dev/tfutada/articles/4dbb9659bb8102)
---
+++ tokio_console_workspace_example/tokio_console_example1/src/main.rs
@@ -0,0 +1,44 @@
+use futures::future;
+use std::thread;
+use std::time::Duration;
+use tokio::time::sleep;
+
+#[tokio::main]
+// #[tokio::main(flavor = "current_thread")]
+// #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() {
+    console_subscriber::init(); // for tokio-console
+
+    // 1. 論理CPU数の取得
+    let cpus = num_cpus::get();
+    println!("logical cores: {}", cpus);
+
+    let mut handles = Vec::new();
+
+    // 2. Task A: ノンブロッキング・スリープ
+    let task_a = tokio::task::Builder::new().name("Task A").spawn(async {
+        loop {
+            println!("   =Task A sleeping... {:?}", thread::current().id());
+            sleep(Duration::from_secs(1)).await; // ノンブロッキング・スリープ
+            println!("   =Task A woke up.");
+        }
+    });
+    handles.push(task_a);
+
+    // 3. Task B-n: ブロッキング・スリープ
+    for i in 0..cpus + 1 {
+        let task_b = tokio::task::Builder::new()
+            .name(&format!("Task B-{}", i))
+            .spawn(async move {
+                loop {
+                    println!("#Task B-{} sleeping... {:?}", i, thread::current().id());
+                    thread::sleep(Duration::from_secs(5)); // ブロッキング・スリープ
+                    println!("#Task B-{} woke up.", i);
+                    // sleep(Duration::from_nanos(1)).await;
+                }
+            });
+        handles.push(task_b)
+    }
+
+    future::join_all(handles).await;
+}
