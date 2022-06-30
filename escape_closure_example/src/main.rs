cc6b827cff3eb85337b6a830b283400426ad0f3b
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-30 18:23:33 +0800

    add code

--- escape_closure_example/src/main.rs
+++ escape_closure_example/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
+    // a is Copy
     let a = 1;
+    // b is not Copy
+    let b = "hello".to_owned();
     let c: Box<dyn Fn() + 'static> = Box::new(move || {
         println!("a: {}", a); // error, borrowed value does not live long enough
+        println!("b :{}", b);
     });
     println!("a in the main :{}", a);
+
+    // can not use b here
+    // println!("b in the main :{}", b);
+
     let d = c;
     println!("d in the main :{:?}", d());
 }
