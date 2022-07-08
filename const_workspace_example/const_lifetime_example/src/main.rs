5567e1c8626ad6025e12183701a3be5383bc602b
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-07-08 16:32:22 +0800

    update code

--- const_workspace_example/const_lifetime_example/src/main.rs
+++ const_workspace_example/const_lifetime_example/src/main.rs
@@ -10,11 +10,27 @@ impl Drop for TypeWithDestructor {
 const ZERO_WITH_DESTRUCTOR: TypeWithDestructor = TypeWithDestructor(0);
 
 fn create_and_drop_zero_with_destructor() {
-    let x = ZERO_WITH_DESTRUCTOR;
+    let mut x = ZERO_WITH_DESTRUCTOR;
     // x gets dropped at end of function, calling drop.
     // prints "Dropped. Held 0.".
+    x.0 = 3;
+    println!(
+        "x is at {:?}",
+        &mut x as *const TypeWithDestructor as *const ()
+    )
 }
 
 fn main() {
-    create_and_drop_zero_with_destructor()
+    create_and_drop_zero_with_destructor();
+    println!("hello world");
+    create_and_drop_zero_with_destructor();
+
+    let mut x = ZERO_WITH_DESTRUCTOR;
+    // x gets dropped at end of function, calling drop.
+    // prints "Dropped. Held 0.".
+    x.0 = 3;
+    println!(
+        "x is at {:?}",
+        &mut x as *const TypeWithDestructor as *const ()
+    );
 }
