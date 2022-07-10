1da50c935882fbf062b6b9a9b2a8347334f443d7
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-07-10 23:48:11 +0800

    add const fn example

---
+++ const_workspace_example/const_fn_example/src/main.rs
@@ -0,0 +1,12 @@
+const fn foo(num: i32) -> i32 {
+    if 3 > 2 {
+        2
+    } else {
+        num * num
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+    println!("foo(3): {}", foo(3));
+}
