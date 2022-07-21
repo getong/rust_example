9a4d266418f02b22b1e87835a02058248fdcb333
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-07-22 00:50:43 +0800

    add rocksdb example

---
+++ rocksdb_example/src/main.rs
@@ -0,0 +1,30 @@
+use rocksdb::{ColumnFamilyDescriptor, Options, DB};
+
+fn main() {
+    // println!("Hello, world!");
+
+    let path = "_path_for_rocksdb_storage_with_cfs";
+    let mut cf_opts = Options::default();
+    cf_opts.set_max_write_buffer_number(16);
+    let cf = ColumnFamilyDescriptor::new("cf1", cf_opts);
+
+    let mut db_opts = Options::default();
+    db_opts.create_missing_column_families(true);
+    db_opts.create_if_missing(true);
+    {
+        let db = DB::open_cf_descriptors(&db_opts, path, vec![cf]).unwrap();
+    }
+
+    {
+        let db = DB::open_default(path).unwrap();
+        db.put(b"my key", b"my value").unwrap();
+        match db.get(b"my key") {
+            Ok(Some(value)) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
+            Ok(None) => println!("value not found"),
+            Err(e) => println!("operational problem encountered: {}", e),
+        }
+        db.delete(b"my key").unwrap();
+    }
+
+    let _ = DB::destroy(&db_opts, path);
+}
