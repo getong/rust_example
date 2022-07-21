8627878fc6b4cd673613bb866964f0bd0f9963e0
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-07-22 00:38:28 +0800

    add merk example

---
+++ merk_example/src/main.rs
@@ -0,0 +1,16 @@
+use merk::*;
+
+fn main() {
+    // println!("Hello, world!");
+    // load or create a Merk store at the given path
+    let mut merk = Merk::open("./merk.db").unwrap();
+
+    // apply some operations
+    let batch = [
+        (b"key", Op::Put(b"value")),
+        (b"key2", Op::Put(b"value2")),
+        (b"key3", Op::Put(b"value3")),
+        (b"key4", Op::Delete),
+    ];
+    merk.apply(&batch).unwrap();
+}
