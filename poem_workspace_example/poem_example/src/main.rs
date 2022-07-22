1f53cfe27de40a894ccb39bb923ece6a0dc39aac
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-07-23 01:06:16 +0800

    add poem workspace example

---
+++ poem_workspace_example/poem_example/src/main.rs
@@ -0,0 +1,14 @@
+use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new().at("/hello/:name", get(hello));
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await
+}
