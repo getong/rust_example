969658797a5ea0b1bdb03af6edeb7e16a14cd6be
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-07-23 23:21:05 +0800

    update code

--- termion_example/src/main.rs
+++ termion_example/src/main.rs
@@ -1,6 +1,39 @@
-use termion;
+use std::io::stdout;
+use std::mem::drop;
+use std::process::exit;
+use std::time::Duration;
 
-fn main() {
-    println!("Hello, world!");
-    println!("terminal size:{:?}", termion::terminal_size().unwrap());
+use futures::{future::Either, StreamExt};
+use tokio::{io::stdin, time::interval};
+
+use termion::{event::Key, raw::IntoRawMode};
+use termion_input_tokio::TermReadAsync;
+use tokio_stream::wrappers::IntervalStream;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let raw_term = stdout().into_raw_mode()?;
+
+    let input = stdin().keys_stream().map(Either::Right);
+    let ticks = IntervalStream::new(interval(Duration::from_secs(3))).map(Either::Left);
+
+    let events = futures::stream::select(ticks, input);
+
+    events
+        .fold(raw_term, |raw_term, it| {
+            println!("Event: {:?}\r", it);
+            match it {
+                Either::Right(Ok(Key::Esc))
+                | Either::Right(Ok(Key::Char('q')))
+                | Either::Right(Ok(Key::Ctrl('c')))
+                | Either::Right(Ok(Key::Ctrl('b'))) => {
+                    drop(raw_term);
+                    exit(0);
+                }
+                _ => (),
+            }
+            async { raw_term }
+        })
+        .await;
+    Ok(())
 }
