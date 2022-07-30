2913b44d5203cd38f67f5a9fc8bda77c1eb7387d
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-07-30 22:29:33 +0800

    add bloom filter example

---
+++ bloom-filter-example/README.md
@@ -0,0 +1,3 @@
+# bloom filter example
+
+copy from [Practical Bloom Filters](https://medium.com/@dillen.dev/practical-bloom-filters-dc49e3deb335)
---
+++ bloom-filter-example/src/main.rs
@@ -0,0 +1,49 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+fn hash(value: &str) -> u64 {
+    let mut s = DefaultHasher::new();
+
+    value.hash(&mut s);
+    s.finish()
+}
+
+struct BloomFilter<const N: usize> {
+    bytes: [u8; N],
+}
+
+impl<const N: usize> BloomFilter<N> {
+    fn new() -> Self {
+        Self { bytes: [0; N] }
+    }
+
+    fn get_positions(&self, key: &str) -> (usize, u64) {
+        let bit_size = N * 8;
+        let pos = hash(key) % (bit_size as u64);
+
+        ((pos / 8) as usize, pos % 8)
+    }
+
+    fn add(&mut self, key: &str) {
+        let (byte_idx, bit_idx) = self.get_positions(key);
+
+        self.bytes[byte_idx] |= 1 << bit_idx;
+    }
+
+    fn contains(&self, key: &str) -> bool {
+        let (byte_idx, bit_idx) = self.get_positions(key);
+
+        self.bytes[byte_idx] & (1 << bit_idx) != 0
+    }
+}
+
+fn main() {
+    let mut filter = BloomFilter::<10>::new();
+
+    filter.add("test1");
+    filter.add("test2");
+
+    println!("test1: {}", filter.contains("test1"));
+    println!("test2: {}", filter.contains("test2"));
+    println!("test3: {}", filter.contains("test3"));
+}
