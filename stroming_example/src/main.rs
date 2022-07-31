6a42cb5411a1ed742bc36583b7c22050ed82c012
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-07-31 23:54:23 +0800

    add stroming example

---
+++ stroming_example/src/main.rs
@@ -0,0 +1,19 @@
+use stroming::*;
+
+fn main() {
+    // println!("Hello, world!");
+    let mut store = MemoryStreamStore::new();
+    let data = r#"{"test": "data"}"#.as_bytes().to_vec();
+    let msg = Message {
+        message_type: "TestMessage".to_owned(),
+        data,
+    };
+
+    let _ = store.write_to_stream("TestStream-1", StreamVersion::NoStream, &[msg]);
+
+    let (version, messages) = store.read_from_stream("TestStream-1", ReadDirection::Forwards);
+
+    assert_eq!(version, StreamVersion::Revision(0));
+    assert_eq!(messages.len(), 1);
+    assert_eq!(messages[0].message_type, "TestMessage");
+}
