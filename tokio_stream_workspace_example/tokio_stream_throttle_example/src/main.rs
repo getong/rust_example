47b829acb61fa52d35d16eb2628b801fc8862c3f
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-08-16 12:49:49 +0800

    add tokio_stream workspace example

---
+++ tokio_stream_workspace_example/tokio_stream_throttle_example/src/main.rs
@@ -0,0 +1,20 @@
+use std::time::Duration;
+use tokio_stream::StreamExt;
+
+
+#[tokio::main]
+async fn main() {
+    // println!("Hello, world!");
+
+    let item_stream = futures::stream::repeat("one").throttle(Duration::from_secs(2));
+    tokio::pin!(item_stream);
+
+    loop {
+        // The string will be produced at most every 2 seconds
+        println!(
+            "current time: {:?}, stream element:{:?}",
+            chrono::offset::Local::now(),
+            item_stream.next().await
+        );
+    }
+}
