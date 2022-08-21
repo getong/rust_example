1a1d3ab446f9067ff62d9042dce7db9a4c42fd11
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-08-21 20:08:35 +0800

    add mut slice example

---
+++ slice_workspace_example/mut_slice_example/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // println!("Hello, world!");
+    let mut arr = [0u8; 10];
+    arr[0] = 1;
+    // let mut sl = &[0u8;10][..];
+    let sl = &mut [0u8; 10][..];
+    sl[0] = 1;
+}
