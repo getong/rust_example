06eaa883baa9412cb3f507156964264c9f44e4dc
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-08-27 23:13:01 +0800

    add pin phantompinned example

---
+++ pin_phantompinned_example/README.md
@@ -0,0 +1,7 @@
+copy from [Pin vs Box: Why is Box not enough?](https://stackoverflow.com/questions/72516441/pin-vs-box-why-is-box-not-enough)
+
+
+>>>
+PhantomPinned does not make data immovable. It just says that once the data is pinned, it will never be able to be unpinned again.
+
+Therefore, to make data with PhantomPinned unmovable, you have to Pin it first.
---
+++ pin_phantompinned_example/src/main.rs
@@ -0,0 +1,38 @@
+// Note that MyData does not implement Clone or Copy
+struct MyData(u32);
+
+impl MyData {
+    fn print_addr(&self) {
+        println!("Address: {:p}", self);
+    }
+}
+
+use std::{marker::PhantomPinned, pin::Pin};
+
+struct MyData2(u32, PhantomPinned);
+
+impl MyData2 {
+    fn print_addr2(self: Pin<&Self>) {
+        println!("Address: {:p}", self);
+    }
+}
+
+fn main() {
+    // On the heap
+    let x_heap = Box::new(MyData(42));
+    x_heap.print_addr();
+
+    // Moved back on the stack
+    let x_stack = *x_heap;
+    x_stack.print_addr();
+
+    // On the heap
+    let x_pinned = Box::pin(MyData2(42, PhantomPinned));
+    x_pinned.as_ref().print_addr2();
+
+    // Moved back on the stack
+    // let x_unpinned = Pin::into_inner(x_pinned); // FAILS!
+    // let x_stack = *x_unpinned;
+    // let x_pinned_again = Box::pin(x_stack);
+    // x_pinned_again.as_ref().print_addr();
+}
