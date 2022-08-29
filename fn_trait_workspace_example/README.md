eab8f57aa2ea34f3e8c00ef17c2df3c5b3f52607
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-08-30 00:31:55 +0800

    add fn trait workspace example

---
+++ fn_trait_workspace_example/README.md
@@ -0,0 +1,3 @@
+# Fn, FnMut, FnOnce workspace example
+
+copy from [Rust的闭包类型(Fn, FnMut, FnOne的区别)](https://tonydeng.github.io/2019/11/09/rust-closure-type/)
---
+++ fn_trait_workspace_example/fn_example/src/main.rs
@@ -0,0 +1,30 @@
+// #[lang = "fn"]
+// pub trait Fn<Args>: FnMut<Args> {
+//     extern "rust-call" fn call(&self, args: Args) -> Self::Output;
+// }
+
+fn fn_immut<F>(func: F)
+where
+    F: Fn(),
+{
+    func();
+    func();
+}
+
+#[derive(Debug)]
+pub struct E {
+    pub a: String,
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let e = E {
+        a: "fn".to_string(),
+    };
+
+    let f = || {
+        println!("Fn closure calls: {:?}", e);
+    };
+
+    fn_immut(f);
+}
