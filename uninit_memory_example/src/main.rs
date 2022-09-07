8d94e0917b0f444b5dcdfd0bc8280bdfa55920a5
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-09-08 01:18:15 +0800

    add uninit memory example

---
+++ uninit_memory_example/src/main.rs
@@ -0,0 +1,21 @@
+use std::mem::MaybeUninit;
+use std::ptr::addr_of_mut;
+
+struct Role {
+    name: String,
+    disabled: bool,
+    flag: u32,
+}
+
+fn main() {
+    let role = unsafe {
+        let mut uninit = MaybeUninit::uninit();
+        let role: *mut Role = uninit.as_mut_ptr();
+        addr_of_mut!((*role).name).write("basic".to_string());
+        (*role).flag = 1;
+        (*role).disabled = false;
+        uninit.assume_init()
+    };
+
+    println!("{} ({}, {})", role.name, role.flag, role.disabled);
+}
