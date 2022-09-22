5216840e5a94e05597926a0181c00b92621168cd
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-09-22 19:04:27 +0800

    update code

--- seaorm_example/src/main.rs
+++ seaorm_example/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use sea_orm::ConnectOptions;
+use sea_orm::Database;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    let mut opt = ConnectOptions::new("protocol://username:password@host/database".to_owned());
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Info);
+
+    let _db = Database::connect(opt).await.unwrap();
 }
