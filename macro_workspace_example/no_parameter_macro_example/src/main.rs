57e4763bf0ac77c222ae51042dc0a7ae0cb43596
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-09-25 15:56:39 +0800

    add no parameter macro example

---
+++ macro_workspace_example/no_parameter_macro_example/src/main.rs
@@ -0,0 +1,17 @@
+macro_rules! four {
+    () => {
+        1 + 3
+    };
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let a = four!();
+    println!("a:{}", a);
+
+    let b = four![];
+    println!("b:{}", b);
+
+    let c = four! {};
+    println!("c:{}", c);
+}
