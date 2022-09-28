50156ba249fafff0bbb91aba41c0d0b0c975b488
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-09-28 16:36:38 +0800

    update code

--- axum_workspace_example/axum_sessions_auth_example/src/main.rs
+++ axum_workspace_example/axum_sessions_auth_example/src/main.rs
@@ -1,32 +1,42 @@
-//use sqlx::{PgPool, ConnectOptions, postgres::{PgPoolOptions, PgConnectOptions}};
-use redis::{aio::Connection, AsyncCommmands, FromRedisValue};
+use anyhow::Result;
+use async_trait::async_trait;
+use axum_sessions_auth::Auth;
+use http::Method;
+// use redis::{aio::Connection, AsyncCommands, FromRedisValue};
+// use redis::AsyncCommands;
 
-use std::net::SocketAddr;
-use axum_database_sessions::{AxumPgPool, AxumSession, AxumSessionConfig, AxumSessionLayer, AxumDatabasePool};
-use axum_sessions_auth::{AuthSession, AuthSessionLayer, Authentication};
-use axum::{
-    Router,
-    routing::get,
+use axum_database_sessions::AxumSessionStore;
+use axum_sessions_auth::HasPermission;
+use axum_sessions_auth::Rights;
+
+use axum::{routing::get, Router};
+use axum_database_sessions::{
+    // AxumDatabasePool, AxumPgPool, AxumSession, AxumSessionConfig, AxumSessionLayer,
+    AxumPgPool,
+    AxumSessionConfig,
+    AxumSessionLayer,
 };
+use axum_sessions_auth::{AuthSession, AuthSessionLayer, Authentication, AxumAuthConfig};
+use sqlx::PgPool;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     //# async {
     let poll = connect_to_database().await.unwrap();
 
-    let session_config = AxumSessionConfig::default()
-        .with_database("test")
-        .with_table_name("test_table");
-
-    let session_store = AxumSessionStore::<AxumPgPool>::new(Some(poll.clone().into()), session_config);
+    let session_config = AxumSessionConfig::default().with_table_name("test_table");
+    let auth_config = AxumAuthConfig::<i64>::default().with_anonymous_user_id(Some(1));
+    let session_store =
+        AxumSessionStore::<AxumPgPool>::new(Some(poll.clone().into()), session_config);
 
     // Build our application with some routes
     let app = Router::new()
         .route("/greet/:name", get(greet))
         .layer(AxumSessionLayer::new(session_store))
-        .layer(AuthSessionLayer::<User, AxumPgPool, PgPool>::new(
-            Some(poll),
-            Some(1))
+        .layer(
+            AuthSessionLayer::<User, i64, AxumPgPool, PgPool>::new(Some(poll))
+                .with_config(auth_config),
         );
 
     // Run it
@@ -41,16 +51,22 @@ async fn main() {
 
 // We can get the Method to compare with what Methods we allow. Useful if this supports multiple methods.
 // When called auth is loaded in the background for you.
-async fn greet(method: Method, session: AxumSession<AxumPgPool>, auth: AuthSession<User, AxumPgPool, PgPool>) -> &'static str {
-    let mut count: usize = session.get("count").unwrap_or(0);
+async fn greet(method: Method, auth: AuthSession<User, i64, AxumPgPool, PgPool>) -> String {
+    let mut count: usize = auth.session.get("count").await.unwrap_or(0);
     count += 1;
 
     // Session is Also included with Auth so no need to require it in the function arguments if your using
     // AuthSession.
-    auth.session.set("count", count);
+    _ = auth.session.set("count", count);
 
-    if let Some(cur_user) = current_user {
-        if !Auth::<User, PgPool>::build([Method::Get], false)
+    // If for some reason you needed to update your Users Permissions or data then you will want to clear the user cache if it is enabled.
+    // The user Cache is enabled by default. To clear simply use.
+    auth.cache_clear_user(1).await;
+    //or to clear all for a large update
+    auth.cache_clear_all().await;
+
+    if let Some(ref cur_user) = auth.current_user {
+        if !Auth::<User, i64, PgPool>::build([Method::GET], false)
             .requires(Rights::none([
                 Rights::permission("Token::UseAdmin"),
                 Rights::permission("Token::ModifyPerms"),
@@ -58,12 +74,12 @@ async fn greet(method: Method, session: AxumSession<AxumPgPool>, auth: AuthSessi
             .validate(&cur_user, &method, None)
             .await
         {
-            return format!("No Permissions! for {}", cur_user.username)[];
+            return format!("No Permissions! for {}", cur_user.username);
         }
 
         let username = if !auth.is_authenticated() {
             // Set the user ID of the User to the Session so it can be Auto Loaded the next load or redirect
-            auth.login_user(2);
+            _ = auth.login_user(2);
             "".to_string()
         } else {
             // If the user is loaded and is Authenticated then we can use it.
@@ -74,23 +90,23 @@ async fn greet(method: Method, session: AxumSession<AxumPgPool>, auth: AuthSessi
             }
         };
 
-        format!("{}-{}", username, count)[..]
+        format!("{}-{}", username, count)
     } else {
         if !auth.is_authenticated() {
             // Set the user ID of the User to the Session so it can be Auto Loaded the next load or redirect
-            auth.login_user(2);
+            _ = auth.login_user(2);
             // Set the session to be long term. Good for Remember me type instances.
-            auth.remember_user(true);
+            _ = auth.remember_user(true);
             // Redirect here after login if we did indeed login.
         }
 
-        "No Permissions!"
+        "No Permissions!".to_owned()
     }
 }
 
 #[derive(Clone, Debug)]
 pub struct User {
-    pub id: i32,
+    pub id: i64,
     pub anonymous: bool,
     pub username: String,
 }
@@ -98,7 +114,7 @@ pub struct User {
 // This is only used if you want to use Token based Authentication checks
 #[async_trait]
 impl HasPermission<PgPool> for User {
-    async fn has(&self, perm: &String, _pool: &Option<&PgPool>) -> bool {
+    async fn has(&self, perm: &str, _pool: &Option<&PgPool>) -> bool {
         match &perm[..] {
             "Token::UseAdmin" => true,
             "Token::ModifyUser" => true,
@@ -108,7 +124,7 @@ impl HasPermission<PgPool> for User {
 }
 
 #[async_trait]
-impl Authentication<User, PgPool> for User {
+impl Authentication<User, i64, PgPool> for User {
     async fn load_user(userid: i64, _pool: Option<&PgPool>) -> Result<User> {
         Ok(User {
             id: userid,
@@ -132,5 +148,5 @@ impl Authentication<User, PgPool> for User {
 
 async fn connect_to_database() -> anyhow::Result<sqlx::Pool<sqlx::Postgres>> {
     // ...
-    # unimplemented!()
+    unimplemented!()
 }
