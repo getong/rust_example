770a276e1e2a6ae3b8dcf8fb20b3dd2f9fb53f1b
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-09-30 14:43:16 +0800

    add reference memory example

---
+++ reference_memory_example/README.md
@@ -0,0 +1,4 @@
+# reference memory example
+
+
+copy from [Visualizing memory management in Rust](https://deepu.tech/memory-management-in-rust/)
---
+++ reference_memory_example/src/main.rs
@@ -0,0 +1,36 @@
+struct Employee<'a> {
+    // The 'a defines the lifetime of the struct. Here it means the reference of `name` field must outlive the `Employee`
+    name: &'a str,
+    salary: i32,
+    sales: i32,
+    bonus: i32,
+}
+
+const BONUS_PERCENTAGE: i32 = 10;
+
+// salary is borrowed
+fn get_bonus_percentage(salary: &i32) -> i32 {
+    let percentage = (salary * BONUS_PERCENTAGE) / 100;
+    return percentage;
+}
+
+// salary is borrowed while no_of_sales is copied
+fn find_employee_bonus(salary: &i32, no_of_sales: i32) -> i32 {
+    let bonus_percentage = get_bonus_percentage(salary);
+    let bonus = bonus_percentage * no_of_sales;
+    return bonus;
+}
+
+fn main() {
+    // variable is declared as mutable
+    let mut john = Employee {
+        name: &format!("{}", "John"), // explicitly making the value dynamic
+        salary: 5000,
+        sales: 5,
+        bonus: 0,
+    };
+
+    // salary is borrowed while sales is copied since i32 is a primitive
+    john.bonus = find_employee_bonus(&john.salary, john.sales);
+    println!("Bonus for {} is {}", john.name, john.bonus);
+}
