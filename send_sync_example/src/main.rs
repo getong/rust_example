28b8837e71abadd57f289b00d5115b717539e37f
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-09 22:26:04 +0800

    add send and sync example

---
+++ send_sync_example/src/main.rs
@@ -0,0 +1,14 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let i = 5;
+    thread::spawn(move || println!("i is {}", *&i));
+    thread::sleep(Duration::from_millis(10));
+    println!("i : {:?}", i);
+
+    let c = 'c';
+    thread::spawn(move || println!("c is {}", &c));
+    thread::sleep(Duration::from_millis(10));
+    println!("c : {:?}", c);
+}
