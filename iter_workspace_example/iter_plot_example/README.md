2f1beed4b2ce4b41a7c7c835e2a920ae0442bf12
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-11 02:24:51 +0800

    add iter workspace example

---
+++ iter_workspace_example/iter_plot_example/README.md
@@ -0,0 +1,3 @@
+# iter plot example
+
+copy from [Producing a zoom-able Bifurcation Diagram in Tauri/Rust](https://applied-math-coding.medium.com/producing-a-zoom-able-bifurcation-diagram-in-tauri-rust-d2867ff1387f)
---
+++ iter_workspace_example/iter_plot_example/src/main.rs
@@ -0,0 +1,12 @@
+mod feigenbaum;
+
+fn plot_feigenbaum_diag(a: f64, b: f64, dark_iters: usize, n_r: usize) -> (Vec<f64>, Vec<f64>) {
+    let delta = (b - a) / n_r as f64;
+    feigenbaum::plot(a, b, dark_iters, 1000, delta, 1)
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let (a, b) = plot_feigenbaum_diag(1.0, 2.0, 3, 4);
+    println!("a: {:#?}, b: {:#?}", a, b);
+}
