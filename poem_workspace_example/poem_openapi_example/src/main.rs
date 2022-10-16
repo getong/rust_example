fcc2c108c5386f14a2860ba68ae38275559de3e5
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-16 13:30:53 +0800

    add poem-openapi example

---
+++ poem_workspace_example/poem_openapi_example/src/main.rs
@@ -0,0 +1,27 @@
+use poem::{listener::TcpListener, Route};
+use poem_openapi::{param::Query, payload::PlainText, OpenApi, OpenApiService};
+
+struct Api;
+
+#[OpenApi]
+impl Api {
+    #[oai(path = "/hello", method = "get")]
+    async fn index(&self, name: Query<Option<String>>) -> PlainText<String> {
+        match name.0 {
+            Some(name) => PlainText(format!("hello, {}!", name)),
+            None => PlainText("hello!".to_string()),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let api_service =
+        OpenApiService::new(Api, "Hello World", "1.0").server("http://localhost:3000/api");
+    let ui = api_service.swagger_ui();
+    let app = Route::new().nest("/api", api_service).nest("/", ui);
+
+    poem::Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await
+}
