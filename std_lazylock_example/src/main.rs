8d2e249ad4cb8b7455c188c370732ca812ade631
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-17 02:01:08 +0800

    add std lazylock example

---
+++ std_lazylock_example/src/main.rs
@@ -0,0 +1,29 @@
+#![feature(once_cell)]
+
+use std::collections::HashMap;
+
+use std::sync::LazyLock;
+
+static HASHMAP: LazyLock<HashMap<i32, String>> = LazyLock::new(|| {
+    println!("initializing");
+    let mut m = HashMap::new();
+    m.insert(13, "Spica".to_string());
+    m.insert(74, "Hoyten".to_string());
+    m
+});
+
+fn main() {
+    println!("ready");
+    std::thread::spawn(|| {
+        println!("{:?}", HASHMAP.get(&13));
+    })
+    .join()
+    .unwrap();
+    println!("{:?}", HASHMAP.get(&74));
+
+    // Prints:
+    //   ready
+    //   initializing
+    //   Some("Spica")
+    //   Some("Hoyten")
+}
