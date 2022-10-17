c534fef55e67e578bd683f9f7e120be25734241b
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-17 13:38:28 +0800

    add drop task example

---
+++ async_cancellation_workspace_example/drop_task_example/src/main.rs
@@ -0,0 +1,17 @@
+use std::time::Duration;
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() {
+    // println!("Hello, world!");
+
+    let handle = tokio::spawn(async {
+        sleep(Duration::from_secs(1)).await;
+        println!("2");
+    });
+
+    println!("1");
+    drop(handle); // Drop the task handle.
+    sleep(Duration::from_secs(2)).await;
+    println!("done");
+}
