6975169400f757003bdf6a14d73a81d7696e052c
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-17 13:49:53 +0800

    abort task example

---
+++ async_cancellation_workspace_example/abort_task_example/src/main.rs
@@ -0,0 +1,16 @@
+use std::time::Duration;
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() {
+    // println!("Hello, world!");
+    let handle = tokio::spawn(async {
+        sleep(Duration::from_secs(1)).await;
+        println!("2");
+    });
+
+    println!("1");
+    handle.abort();
+    sleep(Duration::from_secs(2)).await;
+    println!("done");
+}
