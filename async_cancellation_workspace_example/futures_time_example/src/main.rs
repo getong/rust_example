ef634d159d19ac8fe8a958da39aa3cf2a8070e64
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-17 14:44:05 +0800

    future_time example

---
+++ async_cancellation_workspace_example/futures_time_example/src/main.rs
@@ -0,0 +1,18 @@
+use futures_time::future::IntoFuture;
+//use futures_time::prelude::*;
+use futures_time::{task, time::Duration};
+
+async fn wait_until(timer: impl IntoFuture) {
+    let timer = timer.into_future();
+    timer.await;
+}
+
+#[tokio::main]
+async fn main() {
+    // Wait for a `Duration`.
+    wait_until(Duration::from_secs(1)).await;
+
+    // Wait for a concrete future.
+    let deadline = task::sleep(Duration::from_secs(1));
+    wait_until(deadline).await;
+}
