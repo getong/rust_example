b7b0624d83819c95d1c22ee7872b460a8e3cec16
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-22 16:21:42 +0800

    multi threaded regex example

---
+++ multi_threaded_regex_workspace_example/rayon_regex_example/src/main.rs
@@ -0,0 +1,26 @@
+use lazy_static::lazy_static;
+use rayon::prelude::*;
+use regex::Regex;
+use std::time::Instant;
+
+lazy_static! {
+    static ref IS_INTEGER: Regex = Regex::new("^[0-9]+$").unwrap();
+}
+
+fn main() {
+    let strings: Vec<&str> = ["foo", "bar", "1234", "1234foo", ""]
+        .into_iter()
+        .cycle()
+        .take(100_000_000)
+        .collect();
+
+    let start = Instant::now();
+    // let n_ints = strings.iter().filter(|s| IS_INTEGER.is_match(s)).count();
+
+    let n_ints = strings
+        .par_iter()
+        .filter(|s| IS_INTEGER.is_match(s))
+        .count();
+    let elapsed = start.elapsed().as_secs_f32();
+    println!("{} {}s", n_ints, elapsed);
+}
