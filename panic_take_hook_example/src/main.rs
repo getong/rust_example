0fa7c4502ead49fd889b499716032712f05daa08
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-24 05:52:17 +0800

    panic take_hook example

---
+++ panic_take_hook_example/src/main.rs
@@ -0,0 +1,12 @@
+#![allow(unused)]
+fn main() {
+    use std::panic;
+
+    panic::set_hook(Box::new(|_| {
+        println!("Custom panic hook");
+    }));
+
+    let _ = panic::take_hook();
+
+    panic!("Normal panic");
+}
