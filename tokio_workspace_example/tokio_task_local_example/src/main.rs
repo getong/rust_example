0fca57e7a816d2333f8667706b59c7b4d03ffcec
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-10-29 13:38:32 +0800

    add tokio task local example

---
+++ tokio_workspace_example/tokio_task_local_example/src/main.rs
@@ -0,0 +1,26 @@
+// use tokio::task_local;
+
+tokio::task_local! {
+    static NUMBER: u32;
+}
+
+#[tokio::main]
+async fn main() {
+    NUMBER
+        .scope(1, async move {
+            assert_eq!(NUMBER.get(), 1);
+        })
+        .await;
+
+    NUMBER
+        .scope(2, async move {
+            assert_eq!(NUMBER.get(), 2);
+
+            NUMBER
+                .scope(3, async move {
+                    assert_eq!(NUMBER.get(), 3);
+                })
+                .await;
+        })
+        .await;
+}
