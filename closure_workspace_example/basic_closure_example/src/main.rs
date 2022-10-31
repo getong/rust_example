c4fb1c7905f382d5f7703b3d0ef0d924ad37ad72
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-11-01 00:07:52 +0800

    add code

--- closure_workspace_example/basic_closure_example/src/main.rs
+++ closure_workspace_example/basic_closure_example/src/main.rs
@@ -5,4 +5,18 @@ fn main() {
 
     println!("add result: {}", add(7));
     println!("add result: {}", add(7));
+
+    let mut s = "hello".to_owned();
+    let put_str = |add_str| {
+        s.push_str(add_str);
+        s
+    };
+    println!("{:?}", put_str(" world"));
+    // println!("{:?}", put_str(" world"));
+
+    let mut s = "hello".to_owned();
+    let mut put_str = |add_str| s.push_str(add_str);
+    println!("{:?}", put_str(" world"));
+    println!("{:?}", put_str(" again"));
+    println!("{:?}", s);
 }
