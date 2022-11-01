26ada142660ccd6c9efa30591c62bd3902a2a332
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-11-02 03:36:09 +0800

    add string deref example

---
+++ string_deref_example/src/main.rs
@@ -0,0 +1,10 @@
+use std::ops::Deref;
+fn main() {
+    // println!("Hello, world!");
+    let s: String = "hello".to_string();
+    let s_ref: &str = (&s).deref();
+    println!("s_ref: {:?}", s_ref);
+
+    let s_ref2: &str = &*&s;
+    println!("s_ref2: {:?}", s_ref2);
+}
