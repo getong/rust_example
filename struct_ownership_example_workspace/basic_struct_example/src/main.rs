00f5d878b9b78c43a081373038524df5043cf360
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-11-03 21:37:53 +0800

    clone Option<T> out of struct

--- struct_ownership_example_workspace/basic_struct_example/src/main.rs
+++ struct_ownership_example_workspace/basic_struct_example/src/main.rs
@@ -2,11 +2,16 @@
 pub struct Student {
     name: &'static str,
     score: i32,
+    option: Option<String>,
 }
 
 impl Student {
-    pub fn new(name: &'static str, score: i32) -> Self {
-        Student { name, score }
+    pub fn new(name: &'static str, score: i32, option: Option<String>) -> Self {
+        Student {
+            name,
+            score,
+            option,
+        }
     }
 
     pub fn get_name(&self) -> &str {
@@ -23,11 +28,18 @@ impl Student {
     pub fn set_score(&mut self, score: i32) {
         self.score = score;
     }
+
+    pub fn get_option(&self) -> Option<String> {
+        self.option.as_ref().cloned()
+    }
+    pub fn setoptione(&mut self, option: Option<String>) {
+        self.option = option;
+    }
 }
 
 fn main() {
     // println!("Hello, world!");
-    let mut student: Student = Student::new("zhansan", 59);
+    let mut student: Student = Student::new("zhansan", 59, Some("hello".to_owned()));
     println!(
         "name: {}, score: {}",
         student.get_name(),
@@ -45,4 +57,7 @@ fn main() {
 
     student.set_score(70);
     println!("{:?}", student);
+
+    println!("option: {:?}", student.get_option());
+    println!("option: {:?}", student.get_option());
 }
