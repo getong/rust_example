291fb463c5a614cac6afc885c5e2b4815cf5485c
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-11-11 18:37:23 +0800

    add tokio joinset example

---
+++ tokio_workspace_example/tokio_joinset_example/src/main.rs
@@ -0,0 +1,22 @@
+// copy from https://docs.rs/tokio/latest/tokio/task/struct.JoinSet.html
+
+use tokio::task::JoinSet;
+
+#[tokio::main]
+async fn main() {
+    let mut set = JoinSet::new();
+
+    for i in 0..10 {
+        set.spawn(async move { i });
+    }
+
+    let mut seen = [false; 10];
+    while let Some(res) = set.join_next().await {
+        let idx = res.unwrap();
+        seen[idx] = true;
+    }
+
+    for i in 0..10 {
+        assert!(seen[i]);
+    }
+}
