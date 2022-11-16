f42506833ccd09d272f7717454da271849da225c
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-11-16 17:41:37 +0800

    add more code

--- combinator_unwrap_or_example/src/main.rs
+++ combinator_unwrap_or_example/src/main.rs
@@ -14,4 +14,16 @@ fn find(haystack: &str, needle: char) -> Option<usize> {
 fn main() {
     assert_eq!(extension("foo.rs").unwrap_or("rs"), "rs");
     assert_eq!(extension("foo").unwrap_or("rs"), "rs");
+
+    // unwrap_or_else
+    let k = 10;
+    assert_eq!(Some(4).unwrap_or_else(|| 2 * k), 4);
+    assert_eq!(None.unwrap_or_else(|| 2 * k), 20);
+
+    // unwrap_or_default
+    let x: Option<u32> = None;
+    let y: Option<u32> = Some(12);
+
+    assert_eq!(x.unwrap_or_default(), 0);
+    assert_eq!(y.unwrap_or_default(), 12);
 }
