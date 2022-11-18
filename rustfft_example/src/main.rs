9501f064d771a233785e402f356ffd1dd0556095
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-11-19 01:09:52 +0800

    add rustfft example

---
+++ rustfft_example/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // println!("Hello, world!");
+    // Perform a forward FFT of size 1234
+    use rustfft::{num_complex::Complex, FftPlanner};
+
+    let mut planner = FftPlanner::<f32>::new();
+    let fft = planner.plan_fft_forward(1234);
+
+    let mut buffer = vec![Complex { re: 0.0, im: 0.0 }; 1234];
+
+    fft.process(&mut buffer);
+    println!("buffer: {:?}", buffer);
+}
