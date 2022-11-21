5eb8533d965157b3c6978250412bbce22a5fe810
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-11-21 17:57:46 +0800

    add mlua example

---
+++ mlua_example/src/main.rs
@@ -0,0 +1,16 @@
+use mlua::prelude::*;
+
+fn main() -> LuaResult<()> {
+    let lua = Lua::new();
+
+    let map_table = lua.create_table()?;
+    map_table.set(1, "one")?;
+    map_table.set("two", 2)?;
+
+    lua.globals().set("map_table", map_table)?;
+
+    lua.load("for k,v in pairs(map_table) do print(k,v) end")
+        .exec()?;
+
+    Ok(())
+}
