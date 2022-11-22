1ae09ed8a21d50b4df2c0ae1dca8e4e1795f3d5c
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-11-22 11:06:51 +0800

    mlua workspace example

---
+++ mlua_workspace_example/mlua_lib_example/src/lib.rs
@@ -0,0 +1,28 @@
+use mlua::prelude::*;
+
+fn hello(_: &Lua, name: String) -> LuaResult<()> {
+    println!("hello, {}!", name);
+    Ok(())
+}
+
+#[mlua::lua_module]
+fn my_module(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    exports.set("hello", lua.create_function(hello)?)?;
+    Ok(exports)
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
