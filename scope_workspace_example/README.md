c9a58d15bb4af169a50f29e11561d9b8fdadb938
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-12-02 16:15:15 +0800

    add scope workspace example

---
+++ scope_workspace_example/README.md
@@ -0,0 +1,1 @@
+copy from [Introducing Scoped Threads — The New Addition to Rust 1.63.0](https://betterprogramming.pub/scoped-threads-the-great-addition-of-rust-1-63-0-2a4c1d411c0a)
---
+++ scope_workspace_example/mut_scope_example/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    // println!("Hello, world!");
+
+    let mut a = vec![1, 2, 3];
+    let mut x = 0;
+
+    std::thread::scope(|s| {
+        s.spawn(|| {
+            println!("hello from the first scoped thread");
+            // We can borrow `a` here.
+            dbg!(&a);
+        });
+        s.spawn(|| {
+            println!("hello from the second scoped thread");
+            // We can even mutably borrow `x` here,
+            // because no other threads are using it.
+            x += a[0] + a[2];
+        });
+        println!("hello from the main thread");
+    });
+
+    // After the scope, we can modify and access our variables again:
+    a.push(4);
+    assert_eq!(x, a.len());
+}
