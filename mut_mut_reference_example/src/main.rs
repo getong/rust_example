47e8d335c9a2a4f8e642a25952ce78fa44df098d
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-12-09 11:41:32 +0800

    add code

--- mut_mut_reference_example/src/main.rs
+++ mut_mut_reference_example/src/main.rs
@@ -2,7 +2,12 @@ fn main() {
     // println!("Hello, world!");
     let mut a: i32 = 10;
     let mut b: &mut i32 = &mut a;
-    let ccd : &mut &mut i32 = &mut b;
+    let c: &mut &mut i32 = &mut b;
     **c += 10;
     println!("a:  {}", a);
+
+    let mut d = &mut 10;
+    let e = &mut d;
+    **e += 10;
+    println!("d:  {}", *d);
 }
