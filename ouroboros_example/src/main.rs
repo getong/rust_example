8332090aea7d72ef8da07d04901ded44603c6a91
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-12-09 22:51:18 +0800

    add ouroboros example

---
+++ ouroboros_example/src/main.rs
@@ -0,0 +1,34 @@
+use ouroboros::self_referencing;
+use std::fs::File;
+use std::io;
+use std::io::Read;
+use zip::read::ZipFile;
+use zip::ZipArchive;
+
+#[self_referencing]
+struct ZipStreamer {
+    archive: ZipArchive<File>,
+    #[borrows(mut archive)]
+    #[not_covariant]
+    reader: ZipFile<'this>,
+}
+
+impl Read for ZipStreamer {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.with_reader_mut(|reader| reader.read(buf))
+    }
+}
+
+pub fn zip_streamer(file_name: &str, member_name: &str) -> impl std::io::Read {
+    let file = File::open(file_name).unwrap();
+    let archive = ZipArchive::new(file).unwrap();
+    ZipStreamerBuilder {
+        archive,
+        reader_builder: |archive| archive.by_name(member_name).unwrap(),
+    }
+    .build()
+}
+
+fn main() {
+    println!("Hello, world!");
+}
