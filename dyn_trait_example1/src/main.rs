ef76322c2124724e960f9b618dea99a01f0c8255
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-12-12 15:43:45 +0800

    update code

--- dyn_trait_example1/src/main.rs
+++ dyn_trait_example1/src/main.rs
@@ -24,4 +24,14 @@ fn main() {
     p.walk();
     // p.talk();  // can not run this function like this way
     <dyn Animal>::talk();
+
+    let list: Vec<&dyn Animal> = vec![&Person, &Person];
+    for i in &list {
+        i.walk();
+    }
+
+    let list2: Vec<Box<dyn Animal>> = vec![Box::new(Person), Box::new(Person)];
+    for i in &list2 {
+        i.walk();
+    }
 }
