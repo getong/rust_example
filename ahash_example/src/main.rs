2dcb3d7dae0c44c0a3612b1d22d82d529a347ebe
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-12-25 11:51:07 +0800

    add ahash example

---
+++ ahash_example/src/main.rs
@@ -0,0 +1,9 @@
+use ahash::AHashMap;
+
+fn main() {
+    // println!("Hello, world!");
+    let mut map: AHashMap<i32, i32> = AHashMap::new();
+    map.insert(12, 34);
+    map.insert(56, 78);
+    println!("map: {:?}", map);
+}
