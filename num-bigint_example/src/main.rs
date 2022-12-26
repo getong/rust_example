8a0143ceeb4e4c38f84010710992b2d22f352d43
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-12-26 23:18:37 +0800

    add num_bigint example

---
+++ num-bigint_example/src/main.rs
@@ -0,0 +1,33 @@
+use num_bigint::BigUint;
+use num_traits::{Zero, One};
+use std::mem::replace;
+use num_bigint::{ToBigInt, RandBigInt};
+
+// Calculate large fibonacci numbers.
+fn fib(n: usize) -> BigUint {
+    let mut f0: BigUint = Zero::zero();
+    let mut f1: BigUint = One::one();
+    for _ in 0..n {
+        let f2 = f0 + &f1;
+        // This is a low cost way of swapping f0 with f1 and f1 with f2.
+        f0 = replace(&mut f1, f2);
+    }
+    f0
+}
+
+
+fn main() {
+    // println!("Hello, world!");
+    // This is a very large number.
+    println!("fib(1000) = {}", fib(1000));
+    
+    let mut rng = rand::thread_rng();
+    let a = rng.gen_bigint(1000);
+
+    let low = -10000.to_bigint().unwrap();
+    let high = 10000.to_bigint().unwrap();
+    let b = rng.gen_bigint_range(&low, &high);
+
+    // Probably an even larger number.
+    println!("{}", a * b);
+}
