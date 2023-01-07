dc257bb4b16ef1a43feb40813ab5eb1107e1f1de
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-01-07 21:09:01 +0800

    update code

--- calamine_example/src/main.rs
+++ calamine_example/src/main.rs
@@ -1,5 +1,14 @@
 use calamine::{open_workbook, DataType, Reader, Xlsx};
 
+use std::path::PathBuf;
+
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Read, Write},
+};
+
+use walkdir::WalkDir;
+
 const PATH: &str = "a.xlsx";
 const SHEET_NAME: &str = "client_translate";
 const COMMENT_PREFIX: &str = "//";
@@ -11,23 +20,30 @@ const DEST_DIR: &str = "/Users/gerald/other_project/frontend/src";
 fn main() {
     let mut excel: Xlsx<_> = open_workbook(PATH).unwrap();
     if let Some(Ok(r)) = excel.worksheet_range(SHEET_NAME) {
+        let mut odd_num = 0;
+        let mut whole_line: String = "".to_string();
+        let mut chinese_str: String = "".to_string();
+        let mut english_str: String = "".to_string();
         for row in r.rows().rev() {
             // read file every two lines
-            let mut odd_num = 0;
+            println!("row:{:?}", row);
 
-            let mut whole_line: String = "".to_string();
-            let mut chinese_str: String = "".to_string();
-            let mut english_str: String = "".to_string();
             odd_num = (odd_num + 1) % 2;
+            println!("line:{}, row:{:?}, odd_num:{:?}", line!(), row, odd_num);
             match odd_num {
                 0 => match row.get(1) {
                     Some(DataType::String(filename_and_line)) => {
+                        println!("line:sssssss, {}", line!());
                         let mut split = filename_and_line.split(": ");
                         if let Some(match_filename) = split.next() {
                             if let Some(line_num_str) = split.next() {
                                 // get the match filename and the line_number
                                 match line_num_str.parse::<i32>() {
                                     Ok(line_num) => {
+                                        println!(
+                                            "filename: {}, line_num:{}",
+                                            match_filename, line_num
+                                        );
                                         // get the match filename and the line name
                                         change_file_with_translate_words(
                                             match_filename,
@@ -36,26 +52,40 @@ fn main() {
                                             &chinese_str,
                                             &english_str,
                                         );
-                                        println!(
-                                            "filename: {}, line_num:{}",
-                                            match_filename, line_num
-                                        )
                                     }
-                                    _ => println!("not match"),
+                                    other => {
+                                        println!("line:sssssss, {} , other:{:?}", line!(), other)
+                                    }
                                 }
+                            } else {
+                                println!("not match");
                             }
                         }
                     }
-                    _ => {
+                    other => {
                         // not filename and line_num_str
-                        println!("line0, not found");
+                        println!("other{:?}, not found", other);
                     }
                 },
 
                 _ => {
+                    println!("line:sssssss, {}", line!());
+                    println!(
+                        "row:{:?}, 1 {:?},2 {:?}, 3 {:?}",
+                        row,
+                        row.get(1),
+                        row.get(2),
+                        row.get(3)
+                    );
                     if let Some(DataType::String(origin_whole_line)) = row.get(1) {
                         if let Some(DataType::String(origin_chinese_str)) = row.get(2) {
                             if let Some(DataType::String(origin_english_str)) = row.get(3) {
+                                println!(
+                                    "origin_whole_line:{:?},
+origin_chinese_str:{:?},
+origin_english_str:{:?}",
+                                    origin_whole_line, origin_chinese_str, origin_english_str
+                                );
                                 whole_line = origin_whole_line.to_string();
                                 chinese_str = origin_chinese_str.to_string();
                                 english_str = origin_english_str.to_string();
@@ -75,9 +105,80 @@ fn change_file_with_translate_words(
     chinese_str: &str,
     english_str: &str,
 ) {
-    println!(
-        "match_filename: {:?}, line_num:{:?}
-, whole_line: {:?}, chinese_str: {:?},  english_str:{:?}",
-        match_filename, line_num, whole_line, chinese_str, english_str
-    );
+    //println!(
+    //    "match_filename: {:?}, line_num:{:?}
+    //, whole_line: {:?}, chinese_str: {:?},  english_str:{:?}",
+    //    match_filename, line_num, whole_line, chinese_str, english_str
+    //);
+
+    let mut chinese_str_split = chinese_str.split_whitespace();
+    let replace_whole_line: String = match chinese_str_split.clone().count() {
+        1 => {
+            COMMENT_PREFIX.to_string()
+                + whole_line
+                + &str::replace(whole_line, chinese_str, english_str)
+        }
+        chinese_count => {
+            let mut english_str_split = english_str.split_whitespace();
+            let mut temp_whole_line = whole_line.to_string();
+            if english_str_split.clone().count() == chinese_count {
+                while let Some(chinese_word) = chinese_str_split.next() {
+                    if let Some(english_word) = english_str_split.next() {
+                        temp_whole_line =
+                            str::replace(&temp_whole_line, chinese_word, english_word);
+                    }
+                }
+                COMMENT_PREFIX.to_string() + whole_line + &temp_whole_line
+            } else {
+                println!(
+                    "not_match, match_filename:{}, line_num:{}, whole_line:{},
+chinese_str:{}, english_str:{}",
+                    match_filename, line_num, whole_line, chinese_str, english_str,
+                );
+                // not found
+                "".to_string()
+            }
+        }
+    };
+    if replace_whole_line != "".to_string() {
+        match find_file(match_filename) {
+            None => println!("No rusv file was found."),
+            Some(filepath) => {
+                // println!("Rusv file was found: {:?}", filepath);
+                if let Ok(file) = File::open(filepath) {
+                    read_write_line(file, line_num, replace_whole_line);
+                }
+            }
+        }
+    }
+}
+
+fn read_write_line(file: File, line_num: i32, replace_whole_line: String) {
+    let mut reader = BufReader::new(file);
+    let lines = reader.by_ref().lines();
+    let mut new_lines: Vec<String> = vec![];
+    for (k, v) in lines.enumerate() {
+        if k as i32 == line_num {
+            new_lines.push(replace_whole_line.clone());
+        } else {
+            new_lines.push(v.unwrap());
+        }
+    }
+    let mut out = reader.into_inner();
+    for i in new_lines {
+        out.write_all(i.as_bytes()).unwrap();
+    }
+}
+
+fn find_file(filename: &str) -> Option<PathBuf> {
+    for entry in WalkDir::new(DEST_DIR)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        if entry.file_name().to_string_lossy().ends_with(filename) {
+            return Some(entry.path().to_path_buf());
+        }
+    }
+    return None;
 }
