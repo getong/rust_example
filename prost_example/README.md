332ef077d952151c38c9cb13a874bdcefbd3627c
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-11 18:54:24 +0800

    add proto example

---
+++ prost_example/README.md
@@ -0,0 +1,4 @@
+# rust prost example
+
+copy from [Protocol Buffers in Rust – what to use and how to use it?](https://users.rust-lang.org/t/protocol-buffers-in-rust-what-to-use-and-how-to-use-it/88647)
+copy from [Function glob::glob](https://docs.rs/glob/latest/glob/fn.glob.html)
---
+++ prost_example/src/main.rs
@@ -0,0 +1,23 @@
+const PROTO_DIR: &str = "protobuf";
+
+fn main() -> std::io::Result<()> {
+    let mut protobuf_out = std::path::PathBuf::new();
+    // protobuf_out.push(&std::env::var("OUT_DIR").unwrap());
+    // proto_path
+    protobuf_out.push(&"./src/protobuf");
+    std::fs::create_dir(&protobuf_out).ok();
+
+    prost_build::Config::new()
+        .out_dir(&protobuf_out)
+        .default_package_filename("mod")
+        .compile_protos(
+            &glob::glob(&(PROTO_DIR.to_string() + "/*.proto"))
+                .unwrap()
+                .filter_map(|entry| entry.ok())
+                .map(|entry| entry.display().to_string())
+                .collect::<Vec<_>>(),
+            &[PROTO_DIR],
+        )?;
+
+    Ok(())
+}
