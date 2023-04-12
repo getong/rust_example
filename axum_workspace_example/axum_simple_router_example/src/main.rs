9fb9dc1ddc8fdae9caaeb2113c2b86e4c63c12b9
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-12 21:51:36 +0800

    add axum simple router example

---
+++ axum_workspace_example/axum_simple_router_example/src/main.rs
@@ -0,0 +1,18 @@
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
