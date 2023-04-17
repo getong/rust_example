ccdee043fde901842c5753d5549a548754585664
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-17 21:10:10 +0800

    add axum_stream_protobuf example

---
+++ axum_workspace_example/axum_stream_protobuf_example/src/main.rs
@@ -0,0 +1,45 @@
+use axum::response::IntoResponse;
+use axum::routing::*;
+use axum::Router;
+use std::net::SocketAddr;
+
+use futures::prelude::*;
+use tokio_stream::StreamExt;
+
+use axum_streams::*;
+
+#[derive(Clone, prost::Message)]
+struct MyTestStructure {
+    #[prost(string, tag = "1")]
+    some_test_field: String,
+}
+
+fn source_test_stream() -> impl Stream<Item = MyTestStructure> {
+    // Simulating a stream with a plain vector and throttling to show how it works
+    stream::iter(vec![
+        MyTestStructure {
+            some_test_field: "test1".to_string()
+        };
+        1
+    ])
+    .throttle(std::time::Duration::from_millis(50))
+}
+
+async fn test_protobuf_stream() -> impl IntoResponse {
+    StreamBodyAs::protobuf(source_test_stream())
+}
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/protobuf-stream", get(test_protobuf_stream));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
