a77365cd55c8fee64966c649cfb7f7f33aae64a1
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-18 15:47:19 +0800

    add axum prost server example

---
+++ axum_workspace_example/axum_prost_server_example/src/main.rs
@@ -0,0 +1,32 @@
+ use axum::{
+     Extension,
+    routing::{get, post},
+    Router,
+ };
+
+use axum_extra::protobuf::Protobuf;
+use myapp::Todo;
+use std::sync::Arc;
+
+pub mod myapp {
+    include!("myapp.rs");
+}
+
+async fn create_todo_handler(
+    Extension(todo): Extension<Arc<Todo>>,
+    Protobuf(todo_new): Protobuf<Todo>,
+) -> String {
+    println!("Received todo: {:?}", todo_new);
+    format!("Created new todo with id {}: {}", todo.id, todo_new.title)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/todos", post(create_todo_handler))
+        .route("/todos", get(create_todo_handler));
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
---
+++ axum_workspace_example/axum_prost_server_example/src/myapp.rs
@@ -0,0 +1,10 @@
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Todo {
+    #[prost(uint32, tag = "1")]
+    pub id: u32,
+    #[prost(string, tag = "2")]
+    pub title: ::prost::alloc::string::String,
+    #[prost(bool, tag = "3")]
+    pub completed: bool,
+}
