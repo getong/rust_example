3a8af9cbd6a49f9c4d5195583ec88cb798f4eaed
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-18 14:23:45 +0800

    add axum post example

---
+++ axum_workspace_example/axum_post_example/src/main.rs
@@ -0,0 +1,25 @@
+use axum::{extract::Json, routing::post, Router};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+    id: u32,
+    title: String,
+    completed: bool,
+}
+
+// curl -X POST -H "Content-Type: application/json" -d '{"id":1, "title":"test todo", "completed":false}' http://127.0.0.1:3000/todos
+async fn create_todo_handler(Json(todo): Json<Todo>) -> String {
+    println!("Created new todo: {:?}", todo);
+    format!("Created new todo: {:?}", todo)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/todos", post(create_todo_handler));
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
