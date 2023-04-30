0368a10e3819f877c067184cde7f240f545ac049
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-05-01 01:29:36 +0800

    add another example

--- partial_move_example/src/main.rs
+++ partial_move_example/src/main.rs
@@ -22,6 +22,34 @@ fn main() {
 
     // `person` cannot be used but `person.age` can be used as it is not moved
     println!("The person's age from person struct is {}", person.age);
+
+    let mut state = State { a:0, b:1, result_add:2, result_subtract:3 };
+    println!("before state is {:?}", state);
+    {
+        do_calc(&mut state.a, &mut state.b);
+        finish_calc(&mut state);
+    }
+    println!("after state is {:?}", state);
 }
 
 // copy from https://doc.rust-lang.org/rust-by-example/scope/move/partial_move.html
+
+#[derive(Debug)]
+struct State {
+    a: i32,
+    b: i32,
+    result_add: i32,
+    result_subtract: i32
+}
+
+
+fn do_calc(var1: &mut i32, var2: &mut i32) {
+    *var1 = 4;
+    *var2 = 3;
+}
+fn finish_calc(state: &mut State){
+    state.result_add = state.a + state.b;
+    state.result_subtract = state.a - state.b;
+}
+
+// copy from https://stackoverflow.com/questions/62231909/borrowing-mutable-struct-with-fields
