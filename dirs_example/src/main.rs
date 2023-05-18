5c1d456afd18b17c47fe49e9e0c1b092ce09c281
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-05-19 03:24:07 +0800

    add dirs example

---
+++ dirs_example/src/main.rs
@@ -0,0 +1,16 @@
+use std::path::PathBuf;
+use dirs::home_dir;
+
+fn main() {
+    let path_with_tilde = "~/.zshrc";
+
+    let mut absolute_path = PathBuf::new();
+    if let Some(home) = home_dir() {
+        absolute_path.push(home);
+        absolute_path.push(&path_with_tilde[2..]); // Skip the tilde (~) and slash (/)
+    } else {
+        panic!("Failed to get the home directory.");
+    }
+
+    println!("Absolute path: {:?}", absolute_path);
+}
