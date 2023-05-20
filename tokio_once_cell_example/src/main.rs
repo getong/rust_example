c623cac646ed08d1c3a0f764e2d9f62a22235970
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-05-20 23:55:08 +0800

    add code example

--- tokio_once_cell_example/src/main.rs
+++ tokio_once_cell_example/src/main.rs
@@ -6,8 +6,15 @@ async fn some_computation() -> u32 {
 
 static ONCE: OnceCell<u32> = OnceCell::const_new();
 
+async fn get_global_integer() -> &'static u32 {
+    ONCE.get_or_init(|| async { 1 + 1 }).await
+}
+
 #[tokio::main]
 async fn main() {
     let result = ONCE.get_or_init(some_computation).await;
+    assert_eq!(*result, 2);
+
+    let result = get_global_integer().await;
     assert_eq!(*result, 2);
 }
