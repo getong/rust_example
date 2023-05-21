50efd5925f14a6a39dcc9af660009877c6152886
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-05-22 02:17:58 +0800

    update two directory

--- remove_duplicate_directory_example/src/main.rs
+++ remove_duplicate_directory_example/src/main.rs
@@ -1,56 +1,83 @@
-use error_chain::error_chain;
+// use error_chain::error_chain;
 use std::fs;
+use std::io::Error;
 use std::path::Path;
 use std::path::PathBuf;
 
+// when TWO_DIRECTORY_FLAG is true
+// mkdir -p /tmp/a/b/c/d
+// touch /tmp/a/b/c/d/file.txt
+
+// when TWO_DIRECTORY_FLAG is false
+// mkdir -p /tmp/a/b/c
+// touch /tmp/a/b/c/file.txt
+const TWO_DIRECTORY_FLAG: bool = true;
 // change the directory you want to modify
-const FILE_PATH: &[u8] = b"~/rust/";
+const FILE_PATH: &[u8] = b"/tmp/a/";
 
-error_chain! {
-    foreign_links {
-        Io(std::io::Error);
-        SystemTimeError(std::time::SystemTimeError);
-    }
-}
+// error_chain! {
+//     foreign_links {
+//         Io(std::io::Error);
+//         SystemTimeError(std::time::SystemTimeError);
+//     }
+// }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Error> {
     let dir_name =
         String::from_utf8(tilde_expand::tilde_expand(FILE_PATH)).expect("Invalid UTF-8 sequence");
 
     let current_dir = PathBuf::from(dir_name);
 
     for entry in fs::read_dir(current_dir)? {
-        let entry = entry?;
-        let path = entry.path();
+        let first_entry = entry?;
+        let first_path = first_entry.path();
 
-        let metadata = fs::metadata(&path)?;
+        let first_metadata = fs::metadata(&first_path)?;
 
-        if metadata.is_dir() {
+        if first_metadata.is_dir() {
             // println!("path : {:?}", path);
-            let sub_paths = fs::read_dir(path.clone())?;
+            if TWO_DIRECTORY_FLAG {
+                for second_entry in fs::read_dir(first_path)? {
+                    let second_entry = second_entry?;
+                    let second_path = second_entry.path();
 
-            let mut sub_dir_count = 0;
-            let mut sub_dir_name: PathBuf = PathBuf::new();
-            for sub_entry in sub_paths {
-                let sub_entry = sub_entry?;
-                let sub_path = sub_entry.path();
-                let sub_metadata = fs::metadata(&sub_path)?;
-                if sub_metadata.is_dir() {
-                    sub_dir_count += 1;
-                    sub_dir_name = sub_path.clone();
+                    let second_metadata = fs::metadata(&second_path)?;
+
+                    if second_metadata.is_dir() {
+                        _ = check_path(second_path);
+                    }
                 }
-            }
-            if sub_dir_count == 1 {
-                // println!(
-                //     "path {} has only one  dirs, {}",
-                //     sub_path.display(),
-                //     sub_dir_name.display()
-                // );
-                _ = move_directory(&sub_dir_name, &path);
+            } else {
+                _ = check_path(first_path);
             }
         }
     }
 
+    Ok(())
+}
+
+fn check_path(first_path: PathBuf) -> Result<(), Error> {
+    let sub_paths = fs::read_dir(first_path.clone())?;
+
+    let mut sub_dir_count = 0;
+    let mut sub_dir_name: PathBuf = PathBuf::new();
+    for sub_entry in sub_paths {
+        let sub_entry = sub_entry?;
+        let sub_path = sub_entry.path();
+        let sub_metadata = fs::metadata(&sub_path)?;
+        if sub_metadata.is_dir() {
+            sub_dir_count += 1;
+            sub_dir_name = sub_path.clone();
+        }
+    }
+    if sub_dir_count == 1 {
+        // println!(
+        //     "path {} has only one  dirs, {}",
+        //     sub_path.display(),
+        //     sub_dir_name.display()
+        // );
+        move_directory(&sub_dir_name, &first_path)?
+    }
     Ok(())
 }
 
