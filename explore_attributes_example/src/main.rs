59fbe239bb019b26748c303bfde09b439dc0938f
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-05-31 00:54:25 +0800

    add code

--- explore_attributes_example/src/main.rs
+++ explore_attributes_example/src/main.rs
@@ -3,6 +3,11 @@ fn are_you_on_linux() {
     println!("You're running linux!");
 }
 
+#[cfg(target_os = "macos")]
+fn are_you_on_linux() {
+    println!("You're running macos!");
+}
+
 #[cfg_attr(feature = "debug-mode", derive(Debug))]
 struct Test {
     value: i32,
@@ -15,10 +20,9 @@ fn add(x: i32, y: i32) -> i32 {
 
 fn main() {
     // println!("Hello, world!");
-    // are_you_on_linux();
+    are_you_on_linux();
 
     println!("sum : {}", add(2, 3));
 }
-
 
 // copy from https://medium.com/@luishrsoares/exploring-rust-attributes-in-depth-ac172993d568
