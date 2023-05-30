4aea631a73fab375049ec84ede37e3ce6618fb74
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-05-30 23:33:10 +0800

    update rayon code

--- rayon_example/src/main.rs
+++ rayon_example/src/main.rs
@@ -1,4 +1,6 @@
+use rayon::join;
 use rayon::prelude::*;
+
 fn main() {
     // println!("Hello, world!");
     let mut my_vec = vec![0; 200_000];
@@ -7,4 +9,14 @@ fn main() {
         .enumerate()
         .for_each(|(index, number)| *number += index + 1); // add par_ to iter_mut
     println!("{:?}", &my_vec[5000..5005]);
+
+    // Calculate the sum of the first 100 integers in parallel
+    let sum: i32 = (1..=100).into_par_iter().sum();
+    println!("{}", sum);
+
+    // Calculate the sum of the first 50 integers in one
+    // thread and the sum of the next 50 integers in another thread.
+    let (sum1, sum2) = join(|| (1..=50).sum::<i32>(), || (51..=100).sum::<i32>());
+    let sum = sum1 + sum2;
+    println!("{}", sum);
 }
