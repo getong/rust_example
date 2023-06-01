86f71f3da4e303d6ce3c1a15c2dd340b2bb2a3ef
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-02 00:01:46 +0800

    pub static variable example

---
+++ pub_static_variable_example/src/main.rs
@@ -0,0 +1,17 @@
+pub static STATIC_STR: &str = "testing";
+
+mod my_mod {
+    use crate::STATIC_STR;
+
+    pub fn test_fn() {
+        println!("Here it is: {}", STATIC_STR);
+    }
+}
+
+fn main() {
+    my_mod::test_fn();
+
+    println!("Here it is: {}", STATIC_STR);
+}
+
+// copy from https://www.reddit.com/r/rust/comments/a4lnxg/how_to_access_a_mainrs_static_variable_from_a/
