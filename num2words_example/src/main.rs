62b005c6d539d4619e5b8f613937c552e71a8559
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-02 20:52:50 +0800

    add num2words example

---
+++ num2words_example/src/main.rs
@@ -0,0 +1,17 @@
+use num2words::*;
+
+fn main() {
+    // println!("Hello, world!");
+    assert_eq!(
+        Num2Words::new(42).lang(Lang::English).to_words(),
+        Ok(String::from("forty-two"))
+    );
+    assert_eq!(
+        Num2Words::new(42).ordinal().to_words(),
+        Ok(String::from("forty-second"))
+    );
+    assert_eq!(
+        Num2Words::new(42.01).currency(Currency::DOLLAR).to_words(),
+        Ok(String::from("forty-two dollars and one cent"))
+    );
+}
