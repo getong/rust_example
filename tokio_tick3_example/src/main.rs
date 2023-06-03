a538b2ca38b33a4bb6f8e51c9d03a57f2793abfb
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-03 21:25:40 +0800

    add rand module

--- tokio_tick3_example/src/main.rs
+++ tokio_tick3_example/src/main.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, Local};
+use rand::Rng;
 use std::time::Duration;
 use tokio::time;
 
@@ -6,7 +8,7 @@ async fn main() -> Result<(), std::io::Error> {
     let (send, recv) = tokio::sync::mpsc::channel(100);
 
     let shutdown_trigger = tokio::spawn(async move {
-        tokio::time::sleep(Duration::from_millis(10)).await;
+        tokio::time::sleep(Duration::from_secs(15)).await;
 
         // We get the expected behavior by commenting out this line.
         send.send(()).await.unwrap();
@@ -20,11 +22,23 @@ async fn main() -> Result<(), std::io::Error> {
 }
 
 async fn run_with_shutdown(mut shutdown: tokio::sync::mpsc::Receiver<()>) {
-    let mut interval = time::interval(time::Duration::from_millis(10));
+    let mut interval = time::interval(Duration::from_secs(1));
+
+    let mut rng = rand::thread_rng();
 
     loop {
         tokio::select! {
-           _ = interval.tick() => println!("tick"),
+            _ = interval.tick() => {
+                let current_time: DateTime<Local> = Local::now();
+                // Format the date and time as a string
+                let current_time_str = current_time.format("%Y-%m-%d %H:%M:%S").to_string();
+
+                println!("tick, Current Time: {}", current_time_str);
+                // Generate a random integer within a specific range
+                let random_number = rng.gen_range(1..=4);
+                interval = time::interval(Duration::from_secs(random_number));
+                interval.reset();
+            },
            _ = shutdown.recv() => {
                 println!("shutting down");
                 return
