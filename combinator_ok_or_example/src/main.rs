4b45e9f9181c6533062cc49ecea0dec08beb09cf
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-06 18:37:06 +0800

    update code

--- combinator_ok_or_example/src/main.rs
+++ combinator_ok_or_example/src/main.rs
@@ -1,4 +1,24 @@
-fn main() {
+use std::collections::HashMap;
+
+fn lookup(map: &HashMap<String, i32>, key: &str) -> Result<i32, String> {
+    map.get(key)
+        .copied()
+        .ok_or(format!("key {} not found", key))
+}
+
+fn lookup_eager(map: &HashMap<i32, i32>, key: i32) -> Result<i32, String> {
+    map.get(&key)
+        .copied()
+        .ok_or(format!("key {} not found", key))
+}
+
+fn lookup_lazy(map: &HashMap<i32, i32>, key: i32) -> Result<i32, String> {
+    map.get(&key)
+        .copied()
+        .ok_or_else(|| format!("key {} not found", key))
+}
+
+fn main() -> Result<(), String> {
     // println!("Hello, world!");
     const ERR_DEFAULT: &str = "error message";
 
@@ -10,4 +30,32 @@ fn main() {
 
     assert_eq!(s.ok_or(ERR_DEFAULT), o); // Some(T) -> Ok(T)
     assert_eq!(n.ok_or(ERR_DEFAULT), e); // None -> Err(default)
+
+    let mut map = HashMap::new();
+    map.insert("x".to_string(), 1);
+    assert_eq!(lookup(&map, "x"), Ok(1));
+    assert_eq!(lookup(&map, "y"), Err("key y not found".to_string()));
+
+    let n = 10000000;
+    let mut map = HashMap::new();
+    for key in 0..n {
+        map.insert(key, key);
+    }
+
+    let mut sum = 0;
+    for key in 0..n {
+        sum += lookup_eager(&map, key)? as i64;
+        // sum += lookup_lazy(&map, key)? as i64;
+    }
+
+    println!("sum: {}", sum);
+
+    let mut sum = 0;
+    for key in 0..n {
+        sum += lookup_lazy(&map, key)? as i64;
+        // sum += lookup_lazy(&map, key)? as i64;
+    }
+
+    println!("sum: {}", sum);
+    Ok(())
 }
