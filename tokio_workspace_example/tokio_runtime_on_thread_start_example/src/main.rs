0caf9fa80885eee25cd7f129732c8d5572a8dda3
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-07 19:51:44 +0800

    add tokio runtime on threa start example

---
+++ tokio_workspace_example/tokio_runtime_on_thread_start_example/src/main.rs
@@ -0,0 +1,34 @@
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+use std::sync::Arc;
+use tokio::runtime::Builder;
+use tokio::sync::Barrier;
+
+fn main() {
+    // println!("Hello, world!");
+    let once = AtomicBool::new(true);
+    let barrier = Arc::new(Barrier::new(2));
+
+    let _runtime = Builder::new_multi_thread()
+        .on_thread_start(|| {
+            println!("thread started");
+        })
+        .on_thread_stop(|| {
+            println!("thread stopping");
+        })
+        .on_thread_park({
+            let barrier = barrier.clone();
+            move || {
+                let barrier = barrier.clone();
+                if once.swap(false, Ordering::Relaxed) {
+                    tokio::spawn(async move {
+                        barrier.wait().await;
+                    });
+                }
+            }
+        })
+        .on_thread_unpark(|| {
+            println!("thread unparking");
+        })
+        .build();
+}
