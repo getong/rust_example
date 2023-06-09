18a2df2bcbb94427d85162d8dd430852d0acd895
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-10 05:08:04 +0800

    update code

--- struct_mutates_example/src/main.rs
+++ struct_mutates_example/src/main.rs
@@ -3,25 +3,26 @@ use petgraph::Graph;
 use std::collections::HashMap;
 
 #[derive(Debug)]
-pub struct HaGraph<'a> {
-    pub graph: Graph<&'a str, ()>,
+pub struct HaGraph {
+    pub graph: Graph<String, ()>,
     names: Vec<String>,
-    pub nodes: HashMap<&'a str, NodeIndex>,
+    pub nodes: HashMap<String, NodeIndex>,
 }
 
-impl<'a> HaGraph<'a> {
+impl HaGraph {
     fn new() -> Self {
         HaGraph {
-            graph: Graph::<&'a str, ()>::new(),
-            names: vec![],
+            graph: Graph::<String, ()>::new(),
+            names: Vec::new(),
             nodes: HashMap::new(),
         }
     }
-    fn has_node(&self, str: &str) -> bool {
-        self.nodes.contains_key(str)
+
+    pub fn has_node(&self, name: &str) -> bool {
+        self.nodes.contains_key(name)
     }
 
-    pub fn add_edge<'b>(&mut self, from: &'b str, to: &'b str) -> Result<(), &'b str> {
+    pub fn add_edge<'a>(&'a mut self, from: &'a str, to: &'a str) -> Result<(), &str> {
         let from_node = self.nodes.get(from).ok_or(from)?;
         let to_node = self.nodes.get(to).ok_or(to)?;
         self.graph.add_edge(*from_node, *to_node, ());
@@ -29,13 +30,12 @@ impl<'a> HaGraph<'a> {
     }
 
     pub fn load_names(&mut self, names: &[&str]) {
-        for name in names {
-            self.names.push(name.to_string());
+        for &name in names {
+            let name_string = name.to_string();
+            self.names.push(name_string.clone());
+            let idx = self.graph.add_node(name_string.clone());
+            self.nodes.insert(name_string, idx);
         }
-        self.names.iter().for_each(|name| {
-            let idx = self.graph.add_node(name);
-            self.nodes.insert(name, idx);
-        });
     }
 
     pub fn get_node(&self, name: &str) -> Option<&NodeIndex> {
@@ -51,7 +51,7 @@ fn main() {
         "amzapi.missingean-asin.solvepack_q3",
     ];
     let mut hagraph = HaGraph::new();
-    hagraph.load_names(ttypes.as_slice());
+    hagraph.load_names(&ttypes);
     println!("graph: {:?}", hagraph.graph);
     let transitions = vec![
         (
@@ -72,7 +72,7 @@ fn main() {
         ),
     ];
     for (from, to) in transitions {
-        hagraph.add_edge(from, to);
+        _ = hagraph.add_edge(from, to);
     }
     println!("graph: {:?}", hagraph.graph);
 }
