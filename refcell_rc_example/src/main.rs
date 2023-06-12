2af9df231d7aa16255c2c7df067afeeb0c0b058b
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-13 06:16:15 +0800

    add refcell<Rc<T>> example

---
+++ refcell_rc_example/src/main.rs
@@ -0,0 +1,26 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, Clone)]
+struct Data {
+    value: i32,
+}
+
+fn main() {
+    // Create a RefCell<Rc<T>> holding the data
+    let data = RefCell::new(Rc::new(Data { value: 42 }));
+    {
+        // Clone the Rc<T> and update the value
+        let mut borrowed_data = data.borrow_mut();
+
+        let mut_data = Rc::make_mut(&mut borrowed_data);
+        mut_data.value = 46;
+    }
+
+    // Access the data and print its value
+    let borrowed_data = data.borrow();
+    let x_ptr = Rc::into_raw(borrowed_data.into());
+    unsafe {
+        println!("Value: {:?}", (*x_ptr).value);
+    }
+}
