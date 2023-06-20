83ae7a884486f7f0e56ff163bcd9240d9d7c191f
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-20 11:27:18 +0800

    add tokio::io::stdin example

---
+++ tokio_workspace_example/tokio_stdin_example2/src/main.rs
@@ -0,0 +1,31 @@
+use tokio::io::{AsyncBufReadExt, BufReader};
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let mut reader = BufReader::new(stdin);
+
+    println!("Type 'quit' to exit.");
+
+    loop {
+        let mut line = String::new();
+
+        match reader.read_line(&mut line).await {
+            Ok(0) => break, // End of input
+            Ok(_) => {
+                let input = line.trim();
+                if input == "quit" {
+                    break;
+                }
+
+                println!("Input: {}", input);
+            }
+            Err(err) => {
+                eprintln!("Failed to read input: {}", err);
+                break;
+            }
+        }
+    }
+
+    println!("Goodbye!");
+}
