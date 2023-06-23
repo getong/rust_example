ec39b76936be2b9e810aa0aae17b3d64c67e7a4a
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-23 13:57:31 +0800

    update code

--- prost_workspace_example/prost_route_msg_example/src/main.rs
+++ prost_workspace_example/prost_route_msg_example/src/main.rs
@@ -16,42 +16,55 @@ pub fn handle_other_message(message: mypackage::OtherMessage) {
 }
 
 fn route_message(data: Bytes) {
-    let message = match prost::Message::decode(data.clone()) {
-        Ok(message) => message,
-        Err(err) => {
-            eprintln!("Error decoding message: {}", err);
-            return;
-        }
-    };
-
-    match message {
-        mypackage::MyMessage { .. } => {
-            let my_message = match mypackage::MyMessage::decode(data) {
-                Ok(my_message) => my_message,
-                Err(err) => {
-                    eprintln!("Error decoding MyMessage: {}", err);
-                    return;
-                }
-            };
-            handle_message(my_message);
-        },
-        // mypackage::OtherMessage { .. } => {
-        //     let other_message = match mypackage::OtherMessage::decode(data) {
-        //         Ok(other_message) => other_message,
-        //         Err(err) => {
-        //             eprintln!("Error decoding OtherMessage: {}", err);
-        //             return;
-        //         }
-        //     };
-        //     handle_other_message(other_message);
-        // }
+    if let Ok(my_message) = mypackage::MyMessage::decode(data.clone()) {
+        handle_message(my_message);
+    } else if let Ok(other_message) = mypackage::OtherMessage::decode(data) {
+        handle_other_message(other_message);
+    } else {
+        eprintln!("Unknown message type");
     }
+    // let message = match prost::Message::decode(data.clone()) {
+    //     Ok(message) => message,
+    //     Err(err) => {
+    //         eprintln!("Error decoding message: {}", err);
+    //         return;
+    //     }
+    // };
+
+    // match message {
+    //     mypackage::MyMessage { .. } => {
+    //         let my_message = match mypackage::MyMessage::decode(data) {
+    //             Ok(my_message) => my_message,
+    //             Err(err) => {
+    //                 eprintln!("Error decoding MyMessage: {}", err);
+    //                 return;
+    //             }
+    //         };
+    //         handle_message(my_message);
+    //     },
+    //     mypackage::OtherMessage { .. } => {
+    //         let other_message = match mypackage::OtherMessage::decode(data) {
+    //             Ok(other_message) => other_message,
+    //             Err(err) => {
+    //                 eprintln!("Error decoding OtherMessage: {}", err);
+    //                 return;
+    //             }
+    //         };
+    //         handle_other_message(other_message);
+    //     },
+    // }
 }
 
 fn main() {
     // Simulated received data
-    let data = vec![10, 13, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100];
-    let message_bytes = Bytes::from(data);
+    let mut buf: Vec<u8> = Vec::new();
+    let message = mypackage::MyMessage {
+        content: "hello".to_string(),
+    };
 
-    route_message(message_bytes);
+    if message.encode(&mut buf).is_ok() {
+        let message_bytes = Bytes::from(buf);
+
+        route_message(message_bytes);
+    }
 }
