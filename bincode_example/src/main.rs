2921c03545b588e5b08b2c8c6bd9945a4fd170f1
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-25 06:22:12 +0800

    fix file reading and writing

--- bincode_example/src/main.rs
+++ bincode_example/src/main.rs
@@ -14,34 +14,36 @@ struct Entity {
 struct World(Vec<Entity>);
 
 fn main() {
-    let world = World(vec![Entity { x: 0.0, y: 4.0 }, Entity { x: 10.0, y: 20.5 }]);
-
-    let encoded: Vec<u8> = bincode::serialize(&world).unwrap();
-
     let mut file = OpenOptions::new()
         .write(true)
+        .read(true)
         .create(true)
-        .truncate(true)
+        .append(true)
+        // .truncate(true)
         .open("abc.txt")
         .unwrap();
+
+    // write file
+    let world = World(vec![Entity { x: 0.0, y: 4.0 }, Entity { x: 10.0, y: 20.5 }]);
+    let encoded: Vec<u8> = bincode::serialize(&world).unwrap();
+    println!("encoded:{:?}", encoded);
     file.write_all(&encoded).unwrap();
-
-    file.seek(SeekFrom::Start(0)).unwrap();
-
-    // Read the binary data from the file
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).unwrap();
-
-    // Deserialize the binary data into a MyStruct instance
-    let my_struct: World = bincode::deserialize(&buffer).unwrap();
-
-    // Print the deserialized struct
-    println!("read from file: {:?}", my_struct);
+    _ = file.flush();
 
     // 8 bytes for the length of the vector, 4 bytes per float.
     assert_eq!(encoded.len(), 8 + 4 * 4);
-
     let decoded: World = bincode::deserialize(&encoded[..]).unwrap();
-
     assert_eq!(world, decoded);
+
+    // go back to the beginning of the file
+    file.seek(SeekFrom::Start(0)).unwrap();
+
+    // Read the binary data from the file
+    let mut buffer: Vec<u8> = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    println!("buffer: {:?}", buffer);
+    // Deserialize the binary data into a MyStruct instance
+    let my_struct: World = bincode::deserialize(&buffer).unwrap();
+    // Print the deserialized struct
+    println!("read from file: {:?}", my_struct);
 }
