ef516eceabae5f4ac472584c37659a04b9adf0ca
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-25 08:23:39 +0800

    fix code

--- io_cursor_read_struct_example/src/main.rs
+++ io_cursor_read_struct_example/src/main.rs
@@ -8,6 +8,8 @@ struct DataParse {
     file_dum: u8,
 }
 
+// use byteorder::BigEndian;
+// use byteorder::NativeEndian;
 use serde::Deserialize;
 use serde::Serialize;
 // use bincode::{serialize, deserialize};
@@ -18,6 +20,8 @@ use std::io::Cursor;
 use std::io::{self, Write};
 use std::path::Path;
 
+const FILENAME: &str = "abc.txt";
+
 fn read_parse_file(path_of_file: &str) -> io::Result<()> {
     let path = Path::new(&path_of_file);
     let display = path.display();
@@ -25,25 +29,33 @@ fn read_parse_file(path_of_file: &str) -> io::Result<()> {
 
     let mut f = File::open(path)?;
 
-    let mut buffer = vec![0; 27];
-    let mut string_buffer = vec![0u8; 16];
+    let mut buffer: Vec<u8> = vec![0; 24];
 
     f.read_exact(&mut buffer).unwrap();
 
     let mut vec_pointer = Cursor::new(buffer);
 
     let filenum = vec_pointer.read_u16::<LittleEndian>().unwrap();
+    // println!("filenum:{}", filenum);
 
+    let mut string_buffer = vec![0u8; 13];
     vec_pointer.read_exact(&mut string_buffer).unwrap();
-    let mut filename = String::from_utf8(string_buffer).unwrap();
-    match filename.find('\0') {
-        None => {}
-        Some(v) => filename.truncate(v),
-    }
+    let filename = String::from_utf8(string_buffer).unwrap();
+    // println!("filename:{}" ,filename);
+    // match filename.find('\0') {
+    //     None => {},
+    //     Some(v) =>{
+    //         println!("v:{:?}", v);
+    //         filename.truncate(v);
+    //     },
+    // }
 
     let filesize = vec_pointer.read_u32::<LittleEndian>().unwrap();
+    // println!("filenum:{}", filesize);
     let fileoff = vec_pointer.read_u32::<LittleEndian>().unwrap();
+    // println!("filenum:{}", fileoff);
     let filendum = vec_pointer.read_u8().unwrap();
+    // println!("filenum:{}", filendum);
 
     let dat_binary = DataParse {
         file_num: filenum,
@@ -80,19 +92,24 @@ fn main() {
     // Write the binary representation of the struct to the file
     file.write_all(&bincode::serialize(&my_struct).unwrap())
         .unwrap();
-    // Create an instance of MyStruct
-    let my_struct = DataParse {
-        file_num: 1,
-        file_name: "hello".to_string(),
-        file_size: 32,
-        file_off: 4,
-        file_dum: 8,
-    };
-    // Open a file for writing
-    let mut file = File::create("abc.txt").unwrap();
 
-    // Write the binary representation of the struct to the file
-    file.write_all(&bincode::serialize(&my_struct).unwrap())
-        .unwrap();
-    _ = read_parse_file("abc.txt");
+    {
+        // Create an instance of MyStruct
+        let my_struct = DataParse {
+            file_num: 1,
+            file_name: "hello".to_string(),
+            file_size: 32,
+            file_off: 4,
+            file_dum: 8,
+        };
+        // Open a file for writing
+        let mut file = File::create(FILENAME).unwrap();
+        let bin_buffer = bincode::serialize(&my_struct).unwrap();
+        println!("bin_buffer: {}", bin_buffer.len());
+        // Write the binary representation of the struct to the file
+        file.write_all(&bin_buffer).unwrap();
+        _ = file.flush();
+    }
+
+    _ = read_parse_file(FILENAME);
 }
