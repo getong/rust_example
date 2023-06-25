f7b8bd85a9abd59ed1658f668703840cf2313931
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-25 08:03:09 +0800

    delete unwrap method

--- io_cursor_split_example/src/main.rs
+++ io_cursor_split_example/src/main.rs
@@ -14,35 +14,40 @@ fn main() {
                 let mut buffer = [0u8; 9]; // Buffer size of 9 bytes
 
                 // Read 9 bytes from the TCP stream
-                connection.read_exact(&mut buffer).unwrap();
+                match connection.read_exact(&mut buffer) {
+                    Ok(()) => {
+                        // Create a cursor from the buffer
+                        let mut cursor = Cursor::new(buffer);
 
-                // Create a cursor from the buffer
-                let mut cursor = Cursor::new(buffer);
+                        // Read the first 1 byte from the cursor
+                        let mut first_part = [0u8; 1];
+                        cursor.read_exact(&mut first_part).unwrap();
 
-                // Read the first 1 byte from the cursor
-                let mut first_part = [0u8; 1];
-                cursor.read_exact(&mut first_part).unwrap();
+                        // Read the next 8 bytes from the cursor
+                        let mut second_part = [0u8; 8];
+                        cursor.read_exact(&mut second_part).unwrap();
 
-                // Read the next 8 bytes from the cursor
-                let mut second_part = [0u8; 8];
-                cursor.read_exact(&mut second_part).unwrap();
+                        // Convert the variables from network byte order to host byte order
+                        let u8_value = first_part[0];
+                        let u64_value = u64::from_be_bytes(second_part);
 
-                // Convert the variables from network byte order to host byte order
-                let u8_value = first_part[0];
-                let u64_value = u64::from_be_bytes(second_part);
+                        // Print the values
+                        println!("u8 value: {}", u8_value);
+                        println!("u64 value: {}", u64_value);
 
-                // Print the values
-                println!("u8 value: {}", u8_value);
-                println!("u64 value: {}", u64_value);
+                        // Print the first part of the buffer
+                        println!("First part: {:?}", String::from_utf8_lossy(&first_part));
 
-                // Print the first part of the buffer
-                println!("First part: {:?}", String::from_utf8_lossy(&first_part));
-
-                // Print the second part of the buffer
-                println!("Second part: {:?}", String::from_utf8_lossy(&second_part));
-                _ = connection.write_all(&first_part);
-                _ = connection.write_all(&[b'\n']);
-                _ = connection.write_all(&second_part);
+                        // Print the second part of the buffer
+                        println!("Second part: {:?}", String::from_utf8_lossy(&second_part));
+                        _ = connection.write_all(&first_part);
+                        _ = connection.write_all(&[b'\n']);
+                        _ = connection.write_all(&second_part);
+                    }
+                    Err(_) => {
+                        println!("other error");
+                    }
+                };
             }
         });
     }
