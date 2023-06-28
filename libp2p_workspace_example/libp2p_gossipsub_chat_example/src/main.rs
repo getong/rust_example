86a00a7d7edb42eb016ffbe55c1dd8bac4ecc883
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-28 10:08:54 +0800

    update code, it works

--- libp2p_workspace_example/libp2p_gossipsub_chat_example/src/main.rs
+++ libp2p_workspace_example/libp2p_gossipsub_chat_example/src/main.rs
@@ -1,58 +1,13 @@
-// Copyright 2018 Parity Technologies (UK) Ltd.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-
-//! A basic chat application with logs demonstrating libp2p and the gossipsub protocol.
-//!
-//! Using two terminal windows, start two instances. Type a message in either terminal and hit return: the
-//! message is sent and printed in the other terminal. Close with Ctrl-c.
-//!
-//! You can of course open more terminal windows and add more participants.
-//! Dialing any of the other peers will propagate the new participant to all
-//! chat members and everyone will receive all messages.
-//!
-//! In order to get the nodes to connect, take note of the listening addresses of the first
-//! instance and start the second with one of the addresses as the first argument. In the first
-//! terminal window, run:
-//!
-//! ```sh
-//! cargo run
-//! ```
-//!
-//! It will print the [`PeerId`] and the listening addresses, e.g. `Listening on
-//! "/ip4/0.0.0.0/tcp/24915"`
-//!
-//! In the second terminal window, start a new instance of the example with:
-//!
-//! ```sh
-//! cargo run -- /ip4/127.0.0.1/tcp/24915
-//! ```
-//!
-//! The two nodes should then connect.
-
 use env_logger::{Builder, Env};
 use futures::prelude::*;
 use libp2p::gossipsub::MessageId;
-use libp2p::gossipsub::{
-    GossipsubEvent, GossipsubMessage, IdentTopic as Topic, MessageAuthenticity, ValidationMode,
+use libp2p::gossipsub::{Event, IdentTopic as Topic, Message, MessageAuthenticity, ValidationMode};
+use libp2p::{
+    core::transport::upgrade::Version,
+    gossipsub, identity, noise,
+    swarm::{SwarmBuilder, SwarmEvent},
+    tcp, yamux, Multiaddr, PeerId, Transport,
 };
-use libp2p::{gossipsub, identity, swarm::SwarmEvent, Multiaddr, PeerId};
 use std::collections::hash_map::DefaultHasher;
 use std::error::Error;
 use std::hash::{Hash, Hasher};
@@ -68,8 +23,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let local_peer_id = PeerId::from(local_key.public());
     println!("Local peer id: {:?}", local_peer_id);
 
+    let noise = noise::Config::new(&local_key).unwrap();
     // Set up an encrypted TCP Transport over the Mplex and Yamux protocols
-    let transport = libp2p::development_transport(local_key.clone()).await?;
+    // let transport = libp2p::development_transport(local_key.clone()).await?;
+    let transport = tcp::tokio::Transport::default()
+        .upgrade(Version::V1Lazy)
+        .authenticate(noise)
+        .multiplex(yamux::Config::default())
+        .boxed();
 
     // Create a Gossipsub topic
     let topic = Topic::new("test-net");
@@ -77,14 +38,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Create a Swarm to manage peers and events
     let mut swarm = {
         // To content-address message, we can take the hash of message and use it as an ID.
-        let message_id_fn = |message: &GossipsubMessage| {
+        let message_id_fn = |message: &Message| {
             let mut s = DefaultHasher::new();
             message.data.hash(&mut s);
             MessageId::from(s.finish().to_string())
         };
 
         // Set a custom gossipsub
-        let gossipsub_config = gossipsub::GossipsubConfigBuilder::default()
+        let gossipsub_config = gossipsub::ConfigBuilder::default()
             .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
             .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
             .message_id_fn(message_id_fn) // content-address messages. No two messages of the
@@ -92,8 +53,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .build()
             .expect("Valid config");
         // build a gossipsub network behaviour
-        let mut gossipsub: gossipsub::Gossipsub =
-            gossipsub::Gossipsub::new(MessageAuthenticity::Signed(local_key), gossipsub_config)
+        let mut gossipsub: gossipsub::Behaviour =
+            gossipsub::Behaviour::new(MessageAuthenticity::Signed(local_key), gossipsub_config)
                 .expect("Correct configuration");
 
         // subscribes to our topic
@@ -101,7 +62,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
         // add an explicit peer if one was provided
         if let Some(explicit) = std::env::args().nth(2) {
-            let explicit = explicit.clone();
+            let explicit = explicit;
             match explicit.parse() {
                 Ok(id) => gossipsub.add_explicit_peer(&id),
                 Err(err) => println!("Failed to parse explicit peer id: {:?}", err),
@@ -109,7 +70,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
 
         // build the swarm
-        libp2p::Swarm::new(transport, gossipsub, local_peer_id)
+        // libp2p::Swarm::new(transport, gossipsub, local_peer_id)
+        SwarmBuilder::with_tokio_executor(transport, gossipsub, local_peer_id).build()
     };
 
     // Listen on all interfaces and whatever port the OS assigns
@@ -144,7 +106,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 }
             },
             event = swarm.select_next_some() => match event {
-                SwarmEvent::Behaviour(GossipsubEvent::Message {
+                SwarmEvent::Behaviour(Event::Message {
                     propagation_source: peer_id,
                     message_id: id,
                     message,
