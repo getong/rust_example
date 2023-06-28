1714f768a399a6c7a151b57821dd8b0cf81faa2b
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-28 08:57:38 +0800

    update code, it works

--- libp2p_workspace_example/libp2p_chat_example/src/main.rs
+++ libp2p_workspace_example/libp2p_chat_example/src/main.rs
@@ -1,14 +1,18 @@
 use anyhow::Result;
 use futures::StreamExt;
 use libp2p::{
-    core::upgrade,
+    core::transport::upgrade::Version,
     floodsub::{self, Floodsub, FloodsubEvent, Topic},
     identity,
-    mdns::{Mdns, MdnsEvent},
+    mdns,
     noise,
-    swarm::{NetworkBehaviourEventProcess, SwarmBuilder, SwarmEvent},
-    tcp::TokioTcpConfig,
-    yamux, NetworkBehaviour, PeerId, Swarm, Transport,
+    swarm::{NetworkBehaviour, SwarmBuilder, SwarmEvent},
+    // tcp::TokioTcpConfig,
+    tcp,
+    yamux,
+    PeerId,
+    Swarm,
+    Transport,
 };
 use std::borrow::Cow;
 use tokio::io::{stdin, AsyncBufReadExt, BufReader};
@@ -16,12 +20,12 @@ use tokio::io::{stdin, AsyncBufReadExt, BufReader};
 /// 处理 p2p 网络的 behavior 数据结构
 /// 里面的每个域需要实现 NetworkBehaviour，或者使用 #[behaviour(ignore)]
 #[derive(NetworkBehaviour)]
-#[behaviour(event_process = true)]
+#[behaviour(to_swarm = "ChatBehaviourEvent")]
 struct ChatBehavior {
     /// flood subscription，比较浪费带宽，gossipsub 是更好的选择
     floodsub: Floodsub,
     /// 本地节点发现机制
-    mdns: Mdns,
+    mdns: mdns::tokio::Behaviour,
     // 在 behavior 结构中，你也可以放其它数据，但需要 ignore
     // #[behaviour(ignore)]
     // _useless: String,
@@ -31,42 +35,59 @@ impl ChatBehavior {
     /// 创建一个新的 ChatBehavior
     pub async fn new(id: PeerId) -> Result<Self> {
         Ok(Self {
-            mdns: Mdns::new(Default::default()).await?,
+            mdns: mdns::tokio::Behaviour::new(mdns::Config::default(), id).unwrap(),
             floodsub: Floodsub::new(id),
         })
     }
 }
 
-impl NetworkBehaviourEventProcess<FloodsubEvent> for ChatBehavior {
-    // 处理 floodsub 产生的消息
-    fn inject_event(&mut self, event: FloodsubEvent) {
-        if let FloodsubEvent::Message(msg) = event {
-            let text = String::from_utf8_lossy(&msg.data);
-            println!("{:?}: {:?}", msg.source, text);
-        }
+enum ChatBehaviourEvent {
+    FloodSub(FloodsubEvent),
+    Mdns(mdns::Event),
+}
+
+impl From<FloodsubEvent> for ChatBehaviourEvent {
+    fn from(event: FloodsubEvent) -> Self {
+        ChatBehaviourEvent::FloodSub(event)
     }
 }
 
-impl NetworkBehaviourEventProcess<MdnsEvent> for ChatBehavior {
-    fn inject_event(&mut self, event: MdnsEvent) {
-        match event {
-            MdnsEvent::Discovered(list) => {
-                // 把 mdns 发现的新的 peer 加入到 floodsub 的 view 中
-                for (id, addr) in list {
-                    println!("Got peer: {} with addr {}", &id, &addr);
-                    self.floodsub.add_node_to_partial_view(id);
-                }
-            }
-            MdnsEvent::Expired(list) => {
-                // 把 mdns 发现的离开的 peer 加入到 floodsub 的 view 中
-                for (id, addr) in list {
-                    println!("Removed peer: {} with addr {}", &id, &addr);
-                    self.floodsub.remove_node_from_partial_view(&id);
-                }
-            }
-        }
+impl From<mdns::Event> for ChatBehaviourEvent {
+    fn from(event: mdns::Event) -> Self {
+        ChatBehaviourEvent::Mdns(event)
     }
 }
+
+// impl NetworkBehaviourEventProcess<FloodsubEvent> for ChatBehavior {
+//     // 处理 floodsub 产生的消息
+//     fn inject_event(&mut self, event: FloodsubEvent) {
+//         if let FloodsubEvent::Message(msg) = event {
+//             let text = String::from_utf8_lossy(&msg.data);
+//             println!("{:?}: {:?}", msg.source, text);
+//         }
+//     }
+// }
+
+// impl NetworkBehaviourEventProcess<MdnsEvent> for ChatBehavior {
+//     fn inject_event(&mut self, event: MdnsEvent) {
+//         match event {
+//             MdnsEvent::Discovered(list) => {
+//                 // 把 mdns 发现的新的 peer 加入到 floodsub 的 view 中
+//                 for (id, addr) in list {
+//                     println!("Got peer: {} with addr {}", &id, &addr);
+//                     self.floodsub.add_node_to_partial_view(id);
+//                 }
+//             }
+//             MdnsEvent::Expired(list) => {
+//                 // 把 mdns 发现的离开的 peer 加入到 floodsub 的 view 中
+//                 for (id, addr) in list {
+//                     println!("Removed peer: {} with addr {}", &id, &addr);
+//                     self.floodsub.remove_node_from_partial_view(&id);
+//                 }
+//             }
+//         }
+//     }
+// }
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -105,19 +126,18 @@ async fn main() -> Result<()> {
 
 async fn create_swarm(topic: Topic) -> Result<Swarm<ChatBehavior>> {
     // 创建 identity（密钥对）
-    let id_keys = identity::Keypair::generate_ed25519();
-    let peer_id = PeerId::from(id_keys.public());
+    let id_keys = identity::Keypair::generate_secp256k1();
+    let peer_id = PeerId::from(&id_keys.public());
     println!("Local peer id: {:?}", peer_id);
 
     // 使用 noise protocol 来处理加密和认证
-    let noise_keys = noise::Keypair::<noise::X25519Spec>::new().into_authentic(&id_keys)?;
+    let noise = noise::Config::new(&id_keys).unwrap();
 
     // 创建传输层
-    let transport = TokioTcpConfig::new()
-        .nodelay(true)
-        .upgrade(upgrade::Version::V1)
-        .authenticate(noise::NoiseConfig::xx(noise_keys).into_authenticated())
-        .multiplex(yamux::YamuxConfig::default())
+    let transport = tcp::tokio::Transport::default()
+        .upgrade(Version::V1Lazy)
+        .authenticate(noise)
+        .multiplex(yamux::Config::default())
         .boxed();
 
     // 创建 chat behavior
@@ -125,11 +145,7 @@ async fn create_swarm(topic: Topic) -> Result<Swarm<ChatBehavior>> {
     // 订阅某个主题
     behavior.floodsub.subscribe(topic.clone());
     // 创建 swarm
-    let swarm = SwarmBuilder::new(transport, behavior, peer_id)
-        .executor(Box::new(|fut| {
-            tokio::spawn(fut);
-        }))
-        .build();
+    let swarm = SwarmBuilder::with_tokio_executor(transport, behavior, peer_id).build();
 
     Ok(swarm)
 }
