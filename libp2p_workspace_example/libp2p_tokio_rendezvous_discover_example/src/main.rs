fe6d56f583640bd38f4d7ccfe0438abd9cf01921
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-06-30 20:33:54 +0800

    update code

---
+++ libp2p_workspace_example/libp2p_tokio_rendezvous_discover_example/src/main.rs
@@ -0,0 +1,113 @@
+use futures::StreamExt;
+use libp2p::{
+    core::transport::upgrade::Version,
+    identity,
+    multiaddr::Protocol,
+    noise, ping, rendezvous,
+    swarm::{keep_alive, NetworkBehaviour, SwarmBuilder, SwarmEvent},
+    tcp, yamux, Multiaddr, PeerId, Transport,
+};
+use std::time::Duration;
+
+const NAMESPACE: &str = "rendezvous";
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let key_pair = identity::Keypair::generate_ed25519();
+    let rendezvous_point_address = "/ip4/127.0.0.1/tcp/62649".parse::<Multiaddr>().unwrap();
+    let rendezvous_point = "12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN"
+        .parse()
+        .unwrap();
+
+    let mut swarm = SwarmBuilder::with_tokio_executor(
+        tcp::tokio::Transport::default()
+            .upgrade(Version::V1Lazy)
+            .authenticate(noise::Config::new(&key_pair).unwrap())
+            .multiplex(yamux::Config::default())
+            .boxed(),
+        MyBehaviour {
+            rendezvous: rendezvous::client::Behaviour::new(key_pair.clone()),
+            ping: ping::Behaviour::new(ping::Config::new().with_interval(Duration::from_secs(1))),
+            keep_alive: keep_alive::Behaviour,
+        },
+        PeerId::from(key_pair.public()),
+    )
+    .build();
+
+    log::info!("Local peer id: {}", swarm.local_peer_id());
+
+    swarm.dial(rendezvous_point_address.clone()).unwrap();
+
+    let mut discover_tick = tokio::time::interval(Duration::from_secs(30));
+    let mut cookie = None;
+
+    loop {
+        tokio::select! {
+            event = swarm.select_next_some() => match event {
+                SwarmEvent::ConnectionEstablished { peer_id, .. } if peer_id == rendezvous_point => {
+                    log::info!(
+                        "Connected to rendezvous point, discovering nodes in '{}' namespace ...",
+                        NAMESPACE
+                    );
+
+                    swarm.behaviour_mut().rendezvous.discover(
+                        Some(rendezvous::Namespace::new(NAMESPACE.to_string()).unwrap()),
+                        None,
+                        None,
+                        rendezvous_point,
+                    );
+                }
+                SwarmEvent::Behaviour(MyBehaviourEvent::Rendezvous(rendezvous::client::Event::Discovered {
+                    registrations,
+                    cookie: new_cookie,
+                    ..
+                })) => {
+                    cookie.replace(new_cookie);
+
+                    for registration in registrations {
+                        for address in registration.record.addresses() {
+                            let peer = registration.record.peer_id();
+                            log::info!("Discovered peer {} at {}", peer, address);
+
+                            let p2p_suffix = Protocol::P2p(peer);
+                            let address_with_p2p =
+                                if !address.ends_with(&Multiaddr::empty().with(p2p_suffix.clone())) {
+                                    address.clone().with(p2p_suffix)
+                                } else {
+                                    address.clone()
+                                };
+
+                            swarm.dial(address_with_p2p).unwrap();
+                        }
+                    }
+                }
+                SwarmEvent::Behaviour(MyBehaviourEvent::Ping(ping::Event {
+                    peer,
+                    result: Ok(rtt),
+                    ..
+                })) if peer != rendezvous_point => {
+                    log::info!("Ping to {} is {}ms", peer, rtt.as_millis())
+                }
+                other => {
+                    log::debug!("Unhandled {:?}", other);
+                }
+            },
+            _ = discover_tick.tick(), if cookie.is_some() =>
+                swarm.behaviour_mut().rendezvous.discover(
+                    Some(rendezvous::Namespace::new(NAMESPACE.to_string()).unwrap()),
+                    cookie.clone(),
+                    None,
+                    rendezvous_point
+                )
+        }
+    }
+}
+
+#[derive(NetworkBehaviour)]
+struct MyBehaviour {
+    rendezvous: rendezvous::client::Behaviour,
+    ping: ping::Behaviour,
+    keep_alive: keep_alive::Behaviour,
+}
---
+++ libp2p_workspace_example/libp2p_tokio_rendezvous_server_example/src/main.rs
@@ -0,0 +1,81 @@
+use futures::StreamExt;
+use libp2p::{
+    core::transport::upgrade::Version,
+    identify, identity, noise, ping, rendezvous,
+    swarm::{keep_alive, NetworkBehaviour, SwarmBuilder, SwarmEvent},
+    tcp, yamux, PeerId, Transport,
+};
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let key_pair = identity::Keypair::generate_ed25519();
+
+    let mut swarm = SwarmBuilder::with_tokio_executor(
+        tcp::tokio::Transport::default()
+            .upgrade(Version::V1Lazy)
+            .authenticate(noise::Config::new(&key_pair).unwrap())
+            .multiplex(yamux::Config::default())
+            .boxed(),
+        MyBehaviour {
+            identify: identify::Behaviour::new(identify::Config::new(
+                "rendezvous-example/1.0.0".to_string(),
+                key_pair.public(),
+            )),
+            rendezvous: rendezvous::server::Behaviour::new(rendezvous::server::Config::default()),
+            ping: ping::Behaviour::new(ping::Config::new().with_interval(Duration::from_secs(1))),
+            keep_alive: keep_alive::Behaviour,
+        },
+        PeerId::from(key_pair.public()),
+    )
+        .build();
+
+    log::info!("Local peer id: {}", swarm.local_peer_id());
+
+    let _ = swarm.listen_on("/ip4/0.0.0.0/tcp/62649".parse().unwrap());
+
+    while let Some(event) = swarm.next().await {
+        match event {
+            SwarmEvent::ConnectionEstablished { peer_id, .. } => {
+                log::info!("Connected to {}", peer_id);
+            }
+            SwarmEvent::ConnectionClosed { peer_id, .. } => {
+                log::info!("Disconnected from {}", peer_id);
+            }
+            SwarmEvent::Behaviour(MyBehaviourEvent::Rendezvous(
+                rendezvous::server::Event::PeerRegistered { peer, registration },
+            )) => {
+                log::info!(
+                    "Peer {} registered for namespace '{}'",
+                    peer,
+                    registration.namespace
+                );
+            }
+            SwarmEvent::Behaviour(MyBehaviourEvent::Rendezvous(
+                rendezvous::server::Event::DiscoverServed {
+                    enquirer,
+                    registrations,
+                },
+            )) => {
+                log::info!(
+                    "Served peer {} with {} registrations",
+                    enquirer,
+                    registrations.len()
+                );
+            }
+            other => {
+                log::debug!("Unhandled {:?}", other);
+            }
+        }
+    }
+}
+
+#[derive(NetworkBehaviour)]
+struct MyBehaviour {
+    identify: identify::Behaviour,
+    rendezvous: rendezvous::server::Behaviour,
+    ping: ping::Behaviour,
+    keep_alive: keep_alive::Behaviour,
+}
