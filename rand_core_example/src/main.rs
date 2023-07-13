4ca7cace2e3144dbc31c6f6b0a779af7d24b7401
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-07-13 23:05:26 +0800

    add rand_core example

---
+++ rand_core_example/src/main.rs
@@ -0,0 +1,11 @@
+use rand_core::{OsRng, RngCore};
+
+fn main() {
+    // println!("Hello, world!");
+
+    let mut key = [0u8; 16];
+    OsRng.fill_bytes(&mut key);
+    let random_u64 = OsRng.next_u64();
+
+    println!("key:{:?}, random_u64:{}", key, random_u64);
+}
