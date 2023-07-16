8c9d8755519c6dfd75b0b9844edfb7a69ee52fa3
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-07-16 23:22:13 +0800

    add derefmut example

---
+++ derefmut_example/src/main.rs
@@ -0,0 +1,24 @@
+use std::ops::{Deref, DerefMut};
+struct Portal<T> {
+    data: T,
+}
+
+impl<T> Deref for Portal<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.data
+    }
+}
+
+impl<T> DerefMut for Portal<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.data
+    }
+}
+
+fn main() {
+    // println!("Hello, world!");
+    let mut p = Portal::<i32> { data: 42 };
+    *p = 0;
+    println!("{}", 100 + *p);
+}
