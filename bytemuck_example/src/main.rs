e3c042e0c73c6c5aad5fe61b81ce8bf5f0e27b1e
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-07-18 22:23:59 +0800

    add bytemuck example

---
+++ bytemuck_example/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // println!("Hello, world!");
+
+    let a = 123_i32;
+    println!("123 in bytes : {:?}",bytemuck::bytes_of(&a));
+
+    let mut b = 123_i32;
+    println!("123 in bytes : {:?}",bytemuck::bytes_of_mut(&mut b));
+}
