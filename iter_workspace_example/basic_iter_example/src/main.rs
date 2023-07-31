175d8e9ea5180ea492f967796619704c74329a63
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-01 00:52:58 +0800

    add iter, itermut, intoiter example

--- iter_workspace_example/basic_iter_example/src/main.rs
+++ iter_workspace_example/basic_iter_example/src/main.rs
@@ -1,10 +1,48 @@
+use std::collections::HashMap;
+
 fn main() {
     // println!("Hello, world!");
-    let v = [1, 2, 3];
+    let mut v = [1, 2, 3];
     let mut iter = v.iter();
 
     println!("{:?}", iter.next());
     println!("{:?}", iter.next());
     println!("{:?}", iter.next());
     println!("{:?}", iter.next());
+
+    for i in &v {
+        println!("i is {:?}", i)
+    }
+
+    for i in &mut v {
+        *i += 1;
+    }
+
+    for i in v {
+        println!("i is {:?}", i)
+    }
+
+    // Create a HashMap with some key-value pairs
+    let mut my_map = HashMap::new();
+    my_map.insert("apple", 3);
+    my_map.insert("banana", 2);
+    my_map.insert("orange", 5);
+
+    // Iterate over the key-value pairs using `iter()`
+    for (key, value) in my_map.iter() {
+        println!("Key: {}, Value: {}", key, value);
+    }
+
+    for (key, value) in &my_map {
+        println!("Key: {}, Value: {}", key, value);
+    }
+
+    for (key, value) in &mut my_map {
+        println!("Key: {}, Value: {}", key, value);
+        *value += 1;
+    }
+
+    for (key, value) in my_map {
+        println!("Key: {}, Value: {}", key, value);
+    }
 }
