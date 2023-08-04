1993c1bbaded0d7394b960cb1e80e577d30d0835
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-05 07:19:18 +0800

    add more code

--- trace_macros_example/src/main.rs
+++ trace_macros_example/src/main.rs
@@ -39,8 +39,23 @@ macro_rules! my_vec { //#1
 
 }
 
+fn add_one(n: i32) -> i32 {
+    n + 1
+}
+fn stringify(n: i32) -> String {
+    n.to_string()
+}
+
 fn prefix_with(prefix: &str) -> impl Fn(String) -> String + '_ {
     move |x| format!("{} {}", prefix, x)
+}
+
+fn compose_two<FIRST, SECOND, THIRD, F, G>(f1: F, f2: G) -> impl Fn(FIRST) -> THIRD
+where
+    F: Fn(FIRST) -> SECOND,
+    G: Fn(SECOND) -> THIRD,
+{
+    move |x| f2(f1(x))
 }
 
 fn main() {
@@ -56,4 +71,10 @@ fn main() {
 
     let c = prefix_with("hello");
     println!("{:?}", c("world".to_string()));
+
+    let two_composed_function =
+        compose_two(compose_two(add_one, stringify), prefix_with("Result: "));
+
+    let d = two_composed_function(3);
+    println!("d:{:?}", d);
 }
