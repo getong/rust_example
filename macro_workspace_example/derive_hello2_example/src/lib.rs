a26758358b74555df776b4e17bae406d75ca4349
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-06 03:56:28 +0800

    add hello 2 proc macro example

---
+++ macro_workspace_example/derive_hello2_example/src/lib.rs
@@ -0,0 +1,19 @@
+use proc_macro::{TokenStream, TokenTree};
+
+#[proc_macro_derive(Hello)]
+pub fn hello_alt(item: TokenStream) -> TokenStream {
+    fn ident_name(item: TokenTree) -> String {
+        match item {
+            TokenTree::Ident(i) => i.to_string(),
+            _ => panic!("No ident"),
+        }
+    }
+    let name = ident_name(item.into_iter().nth(1).unwrap());
+    format!(
+        "impl {} {{ fn hello_world(&self) \
+             {{ println!(\"Hello world\") }} }} ",
+        name
+    )
+    .parse()
+    .unwrap()
+}
---
+++ macro_workspace_example/derive_hello2_example/src/main.rs
@@ -0,0 +1,12 @@
+use derive_hello2_example::Hello;
+
+#[derive(Hello)]
+enum Pet {
+    Cat,
+}
+
+fn main() {
+    // previous code
+    let p = Pet::Cat;
+    p.hello_world();
+}
