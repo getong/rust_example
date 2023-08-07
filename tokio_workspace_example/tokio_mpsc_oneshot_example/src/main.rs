5780c302cf2aff6c9019387f4b61062bf157a1f1
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-07 17:13:54 +0800

    add timeout code

--- tokio_workspace_example/tokio_mpsc_oneshot_example/src/main.rs
+++ tokio_workspace_example/tokio_mpsc_oneshot_example/src/main.rs
@@ -1,5 +1,6 @@
 use tokio::sync::mpsc;
 use tokio::sync::oneshot;
+use tokio::time::{timeout, Duration};
 
 type MpscInnerTy<T, U> = (T, oneshot::Sender<U>);
 
@@ -10,7 +11,7 @@ async fn main() {
 
     // Spawn a Tokio task to send a value through the channel
     tokio::spawn(async move {
-        let (send, mut recv) = oneshot::channel();
+        let (send, recv) = oneshot::channel();
         let mpsc_inner = ("Hello from the sender!".to_string(), send);
 
         // Send the MpscInnerTy through the unbounded mpsc channel
@@ -19,9 +20,26 @@ async fn main() {
         // Simulate some work in the sender task
         tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
 
-        // Complete the oneshot channel
-        let msg = recv.try_recv();
-        println!("msg: {:?}", msg);
+        // // Complete the oneshot channel
+        // let msg = recv.try_recv();
+        // println!("msg: {:?}", msg);
+        let timeout_duration = Duration::from_secs(2);
+        match timeout(timeout_duration, recv).await {
+            Ok(result) => match result {
+                Ok(value) => {
+                    println!("Received: {}", value);
+                }
+                Err(e1) => {
+                    println!(
+                        "Timed out: Value not received within the timeout, e:{:?}",
+                        e1
+                    );
+                }
+            },
+            Err(e) => {
+                println!("Error receiving value, the e: {:?}", e);
+            }
+        }
     });
 
     // Receive the MpscInnerTy from the unbounded mpsc channel
