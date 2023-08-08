96f5b76e1da2503328291482f6a26224e1dff276
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-09 01:01:46 +0800

    add clap parser example

---
+++ clap_workspace_example/clap_parser_example/src/main.rs
@@ -0,0 +1,24 @@
+use clap::Parser;
+
+// cargo run -- --name 5 --count 5
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
+}
