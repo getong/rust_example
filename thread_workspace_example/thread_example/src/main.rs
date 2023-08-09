3eb36b36c9cd335aab1d76658f1c83d05dec2426
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-09 21:39:30 +0800

    wait thread result

--- thread_workspace_example/thread_example/src/main.rs
+++ thread_workspace_example/thread_example/src/main.rs
@@ -19,4 +19,19 @@ fn main() {
     let delta = d.as_millis();
 
     println!("value: {}, processed time:{}", value, delta);
+
+    // thread::spawn(f);
+    // thread::spawn(f);
+    // println!("Hello from the main thread.");
+    let t1 = thread::spawn(f);
+    let t2 = thread::spawn(f);
+    println!("Hello from the main thread.");
+    t1.join().unwrap();
+    t2.join().unwrap();
+}
+
+fn f() {
+    println!("Hello from another thread!");
+    let id = thread::current().id();
+    println!("This is my thread id: {id:?}");
 }
