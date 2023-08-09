880867a9e2858fbd064d9edec44d2730d916c5a4
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-09 21:57:55 +0800

    add hyper workspace example

---
+++ hyper_workspace_example/hyper_basic_example/src/main.rs
@@ -0,0 +1,27 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello World")))
+}
+
+#[tokio::main]
+async fn main() {
+    // Construct our SocketAddr to listen on...
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // And a MakeService to handle each connection...
+    let make_service = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle))
+    });
+
+    // Then bind and serve...
+    let server = Server::bind(&addr).serve(make_service);
+
+    // And run forever...
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
