3379c22989e17515262994bfc42f837f4a427729
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-10 16:43:44 +0800

    add tokio watch example 2

---
+++ tokio_workspace_example/tokio_watch_example2/src/main.rs
@@ -0,0 +1,16 @@
+use tokio::sync::watch;
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = watch::channel("hello");
+
+    tokio::spawn(async move {
+        tx.send("goodbye").unwrap();
+    });
+
+    assert!(rx.changed().await.is_ok());
+    assert_eq!(*rx.borrow(), "goodbye");
+
+    // The `tx` handle has been dropped
+    assert!(rx.changed().await.is_err());
+}
