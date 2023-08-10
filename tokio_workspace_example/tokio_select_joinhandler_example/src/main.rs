8f02871d2c7e57110401c14154a365aaf7368914
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-10 19:11:42 +0800

    tokio select joinhandler example

---
+++ tokio_workspace_example/tokio_select_joinhandler_example/src/main.rs
@@ -0,0 +1,29 @@
+use rand::rngs::OsRng;
+use rand::Rng;
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() {
+    let task1 = tokio::spawn(async {
+        let mut rng = OsRng::default();
+        let random_number = rng.gen_range(1..=4);
+        // Some asynchronous task
+        sleep(Duration::from_secs(random_number)).await;
+        println!("Task 1 completed");
+    });
+
+    let task2 = tokio::spawn(async {
+        let mut rng = OsRng::default();
+        let random_number = rng.gen_range(1..=4);
+        // Some other asynchronous task
+        sleep(Duration::from_secs(random_number)).await;
+        println!("Task 2 completed");
+    });
+
+    tokio::select! {
+        _ = task1 => println!("Task 1 joined"),
+        _ = task2 => println!("Task 2 joined"),
+    }
+
+    println!("Both tasks completed");
+}
