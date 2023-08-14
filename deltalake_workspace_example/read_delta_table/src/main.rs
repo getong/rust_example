401d6e8c5be85a92ba1c0bacf5d002d44ebbd107
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-14 08:33:49 +0800

    deltalake workspace example

---
+++ deltalake_workspace_example/read_delta_table/src/main.rs
@@ -0,0 +1,7 @@
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), deltalake::errors::DeltaTableError> {
+    let table_path = "./tests/data/delta-0.8.0";
+    let table = deltalake::open_table(table_path).await?;
+    println!("{table}");
+    Ok(())
+}
