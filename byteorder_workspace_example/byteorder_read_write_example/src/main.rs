2bdafb7c4525b2829ef97d80556af2bb86e84aeb
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-15 11:11:05 +0800

    add code here

--- byteorder_workspace_example/byteorder_read_write_example/src/main.rs
+++ byteorder_workspace_example/byteorder_read_write_example/src/main.rs
@@ -1,18 +1,22 @@
 use byteorder::{BigEndian, ReadBytesExt};
 use byteorder::{LittleEndian, WriteBytesExt};
 use std::io::Cursor;
+use std::pin::Pin;
 
 fn main() {
     // println!("Hello, world!");
-
+    // one byte for 256
     let mut rdr = Cursor::new(vec![2, 5, 3, 0]);
     // Note that we use type parameters to indicate which kind of byte order
     // we want!
     assert_eq!(517, rdr.read_u16::<BigEndian>().unwrap());
+    assert_eq!(&rdr.get_ref()[..], &[2, 5, 3, 0]);
     assert_eq!(768, rdr.read_u16::<BigEndian>().unwrap());
+    assert_eq!(&rdr.get_ref()[..], &[2, 5, 3, 0]);
 
     let mut wtr = vec![];
     wtr.write_u16::<LittleEndian>(517).unwrap();
+    assert_eq!(&Pin::new(&wtr).get_ref()[..], &[5, 2]);
     wtr.write_u16::<LittleEndian>(768).unwrap();
     assert_eq!(wtr, vec![5, 2, 0, 3]);
 }
