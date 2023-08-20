8d00614da14d227ab67fc185e22e56ad38181b43
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-21 02:42:52 +0800

    add mmap-sync workspace example

---
+++ mmap_sync_workspace_example/mmap_sync_read_example/src/main.rs
@@ -0,0 +1,24 @@
+use mmap_sync::synchronizer::Synchronizer;
+
+use bytecheck::CheckBytes;
+use rkyv::{Archive, Deserialize, Serialize};
+
+/// Example data-structure shared between writer and reader(s)
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[archive_attr(derive(CheckBytes))]
+pub struct HelloWorld {
+    pub version: u32,
+    pub messages: Vec<String>,
+}
+
+
+fn main() {
+    // Initialize the Synchronizer
+    let mut synchronizer = Synchronizer::new("/tmp/hello_world");
+
+    // Read data from shared memory
+    let data = unsafe { synchronizer.read::<HelloWorld>() }.expect("failed to read data");
+
+    // Access fields of the struct
+    println!("version: {} | messages: {:?}", data.version, data.messages);
+}
---
+++ mmap_sync_workspace_example/mmap_sync_write_example/src/main.rs
@@ -0,0 +1,31 @@
+use bytecheck::CheckBytes;
+use mmap_sync::synchronizer::Synchronizer;
+use rkyv::{Archive, Deserialize, Serialize};
+use std::time::Duration;
+
+/// Example data-structure shared between writer and reader(s)
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[archive_attr(derive(CheckBytes))]
+pub struct HelloWorld {
+    pub version: u32,
+    pub messages: Vec<String>,
+}
+
+fn main() {
+    // Initialize the Synchronizer
+    let mut synchronizer = Synchronizer::new("/tmp/hello_world");
+
+    // Define the data
+    let data = HelloWorld {
+        version: 7,
+        messages: vec!["Hello".to_string(), "World".to_string(), "!".to_string()],
+    };
+
+    // Write data to shared memory
+    let (written, reset) = synchronizer
+        .write(&data, Duration::from_secs(1))
+        .expect("failed to write data");
+
+    // Show how many bytes written and whether state was reset
+    println!("written: {} bytes | reset: {}", written, reset);
+}
