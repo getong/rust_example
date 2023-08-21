e4a7af7bfc9951d08a87706ad570e9057dee31ad
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-08-22 01:43:58 +0800

    update code

--- suspicious_double_ref_op_example/src/main.rs
+++ suspicious_double_ref_op_example/src/main.rs
@@ -2,17 +2,17 @@ use std::ops::Deref;
 
 fn main() {
     // Define an Option containing a reference to an i32.
-    let some_value: Option<&i32> = Some(&42);
+    let some_value = Some(&42);
 
     // Use Option::as_ref and map to transform Option<&i32> into Option<&&i32>.
-    let result2: Option<&&i32> = some_value.as_ref();
+    let result2 = some_value.as_ref();
     // Check if the result is Some and print the value.
     match result2 {
         Some(ref value) => println!("The inner reference is: {}", *value),
         None => println!("The Option is None."),
     }
 
-    let result3: Option<&&&i32> = result2.as_ref();
+    let result3 = result2.as_ref();
     match result3 {
         Some(ref value) => println!("The inner reference is: {}", **value),
         None => println!("The Option is None."),
@@ -20,7 +20,7 @@ fn main() {
 
     // it might return Option<&&&&i32>, but the deref() will cause suspicious_double_ref_op will
     // not deref again, and return Option<&&&i32>
-    let result4: Option<&&&i32> = result3.as_ref().map(|v| v.deref());
+    let result4 = result3.as_ref().map(|v| v.deref());
     match result4 {
         Some(ref value) => println!("The inner reference is: {}", **value),
         None => println!("The Option is None."),
