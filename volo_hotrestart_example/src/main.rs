95cdde1895516b6f946ffbc9f14ada72ecf01231
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-02 19:42:40 +0800

    add volo hotrestart example

---
+++ volo_hotrestart_example/src/main.rs
@@ -0,0 +1,19 @@
+use std::net::SocketAddr;
+use std::path::Path;
+
+#[volo::main]
+async fn main() {
+    let _addr: SocketAddr = "[::]:8080".parse().unwrap();
+
+    // hotrestart initialize
+    volo::hotrestart::DEFAULT_HOT_RESTART
+        .initialize(Path::new("/tmp"), 1)
+        .await
+        .unwrap();
+
+    // volo_gen::nthrift::test::idl::LearnServiceServer::new(S)
+    //     .byted()
+    //     .run(addr)
+    //     .await
+    //     .unwrap();
+}
