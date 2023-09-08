6d4602437bd26a9b3136f9bf43bfefd4669136e1
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-08 13:55:31 +0800

    add skiplist example

---
+++ skiplist_example/src/main.rs
@@ -0,0 +1,24 @@
+use skiplist::SkipList;
+
+fn main() {
+    // println!("Hello, world!");
+
+    let mut skiplist = SkipList::new();
+
+    skiplist.insert(0, 0);
+    skiplist.insert(5, 1);
+    assert_eq!(skiplist.len(), 2);
+    assert!(!skiplist.is_empty());
+    println!("skiplist:{:?}", skiplist);
+
+    let mut skiplist = SkipList::new();
+    skiplist.push_front(1);
+    skiplist.push_front(2);
+    println!("skiplist:{:?}", skiplist);
+
+    let mut skiplist = SkipList::new();
+    skiplist.push_back(1);
+    skiplist.push_back(2);
+
+    println!("skiplist:{:?}", skiplist);
+}
