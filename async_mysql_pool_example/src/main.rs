9cbe86f33ad088753ce0fe97046db39f86360e15
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-10 16:53:14 +0800

    add async mysql pool example

---
+++ async_mysql_pool_example/src/main.rs
@@ -0,0 +1,15 @@
+use mysql::Pool;
+use mysql::prelude::Queryable;
+
+// docker run --name my-own-mariadb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -d mariadb:10.4.10
+
+#[tokio::main]
+async fn main() {
+    let pool = Pool::new("mysql://root:123456@localhost:3306").unwrap();
+
+    let mut conn = pool.get_conn().unwrap();
+
+    let result: Vec<String> = conn.query("SELECT 1").unwrap();
+
+    assert_eq!(result.len(), 1);
+}
