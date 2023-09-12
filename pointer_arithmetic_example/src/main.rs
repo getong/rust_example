a2479662d21c466d76b4e648d698797de33987d6
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-12 23:03:05 +0800

    add raw pointer example

--- pointer_arithmetic_example/src/main.rs
+++ pointer_arithmetic_example/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(pointer_byte_offsets)] // at top of file
 fn main() {
     let array: [i32; 3] = [10, 20, 30];
     let ptr: *const i32 = array.as_ptr();
@@ -9,4 +10,13 @@ fn main() {
     let new_ptr = unsafe { ptr.add(3) };
     let value = unsafe { *new_ptr };
     println!("Value: {}", value);
+
+    // cargo-run segmentation fault here
+    let base_addr = 0x0112A160 as *mut u32;
+    println!("data: {:?}", unsafe { *base_addr });
+
+    let new_address = unsafe { base_addr.byte_offset(0xF8) };
+    println!("data: {:?}", unsafe { *new_address });
+    let new_address = unsafe { base_addr.offset(0xF8) };
+    println!("data: {:?}", unsafe { *new_address });
 }
