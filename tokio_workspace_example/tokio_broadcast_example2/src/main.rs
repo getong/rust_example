bb1e24f3e02cbde4d2fa8c91d57c6cde8b1c5f78
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-14 18:42:11 +0800

    add tokio broadcast example2

---
+++ tokio_workspace_example/tokio_broadcast_example2/src/main.rs
@@ -0,0 +1,20 @@
+use tokio::sync::broadcast;
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = broadcast::channel(16);
+
+    // Will not be seen
+    tx.send(10).unwrap();
+
+    let value = rx.recv().await.unwrap();
+    assert_eq!(10, value);
+
+    // a new receiver
+    let mut rx2 = tx.subscribe();
+
+    tx.send(20).unwrap();
+
+    let value = rx2.recv().await.unwrap();
+    assert_eq!(20, value);
+}
