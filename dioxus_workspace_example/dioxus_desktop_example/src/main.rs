ba6b21713aba56bb117c4c66ea4b737c24310a39
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-18 00:55:36 +0800

    add dioxus desktop example

---
+++ dioxus_workspace_example/dioxus_desktop_example/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+use dioxus::prelude::*;
+
+fn main() {
+    // launch the dioxus app in a webview
+    dioxus_desktop::launch(App);
+}
+
+// define a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+        }
+    })
+}
+
+// copy from https://dioxuslabs.com/learn/0.4/getting_started/desktop
+// dx serve --hot-reload --platform desktop
