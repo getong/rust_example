aa86410cd0149a511e25789ca91b559ff2114aed
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-12 18:28:40 +0800

    delete file

--- tokio_workspace_example/tokio_read_to_end_example/src/main.rs
+++ tokio_workspace_example/tokio_read_to_end_example/src/main.rs
@@ -3,12 +3,12 @@ use tokio::io::{self, AsyncReadExt};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut f = File::open("foo.txt").await?;
+    let mut f = File::open("src/main.rs").await?;
     let mut buffer = Vec::new();
 
     // read the whole file
     f.read_to_end(&mut buffer).await?;
     println!("buffer : {:?}", &buffer);
-    println!("buffer : {:?}", String::from_utf8_lossy(&buffer));
+    println!("buffer : {}", String::from_utf8_lossy(&buffer));
     Ok(())
 }
