22ec91aeb685d2e20fd6bfbe232b0d63861efc99
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-24 12:14:56 +0800

    add module_path example

---
+++ module_path_example/src/main.rs
@@ -0,0 +1,14 @@
+macro_rules! get_module_name {
+    () => {{
+        // Using the module_path! macro to get the current module's path
+        module_path!()
+    }};
+}
+
+fn main() {
+    // Using the get_module_name! macro to get the module name
+    let module_name = get_module_name!();
+
+    // Printing the module name
+    println!("The current module is: {}", module_name);
+}
