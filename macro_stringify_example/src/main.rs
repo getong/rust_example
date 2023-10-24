9368d41364fefe1d852b6fada8f910d2c9201bfc
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-24 14:54:38 +0800

    add macro stringify example

---
+++ macro_stringify_example/src/main.rs
@@ -0,0 +1,15 @@
+macro_rules! get_struct_name {
+    ($struct_name:ident) => {
+        stringify!($struct_name);
+    };
+}
+
+struct MyStruct {
+    // Fields of your struct
+}
+
+fn main() {
+    // Using the macro to get the name of the struct
+    let struct_name = get_struct_name!(MyStruct);
+    println!("Struct name: {}", struct_name); // Output: Struct name: MyStruct
+}
