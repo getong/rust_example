31e848c1f7dc04884bd3aa774cac2904ec4fca8d
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-25 08:43:39 +0800

    add macro in other file example

---
+++ macro_workspace_example/macro_in_other_file_example/src/main.rs
@@ -0,0 +1,20 @@
+mod my_macro;
+
+fn main() {
+    println!("{}", square!(2));
+    let fx;
+    let fy;
+    let fz;
+    let tx;
+    let ty;
+    let tz;
+
+    make_forces_torques_zero!((fx, fy, fz, tx, ty, tz));
+
+    assert_eq!(fx, 0.0);
+    assert_eq!(fy, 0.0);
+    assert_eq!(fz, 0.0);
+    assert_eq!(tx, 0.0);
+    assert_eq!(ty, 0.0);
+    assert_eq!(tz, 0.0);
+}
---
+++ macro_workspace_example/macro_in_other_file_example/src/my_macro.rs
@@ -0,0 +1,15 @@
+#[macro_export]
+macro_rules! square {
+    ($x:expr) => {
+        $x * $x
+    };
+}
+
+#[macro_export]
+macro_rules! make_forces_torques_zero {
+    (($($dest:ident), *)) => {
+        $(
+            $dest = 0.0;
+        )*
+    };
+}
