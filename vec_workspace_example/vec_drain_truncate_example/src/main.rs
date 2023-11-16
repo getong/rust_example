36d42a281238b5dcc0b523767d3c6ba56cdba430
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-11-16 14:08:01 +0800

    add vec drain and truncate example

---
+++ vec_workspace_example/vec_drain_truncate_example/src/main.rs
@@ -0,0 +1,21 @@
+fn drain_example() {
+    let mut vec = vec![1, 2, 3, 4, 5];
+    let start = 1; // Start of the range (inclusive)
+    let end = 3; // End of the range (exclusive)
+    vec.drain(start..end);
+    assert_eq!(vec![1, 4, 5], vec);
+}
+
+fn truncate_example() {
+    let mut vec = vec![1, 2, 3, 4, 5];
+    let keep_start = 1; // Start of the range to keep
+    let keep_end = 3; // End of the range to keep
+
+    vec.truncate(keep_end - keep_start);
+    assert_eq!(vec![1, 2], vec);
+}
+
+fn main() {
+    drain_example();
+    truncate_example();
+}
