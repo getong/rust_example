3e5c365f677bb33831130cdd3059c52074343cee
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-11-24 03:31:42 +0800

    update code

--- actix_web_workspace_example/actix_web_protobuf_basic_example/src/main.rs
+++ actix_web_workspace_example/actix_web_protobuf_basic_example/src/main.rs
@@ -11,23 +11,39 @@ pub struct MyObj {
     pub name: String,
 }
 
-async fn index(msg: ProtoBuf<MyObj>) -> Result<HttpResponse> {
-    log::info!("model: {msg:?}");
-    HttpResponse::Ok().protobuf(msg.0) // <- send response
+#[derive(Clone, PartialEq, Eq, Message)]
+pub struct MyObj2 {
+    #[prost(int32, tag = "1")]
+    pub number: i32,
+
+    #[prost(string, tag = "2")]
+    pub name: String,
+}
+
+// Handler for MyObj type
+async fn index_myobj(msg: ProtoBuf<MyObj>) -> Result<HttpResponse> {
+    log::info!("Received MyObj: {:?}", msg.0);
+    HttpResponse::Ok().protobuf(msg.0) // Send response
+}
+
+// Handler for MyObj2 type
+async fn index_myobj2(msg: ProtoBuf<MyObj2>) -> Result<HttpResponse> {
+    log::info!("Received MyObj2: {:?}", msg.0);
+    HttpResponse::Ok().protobuf(msg.0) // Send response
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
     log::info!("starting HTTP server at http://localhost:8080");
 
     HttpServer::new(|| {
         App::new()
-            .service(web::resource("/").route(web::post().to(index)))
+            .service(web::resource("/").route(web::post().to(index_myobj)))
+            .service(web::resource("/myobj2").route(web::post().to(index_myobj2)))
             .wrap(middleware::Logger::default())
     })
-    .workers(1)
+    .workers(3)
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
