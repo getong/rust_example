9c917537d78171b71ebea45575069e11b384e951
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-11-24 03:08:30 +0800

    add actix_web protoobuf basic example

---
+++ actix_web_workspace_example/actix_web_protobuf_basic_example/src/main.rs
@@ -0,0 +1,34 @@
+use actix_protobuf::{ProtoBuf, ProtoBufResponseBuilder as _};
+use actix_web::{middleware, web, App, HttpResponse, HttpServer, Result};
+use prost::Message;
+
+#[derive(Clone, PartialEq, Eq, Message)]
+pub struct MyObj {
+    #[prost(int32, tag = "1")]
+    pub number: i32,
+
+    #[prost(string, tag = "2")]
+    pub name: String,
+}
+
+async fn index(msg: ProtoBuf<MyObj>) -> Result<HttpResponse> {
+    log::info!("model: {msg:?}");
+    HttpResponse::Ok().protobuf(msg.0) // <- send response
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting HTTP server at http://localhost:8080");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/").route(web::post().to(index)))
+            .wrap(middleware::Logger::default())
+    })
+    .workers(1)
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
