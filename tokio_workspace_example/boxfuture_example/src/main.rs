84da20e7a83d74bd0a6d0cdc2b46a24866561257
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-11-24 22:29:56 +0800

    add boxfuture example

---
+++ tokio_workspace_example/boxfuture_example/src/main.rs
@@ -0,0 +1,20 @@
+use futures::future::BoxFuture;
+//use futures::future::Future;
+use futures::FutureExt;
+
+fn foo() -> BoxFuture<'static, u32> {
+    async move { 42 }.boxed()
+}
+
+async fn bar() {
+    let _u: u32 = foo().await;
+    println!("_u is {:?}", _u);
+}
+
+#[tokio::main]
+async fn main() {
+    bar().await;
+}
+
+
+// copy from https://users.rust-lang.org/t/how-to-unbox-deference-a-boxfuture/56691
