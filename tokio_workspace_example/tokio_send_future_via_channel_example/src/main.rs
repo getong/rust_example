9789c97f8d16f8d8c13cc0d48115950e9387b831
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-11-28 04:23:01 +0800

    add tokio send future via channel example

---
+++ tokio_workspace_example/tokio_send_future_via_channel_example/src/main.rs
@@ -0,0 +1,37 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::time::Duration;
+use tokio::sync::mpsc;
+use tokio::task;
+
+// Type alias for a boxed future that returns a String and is Sendable
+type BoxedFuture = Pin<Box<dyn Future<Output = String> + Send>>;
+
+#[tokio::main]
+async fn main() {
+    // Create a channel for sending boxed futures
+    let (tx, mut rx) = mpsc::channel::<BoxedFuture>(10);
+
+    // Spawn a task to send futures to the channel
+    let sender_task = task::spawn(async move {
+        for i in 0..5 {
+            let future = Box::pin(async move {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                format!("Task {} completed", i)
+            });
+            tx.send(future).await.expect("Failed to send future");
+        }
+    });
+
+    // Spawn a task to process futures received from the channel
+    let receiver_task = task::spawn(async move {
+        while let Some(future) = rx.recv().await {
+            let result = future.await;
+            println!("{}", result);
+        }
+    });
+
+    // Wait for both tasks to complete
+    let _ = sender_task.await;
+    let _ = receiver_task.await;
+}
