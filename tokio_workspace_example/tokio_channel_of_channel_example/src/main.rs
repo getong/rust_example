33360a28713c34d1fe0a110d90310882f596972d
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-01 01:06:38 +0800

    add tokio stream return remaining stream example

--- tokio_workspace_example/tokio_channel_of_channel_example/src/main.rs
+++ tokio_workspace_example/tokio_channel_of_channel_example/src/main.rs
@@ -13,7 +13,9 @@ async fn main() {
             let (inner_tx, mut inner_rx) = mpsc::channel::<i32>(10);
 
             // Send the inner channel to the outer channel
-            tx.send(inner_tx).await.expect("Failed to send inner channel");
+            tx.send(inner_tx)
+                .await
+                .expect("Failed to send inner channel");
 
             // Spawn a task to receive messages from the inner channel
             task::spawn(async move {
@@ -26,9 +28,12 @@ async fn main() {
 
     // Spawn a task to receive inner channels from the outer channel
     let receiver_task = task::spawn(async move {
-        while let Some( inner_channel) = rx.recv().await {
+        while let Some(inner_channel) = rx.recv().await {
             // Send a message to the inner channel
-            inner_channel.send(42).await.expect("Failed to send message");
+            inner_channel
+                .send(42)
+                .await
+                .expect("Failed to send message");
         }
     });
 
