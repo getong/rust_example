717976b92b21b6356a3b4c9fa16d44a5d0bee353
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-01 16:15:33 +0800

    add tokio_stream tcpstream return byte example

---
+++ tokio_workspace_example/tokio_stream_tcpstream_return_byte_exampe/src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+use tokio::io::AsyncWriteExt;
+use tokio::net::tcp::OwnedReadHalf;
+use tokio::net::TcpStream;
+use tokio_stream::{Stream, StreamExt};
+
+use futures::stream::unfold;
+use tokio::io::AsyncReadExt;
+
+pub fn tcp_stream_into_stream(read_half: OwnedReadHalf) -> impl Stream<Item = io::Result<Vec<u8>>> {
+    unfold(read_half, |mut read_half| async {
+        let mut buffer = vec![0; 1024]; // Adjust the buffer size as needed
+        match read_half.read(&mut buffer).await {
+            Ok(0) => None, // End of stream
+            Ok(n) => {
+                buffer.truncate(n); // Resize buffer to the actual number of bytes read
+                Some((Ok(buffer), read_half))
+            }
+            Err(e) => Some((Err(e), read_half)),
+        }
+    })
+}
+
+// nc -l 3000
+
+#[tokio::main]
+async fn main() {
+    let tcp_stream = TcpStream::connect("127.0.0.1:3000").await.unwrap();
+    let (read_half, mut write_half) = tcp_stream.into_split();
+    write_half.write(r"hello world".as_bytes()).await.unwrap();
+
+    let message_stream = tcp_stream_into_stream(read_half);
+    tokio::pin!(message_stream);
+    while let Some(message_result) = message_stream.next().await {
+        match message_result {
+            Ok(message) => println!("Received message: {:?}", message),
+            Err(e) => eprintln!("Error reading message: {}", e),
+        }
+    }
+}
