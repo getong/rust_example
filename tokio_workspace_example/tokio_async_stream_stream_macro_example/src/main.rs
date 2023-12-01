31d6083fc4725d26039dff74da978b58bdea305e
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-01 23:49:39 +0800

    add tokio async_stream stream macro example

---
+++ tokio_workspace_example/tokio_async_stream_stream_macro_example/src/main.rs
@@ -0,0 +1,18 @@
+use async_stream::stream;
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() {
+    let s = stream! {
+        for i in 0..3 {
+            yield i;
+        }
+    };
+
+    // needed for iteration
+    tokio::pin!(s);
+
+    while let Some(value) = s.next().await {
+        println!("got {}", value);
+    }
+}
