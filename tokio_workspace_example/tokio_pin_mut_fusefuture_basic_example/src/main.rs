b3ba198c5cd0ef498f5d65fb45730626e3723808
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-04 00:01:30 +0800

    add tokio pin_mut fusefuture basic example

---
+++ tokio_workspace_example/tokio_pin_mut_fusefuture_basic_example/src/main.rs
@@ -0,0 +1,40 @@
+use futures::future::FutureExt; // For .fuse()
+use futures::pin_mut; // For pin_mut!
+use std::time::Duration;
+use tokio::time::sleep;
+
+// Example of an asynchronous function
+async fn async_function1() -> String {
+    sleep(Duration::from_secs(1)).await;
+    "Function 1 completed".to_string()
+}
+
+// Another asynchronous function
+async fn async_function2() -> String {
+    sleep(Duration::from_secs(2)).await;
+    "Function 2 completed".to_string()
+}
+
+#[tokio::main]
+async fn main() {
+    // Create futures from the async functions and fuse them
+    let future1 = async_function1().fuse();
+    let future2 = async_function2().fuse();
+
+    // Pin the futures
+    pin_mut!(future1, future2);
+
+    // Using `select!` to wait for the first future to complete
+    loop {
+        tokio::select! {
+            result = &mut future1 => {
+                println!("Future 1: {}", result);
+                // break;
+            },
+            result = &mut future2 => {
+                println!("Future 2: {}", result);
+                // break;
+            },
+        }
+    }
+}
