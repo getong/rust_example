4496757a91714b4d80686084098566130dcb1358
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-04 02:58:42 +0800

    add tokio future of future example

---
+++ tokio_workspace_example/tokio_future_of_future_example/src/main.rs
@@ -0,0 +1,37 @@
+// For .fuse()
+use futures::future::FutureExt;
+use std::future::Future;
+use std::pin::Pin;
+use tokio::time::{sleep, Duration};
+
+async fn inner_future(value: i32) -> i32 {
+    // Simulate some work
+    // sleep(Duration::from_secs(2)).await;
+    value * 2
+}
+
+async fn outer_future() -> Pin<Box<dyn Future<Output = i32>>> {
+    // Simulate some delay
+    // sleep(Duration::from_secs(1)).await;
+    Box::pin(inner_future(5))
+}
+
+#[tokio::main]
+async fn main() {
+    let outer = outer_future().fuse();
+    let timeout_duration = Duration::from_secs(3);
+    tokio::pin!(outer);
+
+    loop {
+        tokio::select! {
+            inner = &mut outer => {
+                // Await the inner future
+                let result = inner.await;
+                println!("Received: {}", result);
+            }
+            _ = sleep(timeout_duration) => {
+                println!("Timeout reached");
+            }
+        }
+    }
+}
