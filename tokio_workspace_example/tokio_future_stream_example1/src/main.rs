311af44d047032da8c14cfdc19fa99a5ec429b16
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-04 02:45:30 +0800

    add tokio Future<Stream>, Stream<Future> example

---
+++ tokio_workspace_example/tokio_future_stream_example1/src/main.rs
@@ -0,0 +1,40 @@
+use async_stream::stream;
+use futures::stream::Stream;
+use futures::StreamExt;
+use tokio::time::{sleep, Duration};
+
+async fn async_stream_generator() -> impl Stream<Item = i32> {
+    stream! {
+        for i in 0..5 {
+
+            yield i;
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let future_of_stream = async_stream_generator();
+    let stream = future_of_stream.await;
+
+    tokio::pin!(stream);
+    let timeout_duration = Duration::from_secs(3);
+
+    loop {
+        tokio::select! {
+            value = stream.next() => {
+                match value {
+                    Some(v) => println!("Stream value: {}", v),
+                    None => {
+                        println!("Stream ended");
+                        break;
+                    },
+                }
+            }
+            _ = sleep(timeout_duration) => {
+                println!("Timeout reached");
+                break;
+            }
+        }
+    }
+}
---
+++ tokio_workspace_example/tokio_stream_future_example/src/main.rs
@@ -0,0 +1,28 @@
+use async_stream::stream;
+use futures::{future::Future, Stream, StreamExt};
+
+async fn delayed_future(value: i32) -> i32 {
+    value
+}
+
+// Function that returns a Stream of Futures
+fn future_stream() -> impl Stream<Item = impl Future<Output = i32>> {
+    stream! {
+        for i in 1..=5 {
+            // Each item in the stream is a Future
+            let future = delayed_future(i);
+            yield future;
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stream = future_stream();
+    tokio::pin!(stream);
+    while let Some(future_item) = stream.next().await {
+        match future_item.await {
+            result => println!("Received: {}", result),
+        }
+    }
+}
