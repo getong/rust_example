89d4ce6f2ece76b753b3b653c93b542b1886d354
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-04 03:07:58 +0800

    add tokio stream of stream example

---
+++ tokio_workspace_example/tokio_stream_of_stream_example/src/main.rs
@@ -0,0 +1,36 @@
+use async_stream::stream;
+use std::pin::Pin;
+use tokio_stream::{Stream, StreamExt};
+
+// Function to create an inner stream
+fn inner_stream(start: i32, end: i32) -> impl Stream<Item = i32> {
+    stream! {
+        for i in start..end {
+            yield i;
+        }
+    }
+}
+
+// Function to create an outer stream of streams
+fn outer_stream() -> impl Stream<Item = Pin<Box<dyn Stream<Item = i32> + Send>>> {
+    stream! {
+        for i in 0..3 {
+            // Each item in the outer stream is an inner stream
+            let inner = inner_stream(i * 10, (i + 1) * 10);
+            yield Box::pin(inner) as Pin<Box<dyn Stream<Item = i32> + Send>>;
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let outer = outer_stream();
+    tokio::pin!(outer);
+    while let Some(inner) = outer.next().await {
+        println!("New inner stream:");
+        tokio::pin!(inner);
+        while let Some(value) = inner.next().await {
+            println!("Value: {}", value);
+        }
+    }
+}
