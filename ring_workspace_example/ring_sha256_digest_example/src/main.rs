3ebbc493a834983212ae5dd8788c9dd0860f01c4
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-07 09:04:24 +0800

    add ring sha256 digest example

---
+++ ring_workspace_example/ring_sha256_digest_example/src/main.rs
@@ -0,0 +1,44 @@
+use data_encoding::HEXUPPER;
+use error_chain::error_chain;
+use ring::digest::{Context, Digest, SHA256};
+use std::fs::File;
+use std::io::{BufReader, Read, Write};
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        Decode(data_encoding::DecodeError);
+    }
+}
+
+fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest> {
+    let mut context = Context::new(&SHA256);
+    let mut buffer = [0; 1024];
+
+    loop {
+        let count = reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        context.update(&buffer[..count]);
+    }
+
+    Ok(context.finish())
+}
+
+fn main() -> Result<()> {
+    let path = "file.txt";
+
+    let mut output = File::create(path)?;
+    write!(output, "We will generate a digest of this text")?;
+
+    let input = File::open(path)?;
+    let reader = BufReader::new(input);
+    let digest = sha256_digest(reader)?;
+
+    println!("SHA-256 digest is {}", HEXUPPER.encode(digest.as_ref()));
+
+    Ok(())
+}
+
+// copy from https://rust-lang-nursery.github.io/rust-cookbook/cryptography/hashing.html#calculate-the-sha-256-digest-of-a-file
