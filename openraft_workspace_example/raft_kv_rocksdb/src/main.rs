d317cdac15803f82f8eb4d419a93c778ec8736a9
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-11 07:38:55 +0800

    update code

--- openraft_workspace_example/raft_kv_rocksdb/src/main.rs
+++ openraft_workspace_example/raft_kv_rocksdb/src/main.rs
@@ -23,13 +23,13 @@ async fn main() -> std::io::Result<()> {
         // ... other configurations ...
         .init();
     // Setup the logger
-    // tracing_subscriber::fmt()
-    //     .with_target(true)
-    //     .with_thread_ids(true)
-    //     .with_level(true)
-    //     .with_ansi(false)
-    //     .with_env_filter(EnvFilter::from_default_env())
-    //     .init();
+    tracing_subscriber::fmt()
+        .with_target(true)
+        .with_thread_ids(true)
+        .with_level(true)
+        .with_ansi(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
 
     // Parse the parameters passed by arguments.
     let options = Opt::parse();
