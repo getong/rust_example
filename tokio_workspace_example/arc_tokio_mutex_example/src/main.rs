74aa9f7ee77167c716b715ef38b38c5358e0a3f0
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-14 04:18:51 +0800

    modify code

--- tokio_workspace_example/arc_tokio_mutex_example/src/main.rs
+++ tokio_workspace_example/arc_tokio_mutex_example/src/main.rs
@@ -1,16 +1,15 @@
-use tokio::sync::Mutex;
 use std::sync::Arc;
+use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() {
   let data1 = Arc::new(Mutex::new(0));
-  let data2 = Arc::clone(&data1);
-
-  tokio::spawn(async move {
-    let mut lock = data2.lock().await;
-    *lock += 1;
-  });
-
-  let mut lock = data1.lock().await;
-  *lock += 1;
+  let data2 = data1.clone();
+  loop {
+    let data3 = data2.clone();
+    tokio::spawn(async move {
+      let mut lock = data3.lock().await;
+      *lock += 1;
+    });
+  }
 }
