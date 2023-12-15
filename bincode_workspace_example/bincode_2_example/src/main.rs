4359f7a0c693d43ab890e607a4eaaf5dc163bae8
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-16 00:51:29 +0800

    add bincode 2 example.

---
+++ bincode_workspace_example/bincode_2_example/src/main.rs
@@ -0,0 +1,28 @@
+use bincode::{config, Decode, Encode};
+
+#[derive(Encode, Decode, PartialEq, Debug)]
+struct Entity {
+  x: f32,
+  y: f32,
+}
+
+#[derive(Encode, Decode, PartialEq, Debug)]
+struct World(Vec<Entity>);
+
+fn main() {
+  let config = config::standard();
+
+  let world = World(vec![Entity { x: 0.0, y: 4.0 }, Entity { x: 10.0, y: 20.5 }]);
+
+  let encoded: Vec<u8> = bincode::encode_to_vec(&world, config).unwrap();
+
+  // The length of the vector is encoded as a varint u64, which in this case gets collapsed to a single byte
+  // See the documentation on varint for more info for that.
+  // The 4 floats are encoded in 4 bytes each.
+  assert_eq!(encoded.len(), 1 + 4 * 4);
+
+  let (decoded, len): (World, usize) = bincode::decode_from_slice(&encoded[..], config).unwrap();
+
+  assert_eq!(world, decoded);
+  assert_eq!(len, encoded.len()); // read all bytes
+}
