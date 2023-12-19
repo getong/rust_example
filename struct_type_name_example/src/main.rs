eed30e91fb0dda3cd4fbd4beba3648218890b8f4
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-20 06:01:39 +0800

    add struct name type example

---
+++ struct_type_name_example/src/main.rs
@@ -0,0 +1,22 @@
+struct SomeThing;
+
+fn _get_name<T>(_: &T) -> &'static str {
+  std::any::type_name::<T>()
+}
+
+macro_rules! name_struct {
+  ($e:expr) => {
+    _get_name(&$e)
+  }
+}
+
+fn main() {
+  let thing = SomeThing {};
+  let name = name_struct!(thing);
+  // current package is struct_type_name_example
+  // so print struct_type_name_example::SomeThing
+  // on playground prints: "playground::SomeThing"
+  println!("{name}");
+}
+
+// copy from https://www.reddit.com/r/rust/comments/x99ojz/simple_macro_to_return_an_instances_struct_name/
