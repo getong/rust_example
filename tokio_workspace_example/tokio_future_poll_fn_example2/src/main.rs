a7cce50be1961dbb4c99083c6cac2010a49e527e
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-21 06:30:58 +0800

    add tokio future poll fn example 2

---
+++ tokio_workspace_example/tokio_future_poll_fn_example2/src/main.rs
@@ -0,0 +1,19 @@
+use futures::Future;
+use std::{pin::Pin, task::Poll, time::Duration};
+use tokio::{macros::support::poll_fn, time::sleep};
+
+#[tokio::main]
+async fn main() {
+  let mut sleep1 = sleep(Duration::from_secs(1));
+  let mut sleep1 = unsafe { Pin::new_unchecked(&mut sleep1) };
+
+  // this creates a future whose poll method is the closure argument
+  poll_fn(|cx| {
+    // we poll `sleep1` once, throwing away the result...
+    let _ = sleep1.as_mut().poll(cx);
+    println!("output from poll_fn");
+    // ...and resolve immediately
+    Poll::Ready(())
+  })
+  .await;
+}
