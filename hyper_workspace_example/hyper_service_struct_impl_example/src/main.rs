baea52cd8a3f8ae1c49715074d7c75fd46dc490f
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-25 23:31:28 +0800

    add hyper service struct impl example

---
+++ hyper_workspace_example/hyper_service_struct_impl_example/src/main.rs
@@ -0,0 +1,74 @@
+use bytes::Bytes;
+use http_body_util::Full;
+use hyper::server::conn::http1;
+use hyper::service::Service;
+use hyper::{body::Incoming as IncomingBody, Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+use std::future::Future;
+use std::net::SocketAddr;
+use std::pin::Pin;
+use std::sync::Mutex;
+
+type Counter = i32;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+  let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
+
+  let listener = TcpListener::bind(addr).await?;
+  println!("Listening on http://{}", addr);
+
+  loop {
+    let (stream, _) = listener.accept().await?;
+    let io = TokioIo::new(stream);
+
+    tokio::task::spawn(async move {
+      if let Err(err) = http1::Builder::new()
+        .serve_connection(
+          io,
+          Svc {
+            counter: Mutex::new(81818),
+          },
+        )
+        .await
+      {
+        println!("Failed to serve connection: {:?}", err);
+      }
+    });
+  }
+}
+
+struct Svc {
+  counter: Mutex<Counter>,
+}
+
+impl Service<Request<IncomingBody>> for Svc {
+  type Response = Response<Full<Bytes>>;
+  type Error = hyper::Error;
+  type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+  fn call(&self, req: Request<IncomingBody>) -> Self::Future {
+    fn mk_response(s: String) -> Result<Response<Full<Bytes>>, hyper::Error> {
+      Ok(Response::builder().body(Full::new(Bytes::from(s))).unwrap())
+    }
+
+    let res = match req.uri().path() {
+      "/" => mk_response(format!("home! counter = {:?}", self.counter)),
+      "/posts" => mk_response(format!("posts, of course! counter = {:?}", self.counter)),
+      "/authors" => mk_response(format!(
+        "authors extraordinare! counter = {:?}",
+        self.counter
+      )),
+      // Return the 404 Not Found for other routes, and don't increment counter.
+      _ => return Box::pin(async { mk_response("oh no! not found".into()) }),
+    };
+
+    if req.uri().path() != "/favicon.ico" {
+      *self.counter.lock().expect("lock poisoned") += 1;
+    }
+
+    Box::pin(async { res })
+  }
+}
