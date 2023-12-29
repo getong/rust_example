fce5205a2dbf7ada30e76c276b73bcb775d2e124
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-12-29 13:15:24 +0800

    disable turmoil grpc

--- tokio_workspace_example/tokio_turmoil_grpc_example/src/main.rs
+++ tokio_workspace_example/tokio_turmoil_grpc_example/src/main.rs
@@ -1,10 +1,7 @@
-use hyper::server::accept::from_stream;
-use hyper::Server;
 use std::net::{IpAddr, Ipv4Addr};
 use tonic::transport::Endpoint;
 use tonic::Status;
 use tonic::{Request, Response};
-use tower::make::Shared;
 use tracing::{info_span, Instrument};
 use turmoil::{net, Builder};
 
@@ -34,16 +31,26 @@ fn main() {
   sim.host("server", move || {
     let greeter = greeter.clone();
     async move {
-      Server::builder(from_stream(async_stream::stream! {
-        let listener = net::TcpListener::bind(addr).await?;
-        loop {
-          yield listener.accept().await.map(|(s, _)| s);
-        }
-      }))
-      .serve(Shared::new(greeter))
-      .await
-      .unwrap();
+      let listener = net::TcpListener::bind(addr).await?;
+      loop {
+        let (tcp_stream, _remote_addr) = listener.accept().await?;
+        let tcp_stream = hyper_util::rt::TokioIo::new(tcp_stream);
 
+        let hyper_service = hyper_util::service::TowerToHyperService::new(greeter);
+
+        let result =
+          hyper_util::server::conn::auto::Builder::new(hyper_util::rt::TokioExecutor::new())
+            .serve_connection_with_upgrades(tcp_stream, hyper_service)
+            .await;
+        if result.is_err() {
+          // This error only appears when the client doesn't send a request and
+          // terminate the connection.
+          //
+          // If client sends one request then terminate connection whenever, it doesn't
+          // appear.
+          break;
+        }
+      }
       Ok(())
     }
     .instrument(info_span!("server"))
@@ -87,68 +94,147 @@ impl Greeter for MyGreeter {
 }
 
 mod connector {
-  use std::{future::Future, pin::Pin};
-
-  use hyper::{
-    client::connect::{Connected, Connection},
-    Uri,
-  };
-  use tokio::io::{AsyncRead, AsyncWrite};
+  use hyper::Uri;
+  use pin_project_lite::pin_project;
+  use std::{future::Future, io::Error, pin::Pin};
+  use tokio::io::AsyncWrite;
   use tower::Service;
   use turmoil::net::TcpStream;
 
-  type Fut = Pin<Box<dyn Future<Output = Result<TurmoilConnection, std::io::Error>> + Send>>;
+  type Fut = Pin<Box<dyn Future<Output = Result<TurmoilConnection, Error>> + Send>>;
 
   pub fn connector(
-  ) -> impl Service<Uri, Response = TurmoilConnection, Error = std::io::Error, Future = Fut> + Clone
-  {
+  ) -> impl Service<Uri, Response = TurmoilConnection, Error = Error, Future = Fut> + Clone {
     tower::service_fn(|uri: Uri| {
       Box::pin(async move {
         let conn = TcpStream::connect(uri.authority().unwrap().as_str()).await?;
-        Ok::<_, std::io::Error>(TurmoilConnection(conn))
+        Ok::<_, Error>(TurmoilConnection { fut: conn })
       }) as Fut
     })
   }
 
-  pub struct TurmoilConnection(turmoil::net::TcpStream);
-
-  impl AsyncRead for TurmoilConnection {
-    fn poll_read(
-      mut self: std::pin::Pin<&mut Self>,
-      cx: &mut std::task::Context<'_>,
-      buf: &mut tokio::io::ReadBuf<'_>,
-    ) -> std::task::Poll<std::io::Result<()>> {
-      Pin::new(&mut self.0).poll_read(cx, buf)
+  pin_project! {
+    pub struct TurmoilConnection{
+      #[pin]
+      fut: turmoil::net::TcpStream
     }
   }
 
-  impl AsyncWrite for TurmoilConnection {
+  impl hyper::rt::Read for TurmoilConnection {
+    fn poll_read(
+      self: Pin<&mut Self>,
+      cx: &mut std::task::Context<'_>,
+      mut buf: hyper::rt::ReadBufCursor<'_>,
+    ) -> std::task::Poll<Result<(), Error>> {
+      let n = unsafe {
+        let mut tbuf = tokio::io::ReadBuf::uninit(buf.as_mut());
+        let result = tokio::io::AsyncRead::poll_read(self.project().fut, cx, &mut tbuf);
+        match result {
+          std::task::Poll::Ready(Ok(())) => tbuf.filled().len(),
+          other => return other,
+        }
+      };
+
+      unsafe {
+        buf.advance(n);
+      }
+      std::task::Poll::Ready(Ok(()))
+    }
+  }
+
+  impl hyper::rt::Write for TurmoilConnection {
     fn poll_write(
       mut self: Pin<&mut Self>,
       cx: &mut std::task::Context<'_>,
       buf: &[u8],
-    ) -> std::task::Poll<Result<usize, std::io::Error>> {
-      Pin::new(&mut self.0).poll_write(cx, buf)
+    ) -> std::task::Poll<Result<usize, Error>> {
+      Pin::new(&mut self.fut).poll_write(cx, buf)
     }
 
     fn poll_flush(
       mut self: Pin<&mut Self>,
       cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), std::io::Error>> {
-      Pin::new(&mut self.0).poll_flush(cx)
+    ) -> std::task::Poll<Result<(), Error>> {
+      Pin::new(&mut self.fut).poll_flush(cx)
     }
 
     fn poll_shutdown(
       mut self: Pin<&mut Self>,
       cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), std::io::Error>> {
-      Pin::new(&mut self.0).poll_shutdown(cx)
+    ) -> std::task::Poll<Result<(), Error>> {
+      Pin::new(&mut self.fut).poll_shutdown(cx)
     }
   }
 
-  impl Connection for TurmoilConnection {
-    fn connected(&self) -> hyper::client::connect::Connected {
-      Connected::new()
+  impl hyper_util::client::legacy::connect::Connection for TurmoilConnection {
+    fn connected(&self) -> hyper_util::client::legacy::connect::Connected {
+      hyper_util::client::legacy::connect::Connected::new()
     }
   }
 }
+// mod connector {
+//   use std::{future::Future, pin::Pin};
+
+//   use hyper::{
+//     client::connect::{Connected, Connection},
+//     Uri,
+//   };
+//   use tokio::io::{AsyncRead, AsyncWrite};
+//   use tower::Service;
+//   use turmoil::net::TcpStream;
+
+//   type Fut = Pin<Box<dyn Future<Output = Result<TurmoilConnection, std::io::Error>> + Send>>;
+
+//   pub fn connector(
+//   ) -> impl Service<Uri, Response = TurmoilConnection, Error = std::io::Error, Future = Fut> + Clone
+//   {
+//     tower::service_fn(|uri: Uri| {
+//       Box::pin(async move {
+//         let conn = TcpStream::connect(uri.authority().unwrap().as_str()).await?;
+//         Ok::<_, std::io::Error>(TurmoilConnection(conn))
+//       }) as Fut
+//     })
+//   }
+
+//   pub struct TurmoilConnection(turmoil::net::TcpStream);
+
+//   impl AsyncRead for TurmoilConnection {
+//     fn poll_read(
+//       mut self: std::pin::Pin<&mut Self>,
+//       cx: &mut std::task::Context<'_>,
+//       buf: &mut tokio::io::ReadBuf<'_>,
+//     ) -> std::task::Poll<std::io::Result<()>> {
+//       Pin::new(&mut self.0).poll_read(cx, buf)
+//     }
+//   }
+
+//   impl AsyncWrite for TurmoilConnection {
+//     fn poll_write(
+//       mut self: Pin<&mut Self>,
+//       cx: &mut std::task::Context<'_>,
+//       buf: &[u8],
+//     ) -> std::task::Poll<Result<usize, std::io::Error>> {
+//       Pin::new(&mut self.0).poll_write(cx, buf)
+//     }
+
+//     fn poll_flush(
+//       mut self: Pin<&mut Self>,
+//       cx: &mut std::task::Context<'_>,
+//     ) -> std::task::Poll<Result<(), std::io::Error>> {
+//       Pin::new(&mut self.0).poll_flush(cx)
+//     }
+
+//     fn poll_shutdown(
+//       mut self: Pin<&mut Self>,
+//       cx: &mut std::task::Context<'_>,
+//     ) -> std::task::Poll<Result<(), std::io::Error>> {
+//       Pin::new(&mut self.0).poll_shutdown(cx)
+//     }
+//   }
+
+//   impl Connection for TurmoilConnection {
+//     fn connected(&self) -> hyper::client::connect::Connected {
+//       Connected::new()
+//     }
+//   }
+// }
