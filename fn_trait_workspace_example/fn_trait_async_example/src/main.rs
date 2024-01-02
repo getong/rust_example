a5a1b287a23f5860661293a0a5a1e19732c8bc84
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-02 23:39:55 +0800

    add fn trait async example

---
+++ fn_trait_workspace_example/fn_trait_async_example/src/main.rs
@@ -0,0 +1,21 @@
+use std::future::Future;
+use std::pin::Pin;
+
+async fn async_square(num: i32) -> i32 {
+  tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+  num * num
+}
+
+fn apply_async_function<F, Fut>(f: F, value: i32) -> Pin<Box<dyn Future<Output = i32>>>
+where
+  F: Fn(i32) -> Fut,
+  Fut: Future<Output = i32> + 'static,
+{
+  Box::pin(f(value))
+}
+
+#[tokio::main]
+async fn main() {
+  let result = apply_async_function(async_square, 5).await;
+  println!("The square of 5 is {}", result);
+}
