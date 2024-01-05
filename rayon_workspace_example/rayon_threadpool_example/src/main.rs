323de3287009e6bf1b9cecbd1e05a28f14aa610d
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-05 19:17:05 +0800

    add running function

--- rayon_workspace_example/rayon_threadpool_example/src/main.rs
+++ rayon_workspace_example/rayon_threadpool_example/src/main.rs
@@ -1,3 +1,7 @@
+// use std::sync::{Arc, Mutex};
+use rayon::iter::IntoParallelRefIterator;
+use rayon::iter::ParallelIterator;
+
 fn main() {
   // println!("Hello, world!");
   let num_cpus = num_cpus::get();
@@ -5,4 +9,16 @@ fn main() {
     .num_threads(num_cpus)
     .build_global()
     .unwrap();
+
+  // let a = Arc::new(Mutex::new(0));
+  perform_parallel_task();
+}
+
+fn perform_parallel_task() {
+  let data: Vec<u64> = (9995..10000).collect();
+
+  let result: Vec<u64> = data.par_iter().map(|&x| x * x).collect();
+
+  // Optionally, do something with the result
+  println!("{:?}", result);
 }
