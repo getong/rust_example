236b2136189e252ba885240bed2884e778d8a056
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-09 12:58:29 +0800

    add Pin::get_unchecked_mut() code example

--- pin_api_workspace_example/get_mut_example/src/main.rs
+++ pin_api_workspace_example/get_mut_example/src/main.rs
@@ -1,8 +1,29 @@
+use std::marker::PhantomPinned;
 use std::pin::Pin;
+
+#[derive(Debug)]
+pub struct Test {
+  a: String,
+  _marker: PhantomPinned,
+}
 
 fn main() {
   let mut i: u8 = 8;
   let p = Pin::new(&mut i);
   *p.get_mut() = 9;
   assert_eq!(i, 9);
+
+  let mut t = Test {
+    a: String::from("abc"),
+    _marker: PhantomPinned,
+  };
+
+  let mut p2 = unsafe { Pin::new_unchecked(&mut t) };
+
+  unsafe {
+    let inner = p2.as_mut().get_unchecked_mut();
+    inner.a = String::from("def");
+  }
+
+  println!("p2: {:?}", p2);
 }
