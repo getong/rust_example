339258e11b30bbabd19fecf2eabe1b6698527932
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-10 02:35:45 +0800

    add send body string example

--- hyper_workspace_example/hyper_client_request_channel_example/src/main.rs
+++ hyper_workspace_example/hyper_client_request_channel_example/src/main.rs
@@ -1,6 +1,7 @@
 use bytes::Bytes;
 use http_body_util::BodyExt;
-use http_body_util::Empty;
+use http_body_util::Full;
+use hyper::Request;
 use hyper::Uri;
 use hyper_tls::HttpsConnector;
 use hyper_util::{client::legacy::Client, rt::TokioExecutor};
@@ -8,29 +9,32 @@ use tokio::sync::{mpsc, oneshot};
 
 type FutureResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
 
-enum Request {
-  Get(Uri, oneshot::Sender<Bytes>),
+enum LocalRequest {
+  Get(Uri, String, oneshot::Sender<Bytes>),
   Quit,
 }
 
-async fn https_layer(mut rx_chan: mpsc::Receiver<Request>) -> FutureResult<()> {
+async fn https_layer(mut rx_chan: mpsc::Receiver<LocalRequest>) -> FutureResult<()> {
   let https = HttpsConnector::new();
-  let client = Client::builder(TokioExecutor::new()).build::<_, Empty<Bytes>>(https);
+  let client = Client::builder(TokioExecutor::new()).build::<_, Full<Bytes>>(https);
 
   while let Some(request) = rx_chan.recv().await {
     match request {
-      Request::Get(url, response) => {
-        let resp = client.get(url).await?;
+      LocalRequest::Get(url, body_string, response) => {
+        let req = Request::builder()
+          .uri(url)
+          .body(Full::new(Bytes::from(body_string)))?;
+
+        let resp = client.request(req).await?;
 
         let reversed_body = resp.collect().await?.to_bytes();
-        // let body = String::from_utf8(reversed_body)?;
 
         _ = match response.send(reversed_body) {
           Ok(()) => Ok(()),
           _ => Err("send_bytes_error".to_string()),
         };
       }
-      Request::Quit => {
+      LocalRequest::Quit => {
         break;
       }
     }
@@ -39,10 +43,16 @@ async fn https_layer(mut rx_chan: mpsc::Receiver<Request>) -> FutureResult<()> {
   Ok(())
 }
 
-async fn get_html(url: Uri, tx_chan: mpsc::Sender<Request>) -> FutureResult<()> {
+async fn get_html(
+  url: Uri,
+  body_str: String,
+  tx_chan: mpsc::Sender<LocalRequest>,
+) -> FutureResult<()> {
   let (resp_tx, resp_rx) = oneshot::channel();
 
-  tx_chan.send(Request::Get(url.clone(), resp_tx)).await?;
+  tx_chan
+    .send(LocalRequest::Get(url.clone(), body_str, resp_tx))
+    .await?;
 
   let res = resp_rx.await?;
 
@@ -53,15 +63,25 @@ async fn get_html(url: Uri, tx_chan: mpsc::Sender<Request>) -> FutureResult<()> 
 
 #[tokio::main]
 async fn main() -> FutureResult<()> {
-  let (http_tx, http_rx) = mpsc::channel::<Request>(100);
+  let (http_tx, http_rx) = mpsc::channel::<LocalRequest>(100);
 
   let join_handle = tokio::spawn(https_layer(http_rx));
 
-  get_html(Uri::from_static("https://www.baidu.com"), http_tx.clone()).await?;
-  get_html(Uri::from_static("https://www.163.com"), http_tx.clone()).await?;
+  get_html(
+    Uri::from_static("https://www.baidu.com"),
+    "hello".to_string(),
+    http_tx.clone(),
+  )
+  .await?;
+  get_html(
+    Uri::from_static("https://www.163.com"),
+    "world".to_string(),
+    http_tx.clone(),
+  )
+  .await?;
 
   // Send a Quit request to stop the https_layer task
-  http_tx.send(Request::Quit).await?;
+  http_tx.send(LocalRequest::Quit).await?;
 
   // Wait for the https_layer task to complete
   let _ = join_handle.await?;
