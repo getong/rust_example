df469b42bbccbaee636cd077bd5f5266a7769151
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-12 13:01:08 +0800

    add leetcode workspace example

--- leetcode_workspace_example/leetcode_maximum_depth_tree_example/src/main.rs
+++ leetcode_workspace_example/leetcode_maximum_depth_tree_example/src/main.rs
@@ -1,21 +1,24 @@
 // Definition for a binary tree node.
-// #[derive(Debug, PartialEq, Eq)]
-// pub struct TreeNode {
-//   pub val: i32,
-//   pub left: Option<Rc<RefCell<TreeNode>>>,
-//   pub right: Option<Rc<RefCell<TreeNode>>>,
-// }
-//
-// impl TreeNode {
-//   #[inline]
-//   pub fn new(val: i32) -> Self {
-//     TreeNode {
-//       val,
-//       left: None,
-//       right: None
-//     }
-//   }
-// }
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None,
+    }
+  }
+}
+
+pub struct Solution;
+
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
@@ -23,7 +26,7 @@ impl Solution {
     Self::next_node(root.as_ref(), 0)
   }
 
-  fn next_node(node: Option<&Rc<RefCell<TreeNode>>>, mut num: i32) -> i32 {
+  pub fn next_node(node: Option<&Rc<RefCell<TreeNode>>>, mut num: i32) -> i32 {
     match node {
       None => num,
       Some(rc_node) => {
