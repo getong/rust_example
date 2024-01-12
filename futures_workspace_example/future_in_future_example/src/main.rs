28298e59db7d99e7e332df272ca97020512224b6
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-13 02:02:41 +0800

    add future in future example

---
+++ futures_workspace_example/future_in_future_example/src/main.rs
@@ -0,0 +1,48 @@
+use futures::channel::oneshot; // futures-preview@0.3.0-alpha.17
+use std::{
+  future::Future,
+  pin::Pin,
+  task::{Context, Poll},
+};
+
+pub struct ShutdownHandle {
+  sender: oneshot::Sender<()>,
+  receiver: oneshot::Receiver<()>,
+}
+
+impl ShutdownHandle {
+  pub fn new() -> Self {
+    let (sender, receiver) = oneshot::channel();
+    Self { sender, receiver }
+  }
+
+  pub fn shutdown(self) -> Result<(), ()> {
+    self.sender.send(())
+  }
+}
+
+// impl Future for ShutdownHandle {
+//   type Output = ();
+
+//   fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+//     self.receiver.poll(&mut cx).map(|_| ())
+//   }
+// }
+
+impl Future for ShutdownHandle {
+  type Output = ();
+
+  fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+    // I copied this code from Stack Overflow without reading the text that
+    // told me how to verify that this code uses `unsafe` correctly.
+    unsafe { self.map_unchecked_mut(|s| &mut s.receiver) }.poll(cx).map(|_| ())
+  }
+}
+
+#[tokio::main]
+async fn main() {
+  let runner = ShutdownHandle::new();
+  assert!(runner.shutdown().is_ok());
+}
+
+// copy from https://stackoverflow.com/questions/57369123/no-method-named-poll-found-for-a-type-that-implements-future
