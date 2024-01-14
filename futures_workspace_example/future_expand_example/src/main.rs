5d89261e58d7a3f440a532aeedec527a156cce36
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-14 10:52:41 +0800

    update code by using loop

--- futures_workspace_example/future_expand_example/src/main.rs
+++ futures_workspace_example/future_expand_example/src/main.rs
@@ -1,14 +1,37 @@
-use std::fs;
 use std::future::Future;
-use std::io::{Error, Write};
+use std::io::Write;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+use std::{fmt, fs};
 
 enum WriteHelloFile {
   Init(String),
   AwaitingCreate(Pin<Box<dyn Future<Output = Result<fs::File, std::io::Error>>>>),
   AwaitingWrite(Pin<Box<dyn Future<Output = Result<(), std::io::Error>>>>),
   Done,
+}
+
+impl fmt::Debug for WriteHelloFile {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      WriteHelloFile::Init(s) => write!(f, "Init({})", s),
+      WriteHelloFile::AwaitingCreate(_) => write!(f, "AwaitingCreate(...)"),
+      WriteHelloFile::AwaitingWrite(_) => write!(f, "AwaitingWrite(...)"),
+      WriteHelloFile::Done => write!(f, "Done"),
+    }
+  }
+}
+
+impl PartialEq for WriteHelloFile {
+  fn eq(&self, other: &Self) -> bool {
+    match (self, other) {
+      (WriteHelloFile::Init(s1), WriteHelloFile::Init(s2)) => s1 == s2,
+      (WriteHelloFile::AwaitingCreate(_), WriteHelloFile::AwaitingCreate(_)) => true,
+      (WriteHelloFile::AwaitingWrite(_), WriteHelloFile::AwaitingWrite(_)) => true,
+      (WriteHelloFile::Done, WriteHelloFile::Done) => true,
+      _ => false,
+    }
+  }
 }
 
 impl WriteHelloFile {
@@ -45,7 +68,7 @@ impl Future for WriteHelloFile {
         Poll::Ready(Err(e)) => return Poll::Ready(Err(e)),
         Poll::Pending => return Poll::Pending,
       },
-      WriteHelloFile::Done => return Poll::Ready(Err(Error::from(std::io::ErrorKind::Other))),
+      WriteHelloFile::Done => return Poll::Ready(Ok(())),
     }
   }
 }
@@ -57,6 +80,18 @@ async fn main() {
   // _ = (&mut write_file).await;
   // _ = (&mut write_file).await;
   // _ = (&mut write_file).await;
-  while let Ok(_) = (&mut write_file).await {}
-  println!("done");
+  loop {
+    match (&mut write_file).await {
+      Ok(_) => {
+        if write_file == WriteHelloFile::Done {
+          println!("done");
+          break;
+        }
+      }
+      Err(e) => {
+        println!("error reason, e: {:?}", e);
+        break;
+      }
+    }
+  }
 }
