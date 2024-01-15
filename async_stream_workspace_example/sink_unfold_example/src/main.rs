7b8aef5e8e04bcb0e2fa2aa6cf9023c4bbea5323
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-16 02:41:20 +0800

    add sink unfold example

---
+++ async_stream_workspace_example/sink_unfold_example/src/main.rs
@@ -0,0 +1,28 @@
+use anyhow::Result;
+use futures::prelude::*;
+use tokio::{fs::File, io::AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  let file_sink = writer(File::create("/tmp/hello").await?);
+  // pin_mut 可以把变量 pin 住
+  futures::pin_mut!(file_sink);
+  if let Err(_) = file_sink.send("hello\n").await {
+    println!("Error on send");
+  }
+  if let Err(_) = file_sink.send("world!\n").await {
+    println!("Error on send");
+  }
+  Ok(())
+}
+
+/// 使用 unfold 生成一个 Sink 数据结构
+fn writer<'a>(file: File) -> impl Sink<&'a str> {
+  sink::unfold(file, |mut file, line: &'a str| async move {
+    file.write_all(line.as_bytes()).await?;
+    eprint!("Received: {}", line);
+    Ok::<_, std::io::Error>(file)
+  })
+}
+
+// copy from https://github.com/tyrchen/geektime-rust
