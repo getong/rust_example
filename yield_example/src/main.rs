417a18fa068104481835668bf82a16aec9d75d97
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-15 23:43:30 +0800

    update code

--- yield_example/src/main.rs
+++ yield_example/src/main.rs
@@ -1,29 +1,33 @@
-#![feature(generators, generator_trait)]
+#![feature(coroutines, coroutine_trait)]
 
-use std::ops::{Generator, GeneratorState};
+use std::ops::Coroutine;
+use std::ops::CoroutineState;
 use std::pin::Pin;
 
 fn main() {
+  // Define a coroutine for Fibonacci sequence
   let mut generator = || {
     let mut curr: u64 = 1;
     let mut next: u64 = 1;
+
     loop {
       let new_next = curr.checked_add(next);
 
       if let Some(new_next) = new_next {
         curr = next;
         next = new_next;
-        yield curr; // <-- 新的关键字
+        yield curr; // Produce the current Fibonacci value
       } else {
-        return;
+        return; // End the coroutine if overflow occurs
       }
     }
   };
 
+  // Resume the coroutine in a loop
   loop {
     match Pin::new(&mut generator).resume(()) {
-      GeneratorState::Yielded(v) => println!("{}", v),
-      GeneratorState::Complete(_) => return,
+      CoroutineState::Yielded(v) => println!("{}", v), // Print the yielded Fibonacci value
+      CoroutineState::Complete(_) => return,           // Exit the loop when the coroutine is done
     }
   }
 }
