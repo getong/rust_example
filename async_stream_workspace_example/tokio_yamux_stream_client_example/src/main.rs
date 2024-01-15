0404c06be4ec58e4bd84bbe60ac38cfcdd3f4e15
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-01-16 05:03:14 +0800

    add tokio yamux stream client example

---
+++ async_stream_workspace_example/tokio_yamux_stream_client_example/src/main.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use futures::prelude::*;
+use tokio::net::TcpStream;
+use tokio_util::{
+  codec::{Framed, LinesCodec},
+  compat::{FuturesAsyncReadCompatExt, TokioAsyncReadCompatExt},
+};
+use tracing::info;
+use yamux::{Config, Connection, Mode};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  tracing_subscriber::fmt::init();
+  let stream = TcpStream::connect("127.0.01:8080").await?;
+  info!("Connected to server");
+  let mut config = Config::default();
+  config.set_split_send_size(4 * 1024);
+  let mut conn = Connection::new(stream.compat(), config, Mode::Client);
+
+  // poll 所有 stream 下的数据
+  tokio::spawn(
+    stream::poll_fn(move |cx| {
+      conn
+        .poll_new_outbound(cx)
+        .map(|result| result.map(Some).transpose())
+    })
+    .try_for_each_concurrent(None, move |stream| async move {
+      let stream = stream.compat();
+      info!("Started a new stream");
+      let mut framed = Framed::new(stream, LinesCodec::new());
+      framed
+        .send("Hello, this is Tyr!".to_string())
+        .await
+        .unwrap();
+      if let Some(Ok(line)) = framed.next().await {
+        println!("Got: {}", line);
+      }
+      Ok(())
+    }),
+  );
+
+  Ok(())
+}
+
+// copy from https://github.com/tyrchen/geektime-rust
+// modified with chatpgpt
