2535aa0a981bbd8a72446bd5b618d9f7b5d676eb
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-02-28 00:14:15 +0800

    add hyper custom connect trait example

---
+++ hyper_workspace_example/hyper_custom_connect_trait_example/src/main.rs
@@ -0,0 +1,53 @@
+use futures_util::future::{BoxFuture, FutureExt};
+use http_body_util::Empty;
+use hyper::{Request, Uri};
+use hyper_util::client::legacy::{connect::HttpConnector, Client};
+use std::error::Error;
+use std::task::{Context, Poll};
+use tower_service::Service;
+
+// Define your custom connector type.
+#[derive(Clone)]
+struct MyConnector {
+  http: HttpConnector,
+}
+
+impl MyConnector {
+  fn new() -> Self {
+    MyConnector {
+      http: HttpConnector::new(),
+    }
+  }
+}
+
+impl Service<Uri> for MyConnector {
+  type Response = <HttpConnector as Service<Uri>>::Response;
+  type Error = <HttpConnector as Service<Uri>>::Error;
+  type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+  fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+    self.http.poll_ready(cx)
+  }
+
+  fn call(&mut self, uri: Uri) -> Self::Future {
+    // Here you can add your custom connection logic.
+    // For demonstration, we're just using the HttpConnector directly.
+    self.http.call(uri).boxed()
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+  let my_connector = MyConnector::new();
+  let client = Client::builder(hyper_util::rt::TokioExecutor::new()).build(my_connector);
+
+  let url = "http://httpbin.org/ip";
+  let req = Request::builder()
+    .uri(url)
+    .body(Empty::<bytes::Bytes>::new())?;
+
+  let response = client.request(req).await?;
+
+  println!("Response: {}", response.status());
+  Ok(())
+}
