c9acd397b57c82358df06c563705cd1f63383d54
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-03-16 16:42:06 +0800

    add mlua hyper client example

---
+++ hyper_workspace_example/mlua_hyper_client_example/src/main.rs
@@ -0,0 +1,69 @@
+use bytes::Bytes;
+use http_body_util::BodyExt;
+use http_body_util::Empty;
+use hyper_util::client::legacy::Client as HyperClient;
+use std::collections::HashMap;
+
+use mlua::{chunk, ExternalResult, Lua, Result, UserData, UserDataMethods};
+
+struct BodyReader(Bytes);
+
+impl UserData for BodyReader {
+  fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
+    methods.add_async_method_mut("read", |lua, reader, ()| async move {
+      let bytes = reader.0.to_vec();
+      return Some(lua.create_string(&bytes)).transpose();
+    });
+  }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+  let lua = Lua::new();
+
+  let fetch_url = lua.create_async_function(|lua, uri: String| async move {
+    let client =
+      HyperClient::builder(hyper_util::rt::TokioExecutor::new()).build_http::<Empty<Bytes>>();
+    let uri = uri.parse().into_lua_err()?;
+    let resp = client.get(uri).await.into_lua_err()?;
+
+    let lua_resp = lua.create_table()?;
+    lua_resp.set("status", resp.status().as_u16())?;
+
+    let mut headers = HashMap::new();
+    for (key, value) in resp.headers() {
+      headers
+        .entry(key.as_str())
+        .or_insert(Vec::new())
+        .push(value.to_str().into_lua_err()?);
+    }
+
+    lua_resp.set("headers", headers)?;
+    lua_resp.set(
+      "body",
+      BodyReader(resp.into_body().collect().await.unwrap().to_bytes()),
+    )?;
+
+    Ok(lua_resp)
+  })?;
+
+  let f = lua
+    .load(chunk! {
+      local res = $fetch_url(...)
+        print("status: "..res.status)
+        for key, vals in pairs(res.headers) do
+        for _, val in ipairs(vals) do
+        print(key..": "..val)
+        end
+        end
+        repeat
+        local body = res.body:read()
+        if body then
+        print(body)
+        end
+        until not body
+    })
+    .into_function()?;
+
+  f.call_async("http://httpbin.org/ip").await
+}
