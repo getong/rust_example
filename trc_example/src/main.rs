b2a6aa6068516728dd076a550a27983a6a9ab7fb
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-03-18 23:00:22 +0800

    update trc code

--- trc_example/src/main.rs
+++ trc_example/src/main.rs
@@ -6,8 +6,7 @@ use trc::Weak;
 fn basic_trc() {
   let mut trc = Trc::new(100);
   assert_eq!(*trc, 100);
-  let mutref = unsafe { trc.deref_mut() };
-  *mutref = 200;
+  *Trc::get_mut(&mut trc).unwrap() = 200;
   assert_eq!(*trc, 200);
 }
 
@@ -15,36 +14,29 @@ fn multiple_thread_trc() {
   let trc = Trc::new(100);
   let shared = SharedTrc::from_trc(&trc);
   let handle = thread::spawn(move || {
-    let mut trc = SharedTrc::to_trc(shared);
-    *unsafe { Trc::deref_mut(&mut trc) } = 200;
+    let _trc = SharedTrc::to_trc(shared);
   });
 
   handle.join().unwrap();
-  assert_eq!(*trc, 200);
+  assert_eq!(*trc, 100);
 }
 
 fn weak_trc() {
   let trc = Trc::new(100);
-  let weak = Weak::from_trc(&trc);
-  let mut new_trc = Weak::to_trc(&weak).unwrap();
-  println!("Deref test! {}", *new_trc);
-  println!("DerefMut test");
-  *unsafe { Trc::deref_mut(&mut new_trc) } = 200;
-  println!("Deref test! {}", *new_trc);
+  let weak = Trc::downgrade(&trc);
+  let new_trc = Weak::upgrade(&weak).unwrap();
+  assert_eq!(*new_trc, 100);
 }
 
 fn multiple_weak_trc() {
   let trc = Trc::new(100);
-  let weak = Weak::from_trc(&trc);
-
+  let shared = SharedTrc::from_trc(&trc);
   let handle = thread::spawn(move || {
-    let mut trc = Weak::to_trc(&weak).unwrap();
-    println!("{:?}", *trc);
-    *unsafe { Trc::deref_mut(&mut trc) } = 200;
+    let trc = SharedTrc::to_trc(shared);
+    assert_eq!(*trc, 100);
   });
   handle.join().unwrap();
-  println!("{}", *trc);
-  assert_eq!(*trc, 200);
+  assert_eq!(*trc, 100);
 }
 
 fn main() {
