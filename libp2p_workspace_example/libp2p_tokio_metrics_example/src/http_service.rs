9837c20f943bea01846aeda90d713d54a4422122
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-03-28 23:01:15 +0800

    delete bytes, return String

--- libp2p_workspace_example/libp2p_tokio_metrics_example/src/http_service.rs
+++ libp2p_workspace_example/libp2p_tokio_metrics_example/src/http_service.rs
@@ -1,9 +1,7 @@
-use bytes::Bytes;
-use http_body_util::Full;
 use hyper::http::StatusCode;
 use hyper::server::conn::http1;
 use hyper::service::Service;
-use hyper::{body::Incoming as IncomingBody, Method, Request, Response};
+use hyper::{body::Incoming, Method, Request, Response};
 use hyper_util::rt::TokioIo;
 use prometheus_client::encoding::text::encode;
 use prometheus_client::registry::Registry;
@@ -18,7 +16,7 @@ const METRICS_CONTENT_TYPE: &str = "application/openmetrics-text;charset=utf-8;v
 pub(crate) async fn metrics_server(registry: Registry) -> Result<(), std::io::Error> {
   // Serve on localhost.
   let addr: SocketAddr = ([127, 0, 0, 1], 0).into();
-
+  tracing::info!(metrics_server=%format!("http://{:?}/metrics", addr));
   let make_metrics_service = MakeMetricService::new(registry);
   let listener = TcpListener::bind(addr).await?;
   loop {
@@ -36,19 +34,19 @@ pub(crate) async fn metrics_server(registry: Registry) -> Result<(), std::io::Er
   }
 }
 
-type SharedRegistry = Arc<Mutex<Registry>>;
-
 #[derive(Debug, Clone)]
 pub(crate) struct MetricService {
   reg: SharedRegistry,
 }
 
+type SharedRegistry = Arc<Mutex<Registry>>;
+
 impl MetricService {
   fn get_reg(&mut self) -> SharedRegistry {
     Arc::clone(&self.reg)
   }
-  fn respond_with_metrics(&mut self) -> Response<Full<Bytes>> {
-    let mut response: Response<Full<Bytes>> = Response::default();
+  fn respond_with_metrics(&mut self) -> Response<String> {
+    let mut response: Response<String> = Response::default();
 
     response.headers_mut().insert(
       hyper::header::CONTENT_TYPE,
@@ -58,29 +56,26 @@ impl MetricService {
     let reg = self.get_reg();
     let mut inner_str = String::new();
     encode(&mut inner_str, &reg.lock().unwrap()).unwrap();
-    *response.body_mut() = Full::new(Bytes::from(inner_str));
+    *response.body_mut() = inner_str;
 
     *response.status_mut() = StatusCode::OK;
 
     response
   }
-
-  fn respond_with_404_not_found(&mut self) -> Response<Full<Bytes>> {
+  fn respond_with_404_not_found(&mut self) -> Response<String> {
     Response::builder()
       .status(StatusCode::NOT_FOUND)
-      .body(Full::new(Bytes::from(
-        "Not found try localhost:[port]/metrics".to_string(),
-      )))
+      .body("Not found try localhost:[port]/metrics".to_string())
       .unwrap()
   }
 }
 
-impl Service<Request<IncomingBody>> for MetricService {
-  type Response = Response<Full<Bytes>>;
+impl Service<Request<Incoming>> for MetricService {
+  type Response = Response<String>;
   type Error = hyper::Error;
   type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
-  fn call(&self, req: Request<IncomingBody>) -> Self::Future {
+  fn call(&self, req: Request<Incoming>) -> Self::Future {
     let req_path = req.uri().path();
     let req_method = req.method();
     let resp = if (req_method == Method::GET) && (req_path == "/metrics") {
@@ -106,12 +101,12 @@ impl MakeMetricService {
   }
 }
 
-impl Service<Request<IncomingBody>> for MakeMetricService {
-  type Response = Response<Full<Bytes>>;
+impl Service<Request<Incoming>> for MakeMetricService {
+  type Response = Response<String>;
   type Error = hyper::Error;
   type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
-  fn call(&self, req: Request<IncomingBody>) -> Self::Future {
+  fn call(&self, req: Request<Incoming>) -> Self::Future {
     let reg = self.reg.clone();
     let fut = async move { Ok(MetricService { reg }.call(req).await.unwrap()) };
     Box::pin(fut)
