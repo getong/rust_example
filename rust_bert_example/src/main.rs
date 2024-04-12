5f8227638c819707662e15220ebe7e2c13c74df5
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-04-12 23:58:11 +0800

    rust_bert example

---
+++ rust_bert_example/src/main.rs
@@ -0,0 +1,12 @@
+// brew install pytorch jq
+// export LIBTORCH=$(brew --cellar pytorch)/$(brew info --json pytorch | jq -r '.[0].installed[0].version')
+// export LD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
+fn main() {
+  let qa_model = QuestionAnsweringModel::new(Default::default())?;
+
+  let question = String::from("Where does Amy live ?");
+  let context = String::from("Amy lives in Amsterdam");
+
+  let answers = qa_model.predict(&[QaInput { question, context }], 1, 32);
+  println!("answers: {:?}", answers);
+}
