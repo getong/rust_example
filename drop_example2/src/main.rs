d3d874c934745c8eb89892d5b3f41da4e7e81560
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-04-26 23:32:06 +0800

    add drop example 2

---
+++ drop_example2/src/main.rs
@@ -0,0 +1,21 @@
+struct DropHelper {
+  int: i32,
+}
+impl Drop for DropHelper {
+  fn drop(&mut self) {
+    println!("Dropping DropHelper with int: {}", self.int);
+  }
+}
+
+fn main() {
+  let mut drop_helper = DropHelper { int: 42 };
+  let join = std::thread::spawn(move || {
+    println!("Thread spawned");
+    drop_helper.int = 43;
+  });
+  _ = join.join();
+}
+
+
+// copy from https://github.com/rust-lang/rust/issues/108808
+// unread_partial_move_field
