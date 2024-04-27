259861f2f9ba5adab1cf1455aa69437f9c03854e
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-04-28 02:43:41 +0800

    fmt code and add linker flag

--- mlua_workspace_example/mlua_exec_lua_script_example/src/main.rs
+++ mlua_workspace_example/mlua_exec_lua_script_example/src/main.rs
@@ -2,11 +2,11 @@ use mlua::{Lua, Result};
 use std::fs;
 
 fn main() -> Result<()> {
-    let lua = Lua::new(); // Create a new Lua state
-    let script_content = fs::read_to_string("src/factorial.lua")?; // Read the Lua script file
+  let lua = Lua::new(); // Create a new Lua state
+  let script_content = fs::read_to_string("src/factorial.lua")?; // Read the Lua script file
 
-    // Load and execute the Lua script
-    lua.load(&script_content).exec()?;
+  // Load and execute the Lua script
+  lua.load(&script_content).exec()?;
 
-    Ok(())
+  Ok(())
 }
