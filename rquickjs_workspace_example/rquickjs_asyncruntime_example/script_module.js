0e53195df4b013bb1022c332e41f626de1724254
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-05-01 05:52:23 +0800

    read file, and then eval

---
+++ rquickjs_workspace_example/rquickjs_asyncruntime_example/script_module.js
@@ -0,0 +1,5 @@
+// export const n = 456;
+// export const s = 233;
+// // export const f = (a, b) => (a * b);
+// s + n;
+11 + 122;
--- rquickjs_workspace_example/rquickjs_asyncruntime_example/src/main.rs
+++ rquickjs_workspace_example/rquickjs_asyncruntime_example/src/main.rs
@@ -1,6 +1,8 @@
 use rquickjs::{async_with, AsyncContext, AsyncRuntime};
 use std::time::Duration;
-use tokio::time::sleep;
+use tokio::{fs::metadata, select, time::sleep};
+
+const FILE_NAME: &str = "script_module.js";
 
 #[tokio::main]
 async fn main() {
@@ -12,13 +14,35 @@ async fn main() {
   async_with!(&ctx => |ctx| {
     // Enter a loop to evaluate JavaScript code repeatedly
     loop {
-      let code_str = "1 + 1";
-      let res: i32 = ctx.eval(code_str).unwrap();
-      println!("res is {}", res);
-      assert_eq!(res, 2i32);
+      // Asynchronously read file information
+      let metadata = metadata(FILE_NAME);
 
-      // Sleep for a duration before the next iteration
-      sleep(Duration::from_secs(1)).await;
+      // Define the code_str variable to store JavaScript code
+      let mut code_str = String::new();
+
+      // Use tokio::select to handle both reading file info and loop iteration concurrently
+      select! {
+        // If metadata reading is successful, read the file and assign its content to code_str
+        Ok(_metadata) = metadata => {
+          if let Ok(file_content) = tokio::fs::read_to_string(FILE_NAME).await {
+            code_str = file_content;
+          } else {
+            println!("Failed to read file content");
+          }
+        }
+        // Handle loop iteration with a sleep duration of 1 second
+        _ = sleep(Duration::from_secs(1)) => {}
+      }
+
+      // Evaluate JavaScript code if code_str is not empty
+      if !code_str.is_empty() {
+        // println!("&*code_str is {}", &*code_str);
+        if let Ok(res) = ctx.eval::<i32, &str>(&*code_str) {
+          println!("Result: {:?}", res);
+        } else {
+          println!("Failed to evaluate JavaScript code");
+        }
+      }
     }
   })
   .await;
