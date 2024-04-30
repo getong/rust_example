9c1c9d5b51c496996e713fc7bafb6da1b66f4d02
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-05-01 05:22:49 +0800

    add loop statement

--- rquickjs_workspace_example/rquickjs_asyncruntime_example/src/main.rs
+++ rquickjs_workspace_example/rquickjs_asyncruntime_example/src/main.rs
@@ -1,15 +1,25 @@
 use rquickjs::{async_with, AsyncContext, AsyncRuntime};
+use std::time::Duration;
+use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
+  // Initialize AsyncRuntime and AsyncContext
   let rt = AsyncRuntime::new().unwrap();
   let ctx = AsyncContext::full(&rt).await.unwrap();
 
-  async_with!(&ctx => |ctx|{
-    let code_str = "1 + 1";
-    let res: i32 = ctx.eval(code_str).unwrap();
-    println!("res is {}", res);
-    assert_eq!(res,2i32);
+  // Call the async_with! macro to execute the asynchronous block
+  async_with!(&ctx => |ctx| {
+    // Enter a loop to evaluate JavaScript code repeatedly
+    loop {
+      let code_str = "1 + 1";
+      let res: i32 = ctx.eval(code_str).unwrap();
+      println!("res is {}", res);
+      assert_eq!(res, 2i32);
+
+      // Sleep for a duration before the next iteration
+      sleep(Duration::from_secs(1)).await;
+    }
   })
   .await;
 }
