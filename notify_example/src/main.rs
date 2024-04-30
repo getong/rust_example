a02547738e74725ab85f80fcc457a94f2df72d8f
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-05-01 03:06:35 +0800

    update code

--- notify_example/src/main.rs
+++ notify_example/src/main.rs
@@ -1,15 +1,25 @@
 use notify::{RecursiveMode, Result, Watcher};
 use std::path::Path;
+use tokio::sync::watch;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
   // Automatically select the best implementation for your platform.
-  let mut watcher = notify::recommended_watcher(|res| match res {
-    Ok(event) => println!("event: {:?}", event),
+  let (tx, mut rx) = watch::channel(());
+
+  let mut watcher = notify::recommended_watcher(move |res| match res {
+    Ok(_event) => {
+      _ = tx.send(());
+    }
     Err(e) => println!("watch error: {:?}", e),
   })?;
 
   // Add a path to be watched. All files and directories at that path and
   // below will be monitored for changes.
   watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
-  loop {}
+
+  loop {
+    _ = rx.changed().await;
+    println!("file changed");
+  }
 }
