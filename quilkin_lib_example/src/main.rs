70dce4fac91ccf25087637f785570008f1a5e1f8
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-05-06 19:48:49 +0800

    update quilkin code

--- quilkin_lib_example/src/main.rs
+++ quilkin_lib_example/src/main.rs
@@ -1,14 +1,23 @@
-use clap::Parser as _;
+fn main() {
+  tokio::runtime::Builder::new_multi_thread()
+    .enable_all()
+    .thread_name_fn(|| {
+      static ATOMIC_ID: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
+      let id = ATOMIC_ID.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+      format!("tokio-main-{id}")
+    })
+    .build()
+    .unwrap()
+    .block_on(async {
+      // Unwrap is safe here as it will only fail if called more than once.
+      stable_eyre::install().unwrap();
 
-#[tokio::main]
-async fn main() -> quilkin::Result<()> {
-  stable_eyre::install().unwrap();
-
-  match quilkin::Cli::parse().drive(None).await {
-    Ok(()) => std::process::exit(0),
-    Err(error) => {
-      tracing::error!(%error, error_debug=?error, "fatal error");
-      std::process::exit(-1)
-    }
-  }
+      match <quilkin::Cli as clap::Parser>::parse().drive(None).await {
+        Ok(()) => std::process::exit(0),
+        Err(error) => {
+          tracing::error!(%error, error_debug=?error, "fatal error");
+          std::process::exit(-1)
+        }
+      }
+    })
 }
