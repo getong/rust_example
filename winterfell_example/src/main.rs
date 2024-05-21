cbc074726106458f3dc35326162947c7c780f9ad
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-05-21 18:52:37 +0800

    add winterfell example

---
+++ winterfell_example/src/main.rs
@@ -0,0 +1,13 @@
+use winterfell::math::{fields::f128::BaseElement, FieldElement};
+
+fn do_work(start: BaseElement, n: usize) -> BaseElement {
+  let mut result = start;
+  for _ in 1..n {
+    result = result.exp(3) + BaseElement::new(42);
+  }
+  result
+}
+
+fn main() {
+  // println!("Hello, world!");
+}
