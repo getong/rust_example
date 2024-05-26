6f1f8672826d165bd1b8f80efbc91bdb0bb0a713
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-05-27 00:32:04 +0800

    output executed result

--- walkdir_workspace_example/walkdir_call_command_example/src/main.rs
+++ walkdir_workspace_example/walkdir_call_command_example/src/main.rs
@@ -1,8 +1,9 @@
 use std::env::set_current_dir;
+use std::io::Read;
 use std::path::Path;
 use std::process::{Command, Stdio};
 use tokio::fs;
-use walkdir::{DirEntry, WalkDir};
+use walkdir::WalkDir;
 
 const RENAME_DIRECTORY: &str = "/tmp/a/";
 const RENAME_ARGUMENT: &str = "s/abc//g";
@@ -26,11 +27,30 @@ async fn main() {
 
 async fn change_dir_and_run_command(dir_path: &Path) {
   if let Ok(_) = set_current_dir(dir_path) {
-    Command::new("prel-rename")
+    let mut all_files: String = String::new();
+    for first_entry in WalkDir::new(dir_path)
+      .min_depth(1)
+      .max_depth(1)
+      .into_iter()
+      .filter_map(|entry| entry.ok())
+    {
+      if let Some(src) = first_entry.path().to_str() {
+        all_files.push_str(" ");
+        all_files.push_str(src);
+      }
+    }
+    if let Ok(process) = Command::new("/usr/bin/perl-rename")
       .arg("-n")
       .arg(RENAME_ARGUMENT)
-      .arg("*")
+      .arg(&all_files)
+      .stdout(Stdio::piped())
       .spawn()
-      .expect("perl-rename command failed to start");
+    {
+      let mut output = String::new();
+      match process.stdout.unwrap().read_to_string(&mut output) {
+        Err(err) => panic!("couldn't read ps stdout: {}", err),
+        Ok(_) => print!("ps output from child process is : \n{}", output),
+      }
+    }
   }
 }
