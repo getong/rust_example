57c4e06602d2faeeb87c87e8ca9f3196e388eadd
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-06-02 00:46:36 +0800

    add sonic_rs example

---
+++ sonic_rs_example/src/main.rs
@@ -0,0 +1,24 @@
+use sonic_rs::JsonValueTrait;
+use sonic_rs::{get, get_unchecked, pointer};
+
+fn main() {
+  let path = pointer!["a", "b", "c", 1];
+  let json = r#"
+        {"u": 123, "a": {"b" : {"c": [null, "found"]}}}
+    "#;
+  let target = unsafe { get_unchecked(json, &path).unwrap() };
+  assert_eq!(target.as_raw_str(), r#""found""#);
+  assert_eq!(target.as_str().unwrap(), "found");
+
+  let target = get(json, &path);
+  assert_eq!(target.as_str().unwrap(), "found");
+  assert_eq!(target.unwrap().as_raw_str(), r#""found""#);
+
+  let path = pointer!["a", "b", "c", "d"];
+  let json = r#"
+        {"u": 123, "a": {"b" : {"c": [null, "found"]}}}
+    "#;
+  // not found from json
+  let target = get(json, &path);
+  assert!(target.is_err());
+}
