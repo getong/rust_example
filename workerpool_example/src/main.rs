43f12140584e13fa1e778d589bc9cd3b1b76e37b
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-07-11 00:34:28 +0800

    add workerpool example

---
+++ workerpool_example/src/main.rs
@@ -0,0 +1,62 @@
+use std::io::prelude::*;
+use std::io::{self, BufReader};
+use std::process::{ChildStdin, ChildStdout, Command, Stdio};
+use std::sync::mpsc::channel;
+use workerpool::{Pool, Worker};
+
+struct LineDelimitedProcess {
+  stdin: ChildStdin,
+  stdout: BufReader<ChildStdout>,
+}
+impl Default for LineDelimitedProcess {
+  fn default() -> Self {
+    let child = Command::new("cat")
+      .stdin(Stdio::piped())
+      .stdout(Stdio::piped())
+      .stderr(Stdio::inherit())
+      .spawn()
+      .unwrap();
+    Self {
+      stdin: child.stdin.unwrap(),
+      stdout: BufReader::new(child.stdout.unwrap()),
+    }
+  }
+}
+impl Worker for LineDelimitedProcess {
+  type Input = Box<[u8]>;
+  type Output = io::Result<String>;
+
+  fn execute(&mut self, inp: Self::Input) -> Self::Output {
+    self.stdin.write_all(&*inp)?;
+    self.stdin.write_all(b"\n")?;
+    self.stdin.flush()?;
+    let mut s = String::new();
+    self.stdout.read_line(&mut s)?;
+    s.pop(); // exclude newline
+    Ok(s)
+  }
+}
+
+fn main() {
+  let n_workers = 4;
+  let n_jobs = 8;
+  let pool = Pool::<LineDelimitedProcess>::new(n_workers);
+
+  let (tx, rx) = channel();
+  for i in 0..n_jobs {
+    let inp = Box::new([97 + i]);
+    pool.execute_to(tx.clone(), inp);
+  }
+
+  // output is a permutation of "abcdefgh"
+  let mut output = rx
+    .iter()
+    .take(n_jobs as usize)
+    .fold(String::new(), |mut a, b| {
+      a.push_str(&b.unwrap());
+      a
+    })
+    .into_bytes();
+  output.sort();
+  assert_eq!(output, b"abcdefgh");
+}
