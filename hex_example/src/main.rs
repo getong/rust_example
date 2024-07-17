45c71da99b3b682a53d2fba4b366b9888a299b53
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-07-17 14:33:28 +0800

    update hex code example

--- hex_example/src/main.rs
+++ hex_example/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
-  // println!("Hello, world!");
   let hex_str: &str = "hello world";
   let hex = hex::encode(hex_str);
 
   println!("hex: {:?}", hex);
+
+  let bytes = hex::decode(hex).expect("Decoding failed");
+  // Convert bytes back to a string
+  let original_str = String::from_utf8(bytes).expect("Invalid UTF-8");
+
+  println!("Original string: {:?}", original_str);
+  assert_eq!(hex_str, original_str);
 }
