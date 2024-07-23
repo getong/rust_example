353e71edd88db0ac46ff73e0cb0014883cfaff03
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-07-23 23:25:41 +0800

    use oneshot to group two task

--- tokio_workspace_example/tokio_two_task_group_example/src/main.rs
+++ tokio_workspace_example/tokio_two_task_group_example/src/main.rs
@@ -1,8 +1,9 @@
+use rand::Rng;
 use tokio::sync::oneshot;
 use tokio::task;
 use tokio::time::{sleep, Duration};
 
-async fn task_1(mut stop_rx: oneshot::Receiver<()>) {
+async fn task_1(mut stop_rx: oneshot::Receiver<()>, send_tx: oneshot::Sender<()>) {
   tokio::select! {
     _ = async {
       loop {
@@ -14,20 +15,24 @@ async fn task_1(mut stop_rx: oneshot::Receiver<()>) {
       println!("Task 1 received stop signal.");
     }
   }
+
+  _ = send_tx.send(());
 }
 
-async fn task_2(mut stop_rx: oneshot::Receiver<()>) {
+async fn task_2(mut stop_rx: oneshot::Receiver<()>, send_tx: oneshot::Sender<()>) {
   tokio::select! {
-    _ = async {
-      loop {
-        println!("Task 2 is working...");
-        sleep(Duration::from_secs(1)).await;
-      }
-    } => {},
-    _ = &mut stop_rx => {
-      println!("Task 2 received stop signal.");
+  _ = async {
+    loop {
+      println!("Task 2 is working...");
+      sleep(Duration::from_secs(1)).await;
     }
+  } => {},
+  _ = &mut stop_rx => {
+    println!("Task 2 received stop signal.");
   }
+  }
+
+  _ = send_tx.send(());
 }
 
 #[tokio::main]
@@ -35,19 +40,33 @@ async fn main() {
   let (stop_tx1, stop_rx1) = oneshot::channel();
   let (stop_tx2, stop_rx2) = oneshot::channel();
 
-  let task1_handle = task::spawn(task_1(stop_rx1));
-  let task2_handle = task::spawn(task_2(stop_rx2));
+  let task1_handle = task::spawn(task_1(stop_rx1, stop_tx2));
+  let task2_handle = task::spawn(task_2(stop_rx2, stop_tx1));
 
   // Simulate some work
-  sleep(Duration::from_secs(5)).await;
+  sleep(Duration::from_secs(3)).await;
 
-  // Send the stop signal
-  let _ = stop_tx1.send(());
-  let _ = stop_tx2.send(());
+  let mut rng = rand::thread_rng();
+  match rng.gen_range(1u64..3u64) {
+    1 => {
+      println!("task 1 stop");
+      task1_handle.abort();
+    }
+
+    2 => {
+      println!("task 2 stop");
+      task2_handle.abort();
+    }
+    _ => {
+      println!("task 2 stop");
+      task2_handle.abort();
+    }
+  }
 
   // Wait for both tasks to finish
   let _ = task1_handle.await;
   let _ = task2_handle.await;
 
+  sleep(Duration::from_secs(3)).await;
   println!("Both tasks have stopped.");
 }
