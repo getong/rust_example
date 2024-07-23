3663b22da5be477d450429435d69491d38dfc084
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-07-23 23:05:38 +0800

    use oneshot

--- tokio_workspace_example/tokio_two_task_group_example/src/main.rs
+++ tokio_workspace_example/tokio_two_task_group_example/src/main.rs
@@ -1,8 +1,8 @@
-use tokio::sync::watch;
+use tokio::sync::oneshot;
 use tokio::task;
 use tokio::time::{sleep, Duration};
 
-async fn task_1(mut stop_rx: watch::Receiver<()>) {
+async fn task_1(mut stop_rx: oneshot::Receiver<()>) {
   tokio::select! {
     _ = async {
       loop {
@@ -10,13 +10,13 @@ async fn task_1(mut stop_rx: watch::Receiver<()>) {
         sleep(Duration::from_secs(1)).await;
       }
     } => {},
-    _ = stop_rx.changed() => {
+    _ = &mut stop_rx => {
       println!("Task 1 received stop signal.");
     }
   }
 }
 
-async fn task_2(mut stop_rx: watch::Receiver<()>) {
+async fn task_2(mut stop_rx: oneshot::Receiver<()>) {
   tokio::select! {
     _ = async {
       loop {
@@ -24,7 +24,7 @@ async fn task_2(mut stop_rx: watch::Receiver<()>) {
         sleep(Duration::from_secs(1)).await;
       }
     } => {},
-    _ = stop_rx.changed() => {
+    _ = &mut stop_rx => {
       println!("Task 2 received stop signal.");
     }
   }
@@ -32,16 +32,18 @@ async fn task_2(mut stop_rx: watch::Receiver<()>) {
 
 #[tokio::main]
 async fn main() {
-  let (stop_tx, stop_rx) = watch::channel(());
+  let (stop_tx1, stop_rx1) = oneshot::channel();
+  let (stop_tx2, stop_rx2) = oneshot::channel();
 
-  let task1_handle = task::spawn(task_1(stop_rx.clone()));
-  let task2_handle = task::spawn(task_2(stop_rx));
+  let task1_handle = task::spawn(task_1(stop_rx1));
+  let task2_handle = task::spawn(task_2(stop_rx2));
 
   // Simulate some work
   sleep(Duration::from_secs(5)).await;
 
   // Send the stop signal
-  let _ = stop_tx.send(());
+  let _ = stop_tx1.send(());
+  let _ = stop_tx2.send(());
 
   // Wait for both tasks to finish
   let _ = task1_handle.await;
