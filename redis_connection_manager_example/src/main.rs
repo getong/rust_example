12c6eb2e9a6d43ed2f2c74ce7089625d56afc609
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-07-28 15:31:21 +0800

    update redis to 0.26

--- redis_connection_manager_example/src/main.rs
+++ redis_connection_manager_example/src/main.rs
@@ -6,14 +6,14 @@ async fn main() {
   let redis_url = "redis://bert:abc123@127.0.0.1:6379/";
   let redis_client = Client::open(redis_url).unwrap();
   let mut redis_connection_manager = redis_client
-    .get_tokio_connection_manager()
+    .get_connection_manager()
     .await
     .expect("can't create redis connection manager");
 
   let _: () = redis::pipe()
     .atomic()
     .set("abc", 1u8)
-    .expire("abc", 60usize)
+    .expire("abc", 60i64)
     .query_async(&mut redis_connection_manager)
     .await
     .unwrap();
