f3823e008255de2e94800eb34d695e82d9e88162
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-08-06 15:16:21 +0800

    add post parameter code

--- axum_workspace_example/axum_handle_parameter_example/src/main.rs
+++ axum_workspace_example/axum_handle_parameter_example/src/main.rs
@@ -1,7 +1,25 @@
-use axum::{routing::get, Router};
-
 use axum::extract::Query;
+use axum::{
+  extract::Path,
+  routing::{get, post},
+  Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize)]
+pub struct Params {
+  pub apikey: String,
+  pub channel: Option<String>,
+  pub ep_name: Option<String>,
+  pub block: Option<String>,
+}
+
+pub async fn sign(Path(project): Path<String>, Query(params): Query<Params>) -> Json<Value> {
+  // Your handler logic here
+  Json(json!({ "project": project, "params": params }))
+}
 
 // curl "http://localhost:3000/hello?data=2"
 // curl "http://localhost:3000/hello?hello=2"
@@ -14,7 +32,9 @@ async fn query(Query(params): Query<HashMap<String, String>>) -> String {
 
 #[tokio::main]
 async fn main() {
-  let router = Router::new().route("/hello", get(query));
+  let router = Router::new()
+    .route("/hello", get(query))
+    .route("/sign/:project", post(sign));
 
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
