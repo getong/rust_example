55580d4724ec183c71c26d2c1245c38149337542
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-08-09 22:21:43 +0800

    add parking_lot crate

--- tokio_workspace_example/tokio_10k_tasks_10_batch_via_mpsc_example/src/main.rs
+++ tokio_workspace_example/tokio_10k_tasks_10_batch_via_mpsc_example/src/main.rs
@@ -1,6 +1,8 @@
 use tokio::sync::mpsc::{self, Sender};
 const BATCH_SIZE: usize = 10;
 const TOTAL_SIZE: usize = 10000;
+use parking_lot::RwLock;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
@@ -9,9 +11,12 @@ async fn main() {
   let mut current_size: usize = BATCH_SIZE;
   let (tx, mut rx) = mpsc::channel(BATCH_SIZE);
 
+  let data = Arc::new(RwLock::new(5));
+
   for i in 0 .. BATCH_SIZE {
     let tx = tx.clone();
-    spawn_task(tx, i).await;
+    let data = data.clone();
+    spawn_task(tx, i, data).await;
   }
 
   while let Some(num) = rx.recv().await {
@@ -24,21 +29,32 @@ async fn main() {
       let tx = tx.clone();
       current_size = current_size + 1;
       if current_size <= TOTAL_SIZE {
-        spawn_task(tx, current_size).await;
+        let data = data.clone();
+        spawn_task(tx, current_size, data).await;
       }
     }
   }
+
+  let data = data.read();
+  println!("data is {:?}", *data);
 }
 
-async fn perform_task(tx: Sender<usize>, id: usize) {
+async fn perform_task(tx: Sender<usize>, id: usize, data: Arc<RwLock<usize>>) {
   println!("Task {} is running", id);
+  if id % 2 == 0 {
+    let mut data = data.write();
+    *data = *data + 1;
+  } else {
+    let data = data.read();
+    println!("data is {:?}", *data);
+  }
   // Simulate some async work
   // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
   _ = tx.try_send(id);
 }
 
-async fn spawn_task(tx: Sender<usize>, id: usize) {
+async fn spawn_task(tx: Sender<usize>, id: usize, data: Arc<RwLock<usize>>) {
   tokio::spawn(async move {
-    perform_task(tx, id).await;
+    perform_task(tx, id, data).await;
   });
 }
