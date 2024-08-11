ec114c8525c02538369fd6384f3f1b8bfd321b2c
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-08-11 11:14:01 +0800

    add axum tower_sessions example

---
+++ axum_workspace_example/axum_tower_sessions_example/src/main.rs
@@ -0,0 +1,33 @@
+use std::net::SocketAddr;
+
+use axum::{response::IntoResponse, routing::get, Router};
+use serde::{Deserialize, Serialize};
+use time::Duration;
+use tower_sessions::{Expiry, MemoryStore, Session, SessionManagerLayer};
+
+const COUNTER_KEY: &str = "counter";
+
+#[derive(Default, Deserialize, Serialize)]
+struct Counter(usize);
+
+async fn handler(session: Session) -> impl IntoResponse {
+  let counter: Counter = session.get(COUNTER_KEY).await.unwrap().unwrap_or_default();
+  session.insert(COUNTER_KEY, counter.0 + 1).await.unwrap();
+  format!("Current count: {}", counter.0)
+}
+
+#[tokio::main]
+async fn main() {
+  let session_store = MemoryStore::default();
+  let session_layer = SessionManagerLayer::new(session_store)
+    .with_secure(false)
+    .with_expiry(Expiry::OnInactivity(Duration::seconds(10)));
+
+  let app = Router::new().route("/", get(handler)).layer(session_layer);
+
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+  let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+  axum::serve(listener, app.into_make_service())
+    .await
+    .unwrap();
+}
