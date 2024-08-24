ce502fd4f5ff452edba475e68dc413f8947930d5
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-08-24 18:47:46 +0800

    add raft_kv_memstore_network_v2 example

---
+++ openraft_workspace_example/raft_kv_memstore_network_v2/src/lib.rs
@@ -0,0 +1,107 @@
+#![allow(clippy::uninlined_format_args)]
+#![deny(unused_qualifications)]
+
+use std::sync::Arc;
+
+use openraft::Config;
+
+use crate::app::App;
+use crate::router::Router;
+use crate::store::Request;
+use crate::store::Response;
+use crate::store::StateMachineData;
+
+pub mod router;
+
+pub mod api;
+pub mod app;
+mod log_store;
+pub mod network;
+pub mod store;
+
+pub type NodeId = u64;
+
+openraft::declare_raft_types!(
+    /// Declare the type configuration for example K/V store.
+    pub TypeConfig:
+        D = Request,
+        R = Response,
+        // In this example, snapshot is just a copy of the state machine.
+        // And it can be any type.
+        SnapshotData = StateMachineData,
+);
+
+pub type LogStore = store::LogStore;
+pub type StateMachineStore = store::StateMachineStore;
+
+pub mod typ {
+
+  use crate::NodeId;
+  use crate::TypeConfig;
+
+  pub type Raft = openraft::Raft<TypeConfig>;
+
+  pub type Vote = openraft::Vote<NodeId>;
+  pub type SnapshotMeta = openraft::SnapshotMeta<TypeConfig>;
+  pub type SnapshotData = <TypeConfig as openraft::RaftTypeConfig>::SnapshotData;
+  pub type Snapshot = openraft::Snapshot<TypeConfig>;
+
+  pub type Infallible = openraft::error::Infallible;
+  pub type Fatal = openraft::error::Fatal<TypeConfig>;
+  pub type RaftError<E = openraft::error::Infallible> = openraft::error::RaftError<TypeConfig, E>;
+  pub type RPCError = openraft::error::RPCError<TypeConfig>;
+  pub type StreamingError = openraft::error::StreamingError<TypeConfig>;
+
+  pub type RaftMetrics = openraft::RaftMetrics<TypeConfig>;
+
+  pub type ClientWriteError = openraft::error::ClientWriteError<TypeConfig>;
+  pub type CheckIsLeaderError = openraft::error::CheckIsLeaderError<TypeConfig>;
+  pub type ForwardToLeader = openraft::error::ForwardToLeader<TypeConfig>;
+  pub type InitializeError = openraft::error::InitializeError<TypeConfig>;
+
+  pub type ClientWriteResponse = openraft::raft::ClientWriteResponse<TypeConfig>;
+}
+
+pub fn encode<T: serde::Serialize>(t: T) -> String {
+  serde_json::to_string(&t).unwrap()
+}
+
+pub fn decode<T: serde::de::DeserializeOwned>(s: &str) -> T {
+  serde_json::from_str(s).unwrap()
+}
+
+pub async fn new_raft(node_id: NodeId, router: Router) -> (typ::Raft, App) {
+  // Create a configuration for the raft instance.
+  let config = Config {
+    heartbeat_interval: 500,
+    election_timeout_min: 1500,
+    election_timeout_max: 3000,
+    // Once snapshot is built, delete the logs at once.
+    // So that all further replication will be based on the snapshot.
+    max_in_snapshot_log_to_keep: 0,
+    ..Default::default()
+  };
+
+  let config = Arc::new(config.validate().unwrap());
+
+  // Create a instance of where the Raft logs will be stored.
+  let log_store = LogStore::default();
+
+  // Create a instance of where the state machine data will be stored.
+  let state_machine_store = Arc::new(StateMachineStore::default());
+
+  // Create a local raft instance.
+  let raft = openraft::Raft::new(
+    node_id,
+    config,
+    router.clone(),
+    log_store,
+    state_machine_store.clone(),
+  )
+  .await
+  .unwrap();
+
+  let app = App::new(node_id, raft.clone(), router, state_machine_store);
+
+  (raft, app)
+}
---
+++ openraft_workspace_example/raft_kv_memstore_network_v2/src/network.rs
@@ -0,0 +1,75 @@
+use std::future::Future;
+
+use openraft::error::ReplicationClosed;
+use openraft::network::v2::RaftNetworkV2;
+use openraft::network::RPCOption;
+use openraft::raft::AppendEntriesRequest;
+use openraft::raft::AppendEntriesResponse;
+use openraft::raft::SnapshotResponse;
+use openraft::raft::VoteRequest;
+use openraft::raft::VoteResponse;
+use openraft::BasicNode;
+use openraft::OptionalSend;
+use openraft::RaftNetworkFactory;
+use openraft::Snapshot;
+use openraft::Vote;
+
+use crate::router::Router;
+use crate::typ;
+use crate::NodeId;
+use crate::TypeConfig;
+
+pub struct Connection {
+  router: Router,
+  target: NodeId,
+}
+
+impl RaftNetworkFactory<TypeConfig> for Router {
+  type Network = Connection;
+
+  async fn new_client(&mut self, target: NodeId, _node: &BasicNode) -> Self::Network {
+    Connection {
+      router: self.clone(),
+      target,
+    }
+  }
+}
+
+impl RaftNetworkV2<TypeConfig> for Connection {
+  async fn append_entries(
+    &mut self,
+    req: AppendEntriesRequest<TypeConfig>,
+    _option: RPCOption,
+  ) -> Result<AppendEntriesResponse<TypeConfig>, typ::RPCError> {
+    let resp = self.router.send(self.target, "/raft/append", req).await?;
+    Ok(resp)
+  }
+
+  /// A real application should replace this method with customized implementation.
+  async fn full_snapshot(
+    &mut self,
+    vote: Vote<NodeId>,
+    snapshot: Snapshot<TypeConfig>,
+    _cancel: impl Future<Output = ReplicationClosed> + OptionalSend + 'static,
+    _option: RPCOption,
+  ) -> Result<SnapshotResponse<TypeConfig>, typ::StreamingError> {
+    let resp = self
+      .router
+      .send(
+        self.target,
+        "/raft/snapshot",
+        (vote, snapshot.meta, snapshot.snapshot),
+      )
+      .await?;
+    Ok(resp)
+  }
+
+  async fn vote(
+    &mut self,
+    req: VoteRequest<TypeConfig>,
+    _option: RPCOption,
+  ) -> Result<VoteResponse<TypeConfig>, typ::RPCError> {
+    let resp = self.router.send(self.target, "/raft/vote", req).await?;
+    Ok(resp)
+  }
+}
