cab886197f23fd5941918d8f140f04a7d2ec824b
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-08-27 15:08:09 +0800

    add bincode example

--- serde_workspace_example/serde-basic-example/src/main.rs
+++ serde_workspace_example/serde-basic-example/src/main.rs
@@ -34,14 +34,31 @@ fn main() {
 
   println!("\n\n");
   {
-    println!("To and from binary");
+    println!("To and from binary by using serde_json");
     let serialized = serde_json::to_string(&config)
       .unwrap()
       .as_bytes()
       .to_owned();
-    println!("{:?}", serialized);
+    println!("{:?}\nlength is {}", serialized, serialized.len());
     let deserialized: ServerConfig =
       serde_json::from_str(&String::from_utf8(serialized).unwrap()).unwrap();
     println!("{:?}", deserialized);
+  }
+
+  println!("\n\n");
+  {
+    println!("To and from binary by using bincode");
+
+    // Serialize the config struct to a binary format
+    let serialized = bincode::serialize(&config).unwrap();
+    println!(
+      "Serialized binary: {:?}\nlength is {}",
+      serialized,
+      serialized.len()
+    );
+
+    // Deserialize the binary data back into a ServerConfig struct
+    let deserialized: ServerConfig = bincode::deserialize(&serialized).unwrap();
+    println!("Deserialized struct: {:?}", deserialized);
   }
 }
