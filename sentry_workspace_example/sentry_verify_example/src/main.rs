0b7ebc2f9c6574cf53d48380f5e9a3bf81ebcd11
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-09-02 17:32:25 +0800

    add sentry verify example

---
+++ sentry_workspace_example/sentry_verify_example/src/main.rs
@@ -0,0 +1,12 @@
+const SENTRY_DSN: &str = "your-sentry-dsn";
+
+fn main() {
+  let _guard = sentry::init((SENTRY_DSN, sentry::ClientOptions {
+    release: sentry::release_name!(),
+    ..Default::default()
+  }));
+
+  println!("Hello, world!");
+  // Sentry will capture this
+  panic!("Everything is on fire!");
+}
