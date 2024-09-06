37038be80064917ed0959ac4627164a04dd9d5b2
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-09-06 17:15:13 +0800

    add more convert between bidecimal and U256 code example

--- sqlx_workspace_example/u256_bigdecimal_example/src/main.rs
+++ sqlx_workspace_example/u256_bigdecimal_example/src/main.rs
@@ -10,12 +10,28 @@ fn u256_to_bigdecimal(value: U256) -> BigDecimal {
   BigDecimal::from_str(&value_str).unwrap()
 }
 
+fn bigdecimal_to_u256(value: BigDecimal) -> U256 {
+  // Convert BigDecimal to string
+  let value_str = value.to_string();
+
+  // Parse the string into U256
+  U256::from_dec_str(&value_str).unwrap()
+}
+
 #[tokio::main]
 async fn main() {
   // Example usage
-  let u256_value = U256::from(123456789012345678901234567890u128); // Example U256 value
+  let u256_value: U256 = U256::from_dec_str("123456789012345678901234567890").unwrap(); // Example U256 value
 
-  let big_decimal_value = u256_to_bigdecimal(u256_value);
-
+  // Convert U256 to BigDecimal
+  let big_decimal_value: BigDecimal = u256_to_bigdecimal(u256_value);
   println!("BigDecimal value: {}", big_decimal_value);
+
+  // Convert BigDecimal back to U256
+  let u256_converted_back: U256 = bigdecimal_to_u256(big_decimal_value.clone());
+  println!("U256 value: {}", u256_converted_back);
+  let big_decimal_value_back: BigDecimal = u256_to_bigdecimal(u256_converted_back.clone());
+
+  assert_eq!(u256_value, u256_converted_back);
+  assert_eq!(big_decimal_value, big_decimal_value_back);
 }
