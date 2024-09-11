338dbe3af15105ece7b5f575b8cea3e2a306ecad
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-09-11 16:10:04 +0800

    add extra info

--- sentry_workspace_example/sentry_crash_example/src/main.rs
+++ sentry_workspace_example/sentry_crash_example/src/main.rs
@@ -1,11 +1,49 @@
-use std::env;
+use sentry::{protocol::Event, Level};
+use std::{env, sync::Arc};
 
 const SENTRY_DSN: &str = env!("SENTRY_DSN");
+
+fn before_send(mut event: Event<'static>) -> Option<Event<'static>> {
+  if let Some(name) = std::thread::current().name() {
+    event.extra.insert("thread.name".to_string(), name.into());
+  }
+
+  event.extra.insert("hello".to_string(), "world".into());
+
+  let ex = match event
+    .exception
+    .values
+    .iter()
+    .next()
+    .and_then(|ex| ex.value.as_ref())
+  {
+    Some(ex) => ex,
+    None => return Some(event),
+  };
+
+  // Group events via fingerprint, or ignore
+
+  if ex.starts_with("DBError failed to open the database") {
+    event.level = Level::Warning;
+  } else if ex.contains("SqliteFailure") {
+    event.level = Level::Warning;
+  } else if ex.starts_with("DBError the database version")
+    || ex.contains("kind: AddrInUse")
+    || ex.contains("kind: AddrNotAvailable")
+    || ex.contains("IO error: No space left")
+  {
+    // ignore
+    return None;
+  }
+
+  Some(event)
+}
 
 fn main() {
   let _sentry = sentry::init((
     SENTRY_DSN,
     sentry::ClientOptions {
+      before_send: Some(Arc::new(Box::new(before_send))),
       release: sentry::release_name!(),
       debug: true,
       auto_session_tracking: true,
