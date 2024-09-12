39b659d4ec6fd0f230df2f7d6cd85da6970e207d
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-09-12 17:28:01 +0800

    update test code

--- reqwest_workspace_example/reqwest_post_data_example/src/main.rs
+++ reqwest_workspace_example/reqwest_post_data_example/src/main.rs
@@ -5,6 +5,7 @@ const URL1: &str = "http://192.168.80.222:8009/statistic-queries";
 const URL2: &str = "http://192.168.80.222:8009/deployment-price-count";
 const URL3: &str =
   "http://192.168.80.222:8009/user_aggregation/0xbf3a286a477967ebd850cee2dbdbfa6e535a9e64";
+const URL4: &str = "http://192.168.80.222:8009/multi_user_aggregation";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -85,6 +86,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
     .await
     .expect("failed to get response")
     .text()
+    .await
+    .expect("failed to get payload");
+
+  println!("{:#?}", res);
+
+  let json4 = json!({
+      "user_list": ["0xbf3a286a477967ebd850cee2dbdbfa6e535a9e64", "0xa499b9c52547db14d13216dcd73da0d4d43ba66e", "0x5e15ce35a3821e15d36988d9e0dd181c7c371a07"],
+      "start": "2024-09-01",
+  });
+
+  let res = client
+    .post(URL4)
+    .json(&json4)
+    .send()
+    .await
+    .expect("failed to get response")
+    .json::<Value>()
     .await
     .expect("failed to get payload");
 
