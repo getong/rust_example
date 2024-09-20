2e191abce8d923ae7bbfa89e3fbcbc901b776a27
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-09-20 13:54:01 +0800

    add string split example

---
+++ string_workspace_example/string_split_example/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = "abc |||cde";
+    let first_part = input.split("|||").next().unwrap_or(input).trim();
+    println!("first part of {} || is {}", input , first_part);
+
+    let input = "abc cde";
+    let first_part = input.split("|||").next().unwrap().trim();
+    println!("first part of {} || is {}", input , first_part);
+}
