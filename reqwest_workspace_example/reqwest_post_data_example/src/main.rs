ce19a5a3628471dfbcf051369fe1637f555003cb
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-09-23 14:38:16 +0800

    update to use base_url

--- reqwest_workspace_example/reqwest_post_data_example/src/main.rs
+++ reqwest_workspace_example/reqwest_post_data_example/src/main.rs
@@ -1,130 +1,116 @@
 use serde_json::{json, Value};
 use std::error::Error;
 
-const URL1: &str = "http://192.168.80.222:8009/statistic-queries";
-const URL2: &str = "http://192.168.80.222:8009/deployment-price-count";
-const URL3: &str =
-  "http://192.168.80.222:8009/user_aggregation/0xbf3a286a477967ebd850cee2dbdbfa6e535a9e64";
-const URL4: &str = "http://192.168.80.222:8009/multi_user_aggregation";
+const BASE_URL: &str = "http://192.168.80.222:8009";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
   let client = reqwest::Client::new();
 
+  let url1 = format!("{}/statistic-queries", BASE_URL);
+
+  // First request
   let json1 = json!({
       "deployment": ["QmbReTnhCweQHmbXxgffkDqkkedo7ojjsUWTKopP1auuTp", "QmTfhYrb3wusYS715KvHfaL56R8M1SrM8vwhuLyYVehfKB"],
-  "start_date": "2024-09-01",
+      "start_date": "2024-09-01",
   });
 
   let res = client
-    .post(URL1)
+    .post(&url1)
     .json(&json1)
     .send()
-    .await
-    .expect("failed to get response")
+    .await?
     .json::<Value>()
-    .await
-    .expect("failed to get payload");
+    .await?;
 
   println!("1: {:#?}", res);
 
+  // Second request
   let json2 = json!({
       "indexer": ["0xbf3a286a477967ebd850cee2dbdbfa6e535a9e64", "0xbf3a286a477967ebd850cee2dbdbfa622222"],
       "start_date": "2024-09-01",
   });
 
-  let res = client
-    .post(URL1)
-    .json(&json2)
-    .send()
-    .await
-    .expect("failed to get response")
-    .text()
-    .await
-    .expect("failed to get payload");
+  let res = client.post(&url1).json(&json2).send().await?.text().await?;
 
   println!("2: {:#?}", res);
 
+  // Third request
   let json3 = json!({
       "indexer": ["0xbf3a286a477967ebd850cee2dbdbfa6e535a9e64", "0xbf3a286a477967ebd850cee2dbdbfa6e5222224"],
       "deployment": ["QmbReTnhCweQHmbXxgffkDqkkedo7ojjsUWTKopP1auuTp", "QmTfhYrb3wusYS715KvHfaL56R8M1SrM8vwhuLyYVehfKB"],
       "start_date": "2024-09-01",
   });
 
-  let res = client
-    .post(URL1)
-    .json(&json3)
-    .send()
-    .await
-    .expect("failed to get response")
-    .text()
-    .await
-    .expect("failed to get payload");
+  let res = client.post(&url1).json(&json3).send().await?.text().await?;
 
   println!("3: {:#?}", res);
 
+  // Fourth request
   let json4 = json!({
       "deployment": ["QmbReTnhCweQHmbXxgffkDqkkedo7ojjsUWTKopP1auuTp", "QmTfhYrb3wusYS715KvHfaL56R8M1SrM8vwhuLyYVehfKB"],
       "start_date": "2024-09-01",
   });
 
-  let res = client
-    .post(URL2)
-    .json(&json4)
-    .send()
-    .await
-    .expect("failed to get response")
-    .text()
-    .await
-    .expect("failed to get payload");
+  let url2 = format!("{}/deployment-price-count", BASE_URL);
+  let res = client.post(&url2).json(&json4).send().await?.text().await?;
 
   println!("4 {:#?}", res);
 
-  let res = client
-    .get(URL3)
-    .send()
-    .await
-    .expect("failed to get response")
-    .text()
-    .await
-    .expect("failed to get payload");
+  let url3 = format!(
+    "{}/user_aggregation/0xbf3a286a477967ebd850cee2dbdbfa6e535a9e64",
+    BASE_URL
+  );
+
+  // Fifth request
+  let res = client.get(&url3).send().await?.text().await?;
 
   println!("5 {:#?}", res);
 
+  // Sixth request
   let json4 = json!({
       "user_list": ["0xbf3a286a477967ebd850cee2dbdbfa6e535a9e64", "0xa499b9c52547db14d13216dcd73da0d4d43ba66e", "0x5e15ce35a3821e15d36988d9e0dd181c7c371a07"],
       "start": "2024-09-01",
       "end": "2024-09-02"
   });
 
+  let url4 = format!("{}/multi_user_aggregation", BASE_URL);
+
   let res = client
-    .post(URL4)
+    .post(&url4)
     .json(&json4)
     .send()
-    .await
-    .expect("failed to get response")
+    .await?
     .json::<Value>()
-    .await
-    .expect("failed to get payload");
+    .await?;
 
   println!("6 {:#?}", res);
 
+  // Seventh request
   let json4 = json!({
       "user_list": ["0xbf3a286a477967ebd850cee2dbdbfa6e535a9e64", "0xa499b9c52547db14d13216dcd73da0d4d43ba66e", "0x5e15ce35a3821e15d36988d9e0dd181c7c371a07"],
       "start": "2024-09-13"
   });
 
   let res = client
-    .post(URL4)
+    .post(&url4)
     .json(&json4)
     .send()
-    .await
-    .expect("failed to get response")
+    .await?
     .json::<Value>()
-    .await
-    .expect("failed to get payload");
+    .await?;
 
   println!("7 {:#?}", res);
+
+  let url5 = format!(
+    "{}/statistic-indexer-channel?indexer=0x5ef51c9f449db7be2f0c636c6c137e65b8b96b9b&\
+     deployment=QmNevi2wSvFzigFXrQdPTQFQxVEbpfmZ2uLX1HKxYj5dY8",
+    BASE_URL
+  );
+  // Eighth request
+  let res = client.get(&url5).send().await?.text().await?;
+
+  println!("8 {:#?}", res);
 
   Ok(())
 }
