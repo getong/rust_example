1170300e91d545e042785527ca1c93ea7dc0f66a
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-10-01 15:08:50 +0800

    add alloy contract example

---
+++ alloy_woekspace_example/alloy_contract_example/src/main.rs
@@ -0,0 +1,50 @@
+use alloy::{
+  node_bindings::Anvil,
+  primitives::{Address, U256},
+  providers::ProviderBuilder,
+  sol_types::sol,
+};
+
+sol! {
+    #[sol(rpc)] // <-- Important! Generates the necessary `MyContract` struct and function methods.
+    #[sol(bytecode = "0x1234")] // <-- Generates the `BYTECODE` static and the `deploy` method.
+    contract MyContract {
+        constructor(address) {} // The `deploy` method will also include any constructor arguments.
+
+        #[derive(Debug)]
+        function doStuff(uint a, bool b) public payable returns(address c, bytes32 d);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let anvil = Anvil::new().try_spawn()?;
+
+  // Create a provider.
+  let rpc_url = anvil.endpoint().parse()?;
+  let provider = ProviderBuilder::new()
+    .with_recommended_fillers()
+    .on_http(rpc_url);
+
+  // If `#[sol(bytecode = "0x...")]` is provided, the contract can be deployed with `MyContract::deploy`,
+  // and a new instance will be created.
+  let constructor_arg = Address::ZERO;
+  let _contract = MyContract::deploy(&provider, constructor_arg).await?;
+
+  // Otherwise, or if already deployed, a new contract instance can be created with `MyContract::new`.
+  let address = Address::ZERO;
+  let contract = MyContract::new(address, &provider);
+
+  // Build a call to the `doStuff` function and configure it.
+  let a = U256::from(123);
+  let b = true;
+  let call_builder = contract.doStuff(a, b).value(U256::from(50e18 as u64));
+
+  // Send the call. Note that this is not broadcasted as a transaction.
+  let call_return = call_builder.call().await?;
+  println!("{call_return:?}"); // doStuffReturn { c: 0x..., d: 0x... }
+
+  // Use `send` to broadcast the call as a transaction.
+  let _pending_tx = call_builder.send().await?;
+  Ok(())
+}
---
+++ alloy_woekspace_example/alloy_contract_example2/src/main.rs
@@ -0,0 +1,76 @@
+//! Example of deploying a contract at runtime from Solidity bytecode to Anvil and interacting with
+//! it.
+
+use alloy::{
+  hex,
+  network::{ReceiptResponse, TransactionBuilder},
+  primitives::U256,
+  providers::{Provider, ProviderBuilder},
+  rpc::types::TransactionRequest,
+  sol,
+};
+use eyre::Result;
+
+// If you have the bytecode known at build time, use the `deploy_from_contract` example.
+// This method benefits from using bytecode at runtime, e.g., from newly deployed contracts, to
+// analyze the behavior.
+sol! {
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    contract Counter {
+        uint256 public number;
+
+        function setNumber(uint256 newNumber) public {
+            number = newNumber;
+        }
+
+        function increment() public {
+            number++;
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  // Spin up a local Anvil node.
+  // Ensure `anvil` is available in $PATH.
+  let provider = ProviderBuilder::new()
+    .with_recommended_fillers()
+    .on_anvil_with_wallet();
+
+  // Deploy the `Counter` contract from bytecode at runtime.
+  let bytecode = hex::decode(
+        // solc v0.8.26; solc Counter.sol --via-ir --optimize --bin
+        "6080806040523460135760df908160198239f35b600080fdfe6080806040526004361015601257600080fd5b60003560e01c9081633fb5c1cb1460925781638381f58a146079575063d09de08a14603c57600080fd5b3460745760003660031901126074576000546000198114605e57600101600055005b634e487b7160e01b600052601160045260246000fd5b600080fd5b3460745760003660031901126074576020906000548152f35b34607457602036600319011260745760043560005500fea2646970667358221220e978270883b7baed10810c4079c941512e93a7ba1cd1108c781d4bc738d9090564736f6c634300081a0033"
+    )?;
+  let tx = TransactionRequest::default().with_deploy_code(bytecode);
+
+  // Deploy the contract.
+  let receipt = provider.send_transaction(tx).await?.get_receipt().await?;
+
+  let contract_address = receipt
+    .contract_address()
+    .expect("Failed to get contract address");
+  let contract = Counter::new(contract_address, &provider);
+  println!("Deployed contract at address: {}", contract.address());
+
+  // Set number
+  let builder = contract.setNumber(U256::from(42));
+  let tx_hash = builder.send().await?.watch().await?;
+
+  println!("Set number to 42: {tx_hash}");
+
+  // Increment the number to 43.
+  let builder = contract.increment();
+  let tx_hash = builder.send().await?.watch().await?;
+
+  println!("Incremented number: {tx_hash}");
+
+  // Retrieve the number, which should be 43.
+  let builder = contract.number();
+  let number = builder.call().await?.number.to_string();
+
+  println!("Retrieved number: {number}");
+
+  Ok(())
+}
