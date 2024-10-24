516e64830b1c14c63c93fed78b56e6498c97d952
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-10-24 16:40:16 +0800

    auto load all key in .env file into env

--- build_rs_example/build.rs
+++ build_rs_example/build.rs
@@ -3,12 +3,18 @@ fn main() {
   // Load the .env file
   dotenv::from_filename(".env").ok();
 
-  // Retrieve environment variables
-  if let Ok(api_key) = std::env::var("API_KEY") {
-    println!("cargo:rustc-env=API_KEY={}", api_key);
-  }
+  // // Retrieve environment variables
+  // if let Ok(api_key) = std::env::var("API_KEY") {
+  //   println!("cargo:rustc-env=API_KEY={}", api_key);
+  // }
 
-  if let Ok(api_url) = std::env::var("API_URL") {
-    println!("cargo:rustc-env=API_URL={}", api_url);
+  // if let Ok(api_url) = std::env::var("API_URL") {
+  //   println!("cargo:rustc-env=API_URL={}", api_url);
+  // }
+
+  // Iterate through all the environment variables loaded from .env
+  for (key, value) in std::env::vars() {
+    // Print each key-value pair to pass it as a cargo environment variable
+    println!("cargo:rustc-env={}={}", key, value);
   }
 }
--- build_rs_example/src/main.rs
+++ build_rs_example/src/main.rs
@@ -2,7 +2,9 @@
 fn main() {
   let api_key = std::env::var("API_KEY").expect("API_KEY not found");
   let api_url = std::env::var("API_URL").expect("API_URL not found");
+  let api_token = std::env::var("API_TOKEN").expect("API_TOKEN not found");
 
   println!("API_KEY: {}", api_key);
   println!("API_URL: {}", api_url);
+  println!("API_TOKEN: {}", api_token);
 }
