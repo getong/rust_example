095c5be013ba60bcd2c5ab697ef72ecf8d24592c
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-11-15 10:32:25 +0800

    add println msg

--- option_result_workspace_example/option_as_ref_example/src/main.rs
+++ option_result_workspace_example/option_as_ref_example/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
   // Converts from &Option<T> to Option<&T>.
 
   let text: Option<String> = Some("Hello, world!".to_string());
+  println!("text is {:?}", text);
   // First, cast `Option<String>` to `Option<&String>` with `as_ref`,
   // then consume *that* with `map`, leaving `text` on the stack.
   let text_length: Option<usize> = text.as_ref().map(|s| s.len());
