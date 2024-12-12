5f04a652e28d42ccf092303695a3d1c536ee262e
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-12-12 22:02:26 +0800

    update code

--- dioxus_workspace_example/dioxus_example/src/main.rs
+++ dioxus_workspace_example/dioxus_example/src/main.rs
@@ -1,9 +1,49 @@
-use dioxus::prelude::*;
+//! Run with:
+//!
+//! ```sh
+//! dx serve --platform web
+//! ```
 
-fn main() {
-  dioxus_desktop::launch(app);
+#![allow(non_snake_case, unused)]
+use dioxus::prelude::*;
+use serde::{Deserialize, Serialize};
+
+fn app() -> Element {
+  let mut count = use_signal(|| 0);
+  let mut text = use_signal(|| "...".to_string());
+  let server_future = use_server_future(get_server_data)?;
+
+  rsx! {
+      document::Link { href: asset!("/assets/hello.css"), rel: "stylesheet" }
+      h1 { "High-Five counter: {count}" }
+      button { onclick: move |_| count += 1, "Up high!" }
+      button { onclick: move |_| count -= 1, "Down low!" }
+      button {
+          onclick: move |_| async move {
+              if let Ok(data) = get_server_data().await {
+                  println!("Client received: {}", data);
+                  text.set(data.clone());
+                  post_server_data(data).await.unwrap();
+              }
+          },
+          "Run a server function!"
+      }
+      "Server said: {text}"
+  }
 }
 
-fn app(cx: Scope) -> Element {
-  cx.render(rsx!("hello world!"))
+#[server]
+async fn post_server_data(data: String) -> Result<(), ServerFnError> {
+  println!("Server received: {}", data);
+
+  Ok(())
+}
+
+#[server]
+async fn get_server_data() -> Result<String, ServerFnError> {
+  Ok(reqwest::get("https://httpbin.org/ip").await?.text().await?)
+}
+
+fn main() {
+  dioxus::launch(app);
 }
