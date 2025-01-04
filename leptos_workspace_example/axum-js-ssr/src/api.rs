4d1c08161e1a1ab9dca2daa0475514ac63ae1040
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-01-04 19:49:01 +0800

    fmt code

--- leptos_workspace_example/axum-js-ssr/src/api.rs
+++ leptos_workspace_example/axum-js-ssr/src/api.rs
@@ -2,7 +2,7 @@ use leptos::{prelude::ServerFnError, server};
 
 #[server]
 pub async fn fetch_code() -> Result<String, ServerFnError> {
-    // emulate loading of code from a database/version control/etc
-    tokio::time::sleep(std::time::Duration::from_millis(50)).await;
-    Ok(crate::consts::CH05_02A.to_string())
+  // emulate loading of code from a database/version control/etc
+  tokio::time::sleep(std::time::Duration::from_millis(50)).await;
+  Ok(crate::consts::CH05_02A.to_string())
 }
