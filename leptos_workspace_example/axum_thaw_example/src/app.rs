9a3539e7df4616890f11e69d731c78f7e33b9f80
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-01-10 22:58:13 +0800

    add two element

--- leptos_workspace_example/axum_thaw_example/src/app.rs
+++ leptos_workspace_example/axum_thaw_example/src/app.rs
@@ -4,7 +4,9 @@ use leptos_router::{
   components::{Route, Router, Routes},
   StaticSegment,
 };
+use leptos_use::{docs::Note, use_active_element};
 use thaw::{ssr::SSRMountStyleProvider, *};
+use wasm_bindgen::JsCast;
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
   view! {
@@ -45,6 +47,11 @@ pub fn App() -> impl IntoView {
               <main>
                   <Routes fallback=|| "Page not found.".into_view()>
                       <Route path=StaticSegment("") view=HomePage />
+                      <Route path=StaticSegment("hello") view=HelloWorld />
+                      <Route
+                          path=StaticSegment("leptos_use_active_element")
+                          view=ActiveElementDemo
+                      />
                   </Routes>
               </main>
           </Router>
@@ -62,5 +69,54 @@ fn HomePage() -> impl IntoView {
   view! {
       <h1>"Welcome to Leptos!"</h1>
       <Button on_click=on_click>"Click Me: " {count}</Button>
+      <p>
+          <a href="/hello">Go to Hello World page</a>
+      </p>
+      <p>
+          <a href="/leptos_use_active_element">Go to leptos use element page</a>
+      </p>
+  }
+}
+
+#[component]
+fn HelloWorld() -> impl IntoView {
+  view! {
+      <h1>"Hello, World!"</h1>
+      <p>
+          <a href="/">Back to Home</a>
+      </p>
+  }
+}
+
+#[component]
+fn ActiveElementDemo() -> impl IntoView {
+  let active_element = use_active_element();
+  let key = move || {
+    format!(
+      "{:?}",
+      active_element
+        .get()
+        .map(|el| el
+          .unchecked_ref::<web_sys::HtmlElement>()
+          .dataset()
+          .get("id"))
+        .unwrap_or_default()
+    )
+  };
+
+  view! {
+      <Note class="mb-3">"Select the inputs below to see the changes"</Note>
+
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
+          <For each=move || (1..7) key=|i| *i let:i>
+              <input type="text" data-id=i class="!my-0 !min-w-0" placeholder=i />
+          </For>
+
+      </div>
+
+      <div class="mt-2">"Current Active Element: " <span class="text-primary">{key}</span></div>
+      <p>
+          <a href="/">Back to Home</a>
+      </p>
   }
 }
