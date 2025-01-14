5a6788b0b84dd54b149aea45c01b241341fdad67
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-01-14 14:56:48 +0800

    add default page for pokemons router

---
+++ tuono_workspace_example/tuono-tutorial/src/routes/pokemons/index.tsx
@@ -0,0 +1,14 @@
+// src/routes/pokemons/index.tsx
+import type { JSX } from "react";
+
+export default function PokemonDefaultPage(): JSX.Element {
+  return (
+    <>
+      <title>Pokemon default page</title>
+
+      <div className="title-wrap">
+        <h1>Hello world</h1>
+      </div>
+    </>
+  );
+}
