9ed5989db9041ae5a69859836400e5a6346d28a3
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-01-16 15:50:13 +0800

    add OnceLock example

--- once_cell_workspace_example/builtin_lazy_cell_example/src/main.rs
+++ once_cell_workspace_example/builtin_lazy_cell_example/src/main.rs
@@ -1,6 +1,4 @@
-#![feature(lazy_cell)]
-
-use std::{cell::LazyCell, collections::HashMap};
+use std::{cell::LazyCell, collections::HashMap, sync::OnceLock};
 
 const MEM: LazyCell<HashMap<usize, usize>> = LazyCell::new(|| {
   let mut m = HashMap::new();
@@ -12,16 +10,63 @@ const MEM: LazyCell<HashMap<usize, usize>> = LazyCell::new(|| {
   m
 });
 
+static MEM2: OnceLock<Vec<usize>> = OnceLock::new();
+
 fn factorial(i: usize) -> usize {
   MEM[&i]
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
   println!("{}", factorial(0));
   println!("{}", factorial(1));
   println!("{}", factorial(2));
   println!("{}", factorial(3));
   println!("{}", factorial(4));
+
+  let handler = tokio::spawn(async {
+    if let Some(mem2_ref) = MEM2.get() {
+      println!(
+        "before initialize, mem2 contains 2: {:?}",
+        mem2_ref.contains(&2)
+      );
+    } else {
+      println!("before initialize, mem2 does not contain 2");
+    }
+  });
+  _ = handler.await;
+
+  mem2();
+
+  // Access the Vec inside the OnceLock and check for the element
+  if let Some(mem2_ref) = MEM2.get() {
+    println!(
+      "after initialized, mem2 contains 2: {:?}",
+      mem2_ref.contains(&2)
+    );
+  }
+
+  let handler = tokio::spawn(async {
+    if let Some(mem2_ref) = MEM2.get() {
+      println!(
+        "in the future object, after initialized, mem2 contains 2: {:?}",
+        mem2_ref.contains(&2)
+      );
+    }
+  });
+  _ = handler.await;
+  _ = tokio::time::sleep(std::time::Duration::from_millis(1000));
 }
 
-// copy from https://www.reddit.com/r/rust/comments/1416qul/how_to_replace_lazy_static_with_the_new_oncecell/
+fn mem2() -> &'static Vec<usize> {
+  MEM2.get_or_init(|| {
+    let mut m = vec![];
+    m.push(0);
+    m.push(1);
+    m.push(2);
+    m.push(3);
+    m.push(4);
+    m.push(5);
+    m
+  })
+}
