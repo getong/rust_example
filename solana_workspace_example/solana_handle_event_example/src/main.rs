129dc1f53987ba260d6b3f644a0094e5be8ce70c
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-01-30 23:32:03 +0800

    add more solana code

--- solana_workspace_example/solana_handle_event_example/src/main.rs
+++ solana_workspace_example/solana_handle_event_example/src/main.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
-use solana_client::rpc_client::RpcClient;
-use solana_sdk::{pubkey::Pubkey, signature::Signature};
+use solana_client::rpc_client::{GetConfirmedSignaturesForAddress2Config, RpcClient};
+use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey, signature::Signature};
 use solana_transaction_status::UiTransactionEncoding;
 
 #[tokio::main]
@@ -19,6 +19,50 @@ async fn main() {
   // Fetching the transaction signatures for the address
   let signatures = client
     .get_signatures_for_address(&pubkey)
+    .expect("Failed to get signatures");
+
+  println!("Signatures for address {}:", address);
+  for sig in signatures {
+    println!("{}", sig.signature);
+
+    // Fetching the transaction details for each signature
+    match client.get_transaction(
+      &Signature::from_str(&sig.signature).unwrap(),
+      UiTransactionEncoding::Json,
+    ) {
+      Ok(transaction) => {
+        println!("Transaction details: {:?}", transaction);
+      }
+      Err(e) => {
+        eprintln!("Error fetching transaction: {}", e);
+      }
+    }
+
+    // Fetching the transaction details for each signature
+    match client.get_transaction(
+      &Signature::from_str(&sig.signature).unwrap(),
+      UiTransactionEncoding::JsonParsed,
+    ) {
+      Ok(transaction) => {
+        println!("Transaction details: {:?}", transaction);
+      }
+      Err(e) => {
+        eprintln!("Error fetching transaction: {}", e);
+      }
+    }
+  }
+
+  // Set the configuration for the request
+  let config = GetConfirmedSignaturesForAddress2Config {
+    before: None,
+    until: None,
+    limit: Some(3),
+    commitment: Some(CommitmentConfig::confirmed()),
+  };
+
+  // Fetching the transaction signatures for the address
+  let signatures = client
+    .get_signatures_for_address_with_config(&pubkey, config)
     .expect("Failed to get signatures");
 
   println!("Signatures for address {}:", address);
