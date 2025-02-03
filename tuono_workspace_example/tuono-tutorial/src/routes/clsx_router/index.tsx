4ffebc6e4ab70c113760a95b3b3ca66e9457a2e4
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-02-03 23:59:55 +0800

    add clsx

---
+++ tuono_workspace_example/tuono-tutorial/src/routes/clsx_router/index.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx'
+
+interface ButtonProps {
+  isActive: boolean
+}
+
+const Button = ({ isActive }: ButtonProps) => {
+  const className = clsx('btn', isActive && 'btn-active')
+
+  return <button className={className}>辰火流光</button>
+}
+
+const Demo = () => {
+  return (
+    <>
+      <Button isActive={true} />
+      <Button isActive={false} />
+    </>
+  )
+}
+export default Demo
