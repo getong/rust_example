ab4bb4211d6775bbbe155a09be17336df445a763
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-03-02 21:41:09 +0800

    update sample

--- rand_workspace_example/rand_uniform_distribution_example/src/main.rs
+++ rand_workspace_example/rand_uniform_distribution_example/src/main.rs
@@ -1,11 +1,14 @@
-use rand::distributions::{Distribution, Uniform};
+use rand::{
+  distr::{Distribution, Uniform},
+  rng,
+};
 
 fn main() {
-  let mut rng = rand::thread_rng();
-  let die = Uniform::from(1 .. 7);
+  let mut rng = rng();
+  let die = Uniform::new_inclusive(1, 6);
 
   loop {
-    let throw = die.sample(&mut rng);
+    let throw = die.expect("not found data").sample(&mut rng);
     println!("Roll the die: {}", throw);
     if throw == 6 {
       break;
