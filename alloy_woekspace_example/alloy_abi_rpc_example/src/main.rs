0001d27f23803ef5cc1ddba75de7dbfc9a94f24e
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-03-02 14:59:01 +0800

    add alloy abi rpc example

---
+++ alloy_woekspace_example/alloy_abi_rpc_example/src/main.rs
@@ -0,0 +1,44 @@
+use alloy::{
+  primitives::{address, utils::format_units},
+  providers::{Provider, ProviderBuilder},
+  sol,
+};
+
+sol!(
+  #[allow(missing_docs)]
+  #[sol(rpc)]
+  RIF,
+  "abi/rif.json"
+);
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+  // Set up the HTTP transport which is consumed by the RPC client.
+  let rpc_url = "https://rpc.testnet.rootstock.io/{YOUR_APIKEY}".parse()?;
+
+  // Create a provider with the HTTP transport using the `reqwest` crate.
+  let provider = ProviderBuilder::new().on_http(rpc_url);
+
+  // Address without 0x prefix
+  let alice = address!("8F1C0185bB6276638774B9E94985d69D3CDB444a");
+
+  let rbtc = provider.get_balance(alice).await?;
+
+  let formatted_balance: String = format_units(rbtc, "ether")?;
+
+  println!("Balance of alice: {formatted_balance} rbtc");
+
+  // Using rif testnet contract address
+  let contract = RIF::new(
+    "0x19f64674D8a5b4e652319F5e239EFd3bc969a1FE".parse()?,
+    provider,
+  );
+
+  let RIF::balanceOfReturn { balance } = contract.balanceOf(alice).call().await?;
+
+  println!("Rif balance: {:?}", balance);
+
+  Ok(())
+}
+
+// copy from https://dev.rootstock.io/resources/tutorials/rootstock-rust/
