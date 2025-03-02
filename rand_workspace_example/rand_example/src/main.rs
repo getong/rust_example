fdeeecc1c5e2bd318677fdb7e625fdd4959a4d4a
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-03-02 21:27:00 +0800

    update rand code

--- rand_workspace_example/rand_example/src/main.rs
+++ rand_workspace_example/rand_example/src/main.rs
@@ -1,17 +1,17 @@
-use rand::{rngs::ThreadRng, seq::SliceRandom, thread_rng, Rng};
+use rand::{prelude::IndexedMutRandom, rng, rngs::ThreadRng,  Rng};
 
 fn generate_float(generator: &mut ThreadRng) -> f64 {
-  let placeholder: f64 = generator.gen();
+  let placeholder: f64 = generator.random();
   return placeholder * 10.0;
 }
 
 fn main() {
   println!("Hello, world!");
-  let choices = [1, 2, 4, 8, 16, 32];
-  let mut rng = thread_rng();
-  println!("{:?}", choices.choose(&mut rng));
-  println!("{:?}", [0, 1, 2, 3, 4, 5].choose(&mut rng).unwrap());
-  assert_eq!(choices[.. 0].choose(&mut rng), None);
+  let mut choices = [1, 2, 4, 8, 16, 32];
+  let mut rng1 = rng();
+  println!("{:?}", choices.choose_mut(&mut rng1));
+  println!("{:?}", [0, 1, 2, 3, 4, 5].choose_mut(&mut rng1).unwrap());
+  assert_eq!(choices[.. 0].choose_mut(&mut rng1), None);
 
   for _ in 0 .. 5 {
     let random_u16 = rand::random::<u16>();
@@ -19,7 +19,7 @@ fn main() {
   }
   println!("");
 
-  let mut rng2: ThreadRng = thread_rng();
+  let mut rng2 = rng();
   let random_number = generate_float(&mut rng2);
   println!("{}", random_number);
 }
