fc604367cddd2bef664a45fdc47b0ca70ae19fcf
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-03-08 21:29:14 +0800

    add docker name

--- surrealdb_workspace_example/start_surrealdb.sh
+++ surrealdb_workspace_example/start_surrealdb.sh
@@ -4,8 +4,8 @@
 
 mkdir -p mydata
 
-docker run --rm -d -p 9000:8000 \
+docker run --rm -d --name my_surrealdb -p 9000:8000 \
   --user $(id -u) -v $(pwd)/mydata:/mydata \
   surrealdb/surrealdb:latest start --log debug \
   --allow-all --user root --pass root \
-  rocksdb:/mydata/rocksdb
+  rocksdb:/mydata/
