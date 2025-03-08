b3f30cb0e2376145d7d44dcb46e73aeba6b7f23d
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-03-08 20:43:13 +0800

    update code, it works

--- surrealdb_workspace_example/surrealdb_tikv_example/src/main.rs
+++ surrealdb_workspace_example/surrealdb_tikv_example/src/main.rs
@@ -6,13 +6,13 @@ use surrealdb::{
   Surreal,
 };
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Name<'a> {
   first: &'a str,
   last: &'a str,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Person<'a> {
   title: &'a str,
   name: Name<'a>,
@@ -33,7 +33,7 @@ struct Record {
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
   let db: Surreal<Client> = Surreal::init();
-  let addr = "ws://127.0.0.1:9000";
+  let addr = "127.0.0.1:9000";
 
   // Create database connection using WebSocket (not TCP)
   db.connect::<Ws>(addr).await?;
@@ -47,7 +47,7 @@ async fn main() -> surrealdb::Result<()> {
   // Select a specific namespace / database
   db.use_ns("test").use_db("test").await?;
   // Create a new person with a random id
-  let created: Vec<Record> = db
+  let created: Record = db
     .create("person")
     .content(Person {
       title: "Founder & CEO",
