f4093ecf2682c618611aa3765eb311ccd4c6a533
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-03-09 22:25:09 +0800

    update version

--- surrealdb_workspace_example/start_surrealdb.sh
+++ surrealdb_workspace_example/start_surrealdb.sh
@@ -6,6 +6,6 @@ mkdir -p mydata
 
 docker run --rm -d --name my_surrealdb -p 9000:8000 \
   --user $(id -u) -v $(pwd)/mydata:/mydata \
-  surrealdb/surrealdb:latest start --log debug \
+  surrealdb/surrealdb:v2 start --log debug \
   --allow-all --user root --pass root \
   rocksdb:/mydata/
