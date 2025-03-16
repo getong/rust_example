9562158f71adc9af6f83e164fffee46cd260bc1a
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-03-16 23:54:32 +0800

    add channel definition for libp2p and openraft

--- libp2p_workspace_example/libp2p_tuono_a_example/src/libp2p/mod.rs
+++ libp2p_workspace_example/libp2p_tuono_a_example/src/libp2p/mod.rs
@@ -1,1 +1,21 @@
 pub mod behaviour;
+use std::{
+  collections::HashMap,
+  sync::{Arc, LazyLock},
+};
+
+use behaviour::{RaftRequest, RaftResponse};
+use libp2p::request_response::OutboundRequestId;
+use parking_lot::Mutex;
+use tokio::sync::{
+  mpsc::{Receiver as MpscReceiver, Sender as MpscSender},
+  oneshot::{Receiver as OneshotReceiver, Sender as OneshotSender},
+};
+
+static LAZY_EVENT_SENDER: LazyLock<
+  Arc<Mutex<Option<MpscSender<(RaftRequest, OneshotSender<RaftResponse>)>>>>,
+> = LazyLock::new(|| Arc::new(Mutex::new(None)));
+
+static RECEIVER_GROUP: LazyLock<
+  Arc<Mutex<HashMap<OutboundRequestId, OneshotSender<RaftResponse>>>>,
+> = LazyLock::new(|| Arc::new(Mutex::new(HashMap::new())));
