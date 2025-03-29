b592b28e37fc422017271ca27f7374b3a9513386
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-03-29 19:11:58 +0800

    add fullstack

--- dioxus_workspace_example/dioxus_hot_dog_example/src/main.rs
+++ dioxus_workspace_example/dioxus_hot_dog_example/src/main.rs
@@ -32,7 +32,7 @@ fn App() -> Element {
       document::Link { rel: "icon", href: FAVICON }
       document::Link { rel: "stylesheet", href: MAIN_CSS }
       document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-
+      document::Stylesheet { href: MAIN_CSS }
       Router::<Route> {}
   }
 }
