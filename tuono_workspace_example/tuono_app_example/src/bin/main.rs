b868b27163e848b13b5f281d4c59c724b6122666
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-03-31 22:59:50 +0800

    update code

--- tuono_workspace_example/tuono_app_example/src/bin/main.rs
+++ tuono_workspace_example/tuono_app_example/src/bin/main.rs
@@ -15,13 +15,13 @@ use tuono_app_example::routes::{api as api_mod, index};
 #[tokio::main]
 async fn main() {
   tuono_internal_init_v8_platform();
-  println!("\n  ⚡ Tuono v0.17.10");
+  println!("\n  ⚡ Tuono v0.19.1");
 
   let router = Router::new()
     // ROUTE_BUILDER
+    .route("/api/mod", get(api_mod::get_tuono_internal_api))
     .route("/", get(index::tuono_internal_route))
-    .route("/__tuono/data/", get(index::tuono_internal_api))
-    .route("/api/mod", get(api_mod::get_tuono_internal_api));
+    .route("/__tuono/data/", get(index::tuono_internal_api));
 
   Server::init(router, MODE).await.start().await
 }
