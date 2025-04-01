7459857bd9423582fa4b886642df12a3519b0f15
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-04-01 16:07:51 +0800

    add deno_core impl fetch example

---
+++ rusty_v8_workspace_example/deno_core_impl_fetch_example/src/example.js
@@ -0,0 +1,25 @@
+// example.js
+// Deno.core.print("Hello runjs!");
+console.log("Hello", "runjs!");
+console.error("Boom!");
+
+const path = "./log.txt";
+try {
+  const contents = await runjs.readFile(path);
+  console.log("Read from a file", contents);
+} catch (err) {
+  console.error("Unable to read file", path, err);
+}
+
+await runjs.writeFile(path, "I can write to a file.");
+const contents = await runjs.readFile(path);
+console.log("Read from a file", path, "contents:", contents);
+console.log("Removing file", path);
+runjs.removeFile(path);
+console.log("File removed");
+
+console.log("Hello", "runjs!");
+const content = await runjs.fetch(
+  "https://deno.land/std@0.177.0/examples/welcome.ts",
+);
+console.log("Content from fetch", content);
