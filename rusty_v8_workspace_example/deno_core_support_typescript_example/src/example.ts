49b9fa5e70bc2dff2a41c768c127e7eed4a04879
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-04-01 16:21:17 +0800

    add deno_core support typescript example

---
+++ rusty_v8_workspace_example/deno_core_support_typescript_example/src/example.ts
@@ -0,0 +1,15 @@
+// example.ts
+console.log("Hello", "runjs!");
+
+interface Foo {
+  bar: string;
+  fizz: number;
+}
+
+let content: string;
+
+content = await runjs.fetch(
+  "https://deno.land/std@0.177.0/examples/welcome.ts",
+);
+
+console.log("Content from fetch", content);
