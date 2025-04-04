b585110b68f7cfde285a99c8bcc6518dcf61912a
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-04-04 15:32:49 +0800

    update code

--- local_ip_address_example/src/main.rs
+++ local_ip_address_example/src/main.rs
@@ -1,7 +1,13 @@
-use local_ip_address::local_ip;
+use local_ip_address::{list_afinet_netifas, local_ip};
 
 fn main() {
   let my_local_ip = local_ip().unwrap();
 
-  println!("This is my local IP address: {:?}", my_local_ip);
+  println!("\nThis is my local IP address: {:?}\n", my_local_ip);
+
+  let network_interfaces = list_afinet_netifas().unwrap();
+
+  for (name, ip) in network_interfaces.iter() {
+    println!("{}:\t{:?}", name, ip);
+  }
 }
