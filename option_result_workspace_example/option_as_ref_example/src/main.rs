10a03abb872d8d607133be94b972818bc9c6df30
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-04-09 14:11:46 +0800

    add Option<String> example

--- option_result_workspace_example/option_as_ref_example/src/main.rs
+++ option_result_workspace_example/option_as_ref_example/src/main.rs
@@ -13,4 +13,19 @@ fn main() {
     "still can print text: {:?}, text_length: {:?}",
     text, text_length
   );
+
+  let a = Some("".to_string());
+  let a1 = change_empty_string_to_none(&a);
+  println!("a1 is {:?}", a1);
+
+  let b = None;
+  let b1 = change_empty_string_to_none(&b);
+  println!("b1 is {:?}", b1);
+}
+
+fn change_empty_string_to_none(s: &Option<String>) -> Option<String> {
+  s.as_ref()
+    .map(|s| s.trim())
+    .filter(|s| !s.is_empty())
+    .map(|s| s.to_string())
 }
