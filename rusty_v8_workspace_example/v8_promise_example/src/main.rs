ca6899a3dc9f76fc63b3add66854cb9ec81b6b7f
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-06-08 23:36:45 +0800

    format code

--- rusty_v8_workspace_example/v8_promise_example/src/main.rs
+++ rusty_v8_workspace_example/v8_promise_example/src/main.rs
@@ -8,42 +8,44 @@ fn main() {
   V8::initialize_platform(platform);
   V8::initialize();
 
-  let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
+  {
+    let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
 
-  let handle_scope = &mut v8::HandleScope::new(isolate);
+    let handle_scope = &mut v8::HandleScope::new(isolate);
 
-  let context = v8::Context::new(handle_scope, v8::ContextOptions::default());
-  let scope = &mut v8::ContextScope::new(handle_scope, context);
+    let context = v8::Context::new(handle_scope, v8::ContextOptions::default());
+    let scope = &mut v8::ContextScope::new(handle_scope, context);
 
-  let file_path = Path::new("src/index.js");
-  let code = fs::read_to_string(file_path).expect("Unable to read JavaScript file.");
+    let file_path = Path::new("src/index.js");
+    let code = fs::read_to_string(file_path).expect("Unable to read JavaScript file.");
 
-  let source = v8::String::new(scope, &code).unwrap();
-  let script = v8::Script::compile(scope, source, None).unwrap();
-  let _ = script.run(scope).unwrap();
+    let source = v8::String::new(scope, &code).unwrap();
+    let script = v8::Script::compile(scope, source, None).unwrap();
+    let _ = script.run(scope).unwrap();
 
-  let global = context.global(scope);
-  let add_key = v8::String::new(scope, "add").unwrap();
-  let add_val = global.get(scope, add_key.into()).unwrap();
-  let add_func = v8::Local::<v8::Function>::try_from(add_val).expect("Function not found.");
+    let global = context.global(scope);
+    let add_key = v8::String::new(scope, "add").unwrap();
+    let add_val = global.get(scope, add_key.into()).unwrap();
+    let add_func = v8::Local::<v8::Function>::try_from(add_val).expect("Function not found.");
 
-  let arg_a = v8::Number::new(scope, 20.0);
-  let arg_b = v8::Number::new(scope, 12.0);
-  let args = &[arg_a.into(), arg_b.into()];
+    let arg_a = v8::Number::new(scope, 20.0);
+    let arg_b = v8::Number::new(scope, 12.0);
+    let args = &[arg_a.into(), arg_b.into()];
 
-  let call_func = add_func.call(scope, global.into(), args).unwrap();
+    let call_func = add_func.call(scope, global.into(), args).unwrap();
 
-  let result = block_on(async {
-    let promise = v8::Local::<v8::Promise>::try_from(call_func).unwrap();
-    while promise.state() == v8::PromiseState::Pending {
-      scope.perform_microtask_checkpoint();
-    }
-    promise
-  });
+    let result = block_on(async {
+      let promise = v8::Local::<v8::Promise>::try_from(call_func).unwrap();
+      while promise.state() == v8::PromiseState::Pending {
+        scope.perform_microtask_checkpoint();
+      }
+      promise
+    });
 
-  let result_str = result.result(scope).to_string(scope).unwrap();
-  println!("Result: {}", result_str.to_rust_string_lossy(scope));
-  // => Result: 32
+    let result_str = result.result(scope).to_string(scope).unwrap();
+    println!("Result: {}", result_str.to_rust_string_lossy(scope));
+    // => Result: 32
+  }
 
   unsafe {
     V8::dispose();
