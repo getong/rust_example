b51e3a463582fd799df541833eba1e8e8de2fa3b
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-07-11 11:59:05 +0800

    add solana pda example

---
+++ solana_workspace_example/solana_pda_example/src/main.rs
@@ -0,0 +1,10 @@
+use std::str::FromStr;
+
+use solana_program::pubkey::Pubkey;
+
+fn main() {
+  let program_id = Pubkey::from_str("G1DCNUQTSGHehwdLCAmRyAG8hf51eCHrLNUqkgGKYASj").unwrap();
+
+  let (pda, bump_seed) = Pubkey::find_program_address(&[b"test"], &program_id);
+  println!("pda: {}, bump: {}", pda, bump_seed);
+}
