fb9c50dd88372e15d79c5b25fd446aee46e07001
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-07-12 23:57:55 +0800

    add more data and code

--- polars_example/src/main.rs
+++ polars_example/src/main.rs
@@ -5,9 +5,30 @@ fn main() -> PolarsResult<()> {
     .with_has_header(true)
     .finish()?;
 
-  let filtered = df.lazy().filter(col("age").gt(lit(30))).collect()?;
+  let filtered = df.clone().lazy().filter(col("age").gt(lit(30))).collect()?;
 
   println!("{:?}", filtered);
+
+  let result = df
+    .clone()
+    .group_by([col("name")])
+    .agg([col("score").mean().alias("average_score")])
+    .collect()?;
+
+  println!("{:?}", result);
+
+  let result = df
+    .clone()
+    .select([col("name"), col("score").alias("final_score"), col("age")])
+    .drop(["age"])
+    .collect()?;
+
+  println!("{:?}", result);
+
+  // Drop rows where name == "Alice"
+  let result = df.filter(col("name").neq(lit("Alice"))).collect()?;
+
+  println!("{:?}", result);
 
   Ok(())
 }
