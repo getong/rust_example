fe88f42115fc287ed964bccf6cfca5dae56e2e63
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-07-14 11:14:32 +0800

    init chitchat axum example

---
+++ chitchat_workspace_example/chitchat_axum_example/src/lib.rs
@@ -0,0 +1,15 @@
+use chitchat::{ChitchatId, ClusterStateSnapshot};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ApiResponse {
+  pub cluster_id: String,
+  pub cluster_state: ClusterStateSnapshot,
+  pub live_nodes: Vec<ChitchatId>,
+  pub dead_nodes: Vec<ChitchatId>,
+}
+
+#[derive(Debug, Serialize, Deserialize, schemars::JsonSchema)]
+pub struct SetKeyValueResponse {
+  pub status: bool,
+}
