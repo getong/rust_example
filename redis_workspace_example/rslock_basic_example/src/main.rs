2cdbe7702e4448b4b6220a3b0f2e811d95f51118
gerald <3949379+getong@users.noreply.github.com>
2025-08-24 12:12:09 +0800

    add rslock basic example

---
+++ redis_workspace_example/rslock_basic_example/src/main.rs
@@ -0,0 +1,36 @@
+use std::time::Duration;
+
+use rslock::LockManager;
+
+#[tokio::main]
+async fn main() {
+  // Define Redis URIs
+  let uris = vec![
+    "redis://127.0.0.1:6380/",
+    "redis://127.0.0.1:6381/",
+    "redis://127.0.0.1:6382/",
+  ];
+
+  // Initialize the LockManager using `new`
+  let rl = LockManager::new(uris);
+
+  // Acquire a lock
+  let lock = loop {
+    if let Ok(lock) = rl.lock("my_mutex", Duration::from_millis(1000)).await {
+      break lock;
+    }
+  };
+
+  println!("Lock acquired!");
+
+  // Extend the lock
+  if rl.extend(&lock, Duration::from_millis(1000)).await.is_ok() {
+    println!("Lock extended!");
+  } else {
+    println!("Failed to extend the lock.");
+  }
+
+  // Unlock the lock
+  rl.unlock(&lock).await;
+  println!("Lock released!");
+}
