ff0d7e4d8f8062c02e7349d88c346d177b6f31d8
gerald <3949379+getong@users.noreply.github.com>
2025-08-26 21:21:45 +0800

    add localnet api version

--- sui_workspace_example/sui_api_version_example/src/main.rs
+++ sui_workspace_example/sui_api_version_example/src/main.rs
@@ -2,6 +2,15 @@ use sui_sdk::SuiClientBuilder;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+  let sui = SuiClientBuilder::default()
+    .build("http://127.0.0.1:9000") // local network address
+    .await?;
+  println!("Sui local network version: {}", sui.api_version());
+
+  // local Sui network, like the above one but using the dedicated function
+  let sui_local = SuiClientBuilder::default().build_localnet().await?;
+  println!("Sui local network version: {}", sui_local.api_version());
+
   // Sui testnet -- https://fullnode.testnet.sui.io:443
   let sui_testnet = SuiClientBuilder::default().build_testnet().await?;
   println!("Sui testnet version: {}", sui_testnet.api_version());
