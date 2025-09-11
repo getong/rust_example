f7b2b6d6a86b2412a9afa67cd2d962399bbee821
gerald <3949379+getong@users.noreply.github.com>
2025-09-11 15:03:32 +0800

    add deno_core call js example

---
+++ deno_component_workspace_example/deno_core_call_js_example/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+use deno_core::{JsRuntime, v8};
+
+fn main() {
+  let source_code = fs::read_to_string("./src/index.js").unwrap();
+
+  let mut runtime = JsRuntime::new(Default::default());
+
+  let handle = runtime.execute_script("hello", source_code).unwrap();
+  let scope = &mut runtime.handle_scope();
+  let value = v8::Local::new(scope, handle);
+  let value = value.to_rust_string_lossy(scope);
+  println!("{}", value)
+}
