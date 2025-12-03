a61975c182146095f3f29237356c762fd4c68d79
gerald <3949379+getong@users.noreply.github.com>
2025-12-03 15:52:51 +0800

    add insta review example

---
+++ insta_review_example/src/main.rs
@@ -0,0 +1,16 @@
+fn split_words(s: &str) -> Vec<&str> {
+    s.split_whitespace().collect()
+}
+
+#[test]
+fn test_split_words() {
+    let words = split_words("hello from the other side");
+    insta::assert_yaml_snapshot!(words);
+}
+
+fn main() {
+  println!("Hello, world!");
+}
+
+
+// cargo insta test --review
