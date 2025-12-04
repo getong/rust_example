3792872714a15b78a4ef08a122d1280476962fef
gerald <3949379+getong@users.noreply.github.com>
2025-12-05 01:19:18 +0800

    update loro code

--- loro_sync_example/src/main.rs
+++ loro_sync_example/src/main.rs
@@ -1,19 +1,21 @@
-use loro::{LoroDoc, ToJson};
+use loro::{ExportMode, LoroDoc, ToJson};
 use serde_json::json;
 
 fn main() {
   let doc = LoroDoc::new();
   let text = doc.get_text("text");
   text.insert(0, "Hello world!").unwrap();
-  let bytes = doc.export_from(&Default::default());
+  let bytes = doc.export(ExportMode::all_updates()).unwrap();
   let doc_b = LoroDoc::new();
   doc_b.import(&bytes).unwrap();
   assert_eq!(doc.get_deep_value(), doc_b.get_deep_value());
   let text_b = doc_b.get_text("text");
   text_b.mark(0 .. 5, "bold", true).unwrap();
-  doc.import(&doc_b.export_from(&doc.oplog_vv())).unwrap();
+  doc
+    .import(&doc_b.export(ExportMode::updates(&doc.oplog_vv())).unwrap())
+    .unwrap();
   assert_eq!(
-    text.to_delta().to_json_value(),
+    text.get_richtext_value().to_json_value(),
     json!([
         { "insert": "Hello", "attributes": {"bold": true} },
         { "insert": " world!" },
