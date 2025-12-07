f5e178853c1da20421147fc4ac99377ee34b35f3
gerald <3949379+getong@users.noreply.github.com>
2025-12-07 22:58:42 +0800

    add tantivy workspace example

---
+++ tantivy_workspace_example/lindera_tantivy_ko_dic/src/main.rs
@@ -0,0 +1,119 @@
+use lindera::{dictionary::load_dictionary, mode::Mode, segmenter::Segmenter};
+use lindera_tantivy::tokenizer::LinderaTokenizer;
+use tantivy::{
+  Document, Index, TantivyDocument,
+  collector::TopDocs,
+  doc,
+  query::QueryParser,
+  schema::{IndexRecordOption, Schema, TextFieldIndexing, TextOptions},
+};
+
+fn main() -> tantivy::Result<()> {
+  // create schema builder
+  let mut schema_builder = Schema::builder();
+
+  // add id field
+  let id = schema_builder.add_text_field(
+    "id",
+    TextOptions::default()
+      .set_indexing_options(
+        TextFieldIndexing::default()
+          .set_tokenizer("raw")
+          .set_index_option(IndexRecordOption::Basic),
+      )
+      .set_stored(),
+  );
+
+  // add title field
+  let title = schema_builder.add_text_field(
+    "title",
+    TextOptions::default()
+      .set_indexing_options(
+        TextFieldIndexing::default()
+          .set_tokenizer("lang_ko")
+          .set_index_option(IndexRecordOption::WithFreqsAndPositions),
+      )
+      .set_stored(),
+  );
+
+  // add body field
+  let body = schema_builder.add_text_field(
+    "body",
+    TextOptions::default()
+      .set_indexing_options(
+        TextFieldIndexing::default()
+          .set_tokenizer("lang_ko")
+          .set_index_option(IndexRecordOption::WithFreqsAndPositions),
+      )
+      .set_stored(),
+  );
+
+  // build schema
+  let schema = schema_builder.build();
+
+  // create index on memory
+  let index = Index::create_in_ram(schema.clone());
+
+  // Tokenizer with ko-dic
+  let mode = Mode::Normal;
+  let dictionary = load_dictionary("embedded://ko-dic").unwrap();
+  let user_dictionary = None;
+  let segmenter = Segmenter::new(mode, dictionary, user_dictionary);
+  let tokenizer = LinderaTokenizer::from_segmenter(segmenter);
+
+  // register Lindera tokenizer
+  index.tokenizers().register("lang_ko", tokenizer);
+
+  // create index writer
+  let mut index_writer = index.writer(50_000_000)?;
+
+  // add document
+  index_writer.add_document(doc!(
+        id => "1",
+        title => "나리타 국제공항",
+        body => "나리타 국제공항(일본어: 成田国際空港, 영어: Narita International Airport, IATA: NRT, ICAO: RJAA)은 일본 지바현 나리타시에 위치한 국제공항으로, 도쿄도 도심에서 동북쪽으로 약 62km 떨어져 있다."
+    )).unwrap();
+
+  // add document
+  index_writer.add_document(doc!(
+        id => "2",
+        title => "도쿄 국제공항",
+        body => "도쿄국제공항(일본어: 東京国際空港、とうきょうこくさいくうこう, 영어: Tokyo International Airport)은 일본 도쿄도 오타구에 있는 공항이다. 보통 이 일대의 옛 지명을 본뜬 하네다 공항(일본어: 羽田空港, 영어: Haneda Airport)이라고 불린다."
+    )).unwrap();
+
+  // add document
+  index_writer.add_document(doc!(
+        id => "3",
+        title => "간사이 국제공항",
+        body => "간사이 국제공항(일본어: 関西国際空港, IATA: KIX, ICAO: RJBB)은 일본 오사카부 오사카 만에 조성된 인공섬에 위치한 일본의 공항으로, 대한민국의 인천국제공항보다 6년 반 앞선 1994년 9월 4일에 개항했다."
+    )).unwrap();
+
+  // commit
+  index_writer.commit()?;
+
+  // create reader
+  let reader = index.reader()?;
+
+  // create searcher
+  let searcher = reader.searcher();
+
+  // create querhy parser
+  let query_parser = QueryParser::for_index(&index, vec![title, body]);
+
+  // parse query
+  let query_str = "도쿄";
+  let query = query_parser.parse_query(query_str)?;
+  println!("Query String: {query_str}");
+
+  // search
+  let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
+  println!("Search Result:");
+  for (_, doc_address) in top_docs {
+    let retrieved_doc: TantivyDocument = searcher.doc(doc_address)?;
+    println!("{}", retrieved_doc.to_json(&schema));
+  }
+
+  Ok(())
+}
+
+// copy from https://github.com/lindera/lindera-tantivy/blob/main/examples/ko-dic.rs
---
+++ tantivy_workspace_example/lindera_tantivy_tokenize_with_config/src/main.rs
@@ -0,0 +1,120 @@
+use std::path::PathBuf;
+
+use lindera_tantivy::tokenizer::LinderaTokenizer;
+use tantivy::{
+  Document, Index, TantivyDocument,
+  collector::TopDocs,
+  doc,
+  query::QueryParser,
+  schema::{IndexRecordOption, Schema, TextFieldIndexing, TextOptions},
+};
+
+fn main() -> tantivy::Result<()> {
+  // create schema builder
+  let mut schema_builder = Schema::builder();
+
+  // add id field
+  let id = schema_builder.add_text_field(
+    "id",
+    TextOptions::default()
+      .set_indexing_options(
+        TextFieldIndexing::default()
+          .set_tokenizer("raw")
+          .set_index_option(IndexRecordOption::Basic),
+      )
+      .set_stored(),
+  );
+
+  // add title field
+  let title = schema_builder.add_text_field(
+    "title",
+    TextOptions::default()
+      .set_indexing_options(
+        TextFieldIndexing::default()
+          .set_tokenizer("lang_ja")
+          .set_index_option(IndexRecordOption::WithFreqsAndPositions),
+      )
+      .set_stored(),
+  );
+
+  // add body field
+  let body = schema_builder.add_text_field(
+    "body",
+    TextOptions::default()
+      .set_indexing_options(
+        TextFieldIndexing::default()
+          .set_tokenizer("lang_ja")
+          .set_index_option(IndexRecordOption::WithFreqsAndPositions),
+      )
+      .set_stored(),
+  );
+
+  // build schema
+  let schema = schema_builder.build();
+
+  // create index on memory
+  let index = Index::create_in_ram(schema.clone());
+
+  // Build tokenizer with config file
+  let config_file = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+    .join("./")
+    .join("lindera.yml");
+  let tokenizer = LinderaTokenizer::from_file(config_file.as_path())?;
+
+  // register Lindera tokenizer
+  index.tokenizers().register("lang_ja", tokenizer);
+
+  // create index writer
+  let mut index_writer = index.writer(50_000_000)?;
+
+  // add document
+  index_writer.add_document(doc!(
+        id => "1",
+        title => "成田国際空港",
+        body => "成田国際空港（なりたこくさいくうこう、英: Narita International Airport）は、千葉県成田市南東部から芝山町北部にかけて建設された日本最大の国際拠点空港である。首都圏東部（東京の東60km）に位置している。空港コードはNRT。"
+    )).unwrap();
+
+  // add document
+  index_writer.add_document(doc!(
+        id => "2",
+        title => "東京国際空港",
+        body => "東京国際空港（とうきょうこくさいくうこう、英語: Tokyo International Airport）は、東京都大田区にある日本最大の空港。通称は羽田空港（はねだくうこう、英語: Haneda Airport）であり、単に「羽田」と呼ばれる場合もある。空港コードはHND。"
+    )).unwrap();
+
+  // add document
+  index_writer.add_document(doc!(
+        id => "3",
+        title => "関西国際空港",
+        body => "関西国際空港（かんさいこくさいくうこう、英: Kansai International Airport）は大阪市の南西35㎞に位置する西日本の国際的な玄関口であり、関西三空港の一つとして大阪国際空港（伊丹空港）、神戸空港とともに関西エアポート株式会社によって一体運営が行われている。"
+    )).unwrap();
+
+  // commit
+  index_writer.commit()?;
+
+  // create reader
+  let reader = index.reader()?;
+
+  // create searcher
+  let searcher = reader.searcher();
+
+  // create querhy parser
+  let query_parser = QueryParser::for_index(&index, vec![title, body]);
+
+  // parse query
+  let query_str = "ＴＯＫＹＯ";
+  let query = query_parser.parse_query(query_str)?;
+  println!("Query String: {query_str}");
+
+  // search
+  println!("Parsed Query: {query:?}");
+  let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
+  println!("Search Result:");
+  for (_, doc_address) in top_docs {
+    let retrieved_doc: TantivyDocument = searcher.doc(doc_address)?;
+    println!("{}", retrieved_doc.to_json(&schema));
+  }
+
+  Ok(())
+}
+
+// copy from https://github.com/lindera/lindera-tantivy/blob/main/examples/tokenize_with_config.rs
