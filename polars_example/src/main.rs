5b3585091680e8abb9f89b159f3986512e5f256e
gerald <3949379+getong@users.noreply.github.com>
2025-12-09 18:55:22 +0800

    update polars source code

--- polars_example/src/main.rs
+++ polars_example/src/main.rs
@@ -1,34 +1,33 @@
 use polars::{lazy::prelude::*, prelude::*};
 
 fn main() -> PolarsResult<()> {
-  let df = LazyCsvReader::new("data.csv")
+  let df = LazyCsvReader::new(PlPath::new("data.csv"))
     .with_has_header(true)
     .finish()?;
 
-  let filtered = df.clone().lazy().filter(col("age").gt(lit(30))).collect()?;
+  let filtered = df.clone().filter(col("age").gt(lit(30))).collect()?;
 
   println!("{:?}", filtered);
 
-  let result = df
+  let grouped = df
     .clone()
     .group_by([col("name")])
     .agg([col("score").mean().alias("average_score")])
     .collect()?;
 
-  println!("{:?}", result);
+  println!("{:?}", grouped);
 
-  let result = df
+  let selected = df
     .clone()
-    .select([col("name"), col("score").alias("final_score"), col("age")])
-    .drop(["age"])
+    .select([col("name"), col("score").alias("final_score")])
     .collect()?;
 
-  println!("{:?}", result);
+  println!("{:?}", selected);
 
   // Drop rows where name == "Alice"
-  let result = df.filter(col("name").neq(lit("Alice"))).collect()?;
+  let without_alice = df.filter(col("name").neq(lit("Alice"))).collect()?;
 
-  println!("{:?}", result);
+  println!("{:?}", without_alice);
 
   Ok(())
 }
