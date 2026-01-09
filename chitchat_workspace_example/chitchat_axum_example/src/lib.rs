db96b194fcadd8898bb632be0642d33ba711c3e4
gerald <3949379+getong@users.noreply.github.com>
2026-01-09 20:06:07 +0800

    update chitchat

--- chitchat_workspace_example/chitchat_axum_example/src/lib.rs
+++ chitchat_workspace_example/chitchat_axum_example/src/lib.rs
@@ -1,4 +1,4 @@
-use aide::OperationOutput;
+use aide::{OperationOutput, openapi::StatusCode};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -32,7 +32,7 @@ impl OperationOutput for ApiResponse {
   fn inferred_responses(
     ctx: &mut aide::generate::GenContext,
     operation: &mut aide::openapi::Operation,
-  ) -> Vec<(Option<u16>, aide::openapi::Response)> {
+  ) -> Vec<(Option<StatusCode>, aide::openapi::Response)> {
     <axum::Json<Self> as OperationOutput>::inferred_responses(ctx, operation)
   }
 }
@@ -55,7 +55,7 @@ impl OperationOutput for SetKeyValueResponse {
   fn inferred_responses(
     ctx: &mut aide::generate::GenContext,
     operation: &mut aide::openapi::Operation,
-  ) -> Vec<(Option<u16>, aide::openapi::Response)> {
+  ) -> Vec<(Option<StatusCode>, aide::openapi::Response)> {
     <axum::Json<Self> as OperationOutput>::inferred_responses(ctx, operation)
   }
 }
@@ -78,7 +78,7 @@ impl OperationOutput for ClusterMembersResponse {
   fn inferred_responses(
     ctx: &mut aide::generate::GenContext,
     operation: &mut aide::openapi::Operation,
-  ) -> Vec<(Option<u16>, aide::openapi::Response)> {
+  ) -> Vec<(Option<StatusCode>, aide::openapi::Response)> {
     <axum::Json<Self> as OperationOutput>::inferred_responses(ctx, operation)
   }
 }
@@ -102,7 +102,7 @@ impl OperationOutput for ServiceUpdateResponse {
   fn inferred_responses(
     ctx: &mut aide::generate::GenContext,
     operation: &mut aide::openapi::Operation,
-  ) -> Vec<(Option<u16>, aide::openapi::Response)> {
+  ) -> Vec<(Option<StatusCode>, aide::openapi::Response)> {
     <axum::Json<Self> as OperationOutput>::inferred_responses(ctx, operation)
   }
 }
