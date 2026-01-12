61c55053aca3fcf3bc2522868b5780f47fdd7f40
gerald <3949379+getong@users.noreply.github.com>
2026-01-12 23:49:19 +0800

    add fluvio produce key value

---
+++ fluvio_workspace_example/fluvio_produce_key_value/src/main.rs
@@ -0,0 +1,46 @@
+//! A minimal example showing how to produce batches of records with Fluvio
+//!
+//! Before running this example, make sure you have created a topic
+//! named `batch` with the following command:
+//!
+//! ```text
+//! $ fluvio topic create batch
+//! ```
+//!
+//! Run this example using the following:
+//!
+//! ```text
+//! $ cargo run --bin produce-batch
+//! ```
+//!
+//! After running this example, you can see the messages that have
+//! been sent to the topic using the following command:
+//!
+//! ```text
+//! $ fluvio consume batch -B -d
+//! [Hello] Fluvio
+//! ```
+
+#[tokio::main]
+async fn main() {
+  if let Err(e) = produce_batch().await {
+    println!("Produce error: {e:?}");
+  }
+}
+
+async fn produce_batch() -> anyhow::Result<()> {
+  let producer = fluvio::producer("batch").await?;
+
+  for i in 0 .. 10 {
+    producer
+      .send(i.to_string(), format!("This is record {i}"))
+      .await?;
+  }
+  // Only flush after `.send`ing all records
+  // Be sure not to flush after every single send
+  producer.flush().await?;
+
+  Ok(())
+}
+
+// copy from https://github.com/fluvio-community/fluvio/blob/master/examples/01-produce-batch/src/main.rs
