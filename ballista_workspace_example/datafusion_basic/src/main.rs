79dded3e7f7cc5e130ff0b3111c44ae19354ab91
gerald <3949379+getong@users.noreply.github.com>
2026-01-19 23:08:21 +0800

    add ballista workspace example

---
+++ ballista_workspace_example/datafusion_basic/src/main.rs
@@ -0,0 +1,17 @@
+use datafusion::prelude::*;
+
+#[tokio::main]
+async fn main() -> datafusion::error::Result<()> {
+  // datafusion context
+  let ctx = SessionContext::new();
+
+  // register the table
+  ctx.register_csv("example", "example.csv", CsvReadOptions::new()).await?;
+
+  // create a plan to run a SQL query
+  let df = ctx.sql("SELECT a, MIN(b) FROM example WHERE a <= b GROUP BY a LIMIT 100").await?;
+
+  // execute and print results
+  df.show().await?;
+  Ok(())
+}
