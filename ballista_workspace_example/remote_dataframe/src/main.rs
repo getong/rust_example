0a343ecef8c69d5f82718faa4b29102e4979cda1
gerald <3949379+getong@users.noreply.github.com>
2026-01-20 20:14:22 +0800

    add remote dataframe

---
+++ ballista_workspace_example/remote_dataframe/src/main.rs
@@ -0,0 +1,53 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use ballista::{
+  datafusion::{
+    common::Result,
+    execution::SessionStateBuilder,
+    prelude::{ParquetReadOptions, SessionConfig, SessionContext, col, lit},
+  },
+  prelude::*,
+};
+use ballista_examples::test_util;
+
+/// This example demonstrates executing a simple query against an Arrow data source (Parquet) and
+/// fetching results, using the DataFrame trait
+#[tokio::main]
+async fn main() -> Result<()> {
+  let config = SessionConfig::new_with_ballista().with_target_partitions(4);
+
+  let state = SessionStateBuilder::new()
+    .with_config(config)
+    .with_default_features()
+    .build();
+
+  let ctx = SessionContext::remote_with_state("df://localhost:50050", state).await?;
+
+  let test_data = test_util::examples_test_data();
+  let filename = format!("{test_data}/alltypes_plain.parquet");
+
+  let df = ctx
+    .read_parquet(filename, ParquetReadOptions::default())
+    .await?
+    .select_columns(&["id", "bool_col", "timestamp_col"])?
+    .filter(col("id").gt(lit(1)))?;
+
+  df.show().await?;
+
+  Ok(())
+}
