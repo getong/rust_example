e716bbd3baca7523b5a579be11d2893c499ba034
gerald <3949379+getong@users.noreply.github.com>
2026-02-01 15:39:46 +0800

    fix autocxx code

--- autocxx_demo_example/build.rs
+++ autocxx_demo_example/build.rs
@@ -1,9 +1,10 @@
-fn main() -> miette::Result<()> {
+fn main() {
   let path = std::path::PathBuf::from("src");
-  let mut b = autocxx_build::Builder::new("src/main.rs", [&path]).build()?;
+  let mut b = autocxx_build::Builder::new("src/main.rs", [&path])
+    .build()
+    .expect("Failed to build autocxx");
   b.flag_if_supported("-std=c++14").compile("autocxx-demo");
 
   println!("cargo:rerun-if-changed=src/main.rs");
   println!("cargo:rerun-if-changed=src/input.h");
-  Ok(())
 }
