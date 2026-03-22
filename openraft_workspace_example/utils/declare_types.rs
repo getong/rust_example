655920fa9d248c78f95b4f9c900f2f6622b43aef
gerald <3949379+getong@users.noreply.github.com>
2026-03-22 14:19:49 +0800

    update code

--- openraft_workspace_example/utils/declare_types.rs
+++ openraft_workspace_example/utils/declare_types.rs
@@ -1,17 +1,20 @@
 //! Declare the Raft type with the TypeConfig.
 
 // Reference the containing module's type config and re-export it.
+// Re-export `Raft` from the parent module so `typ::Raft` works.
+pub use super::Raft;
 pub use super::TypeConfig;
-
-pub type Raft = openraft::Raft<TypeConfig>;
 
 pub type Vote = <TypeConfig as openraft::RaftTypeConfig>::Vote;
 pub type LeaderId = <TypeConfig as openraft::RaftTypeConfig>::LeaderId;
-pub type LogId = openraft::LogId<TypeConfig>;
+pub type LogId = openraft::alias::LogIdOf<TypeConfig>;
 pub type Entry = <TypeConfig as openraft::RaftTypeConfig>::Entry;
-pub type EntryPayload = openraft::EntryPayload<TypeConfig>;
-pub type Membership = openraft::membership::Membership<TypeConfig>;
-pub type StoredMembership = openraft::StoredMembership<TypeConfig>;
+pub type EntryPayload = openraft::alias::EntryPayloadOf<TypeConfig>;
+pub type Membership = openraft::membership::Membership<
+    <TypeConfig as openraft::RaftTypeConfig>::NodeId,
+    <TypeConfig as openraft::RaftTypeConfig>::Node,
+>;
+pub type StoredMembership = openraft::alias::StoredMembershipOf<TypeConfig>;
 
 pub type ApplyResponder = openraft::storage::ApplyResponder<TypeConfig>;
 pub type EntryResponder = openraft::storage::EntryResponder<TypeConfig>;
@@ -20,27 +23,27 @@ pub type Node = <TypeConfig as openraft::RaftTypeConfig>::Node;
 
 pub type LogState = openraft::storage::LogState<TypeConfig>;
 
-pub type SnapshotMeta = openraft::SnapshotMeta<TypeConfig>;
-pub type Snapshot = openraft::Snapshot<TypeConfig>;
+pub type SnapshotMeta = openraft::alias::SnapshotMetaOf<TypeConfig>;
+pub type Snapshot = openraft::alias::SnapshotOf<TypeConfig>;
 pub type SnapshotData = <TypeConfig as openraft::RaftTypeConfig>::SnapshotData;
 
 pub type IOFlushed = openraft::storage::IOFlushed<TypeConfig>;
 
-pub type Infallible = openraft::error::Infallible;
-pub type Fatal = openraft::error::Fatal<TypeConfig>;
-pub type RaftError<E = openraft::error::Infallible> = openraft::error::RaftError<TypeConfig, E>;
-pub type RPCError<E = openraft::error::Infallible> = openraft::error::RPCError<TypeConfig, E>;
+pub type Infallible = openraft::errors::Infallible;
+pub type Fatal = openraft::errors::Fatal<TypeConfig>;
+pub type RaftError<E = openraft::errors::Infallible> = openraft::errors::RaftError<TypeConfig, E>;
+pub type RPCError<E = openraft::errors::Infallible> = openraft::errors::RPCError<TypeConfig, E>;
 
 pub type ErrorSubject = openraft::ErrorSubject<TypeConfig>;
 pub type StorageError = openraft::StorageError<TypeConfig>;
-pub type StreamingError = openraft::error::StreamingError<TypeConfig>;
+pub type StreamingError = openraft::errors::StreamingError<TypeConfig>;
 
 pub type RaftMetrics = openraft::RaftMetrics<TypeConfig>;
 
-pub type ClientWriteError = openraft::error::ClientWriteError<TypeConfig>;
-pub type LinearizableReadError = openraft::error::LinearizableReadError<TypeConfig>;
-pub type ForwardToLeader = openraft::error::ForwardToLeader<TypeConfig>;
-pub type InitializeError = openraft::error::InitializeError<TypeConfig>;
+pub type ClientWriteError = openraft::errors::ClientWriteError<TypeConfig>;
+pub type LinearizableReadError = openraft::errors::LinearizableReadError<TypeConfig>;
+pub type ForwardToLeader = openraft::errors::ForwardToLeader<TypeConfig>;
+pub type InitializeError = openraft::errors::InitializeError<TypeConfig>;
 
 pub type VoteRequest = openraft::raft::VoteRequest<TypeConfig>;
 pub type VoteResponse = openraft::raft::VoteResponse<TypeConfig>;
