c4dfcdec26df78610ad5c5f0b83f80eb96f9c139
gerald <3949379+getong@users.noreply.github.com>
2026-04-16 07:50:45 +0800

    axum socketioxide echo tls

---
+++ axum_workspace_example/axum_socketioxide_echo_tls/src/main.rs
@@ -0,0 +1,66 @@
+use std::{net::SocketAddr, path::PathBuf};
+
+use axum::routing::get;
+use axum_server::tls_rustls::RustlsConfig;
+use rmpv::Value;
+use socketioxide::{
+  SocketIo,
+  extract::{AckSender, Data, SocketRef},
+};
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+
+async fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+  info!(ns = socket.ns(), ?socket.id, "Socket.IO connected");
+  socket.emit("auth", &data).ok();
+
+  socket.on("message", async |socket: SocketRef, Data::<Value>(data)| {
+    info!(?data, "Received event");
+    socket.emit("message-back", &data).ok();
+  });
+
+  socket.on(
+    "message-with-ack",
+    async |Data::<Value>(data), ack: AckSender| {
+      info!(?data, "Received event");
+      ack.send(&data).ok();
+    },
+  );
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+
+  // configure certificate and private key used by https
+  let config = RustlsConfig::from_pem_file(
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+      .join("self_signed_certs")
+      .join("cert.pem"),
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+      .join("self_signed_certs")
+      .join("key.pem"),
+  )
+  .await
+  .unwrap();
+
+  let (layer, io) = SocketIo::new_layer();
+
+  io.ns("/", on_connect);
+  io.ns("/custom", on_connect);
+
+  let app = axum::Router::new()
+    .route("/", get(|| async { "Hello, World!" }))
+    .layer(layer);
+
+  info!("Starting server");
+
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+  tracing::debug!("listening on {}", addr);
+  axum_server::bind_rustls(addr, config)
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
+
+  Ok(())
+}
