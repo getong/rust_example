aceca288358b9734af98b685650a167e74e57334
gerald <3949379+getong@users.noreply.github.com>
2026-05-07 20:03:15 +0800

    add sonic_rs workspace example

---
+++ sonic_rs_workspace_example/serde_example/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+
+use sonic_rs::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+  name: String,
+  age: u8,
+  phones: Vec<String>,
+}
+
+fn main() {
+  // parse a string
+  let data = r#"{
+  "name": "Xiaoming",
+  "age": 18,
+  "phones": [
+    "+123456"
+  ]
+}"#;
+  let p: Person = sonic_rs::from_str(data).unwrap();
+  assert_eq!(p.age, 18);
+  assert_eq!(p.name, "Xiaoming");
+  let out = sonic_rs::to_string_pretty(&p).unwrap();
+  assert_eq!(out, data);
+
+  // parse a file reader
+  let p: Person =
+    sonic_rs::from_reader(File::open("../testdata/person.json").unwrap()).unwrap();
+  assert_eq!(p.age, 18);
+  assert_eq!(p.name, "Xiaoming");
+  let out = sonic_rs::to_string_pretty(&p).unwrap();
+  assert_eq!(out, data);
+}
