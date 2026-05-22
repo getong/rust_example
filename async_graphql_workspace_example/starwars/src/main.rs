547a0235255552783459b4eeeae2ce32192404fe
gerald <3949379+getong@users.noreply.github.com>
2026-05-22 14:44:54 +0800

    add async_graphql workspace example

---
+++ async_graphql_workspace_example/starwars/src/main.rs
@@ -0,0 +1,27 @@
+use async_graphql::{EmptyMutation, EmptySubscription, Schema, http::GraphiQLSource};
+use async_graphql_axum::GraphQL;
+use axum::{
+  Router,
+  response::{self, IntoResponse},
+  routing::get,
+};
+use starwars::{QueryRoot, StarWars};
+use tokio::net::TcpListener;
+
+async fn graphiql() -> impl IntoResponse {
+  response::Html(GraphiQLSource::build().endpoint("/").finish())
+}
+
+#[tokio::main]
+async fn main() {
+  let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+    .data(StarWars::new())
+    .finish();
+
+  let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
+  println!("GraphiQL IDE: http://localhost:8000");
+
+  axum::serve(TcpListener::bind("127.0.0.1:8000").await.unwrap(), app)
+    .await
+    .unwrap();
+}
