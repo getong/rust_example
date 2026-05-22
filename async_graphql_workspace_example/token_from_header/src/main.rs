14c3b518a5e640ca9e4cf7f6ae73058be3be97ae
gerald <3949379+getong@users.noreply.github.com>
2026-05-22 15:05:34 +0800

    add token from header

---
+++ async_graphql_workspace_example/token_from_header/src/main.rs
@@ -0,0 +1,71 @@
+use async_graphql::{
+  EmptyMutation, Schema,
+  http::{ALL_WEBSOCKET_PROTOCOLS, GraphiQLSource},
+};
+use async_graphql_axum::{GraphQLProtocol, GraphQLRequest, GraphQLResponse, GraphQLWebSocket};
+use axum::{
+  Router,
+  extract::{State, ws::WebSocketUpgrade},
+  http::header::HeaderMap,
+  response::{Html, IntoResponse, Response},
+  routing::get,
+};
+use token_from_header::{QueryRoot, SubscriptionRoot, Token, TokenSchema, on_connection_init};
+use tokio::net::TcpListener;
+
+async fn graphql_playground() -> impl IntoResponse {
+  Html(
+    GraphiQLSource::build()
+      .endpoint("/")
+      .subscription_endpoint("/ws")
+      .finish(),
+  )
+}
+
+fn get_token_from_headers(headers: &HeaderMap) -> Option<Token> {
+  headers
+    .get("Token")
+    .and_then(|value| value.to_str().map(|s| Token(s.to_string())).ok())
+}
+
+async fn graphql_handler(
+  State(schema): State<TokenSchema>,
+  headers: HeaderMap,
+  req: GraphQLRequest,
+) -> GraphQLResponse {
+  let mut req = req.into_inner();
+  if let Some(token) = get_token_from_headers(&headers) {
+    req = req.data(token);
+  }
+  schema.execute(req).await.into()
+}
+
+async fn graphql_ws_handler(
+  State(schema): State<TokenSchema>,
+  protocol: GraphQLProtocol,
+  websocket: WebSocketUpgrade,
+) -> Response {
+  websocket
+    .protocols(ALL_WEBSOCKET_PROTOCOLS)
+    .on_upgrade(move |stream| {
+      GraphQLWebSocket::new(stream, schema.clone(), protocol)
+        .on_connection_init(on_connection_init)
+        .serve()
+    })
+}
+
+#[tokio::main]
+async fn main() {
+  let schema = Schema::new(QueryRoot, EmptyMutation, SubscriptionRoot);
+
+  let app = Router::new()
+    .route("/", get(graphql_playground).post(graphql_handler))
+    .route("/ws", get(graphql_ws_handler))
+    .with_state(schema);
+
+  println!("Playground: http://localhost:8000");
+
+  axum::serve(TcpListener::bind("127.0.0.1:8000").await.unwrap(), app)
+    .await
+    .unwrap();
+}
