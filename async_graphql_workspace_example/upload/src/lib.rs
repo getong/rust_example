bceb66fcee6c88f77170bfb8c2602459119e87c0
gerald <3949379+getong@users.noreply.github.com>
2026-05-22 15:13:11 +0800

    add upload

---
+++ async_graphql_workspace_example/upload/src/lib.rs
@@ -0,0 +1,56 @@
+use async_graphql::{Context, EmptySubscription, ID, Object, Schema, SimpleObject, Upload};
+use futures::lock::Mutex;
+use slab::Slab;
+
+pub type FilesSchema = Schema<QueryRoot, MutationRoot, EmptySubscription>;
+
+#[derive(Clone, SimpleObject)]
+pub struct FileInfo {
+  id: ID,
+  url: String,
+}
+
+pub type Storage = Mutex<Slab<FileInfo>>;
+
+pub struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+  async fn uploads(&self, ctx: &Context<'_>) -> Vec<FileInfo> {
+    let storage = ctx.data_unchecked::<Storage>().lock().await;
+    storage.iter().map(|(_, file)| file).cloned().collect()
+  }
+}
+
+pub struct MutationRoot;
+
+#[Object]
+impl MutationRoot {
+  async fn single_upload(&self, ctx: &Context<'_>, file: Upload) -> FileInfo {
+    let mut storage = ctx.data_unchecked::<Storage>().lock().await;
+    let entry = storage.vacant_entry();
+    let upload = file.value(ctx).unwrap();
+    let info = FileInfo {
+      id: entry.key().into(),
+      url: upload.filename,
+    };
+    entry.insert(info.clone());
+    info
+  }
+
+  async fn multiple_upload(&self, ctx: &Context<'_>, files: Vec<Upload>) -> Vec<FileInfo> {
+    let mut infos = Vec::new();
+    let mut storage = ctx.data_unchecked::<Storage>().lock().await;
+    for file in files {
+      let entry = storage.vacant_entry();
+      let upload = file.value(ctx).unwrap();
+      let info = FileInfo {
+        id: entry.key().into(),
+        url: upload.filename.clone(),
+      };
+      entry.insert(info.clone());
+      infos.push(info)
+    }
+    infos
+  }
+}
---
+++ async_graphql_workspace_example/upload/src/main.rs
@@ -0,0 +1,36 @@
+use async_graphql::{EmptySubscription, Schema, http::GraphiQLSource};
+use async_graphql_axum::GraphQL;
+use axum::{
+  Router,
+  http::Method,
+  response::{Html, IntoResponse},
+  routing::get,
+};
+use upload::{MutationRoot, QueryRoot, Storage};
+use tokio::net::TcpListener;
+use tower_http::cors::{AllowOrigin, CorsLayer};
+
+async fn graphiql() -> impl IntoResponse {
+  Html(GraphiQLSource::build().endpoint("/").finish())
+}
+
+#[tokio::main]
+async fn main() {
+  let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
+    .data(Storage::default())
+    .finish();
+
+  println!("GraphiQL IDE: http://localhost:8000");
+
+  let app = Router::new()
+    .route("/", get(graphiql).post_service(GraphQL::new(schema)))
+    .layer(
+      CorsLayer::new()
+        .allow_origin(AllowOrigin::predicate(|_, _| true))
+        .allow_methods([Method::GET, Method::POST]),
+    );
+
+  axum::serve(TcpListener::bind("127.0.0.1:8000").await.unwrap(), app)
+    .await
+    .unwrap();
+}
