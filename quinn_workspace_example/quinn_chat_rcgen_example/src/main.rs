075f3984a3fc565d44f7e4d7989b14cb6400e7c9
gerald <3949379+getong@users.noreply.github.com>
2026-05-30 21:37:31 +0800

    update version

--- quinn_workspace_example/quinn_chat_rcgen_example/src/main.rs
+++ quinn_workspace_example/quinn_chat_rcgen_example/src/main.rs
@@ -7,14 +7,14 @@ const CERTS_DIR: &str = "/tmp/quinn-chat-certs";
 fn main() {
   let subject_alt_names = vec!["localhost".to_string()];
 
-  let CertifiedKey { cert, key_pair } = generate_simple_self_signed(subject_alt_names).unwrap();
+  let CertifiedKey { cert, signing_key } = generate_simple_self_signed(subject_alt_names).unwrap();
 
   if !Path::new(CERTS_DIR).exists() {
     fs::create_dir_all(CERTS_DIR).expect("Failed to create 'certs' directory");
   }
 
   let _ = fs::write(format!("{}/cert.pem", CERTS_DIR), cert.pem());
-  let _ = fs::write(format!("{}/cert.key", CERTS_DIR), key_pair.serialize_pem());
+  let _ = fs::write(format!("{}/cert.key", CERTS_DIR), signing_key.serialize_pem());
 }
 
 // copy from https://github.com/Matheus-git/quic-chat
