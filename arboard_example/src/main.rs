9d8b63f756b016f9a77754f863d92e65c0192967
gerald <3949379+getong@users.noreply.github.com>
2026-06-21 21:48:37 +0800

    add arboard example

---
+++ arboard_example/src/main.rs
@@ -0,0 +1,10 @@
+use arboard::Clipboard;
+
+fn main() {
+  let mut clipboard = Clipboard::new().unwrap();
+  println!("Clipboard text was: {}", clipboard.get_text().unwrap());
+
+  let the_string = "Hello, world!";
+  clipboard.set_text(the_string).unwrap();
+  println!("But now the clipboard text should be: \"{}\"", the_string);
+}
