b05f95e133c03d55d5b79eb9134a11d9ffcc7726
gerald <3949379+getong@users.noreply.github.com>
2026-07-04 19:26:52 +0800

    add rkyv example

---
+++ rkyv_example/src/main.rs
@@ -0,0 +1,47 @@
+use rkyv::{
+  Archive, Deserialize, Serialize, api::high::to_bytes_with_alloc, deserialize, rancor::Error,
+  ser::allocator::Arena,
+};
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[rkyv(
+  // This will generate a PartialEq impl between our unarchived
+  // and archived types
+  compare(PartialEq),
+  // Derives can be passed through to the generated type:
+  derive(Debug),
+)]
+struct Test {
+  int: u8,
+  string: String,
+  option: Option<Vec<i32>>,
+}
+
+fn main() {
+  let value = Test {
+    int: 42,
+    string: "hello world".to_string(),
+    option: Some(vec![1, 2, 3, 4]),
+  };
+
+  // Serializing is as easy as a single function call
+  let _bytes = rkyv::to_bytes::<Error>(&value).unwrap();
+
+  // Or you can customize your serialization for better performance or control
+  // over resource usage
+
+  let mut arena = Arena::new();
+  let bytes = to_bytes_with_alloc::<_, Error>(&value, arena.acquire()).unwrap();
+
+  // You can use the safe API for fast zero-copy deserialization
+  let archived = rkyv::access::<ArchivedTest, Error>(&bytes[..]).unwrap();
+  assert_eq!(archived, &value);
+
+  // Or you can use the unsafe API for maximum performance
+  let archived = unsafe { rkyv::access_unchecked::<ArchivedTest>(&bytes[..]) };
+  assert_eq!(archived, &value);
+
+  // And you can always deserialize back to the original type
+  let deserialized = deserialize::<Test, Error>(archived).unwrap();
+  assert_eq!(deserialized, value);
+}
