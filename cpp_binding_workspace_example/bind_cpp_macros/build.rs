b225134ad2e2ee733f857586c2a7f8818851bf2e
gerald <3949379+getong@users.noreply.github.com>
2026-07-23 23:53:07 +0800

    update to use cpp 20

--- cpp_binding_workspace_example/bind_cpp_macros/build.rs
+++ cpp_binding_workspace_example/bind_cpp_macros/build.rs
@@ -1,7 +1,7 @@
 fn main() {
   cpp_build::Config::new()
-    .flag_if_supported("-std=c++17")
-    .flag_if_supported("/std:c++17")
+    .flag_if_supported("-std=c++20")
+    .flag_if_supported("/std:c++20")
     .define("TOML_EXCEPTIONS", Some("0"))
     .build("src/main.rs");
 }
--- cpp_binding_workspace_example/bind_cpp_manual/build.rs
+++ cpp_binding_workspace_example/bind_cpp_manual/build.rs
@@ -1,7 +1,7 @@
 fn main() {
   cc::Build::new()
     .cpp(true)
-    .std("c++17")
+    .std("c++20")
     .define("TOML_EXCEPTIONS", "0")
     .file("api.cpp")
     .compile("api");
