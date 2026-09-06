cee0d1f70d905f9227bd39a9a090f39551407c74
gerald <3949379+getong@users.noreply.github.com>
2026-09-07 05:20:26 +0800

    update topcoat

--- axum_workspace_example/topcoat_example/src/main.rs
+++ axum_workspace_example/topcoat_example/src/main.rs
@@ -1,7 +1,7 @@
 use topcoat::{
   Result,
   router::{Router, RouterBuilderDiscoverExt, page},
-  view::{component, view},
+  view::{View, component, view},
 };
 
 #[tokio::main]
@@ -12,18 +12,18 @@ async fn main() {
 }
 
 #[page("/")]
-async fn home() -> Result {
-  view! {
+async fn home() -> Result<impl View> {
+  Ok(view! {
     <!DOCTYPE html>
       <html>
       <body>
       hello(name: "World")
       </body>
       </html>
-  }
+  })
 }
 
 #[component]
-async fn hello(name: &str) -> Result {
-  view! { <h1>"Hello, " (name) "!"</h1> }
+async fn hello(name: &str) -> Result<impl View> {
+  Ok(view! { <h1>"Hello, " (name) "!"</h1> })
 }
