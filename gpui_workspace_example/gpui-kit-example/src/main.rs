aafd0b66893aa5d56ad6c877d512f382a02af7ed
gerald <3949379+getong@users.noreply.github.com>
2026-09-15 20:49:43 +0800

    add gpui workspace example

---
+++ gpui_workspace_example/gpui-kit-example/src/main.rs
@@ -0,0 +1,40 @@
+use gpui_kit::{
+  component::{button::*, *},
+  *,
+};
+
+pub struct HelloWorld;
+impl Render for HelloWorld {
+  fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
+    div()
+      .v_flex()
+      .gap_2()
+      .size_full()
+      .items_center()
+      .justify_center()
+      .child("Hello, World!")
+      .child(
+        Button::new("ok")
+          .primary()
+          .label("Let's Go!")
+          .on_click(|_, _, _| println!("Clicked!")),
+      )
+  }
+}
+
+fn main() {
+  gpui_kit::application().run(move |cx| {
+    // This must be called before using any GPUI Component features.
+    gpui_kit::init(cx);
+
+    cx.spawn(async move |cx| {
+      cx.open_window(WindowOptions::default(), |window, cx| {
+        let view = cx.new(|_| HelloWorld);
+        // This first level on the window, should be a Root.
+        cx.new(|cx| Root::new(view, window, cx))
+      })
+      .expect("Failed to open window");
+    })
+    .detach();
+  });
+}
