7f8ea0abbe36044120cd23ea4db8de732d7aea73
gerald <3949379+getong@users.noreply.github.com>
2026-09-22 01:41:28 +0800

    add counter view

--- gpui_workspace_example/gpui-kit-example/src/main.rs
+++ gpui_workspace_example/gpui-kit-example/src/main.rs
@@ -3,7 +3,9 @@ use gpui_kit::{
   *,
 };
 
-pub struct HelloWorld;
+pub struct HelloWorld {
+  counter: Entity<Counter>,
+}
 impl Render for HelloWorld {
   fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
     div()
@@ -19,6 +21,31 @@ impl Render for HelloWorld {
           .label("Let's Go!")
           .on_click(|_, _, _| println!("Clicked!")),
       )
+      .child(self.counter.clone())
+  }
+}
+
+struct Counter {
+  count: i32,
+}
+
+impl Render for Counter {
+  fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+    div()
+      .flex()
+      .gap_3()
+      .p_4()
+      .bg(rgb(0x131315))
+      .child(format!("count:{}", self.count))
+      .child(
+        Button::new("increment")
+          .label("+1")
+          .on_click(cx.listener(|view, _, _, cx| {
+            view.count += 1;
+
+            cx.notify(); //标记需要重绘
+          })),
+      )
   }
 }
 
@@ -29,7 +56,8 @@ fn main() {
 
     cx.spawn(async move |cx| {
       cx.open_window(WindowOptions::default(), |window, cx| {
-        let view = cx.new(|_| HelloWorld);
+        let counter = cx.new(|_| Counter { count: 0 });
+        let view = cx.new(|_| HelloWorld { counter });
         // This first level on the window, should be a Root.
         cx.new(|cx| Root::new(view, window, cx))
       })
